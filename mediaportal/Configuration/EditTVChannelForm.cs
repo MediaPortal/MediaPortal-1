baa7154ecf23f98223d5f00c43c7ff9bf2143bc1
bonkalord <bonkalord@team-mediaportal.com>
2004-07-25 15:43:48 +0000

    Added support for four (4) digit channels.

--- mediaportal/Configuration/EditTVChannelForm.cs
+++ mediaportal/Configuration/EditTVChannelForm.cs
@@ -63,131 +63,131 @@ namespace MediaPortal.Configuration
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.frequencyTextBox = new System.Windows.Forms.TextBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.channelTextBox = new System.Windows.Forms.TextBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.nameTextBox = new System.Windows.Forms.TextBox();
-			this.label2 = new System.Windows.Forms.Label();
-			this.cancelButton = new System.Windows.Forms.Button();
-			this.okButton = new System.Windows.Forms.Button();
-			this.groupBox1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-				| System.Windows.Forms.AnchorStyles.Left) 
-				| System.Windows.Forms.AnchorStyles.Right)));
-			this.groupBox1.Controls.Add(this.frequencyTextBox);
-			this.groupBox1.Controls.Add(this.label1);
-			this.groupBox1.Controls.Add(this.channelTextBox);
-			this.groupBox1.Controls.Add(this.label3);
-			this.groupBox1.Controls.Add(this.nameTextBox);
-			this.groupBox1.Controls.Add(this.label2);
-			this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.groupBox1.Location = new System.Drawing.Point(8, 8);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(360, 128);
-			this.groupBox1.TabIndex = 0;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "Channel Settings";
-			// 
-			// frequencyTextBox
-			// 
-			this.frequencyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-				| System.Windows.Forms.AnchorStyles.Right)));
-			this.frequencyTextBox.Location = new System.Drawing.Point(120, 77);
-			this.frequencyTextBox.MaxLength = 10;
-			this.frequencyTextBox.Name = "frequencyTextBox";
-			this.frequencyTextBox.Size = new System.Drawing.Size(224, 20);
-			this.frequencyTextBox.TabIndex = 9;
-			this.frequencyTextBox.Text = "0";
-			this.frequencyTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frequencyTextBox_KeyPress);
-			// 
-			// label1
-			// 
-			this.label1.Location = new System.Drawing.Point(16, 80);
-			this.label1.Name = "label1";
-			this.label1.TabIndex = 10;
-			this.label1.Text = "Frequency";
-			// 
-			// channelTextBox
-			// 
-			this.channelTextBox.Location = new System.Drawing.Point(120, 52);
-			this.channelTextBox.MaxLength = 3;
-			this.channelTextBox.Name = "channelTextBox";
-			this.channelTextBox.Size = new System.Drawing.Size(40, 20);
-			this.channelTextBox.TabIndex = 7;
-			this.channelTextBox.Text = "0";
-			this.channelTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.channelTextBox_KeyPress);
-			// 
-			// label3
-			// 
-			this.label3.Location = new System.Drawing.Point(16, 55);
-			this.label3.Name = "label3";
-			this.label3.TabIndex = 8;
-			this.label3.Text = "Channel";
-			// 
-			// nameTextBox
-			// 
-			this.nameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-				| System.Windows.Forms.AnchorStyles.Right)));
-			this.nameTextBox.Location = new System.Drawing.Point(120, 27);
-			this.nameTextBox.Name = "nameTextBox";
-			this.nameTextBox.Size = new System.Drawing.Size(224, 20);
-			this.nameTextBox.TabIndex = 5;
-			this.nameTextBox.Text = "";
-			// 
-			// label2
-			// 
-			this.label2.Location = new System.Drawing.Point(16, 30);
-			this.label2.Name = "label2";
-			this.label2.TabIndex = 6;
-			this.label2.Text = "Name";
-			// 
-			// cancelButton
-			// 
-			this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.cancelButton.Location = new System.Drawing.Point(293, 143);
-			this.cancelButton.Name = "cancelButton";
-			this.cancelButton.TabIndex = 1;
-			this.cancelButton.Text = "Cancel";
-			this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
-			// 
-			// okButton
-			// 
-			this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.okButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.okButton.Location = new System.Drawing.Point(213, 143);
-			this.okButton.Name = "okButton";
-			this.okButton.TabIndex = 2;
-			this.okButton.Text = "OK";
-			this.okButton.Click += new System.EventHandler(this.okButton_Click);
-			// 
-			// EditTVChannelForm
-			// 
-			this.AcceptButton = this.okButton;
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.CancelButton = this.cancelButton;
-			this.ClientSize = new System.Drawing.Size(376, 174);
-			this.Controls.Add(this.okButton);
-			this.Controls.Add(this.cancelButton);
-			this.Controls.Add(this.groupBox1);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
-			this.MinimumSize = new System.Drawing.Size(384, 200);
-			this.Name = "EditTVChannelForm";
-			this.ShowInTaskbar = false;
-			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "EditTVChannelForm";
-			this.groupBox1.ResumeLayout(false);
-			this.ResumeLayout(false);
+      this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.frequencyTextBox = new System.Windows.Forms.TextBox();
+      this.label1 = new System.Windows.Forms.Label();
+      this.channelTextBox = new System.Windows.Forms.TextBox();
+      this.label3 = new System.Windows.Forms.Label();
+      this.nameTextBox = new System.Windows.Forms.TextBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.cancelButton = new System.Windows.Forms.Button();
+      this.okButton = new System.Windows.Forms.Button();
+      this.groupBox1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+        | System.Windows.Forms.AnchorStyles.Left) 
+        | System.Windows.Forms.AnchorStyles.Right)));
+      this.groupBox1.Controls.Add(this.frequencyTextBox);
+      this.groupBox1.Controls.Add(this.label1);
+      this.groupBox1.Controls.Add(this.channelTextBox);
+      this.groupBox1.Controls.Add(this.label3);
+      this.groupBox1.Controls.Add(this.nameTextBox);
+      this.groupBox1.Controls.Add(this.label2);
+      this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.groupBox1.Location = new System.Drawing.Point(8, 8);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(360, 128);
+      this.groupBox1.TabIndex = 0;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "Channel Settings";
+      // 
+      // frequencyTextBox
+      // 
+      this.frequencyTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+        | System.Windows.Forms.AnchorStyles.Right)));
+      this.frequencyTextBox.Location = new System.Drawing.Point(120, 77);
+      this.frequencyTextBox.MaxLength = 10;
+      this.frequencyTextBox.Name = "frequencyTextBox";
+      this.frequencyTextBox.Size = new System.Drawing.Size(224, 20);
+      this.frequencyTextBox.TabIndex = 9;
+      this.frequencyTextBox.Text = "0";
+      this.frequencyTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frequencyTextBox_KeyPress);
+      // 
+      // label1
+      // 
+      this.label1.Location = new System.Drawing.Point(16, 80);
+      this.label1.Name = "label1";
+      this.label1.TabIndex = 10;
+      this.label1.Text = "Frequency";
+      // 
+      // channelTextBox
+      // 
+      this.channelTextBox.Location = new System.Drawing.Point(120, 52);
+      this.channelTextBox.MaxLength = 4;
+      this.channelTextBox.Name = "channelTextBox";
+      this.channelTextBox.Size = new System.Drawing.Size(40, 20);
+      this.channelTextBox.TabIndex = 7;
+      this.channelTextBox.Text = "0";
+      this.channelTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.channelTextBox_KeyPress);
+      // 
+      // label3
+      // 
+      this.label3.Location = new System.Drawing.Point(16, 55);
+      this.label3.Name = "label3";
+      this.label3.TabIndex = 8;
+      this.label3.Text = "Channel";
+      // 
+      // nameTextBox
+      // 
+      this.nameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+        | System.Windows.Forms.AnchorStyles.Right)));
+      this.nameTextBox.Location = new System.Drawing.Point(120, 27);
+      this.nameTextBox.Name = "nameTextBox";
+      this.nameTextBox.Size = new System.Drawing.Size(224, 20);
+      this.nameTextBox.TabIndex = 5;
+      this.nameTextBox.Text = "";
+      // 
+      // label2
+      // 
+      this.label2.Location = new System.Drawing.Point(16, 30);
+      this.label2.Name = "label2";
+      this.label2.TabIndex = 6;
+      this.label2.Text = "Name";
+      // 
+      // cancelButton
+      // 
+      this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.cancelButton.Location = new System.Drawing.Point(293, 143);
+      this.cancelButton.Name = "cancelButton";
+      this.cancelButton.TabIndex = 1;
+      this.cancelButton.Text = "Cancel";
+      this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+      // 
+      // okButton
+      // 
+      this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.okButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.okButton.Location = new System.Drawing.Point(213, 143);
+      this.okButton.Name = "okButton";
+      this.okButton.TabIndex = 2;
+      this.okButton.Text = "OK";
+      this.okButton.Click += new System.EventHandler(this.okButton_Click);
+      // 
+      // EditTVChannelForm
+      // 
+      this.AcceptButton = this.okButton;
+      this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+      this.CancelButton = this.cancelButton;
+      this.ClientSize = new System.Drawing.Size(376, 174);
+      this.Controls.Add(this.okButton);
+      this.Controls.Add(this.cancelButton);
+      this.Controls.Add(this.groupBox1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+      this.MinimumSize = new System.Drawing.Size(384, 200);
+      this.Name = "EditTVChannelForm";
+      this.ShowInTaskbar = false;
+      this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+      this.Text = "EditTVChannelForm";
+      this.groupBox1.ResumeLayout(false);
+      this.ResumeLayout(false);
 
-		}
+    }
 		#endregion
 
 		private void channelTextBox_KeyPress(object sender, System.Windows.Forms.KeyPressEventArgs e)
