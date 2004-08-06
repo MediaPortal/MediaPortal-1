af560e7bc785f4b631f1d626072758722e8e6729
bonkalord <bonkalord@team-mediaportal.com>
2004-08-06 21:57:03 +0000

    *** empty log message ***

--- mediaportal/Configuration/Sections/General.cs
+++ mediaportal/Configuration/Sections/General.cs
@@ -130,86 +130,86 @@ namespace MediaPortal.Configuration.Sections
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.mpGroupBox1 = new MediaPortal.UserInterface.Controls.MPGroupBox();
-			this.languageComboBox = new System.Windows.Forms.ComboBox();
-			this.label2 = new System.Windows.Forms.Label();
-			this.groupBox1 = new MediaPortal.UserInterface.Controls.MPGroupBox();
-			this.settingsCheckedListBox = new System.Windows.Forms.CheckedListBox();
-			this.mpGroupBox1.SuspendLayout();
-			this.groupBox1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// mpGroupBox1
-			// 
-			this.mpGroupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-				| System.Windows.Forms.AnchorStyles.Right)));
-			this.mpGroupBox1.Controls.Add(this.languageComboBox);
-			this.mpGroupBox1.Controls.Add(this.label2);
-			this.mpGroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.mpGroupBox1.Location = new System.Drawing.Point(8, 8);
-			this.mpGroupBox1.Name = "mpGroupBox1";
-			this.mpGroupBox1.Size = new System.Drawing.Size(440, 72);
-			this.mpGroupBox1.TabIndex = 1;
-			this.mpGroupBox1.TabStop = false;
-			this.mpGroupBox1.Text = "Language Settings";
-			// 
-			// languageComboBox
-			// 
-			this.languageComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.languageComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.languageComboBox.Location = new System.Drawing.Point(168, 27);
-			this.languageComboBox.Name = "languageComboBox";
-			this.languageComboBox.Size = new System.Drawing.Size(256, 21);
-			this.languageComboBox.TabIndex = 5;
-			// 
-			// label2
-			// 
-			this.label2.Location = new System.Drawing.Point(16, 30);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(150, 23);
-			this.label2.TabIndex = 4;
-			this.label2.Text = "Display language";
-			// 
-			// groupBox1
-			// 
-			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-				| System.Windows.Forms.AnchorStyles.Right)));
-			this.groupBox1.Controls.Add(this.settingsCheckedListBox);
-			this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
-			this.groupBox1.Location = new System.Drawing.Point(8, 88);
-			this.groupBox1.Name = "groupBox1";
-			this.groupBox1.Size = new System.Drawing.Size(440, 152);
-			this.groupBox1.TabIndex = 2;
-			this.groupBox1.TabStop = false;
-			this.groupBox1.Text = "General Settings";
-			// 
-			// settingsCheckedListBox
-			// 
-			this.settingsCheckedListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-				| System.Windows.Forms.AnchorStyles.Left) 
-				| System.Windows.Forms.AnchorStyles.Right)));
-			this.settingsCheckedListBox.Items.AddRange(new object[] {
-																		"Start MediaPlayer in fullscreen mode",
-																		"Auto hide mouse cursor",
-																		"Show special mouse controls (scrollbars, etc)",
-																		"Hide file extensions for known file types"});
-			this.settingsCheckedListBox.Location = new System.Drawing.Point(16, 24);
-			this.settingsCheckedListBox.Name = "settingsCheckedListBox";
-			this.settingsCheckedListBox.Size = new System.Drawing.Size(408, 109);
-			this.settingsCheckedListBox.TabIndex = 0;
-			// 
-			// General
-			// 
-			this.BackColor = System.Drawing.SystemColors.Control;
-			this.Controls.Add(this.groupBox1);
-			this.Controls.Add(this.mpGroupBox1);
-			this.Name = "General";
-			this.Size = new System.Drawing.Size(456, 448);
-			this.mpGroupBox1.ResumeLayout(false);
-			this.groupBox1.ResumeLayout(false);
-			this.ResumeLayout(false);
+      this.mpGroupBox1 = new MediaPortal.UserInterface.Controls.MPGroupBox();
+      this.languageComboBox = new System.Windows.Forms.ComboBox();
+      this.label2 = new System.Windows.Forms.Label();
+      this.groupBox1 = new MediaPortal.UserInterface.Controls.MPGroupBox();
+      this.settingsCheckedListBox = new System.Windows.Forms.CheckedListBox();
+      this.mpGroupBox1.SuspendLayout();
+      this.groupBox1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // mpGroupBox1
+      // 
+      this.mpGroupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+        | System.Windows.Forms.AnchorStyles.Right)));
+      this.mpGroupBox1.Controls.Add(this.languageComboBox);
+      this.mpGroupBox1.Controls.Add(this.label2);
+      this.mpGroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.mpGroupBox1.Location = new System.Drawing.Point(8, 8);
+      this.mpGroupBox1.Name = "mpGroupBox1";
+      this.mpGroupBox1.Size = new System.Drawing.Size(440, 72);
+      this.mpGroupBox1.TabIndex = 1;
+      this.mpGroupBox1.TabStop = false;
+      this.mpGroupBox1.Text = "Language Settings";
+      // 
+      // languageComboBox
+      // 
+      this.languageComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.languageComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.languageComboBox.Location = new System.Drawing.Point(168, 27);
+      this.languageComboBox.Name = "languageComboBox";
+      this.languageComboBox.Size = new System.Drawing.Size(256, 21);
+      this.languageComboBox.TabIndex = 5;
+      // 
+      // label2
+      // 
+      this.label2.Location = new System.Drawing.Point(16, 30);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(150, 23);
+      this.label2.TabIndex = 4;
+      this.label2.Text = "Display language";
+      // 
+      // groupBox1
+      // 
+      this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+        | System.Windows.Forms.AnchorStyles.Right)));
+      this.groupBox1.Controls.Add(this.settingsCheckedListBox);
+      this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.groupBox1.Location = new System.Drawing.Point(8, 88);
+      this.groupBox1.Name = "groupBox1";
+      this.groupBox1.Size = new System.Drawing.Size(440, 152);
+      this.groupBox1.TabIndex = 2;
+      this.groupBox1.TabStop = false;
+      this.groupBox1.Text = "General Settings";
+      // 
+      // settingsCheckedListBox
+      // 
+      this.settingsCheckedListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+        | System.Windows.Forms.AnchorStyles.Left) 
+        | System.Windows.Forms.AnchorStyles.Right)));
+      this.settingsCheckedListBox.Items.AddRange(new object[] {
+                                                                "Start MediaPlayer in fullscreen mode",
+                                                                "Auto hide mouse cursor",
+                                                                "Show special mouse controls (scrollbars, etc)",
+                                                                "Hide file extensions for known file types"});
+      this.settingsCheckedListBox.Location = new System.Drawing.Point(16, 24);
+      this.settingsCheckedListBox.Name = "settingsCheckedListBox";
+      this.settingsCheckedListBox.Size = new System.Drawing.Size(408, 109);
+      this.settingsCheckedListBox.TabIndex = 0;
+      // 
+      // General
+      // 
+      this.BackColor = System.Drawing.SystemColors.Control;
+      this.Controls.Add(this.groupBox1);
+      this.Controls.Add(this.mpGroupBox1);
+      this.Name = "General";
+      this.Size = new System.Drawing.Size(456, 448);
+      this.mpGroupBox1.ResumeLayout(false);
+      this.groupBox1.ResumeLayout(false);
+      this.ResumeLayout(false);
 
-		}
+    }
 		#endregion
 	}
 }
