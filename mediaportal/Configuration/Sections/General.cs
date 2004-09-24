f682ca5b5adc97ae497e1fee62500e55e6b5a136
yamp <yamp@team-mediaportal.com>
2004-09-24 17:03:51 +0000

    no message

--- mediaportal/Configuration/Sections/General.cs
+++ mediaportal/Configuration/Sections/General.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace MediaPortal.Configuration.Sections
 {
@@ -15,6 +16,13 @@ namespace MediaPortal.Configuration.Sections
 		private System.Windows.Forms.Label label2;
 		private MediaPortal.UserInterface.Controls.MPGroupBox groupBox1;
 		private System.Windows.Forms.CheckedListBox settingsCheckedListBox;
+    private System.Windows.Forms.GroupBox groupBox2;
+    private System.Windows.Forms.CheckBox checkBoxDaemonTools;
+    private System.Windows.Forms.TextBox textBoxDaemonTools;
+    private System.Windows.Forms.Button buttonSelectFolder;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.Label label3;
+    private System.Windows.Forms.ComboBox comboBoxDrive;
 		private System.ComponentModel.IContainer components = null;
 
 		public General() : this("General")
@@ -101,7 +109,25 @@ namespace MediaPortal.Configuration.Sections
 				// Set language
 				//
 				languageComboBox.Text = xmlreader.GetValueAsString("skin", "language", "English");
+        checkBoxDaemonTools.Checked= xmlreader.GetValueAsBool("daemon", "enabled", false);
+        textBoxDaemonTools.Text= xmlreader.GetValueAsString("daemon", "path", "");
+        comboBoxDrive.SelectedItem=xmlreader.GetValueAsString("daemon", "drive", "E:");
 			}
+      checkBoxDaemonTools_CheckedChanged(null,null);
+
+      if (textBoxDaemonTools.Text.Length==0)
+      {
+        try
+        {
+          RegistryKey hklm = Registry.LocalMachine;
+          RegistryKey subkey = hklm.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\UserData\S-1-5-18\Components\5DF1C3B1C87EFD376582F3A8B81F52D4");
+          if (subkey != null)
+          {
+            textBoxDaemonTools.Text=(string)subkey.GetValue("27A3DED38A1678B4895AFEB08C30A80A");
+          }
+        }
+        catch(Exception){}
+      }
 		}
 
 		public override void SaveSettings()
@@ -121,7 +147,11 @@ namespace MediaPortal.Configuration.Sections
 				// Set language
 				//
 				xmlwriter.SetValue("skin", "language", languageComboBox.Text);
-			}
+        
+        xmlwriter.SetValueAsBool("daemon", "enabled", checkBoxDaemonTools.Checked);
+        xmlwriter.SetValue("daemon", "path", textBoxDaemonTools.Text);
+        xmlwriter.SetValue("daemon", "drive", (string)comboBoxDrive.SelectedItem);
+      }
 		}
 
 		#region Designer generated code
@@ -136,8 +166,16 @@ namespace MediaPortal.Configuration.Sections
       this.label2 = new System.Windows.Forms.Label();
       this.groupBox1 = new MediaPortal.UserInterface.Controls.MPGroupBox();
       this.settingsCheckedListBox = new System.Windows.Forms.CheckedListBox();
+      this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.checkBoxDaemonTools = new System.Windows.Forms.CheckBox();
+      this.textBoxDaemonTools = new System.Windows.Forms.TextBox();
+      this.buttonSelectFolder = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.comboBoxDrive = new System.Windows.Forms.ComboBox();
+      this.label3 = new System.Windows.Forms.Label();
       this.mpGroupBox1.SuspendLayout();
       this.groupBox1.SuspendLayout();
+      this.groupBox2.SuspendLayout();
       this.SuspendLayout();
       // 
       // mpGroupBox1
@@ -200,19 +238,148 @@ namespace MediaPortal.Configuration.Sections
       this.settingsCheckedListBox.Size = new System.Drawing.Size(408, 109);
       this.settingsCheckedListBox.TabIndex = 0;
       // 
+      // groupBox2
+      // 
+      this.groupBox2.Controls.Add(this.label3);
+      this.groupBox2.Controls.Add(this.comboBoxDrive);
+      this.groupBox2.Controls.Add(this.label1);
+      this.groupBox2.Controls.Add(this.buttonSelectFolder);
+      this.groupBox2.Controls.Add(this.textBoxDaemonTools);
+      this.groupBox2.Controls.Add(this.checkBoxDaemonTools);
+      this.groupBox2.Location = new System.Drawing.Point(8, 248);
+      this.groupBox2.Name = "groupBox2";
+      this.groupBox2.Size = new System.Drawing.Size(440, 120);
+      this.groupBox2.TabIndex = 3;
+      this.groupBox2.TabStop = false;
+      this.groupBox2.Text = "Daemon tools";
+      // 
+      // checkBoxDaemonTools
+      // 
+      this.checkBoxDaemonTools.Location = new System.Drawing.Point(16, 24);
+      this.checkBoxDaemonTools.Name = "checkBoxDaemonTools";
+      this.checkBoxDaemonTools.Size = new System.Drawing.Size(296, 16);
+      this.checkBoxDaemonTools.TabIndex = 0;
+      this.checkBoxDaemonTools.Text = "Automount .iso/.bin files using Daemon tools";
+      this.checkBoxDaemonTools.CheckedChanged += new System.EventHandler(this.checkBoxDaemonTools_CheckedChanged);
+      // 
+      // textBoxDaemonTools
+      // 
+      this.textBoxDaemonTools.Location = new System.Drawing.Point(128, 48);
+      this.textBoxDaemonTools.Name = "textBoxDaemonTools";
+      this.textBoxDaemonTools.Size = new System.Drawing.Size(232, 20);
+      this.textBoxDaemonTools.TabIndex = 1;
+      this.textBoxDaemonTools.Text = "";
+      // 
+      // buttonSelectFolder
+      // 
+      this.buttonSelectFolder.Location = new System.Drawing.Point(368, 48);
+      this.buttonSelectFolder.Name = "buttonSelectFolder";
+      this.buttonSelectFolder.Size = new System.Drawing.Size(24, 23);
+      this.buttonSelectFolder.TabIndex = 2;
+      this.buttonSelectFolder.Text = "...";
+      this.buttonSelectFolder.Click += new System.EventHandler(this.buttonSelectFolder_Click);
+      // 
+      // label1
+      // 
+      this.label1.Location = new System.Drawing.Point(32, 80);
+      this.label1.Name = "label1";
+      this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+      this.label1.Size = new System.Drawing.Size(80, 16);
+      this.label1.TabIndex = 3;
+      this.label1.Text = "Virtual drive:";
+      // 
+      // comboBoxDrive
+      // 
+      this.comboBoxDrive.Items.AddRange(new object[] {
+                                                       "D:",
+                                                       "E:",
+                                                       "F:",
+                                                       "G",
+                                                       "H:",
+                                                       "I:",
+                                                       "J:",
+                                                       "K:",
+                                                       "L:",
+                                                       "M:",
+                                                       "N:",
+                                                       "O:",
+                                                       "P:",
+                                                       "Q:",
+                                                       "R:",
+                                                       "S:",
+                                                       "T:",
+                                                       "U:",
+                                                       "V:",
+                                                       "W:",
+                                                       "X:",
+                                                       "Y:",
+                                                       "Z:"});
+      this.comboBoxDrive.Location = new System.Drawing.Point(128, 80);
+      this.comboBoxDrive.Name = "comboBoxDrive";
+      this.comboBoxDrive.Size = new System.Drawing.Size(121, 21);
+      this.comboBoxDrive.TabIndex = 4;
+      this.comboBoxDrive.Text = "L:";
+      // 
+      // label3
+      // 
+      this.label3.Location = new System.Drawing.Point(32, 56);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(80, 16);
+      this.label3.TabIndex = 5;
+      this.label3.Text = "Daemon tools:";
+      // 
       // General
       // 
       this.BackColor = System.Drawing.SystemColors.Control;
+      this.Controls.Add(this.groupBox2);
       this.Controls.Add(this.groupBox1);
       this.Controls.Add(this.mpGroupBox1);
       this.Name = "General";
       this.Size = new System.Drawing.Size(456, 448);
       this.mpGroupBox1.ResumeLayout(false);
       this.groupBox1.ResumeLayout(false);
+      this.groupBox2.ResumeLayout(false);
       this.ResumeLayout(false);
 
     }
 		#endregion
+
+    private void buttonSelectFolder_Click(object sender, System.EventArgs e)
+    {
+      using(OpenFileDialog openFileDialog = new OpenFileDialog())
+      {
+        openFileDialog.FileName = textBoxDaemonTools.Text;
+        openFileDialog.CheckFileExists = true;
+        openFileDialog.RestoreDirectory=true;
+        openFileDialog.Filter= "exe files (*.exe)|*.exe";
+        openFileDialog.FilterIndex = 0;
+        openFileDialog.Title = "Select Daemon Tools";
+
+        DialogResult dialogResult = openFileDialog.ShowDialog();
+
+        if(dialogResult == DialogResult.OK)
+        {
+          textBoxDaemonTools.Text = openFileDialog.FileName;
+        }
+      }
+    }
+
+    private void checkBoxDaemonTools_CheckedChanged(object sender, System.EventArgs e)
+    {
+      if (checkBoxDaemonTools.Checked)
+      {
+        textBoxDaemonTools.Enabled=true;
+        comboBoxDrive.Enabled=true;
+        buttonSelectFolder.Enabled=true;
+      }
+      else
+      {
+        textBoxDaemonTools.Enabled=false;
+        comboBoxDrive.Enabled=false;
+        buttonSelectFolder.Enabled=false;
+      }
+
+    }
 	}
 }
 
