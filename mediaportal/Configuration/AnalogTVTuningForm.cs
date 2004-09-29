0c43059e3c6bc287916244b0dec7f76fec6b8dc6
yamp <yamp@team-mediaportal.com>
2004-09-29 20:18:09 +0000

    added tv-autotune for  (not finished yet)

--- mediaportal/Configuration/AnalogTVTuningForm.cs
+++ mediaportal/Configuration/AnalogTVTuningForm.cs
@@ -22,13 +22,15 @@ namespace MediaPortal
     private System.Windows.Forms.Label labelStatus;
     private System.Windows.Forms.Button btnOk;
     private System.Windows.Forms.Panel panel1;
-    private System.Windows.Forms.ComboBox comboBoxTV;
     private System.Windows.Forms.Button buttonMap;
     private System.Windows.Forms.Label label1;
     private System.Windows.Forms.Timer timer1;
     private System.ComponentModel.IContainer components;
     ArrayList     m_tvcards    = new ArrayList();
     private System.Windows.Forms.Button buttonSkip;
+    private System.Windows.Forms.ListView listView1;
+    private System.Windows.Forms.ColumnHeader columnHeader1;
+    private System.Windows.Forms.ColumnHeader columnHeader2;
     int           m_iChannel   =1;
 
 		public AnalogTVTuningForm()
@@ -38,18 +40,20 @@ namespace MediaPortal
 			//
 			InitializeComponent();
 
-      comboBoxTV.Items.Clear();
+      listView1.Items.Clear();
+      
       ArrayList channels=new ArrayList();
 			TVDatabase.GetChannels(ref channels);
       foreach (TVChannel channel in channels)
       {
         if (channel.Number<1000)
         {
-          comboBoxTV.Items.Add(channel.Name);
+          ListViewItem item = new ListViewItem();
+          item.Text=channel.Name;
+          item.SubItems.Add(channel.Number.ToString());
+          listView1.Items.Add(item);
         }
       }
-      if (comboBoxTV.Items.Count>0)
-        comboBoxTV.SelectedIndex=0;
 
       m_tvcards.Clear();
       try
@@ -68,7 +72,6 @@ namespace MediaPortal
       {
         MessageBox.Show(this,"No TVcapture cards configured. Please configure your capture card first","AutoTUne",MessageBoxButtons.OK);
         buttonMap.Enabled=false;
-        comboBoxTV.Enabled=false;
         return;
       }
       for (int i=0; i < m_tvcards.Count;i++)
@@ -105,11 +108,13 @@ namespace MediaPortal
       this.labelStatus = new System.Windows.Forms.Label();
       this.btnOk = new System.Windows.Forms.Button();
       this.panel1 = new System.Windows.Forms.Panel();
-      this.comboBoxTV = new System.Windows.Forms.ComboBox();
       this.buttonMap = new System.Windows.Forms.Button();
       this.label1 = new System.Windows.Forms.Label();
       this.timer1 = new System.Windows.Forms.Timer(this.components);
       this.buttonSkip = new System.Windows.Forms.Button();
+      this.listView1 = new System.Windows.Forms.ListView();
+      this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+      this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
       this.SuspendLayout();
       // 
       // labelStatus
@@ -121,8 +126,9 @@ namespace MediaPortal
       // 
       // btnOk
       // 
-      this.btnOk.Location = new System.Drawing.Point(424, 232);
+      this.btnOk.Location = new System.Drawing.Point(456, 240);
       this.btnOk.Name = "btnOk";
+      this.btnOk.Size = new System.Drawing.Size(48, 23);
       this.btnOk.TabIndex = 2;
       this.btnOk.Text = "Ok";
       this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
@@ -134,17 +140,10 @@ namespace MediaPortal
       this.panel1.Size = new System.Drawing.Size(248, 200);
       this.panel1.TabIndex = 3;
       // 
-      // comboBoxTV
-      // 
-      this.comboBoxTV.Location = new System.Drawing.Point(296, 88);
-      this.comboBoxTV.Name = "comboBoxTV";
-      this.comboBoxTV.Size = new System.Drawing.Size(136, 21);
-      this.comboBoxTV.TabIndex = 4;
-      // 
       // buttonMap
       // 
       this.buttonMap.Enabled = false;
-      this.buttonMap.Location = new System.Drawing.Point(448, 88);
+      this.buttonMap.Location = new System.Drawing.Point(352, 240);
       this.buttonMap.Name = "buttonMap";
       this.buttonMap.Size = new System.Drawing.Size(40, 23);
       this.buttonMap.TabIndex = 5;
@@ -153,7 +152,7 @@ namespace MediaPortal
       // 
       // label1
       // 
-      this.label1.Location = new System.Drawing.Point(296, 64);
+      this.label1.Location = new System.Drawing.Point(288, 48);
       this.label1.Name = "label1";
       this.label1.Size = new System.Drawing.Size(112, 23);
       this.label1.TabIndex = 6;
@@ -166,21 +165,42 @@ namespace MediaPortal
       // buttonSkip
       // 
       this.buttonSkip.Enabled = false;
-      this.buttonSkip.Location = new System.Drawing.Point(448, 128);
+      this.buttonSkip.Location = new System.Drawing.Point(400, 240);
       this.buttonSkip.Name = "buttonSkip";
       this.buttonSkip.Size = new System.Drawing.Size(40, 23);
       this.buttonSkip.TabIndex = 7;
       this.buttonSkip.Text = "Skip";
       this.buttonSkip.Click += new System.EventHandler(this.buttonSkip_Click);
       // 
+      // listView1
+      // 
+      this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+                                                                                this.columnHeader1,
+                                                                                this.columnHeader2});
+      this.listView1.Location = new System.Drawing.Point(288, 72);
+      this.listView1.MultiSelect = false;
+      this.listView1.Name = "listView1";
+      this.listView1.Size = new System.Drawing.Size(216, 152);
+      this.listView1.TabIndex = 8;
+      this.listView1.View = System.Windows.Forms.View.Details;
+      // 
+      // columnHeader1
+      // 
+      this.columnHeader1.Text = "Name";
+      this.columnHeader1.Width = 147;
+      // 
+      // columnHeader2
+      // 
+      this.columnHeader2.Text = "Channel";
+      // 
       // AnalogTVTuningForm
       // 
       this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
       this.ClientSize = new System.Drawing.Size(520, 273);
+      this.Controls.Add(this.listView1);
       this.Controls.Add(this.buttonSkip);
       this.Controls.Add(this.label1);
       this.Controls.Add(this.buttonMap);
-      this.Controls.Add(this.comboBoxTV);
       this.Controls.Add(this.panel1);
       this.Controls.Add(this.btnOk);
       this.Controls.Add(this.labelStatus);
@@ -194,13 +214,35 @@ namespace MediaPortal
 
     private void btnOk_Click(object sender, System.EventArgs e)
     {
-      Close();
-    
+      for (int i=0; i < listView1.Items.Count;++i)
+      {
+        try
+        {
+          int chan=Int32.Parse(listView1.Items[i].SubItems[1].Text);
+          if (chan>0)
+          {
+            TVDatabase.SetChannelNumber(listView1.Items[i].Text,chan);
+            TVDatabase.SetChannelFrequency(listView1.Items[i].Text,"0");
+          }
+        }
+        catch(Exception){}
+      }
+      this.Close();
     }
 
     private void buttonMap_Click(object sender, System.EventArgs e)
     {
-    
+      if (listView1.SelectedItems.Count<=0) return;
+      string selectedChannel=listView1.SelectedItems[0].Text;
+      for (int i=0; i < listView1.Items.Count;++i)
+      {
+        if (listView1.Items[i].Text==(string)selectedChannel)
+        {
+          listView1.Items[i].SubItems[1].Text=m_iChannel.ToString();
+          NextChannel();
+          return;
+        }
+      }
     }
 
     private void AnalogTVTuningForm_Load(object sender, System.EventArgs e)
