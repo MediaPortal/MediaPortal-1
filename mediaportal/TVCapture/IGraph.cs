82893499233ff3d86bf52f4594686527eb4f788f
yamp <yamp@team-mediaportal.com>
2004-10-10 17:25:45 +0000

    no message

--- mediaportal/TVCapture/IGraph.cs
+++ mediaportal/TVCapture/IGraph.cs
@@ -10,7 +10,46 @@ namespace MediaPortal.TV.Recording
 	/// -tv viewing
 	/// -tv recording
 	/// -tv timeshifting
+	/// 
+	/// Currently there are 2 implementations of IGraph
+  /// Sinkgraph.cs      : support for cards with an onboard MPEG2 encoder like the hauppauge PVR 250/350/USB2
+  /// SWEncodingGraph.cs: support for cards without an onboard mpeg2 encoder like the pinnacle PCTV Pro, WinTV,...
+  /// 
+  /// NOTE!!! IGraph instances are created by the GraphFactory.cs  
+  ///   
+	/// Basicly the flow is
+	/// 
+	/// for viewing:
+	///   CreateGraph()
+	///   if SupportsTimeshifting() then 
+	///     StartTimeshifting()
+  ///     ....
+  ///     StopTimeshifting()
+  ///     DeleteGraph()
+	///   else
+	///     StartViewing
+	///     ...
+	///     StopViewing()
+	///     DeleteGraph()
+	///     
+	/// for recording:    
+  ///   CreateGraph()
+  ///   if SupportsTimeshifting() then 
+  ///     StartTimeshifting()
+	///     StartRecording()
+	///     .....
+	///     StopRecording()
+	///     DeleteGraph()
+	///   else
+  ///     StartRecording()
+  ///     .....
+  ///     StopRecording()
+  ///     DeleteGraph()
+  ///     
+  ///     
 	/// </summary>
+  /// <seealso cref="MediaPortal.TV.Recording.GraphFactory"/>
+
 	public interface IGraph
 	{
     /// <summary>
@@ -133,8 +172,20 @@ namespace MediaPortal.TV.Recording
     /// </returns>
     bool ShouldRebuildGraph(int iChannel);
 
+    /// <summary>
+    /// This method returns whether a signal is present. Meaning that the
+    /// TV tuner (or video input) is tuned to a channel
+    /// </summary>
+    /// <returns>true:  tvtuner is tuned to a channel (or video-in has a video signal)
+    ///          false: tvtuner is not tuned to a channel (or video-in has no video signal)
+    /// </returns>
     bool SignalPresent();
 
+    /// <summary>
+    /// This method returns the frequency to which the tv tuner is currently tuned
+    /// </summary>
+    /// <returns>frequency in Hertz
+    /// </returns>
     long VideoFrequency();
 	}
 }
