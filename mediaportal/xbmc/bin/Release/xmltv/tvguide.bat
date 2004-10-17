2489d469e4dd482d0bdffc2b38b79a7e58c05c20
mrmario64 <mrmario64@team-mediaportal.com>
2004-10-17 15:19:39 +0000

    removed junk xmltv stuff

--- mediaportal/xbmc/bin/Release/xmltv/tvguide.bat
+++
@@ -1,9 +0,0 @@
-:start
-del tvguidenew.xml
-del tvguideold.xml
-Sleep 10
-tv_grab_nl.py  --output=tvguidenew.xml
-if errorlevel 10 goto start
-
-ren tvguide.xml tvguideold.xml
-ren tvguidenew.xml tvguide.xml
