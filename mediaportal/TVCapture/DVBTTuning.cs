693a453f39e21c01661850ed2632288b587abc42
yamp <yamp@team-mediaportal.com>
2005-01-02 07:41:55 +0000

    no message

--- mediaportal/TVCapture/DVBTTuning.cs
+++ mediaportal/TVCapture/DVBTTuning.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Windows.Forms;
 using DShowNET;
 using MediaPortal.TV.Database;
+using System.Xml;
 
 namespace MediaPortal.TV.Recording
 {
@@ -14,6 +15,9 @@ namespace MediaPortal.TV.Recording
 	{
 		TVCaptureDevice											captureCard;
 		AutoTuneCallback										callback = null;
+		ArrayList                           frequencies=new ArrayList();
+		int                                 currentFrequencyIndex=0;
+		private System.Windows.Forms.Timer  timer1;
 		public DVBTTuning()
 		{
 		}
@@ -23,6 +27,22 @@ namespace MediaPortal.TV.Recording
 		{
 			captureCard=card;
 			callback=statusCallback;
+
+			frequencies.Clear();
+			currentFrequencyIndex=0;
+			XmlDocument doc= new XmlDocument();
+			doc.Load("dvbt.xml");
+			XmlNodeList frequencyList= doc.DocumentElement.SelectNodes("/dvbt/frequencies/frequency");
+			foreach (XmlNode node in frequencyList)
+			{
+				int carrierFrequency= XmlConvert.ToInt32(node.Attributes.GetNamedItem(@"carrier").InnerText);
+				frequencies.Add(carrierFrequency);
+			}
+			this.timer1 = new System.Windows.Forms.Timer();
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			timer1.Interval=100;
+			timer1.Enabled=true;
+			callback.OnProgress(0);
 			return;
 		}
 
@@ -40,6 +60,48 @@ namespace MediaPortal.TV.Recording
 		{
 			// TODO:  Add DVBTTuning.MapToChannel implementation
 			return 0;
+		}
+
+		private void timer1_Tick(object sender, System.EventArgs e)
+		{
+			float percent = ((float)currentFrequencyIndex) / ((float)frequencies.Count);
+			percent *= 100.0f;
+			callback.OnProgress((int)percent);
+			float frequency=(float)frequencies[currentFrequencyIndex];
+			frequency /=1000;
+			string description=String.Format("frequency:{1:###.##} MHz.", frequency);
+			callback.OnStatus(description);
+
+			if (captureCard.SignalPresent())
+			{
+				timer1.Enabled=false;
+				callback.OnNewChannel();
+				return;
+			}
+			ScanNextFrequency();
+		}
+
+		void ScanNextFrequency()
+		{
+			
+			currentFrequencyIndex++;
+			if (currentFrequencyIndex>=frequencies.Count)
+			{
+				timer1.Enabled=false;
+				callback.OnProgress(100);
+				callback.OnEnded();
+				captureCard.DeleteGraph();
+				return;
+			}
+
+			/*if (!captureCard.ViewChannel(currentChannel,captureCard.CountryCode,AnalogVideoStandard.None) )
+			{
+				timer1.Enabled=false;
+				callback.OnProgress(100);
+				callback.OnEnded();
+				captureCard.DeleteGraph();
+				return;
+			}*/
 		}
 
 		#endregion
