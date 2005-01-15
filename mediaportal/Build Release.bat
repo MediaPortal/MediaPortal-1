36e4a1139bb3feb0e3496331650e4ffcc6b72306
mrmario64 <mrmario64@team-mediaportal.com>
2005-01-15 20:24:28 +0000

    *** empty log message ***

--- mediaportal/Build Release.bat
+++ mediaportal/Build Release.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.com" /rebuild UseCaptureDefintionsRelease MediaPortal.sln
+"C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\IDE\devenv.com" /rebuild Release MediaPortal.sln
