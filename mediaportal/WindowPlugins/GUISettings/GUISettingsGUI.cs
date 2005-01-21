aded24a7f69a1cafb66bd63f9421ef1e5cd997a1
yamp <yamp@team-mediaportal.com>
2005-01-21 19:38:07 +0000

    no message

--- mediaportal/WindowPlugins/GUISettings/GUISettingsGUI.cs
+++ mediaportal/WindowPlugins/GUISettings/GUISettingsGUI.cs
@@ -98,7 +98,7 @@ namespace MediaPortal.GUI.Settings
 					if (iControl==(int)Controls.CONTROL_FPS)
 					{
 						string strLabel=message.Label;
-						GUIGraphicsContext.MaxFPS=Int32.Parse(strLabel);
+						GUIGraphicsContext.MaxFPS=Int32.Parse(strLabel)+10;
 					}
 
         }
