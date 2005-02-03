f80d21e369cc4e12afcac05c07a626f88c908038
corl <corl@team-mediaportal.com>
2005-02-03 18:32:58 +0000

    Changes for skin "autohide" setting and improvements

--- mediaportal/WindowPlugins/GUITopbar/GUITopbarHome.cs
+++ mediaportal/WindowPlugins/GUITopbar/GUITopbarHome.cs
@@ -10,19 +10,16 @@ namespace MediaPortal.Topbar
   {
     const int HIDE_SPEED = 8;
 
-    enum Controls 
-    {
-      TOPBAR_BACKGROUND=1
-    };
-
     bool m_bFocused=false;
     bool m_bEnabled=false;
-    bool m_bAutoHide=false;
+    bool m_bTopBarAutoHide=false;
     bool m_bTopBarEffect=false;
+    bool m_bTopBarHide=false;
     bool m_bTopBarHidden=false;
-    int m_iMoveUp=0;
-    int m_iMoveUpCount=0;
-    int m_iAutoHideTimeOut=25;
+    bool m_bOverrideSkinAutoHide=false;
+    int m_iMoveUp=0;    
+    int m_iTopbarRegion=10;
+    int m_iAutoHideTimeOut=15;
         
     public GUITopbarHome()
 		{
@@ -37,8 +34,21 @@ namespace MediaPortal.Topbar
       {
         m_iAutoHideTimeOut = xmlreader.GetValueAsInt("TopBar", "autohidetimeout", 15);
 
-        m_bAutoHide = false;
-        if (xmlreader.GetValueAsInt("TopBar", "autohide", 0) == 1) m_bAutoHide = true;
+        m_bOverrideSkinAutoHide = false;
+        if (xmlreader.GetValueAsInt("TopBar", "overrideskinautohide", 0) == 1) m_bOverrideSkinAutoHide = true;
+
+        m_bTopBarAutoHide = this.AutoHide;     
+        if (m_bOverrideSkinAutoHide)
+        {          
+          m_bTopBarAutoHide = false;
+          if (xmlreader.GetValueAsInt("TopBar", "autohide", 0) == 1) m_bTopBarAutoHide = true;
+        }
+      }
+
+      // Topbar region
+      foreach (CPosition pos in m_vecPositions)
+      {
+        if ((pos.YPos+pos.control.Height) > m_iTopbarRegion) m_iTopbarRegion=pos.YPos+pos.control.Height;
       }
 
       return bResult;
@@ -67,62 +77,72 @@ namespace MediaPortal.Topbar
       if (iLayer !=1) return;
       CheckFocus();
 
-      // Check autohide timeout
-      if (m_bFocused)
+      // Check auto hide topbar
+      if (m_bTopBarAutoHide)
       {
-        m_bTopBarHidden = false;
-        GUIGraphicsContext.TopBarTimeOut = DateTime.Now;
-      }
-
-      TimeSpan ts=DateTime.Now-GUIGraphicsContext.TopBarTimeOut;
-      if (m_bAutoHide && (ts.TotalSeconds > m_iAutoHideTimeOut) && !m_bTopBarHidden)
-      {
-        // Hide topbar with effect
-        m_bTopBarHidden = true;
-
-        GUIControl cntl=GetControl((int)Controls.TOPBAR_BACKGROUND);
-        if (cntl!=null)
+        // Check autohide timeout
+        if (m_bFocused)
         {
-          m_iMoveUpCount = cntl.Height;
+          m_bTopBarHide = false;
+          GUIGraphicsContext.TopBarTimeOut = DateTime.Now;
+        }
+
+        TimeSpan ts=DateTime.Now-GUIGraphicsContext.TopBarTimeOut;
+        if ((ts.TotalSeconds > m_iAutoHideTimeOut) && !m_bTopBarHide)
+        {
+          // Hide topbar with effect
+          m_bTopBarHide = true;
           m_iMoveUp=0;
-        }							
-      }
-
-      if (GUIGraphicsContext.TopBarHidden != m_bTopBarHidden)
-      {
-        m_bTopBarEffect = true;
-      }
-
-      if (m_bTopBarEffect)
-      {
-        if (m_bTopBarHidden)
+        }
+        
+        if (GUIGraphicsContext.TopBarHidden != m_bTopBarHidden)
         {
-          m_iMoveUp+=HIDE_SPEED;
-          if (m_iMoveUp >= m_iMoveUpCount) 
+          // Rest to new settings
+          m_bTopBarHidden = GUIGraphicsContext.TopBarHidden;
+          m_bTopBarHide = GUIGraphicsContext.TopBarHidden;
+          m_bTopBarEffect = false;
+
+          m_iMoveUp = 0;
+          if (m_bTopBarHidden) m_iMoveUp = m_iTopbarRegion;
+          foreach (CPosition pos in m_vecPositions)
           {
-            GUIGraphicsContext.TopBarHidden = true;
-            m_bTopBarEffect = false;
+            pos.control.SetPosition((int)pos.XPos,(int)pos.YPos - m_iMoveUp);         
           }
         }
-        else
+        else if (m_bTopBarHidden != m_bTopBarHide)
         {
-          GUIGraphicsContext.TopBarHidden = false;
-          m_iMoveUp = 0;            
-          /*m_iMoveUp-=HIDE_SPEED;
-          if (m_iMoveUp < 0) 
-          {
-            m_iMoveUp = 0;            
-            m_bTopBarEffect = false;
-          }*/
+          m_bTopBarEffect = true;
+          //GUIGraphicsContext.TopBarHidden = m_bTopBarHide;
         }
 
-        foreach (CPosition pos in m_vecPositions)
+
+        if (m_bTopBarEffect)
         {
-          pos.control.SetPosition((int)pos.XPos,(int)pos.YPos - m_iMoveUp);         
+          if (m_bTopBarHide)
+          {
+            m_iMoveUp+=HIDE_SPEED;
+            if (m_iMoveUp >= m_iTopbarRegion) 
+            {
+              m_iMoveUp = m_iTopbarRegion;
+              m_bTopBarHidden = true;
+              GUIGraphicsContext.TopBarHidden = true;
+              m_bTopBarEffect = false;
+            }
+          }
+          else
+          {
+            m_bTopBarHidden = false;
+            GUIGraphicsContext.TopBarHidden = false;
+            m_iMoveUp = 0;            
+          }
+
+          foreach (CPosition pos in m_vecPositions)
+          {
+            pos.control.SetPosition((int)pos.XPos,(int)pos.YPos - m_iMoveUp);         
+          }
         }
       }
-
-      if (GUIGraphicsContext.TopBarHidden) return;     
+      if (m_bTopBarHidden && m_bTopBarEffect) return;     
      
       base.Render();
     }
@@ -152,8 +172,11 @@ namespace MediaPortal.Topbar
         if (m_bFocused==true)
         {
           // reset autohide timer
-          GUIGraphicsContext.TopBarTimeOut = DateTime.Now;
-          m_bTopBarHidden = false;
+          if (m_bTopBarAutoHide) 
+          {
+            GUIGraphicsContext.TopBarTimeOut = DateTime.Now;
+            m_bTopBarHide = false;
+          }
 
           GUIMessage msg = new GUIMessage(GUIMessage.MessageType.GUI_MSG_SETFOCUS,GetID, 0,m_dwDefaultFocusControlID,0,0,null);
           OnMessage(msg);
@@ -174,12 +197,12 @@ namespace MediaPortal.Topbar
       if (action.wID == Action.ActionType.ACTION_MOUSE_MOVE)
       {
         // reset autohide timer       
-        if (m_bTopBarHidden)
+        if (m_bTopBarHidden && m_bTopBarAutoHide)
         {
-          if (action.fAmount2 < m_iMoveUpCount)
+          if (action.fAmount2 < m_iTopbarRegion)
           {
             GUIGraphicsContext.TopBarTimeOut = DateTime.Now;
-            m_bTopBarHidden = false;
+            m_bTopBarHide = false;
           }
         }
 
@@ -208,7 +231,7 @@ namespace MediaPortal.Topbar
       if (action.wID==Action.ActionType.ACTION_MOVE_DOWN)
       {
         // reset autohide timer
-        GUIGraphicsContext.TopBarTimeOut = DateTime.Now;
+        if (m_bTopBarAutoHide) GUIGraphicsContext.TopBarTimeOut = DateTime.Now;
         Focused=false;
       }
     }
--- mediaportal/WindowPlugins/GUITopbar/TopBarSetupForm.cs
+++ mediaportal/WindowPlugins/GUITopbar/TopBarSetupForm.cs
@@ -12,10 +12,11 @@ namespace MediaPortal.Topbar
 	public class TopBarSetupForm : System.Windows.Forms.Form
 	{
     private System.Windows.Forms.CheckBox chkAutoHide;
-    private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.Label label2;
-    private System.Windows.Forms.TextBox textTimeOut;
     private System.Windows.Forms.Button buttonOk;
+    private System.Windows.Forms.Label label2;
+    private System.Windows.Forms.Label label1;
+    private System.Windows.Forms.TextBox textTimeOut;
+    private System.Windows.Forms.CheckBox chkOverrideSkinAutoHide;
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
@@ -56,54 +57,67 @@ namespace MediaPortal.Topbar
 		private void InitializeComponent()
 		{
       this.chkAutoHide = new System.Windows.Forms.CheckBox();
-      this.textTimeOut = new System.Windows.Forms.TextBox();
-      this.label1 = new System.Windows.Forms.Label();
-      this.label2 = new System.Windows.Forms.Label();
       this.buttonOk = new System.Windows.Forms.Button();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
+      this.textTimeOut = new System.Windows.Forms.TextBox();
+      this.chkOverrideSkinAutoHide = new System.Windows.Forms.CheckBox();
       this.SuspendLayout();
       // 
       // chkAutoHide
       // 
-      this.chkAutoHide.Location = new System.Drawing.Point(16, 24);
+      this.chkAutoHide.Enabled = false;
+      this.chkAutoHide.Location = new System.Drawing.Point(16, 40);
       this.chkAutoHide.Name = "chkAutoHide";
-      this.chkAutoHide.TabIndex = 0;
+      this.chkAutoHide.TabIndex = 1;
       this.chkAutoHide.Text = "Autohide";
-      // 
-      // textTimeOut
-      // 
-      this.textTimeOut.Location = new System.Drawing.Point(80, 53);
-      this.textTimeOut.Name = "textTimeOut";
-      this.textTimeOut.Size = new System.Drawing.Size(48, 20);
-      this.textTimeOut.TabIndex = 1;
-      this.textTimeOut.Text = "15";
-      // 
-      // label1
-      // 
-      this.label1.Location = new System.Drawing.Point(16, 56);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(48, 23);
-      this.label1.TabIndex = 2;
-      this.label1.Text = "Timeout:";
-      // 
-      // label2
-      // 
-      this.label2.Location = new System.Drawing.Point(128, 56);
-      this.label2.Name = "label2";
-      this.label2.TabIndex = 3;
-      this.label2.Text = "sec.";
       // 
       // buttonOk
       // 
       this.buttonOk.Location = new System.Drawing.Point(200, 224);
       this.buttonOk.Name = "buttonOk";
-      this.buttonOk.TabIndex = 4;
+      this.buttonOk.TabIndex = 3;
       this.buttonOk.Text = "Ok";
       this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+      // 
+      // label2
+      // 
+      this.label2.Location = new System.Drawing.Point(128, 80);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(100, 24);
+      this.label2.TabIndex = 3;
+      this.label2.Text = "sec.";
+      // 
+      // label1
+      // 
+      this.label1.Location = new System.Drawing.Point(16, 80);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(48, 24);
+      this.label1.TabIndex = 2;
+      this.label1.Text = "Timeout:";
+      // 
+      // textTimeOut
+      // 
+      this.textTimeOut.Location = new System.Drawing.Point(80, 80);
+      this.textTimeOut.Name = "textTimeOut";
+      this.textTimeOut.Size = new System.Drawing.Size(48, 20);
+      this.textTimeOut.TabIndex = 2;
+      this.textTimeOut.Text = "15";
+      // 
+      // chkOverrideSkinAutoHide
+      // 
+      this.chkOverrideSkinAutoHide.Location = new System.Drawing.Point(16, 16);
+      this.chkOverrideSkinAutoHide.Name = "chkOverrideSkinAutoHide";
+      this.chkOverrideSkinAutoHide.Size = new System.Drawing.Size(200, 24);
+      this.chkOverrideSkinAutoHide.TabIndex = 0;
+      this.chkOverrideSkinAutoHide.Text = "Override skin \"AutoHide\" setting";
+      this.chkOverrideSkinAutoHide.CheckedChanged += new System.EventHandler(this.chkOverrideSkinAutoHide_CheckedChanged);
       // 
       // TopBarSetupForm
       // 
       this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
       this.ClientSize = new System.Drawing.Size(292, 266);
+      this.Controls.Add(this.chkOverrideSkinAutoHide);
       this.Controls.Add(this.buttonOk);
       this.Controls.Add(this.label2);
       this.Controls.Add(this.label1);
@@ -118,14 +132,16 @@ namespace MediaPortal.Topbar
 		#endregion
 
     private void TopBarSetup_Load(object sender, System.EventArgs e)
-    {
-      
+    {      
       using (AMS.Profile.Xml   xmlreader=new AMS.Profile.Xml("MediaPortal.xml"))
       {
         textTimeOut.Text = xmlreader.GetValueAsString("TopBar","autohidetimeout","15");
 
         chkAutoHide.Checked = false;
         if (xmlreader.GetValueAsInt("TopBar", "autohide", 0) == 1) chkAutoHide.Checked = true;
+        chkOverrideSkinAutoHide.Checked = false;
+        if (xmlreader.GetValueAsInt("TopBar", "overrideskinautohide", 0) == 1) chkOverrideSkinAutoHide.Checked = true;
+        chkAutoHide.Enabled = chkOverrideSkinAutoHide.Checked;
       }
     }
 
@@ -138,8 +154,17 @@ namespace MediaPortal.Topbar
         int iAutoHide=0;
         if (chkAutoHide.Checked) iAutoHide=1;
         xmlWriter.SetValue("TopBar","autohide",iAutoHide.ToString());
+        
+        int iBoolean=0;
+        if (chkOverrideSkinAutoHide.Checked) iBoolean=1;
+        xmlWriter.SetValue("TopBar","overrideskinautohide",iBoolean.ToString());
       }
       this.Close();
+    }
+
+    private void chkOverrideSkinAutoHide_CheckedChanged(object sender, System.EventArgs e)
+    {
+      chkAutoHide.Enabled = chkOverrideSkinAutoHide.Checked;
     }
 	}
 }
