54364143a44dd4ef69f66de2f70d24927f93e1d4
yamp <yamp@team-mediaportal.com>
2005-02-03 06:00:53 +0000

    no message

--- mediaportal/TVCapture/DVBSections.cs
+++ mediaportal/TVCapture/DVBSections.cs
@@ -1907,7 +1907,7 @@ namespace MediaPortal.TV.Recording
 				tv.End=GetLongFromDate(dur.Year,dur.Month,dur.Day,dur.Hour,dur.Minute,dur.Second);
 				Log.Write("program:{0} {1} {2} {3} {4}-{5}",
 						tv.Channel,tv.Genre,tv.Description,tv.Title,tv.StartTime,tv.EndTime);
-				ArrayList programsInDatabase;
+				ArrayList programsInDatabase = new ArrayList();
 				TVDatabase.GetProgramsPerChannel(tv.Channel,tv.Start,tv.End,ref programsInDatabase);
 				if(programsInDatabase.Count==0)
 				{
