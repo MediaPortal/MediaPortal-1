62eb962e0380f0a1c9ec898e72aa08294445328a
agree <agree@team-mediaportal.com>
2005-02-13 13:13:37 +0000

    removed unused code

--- mediaportal/dvblib/dvblib.cpp
+++ mediaportal/dvblib/dvblib.cpp
@@ -44,7 +44,6 @@ DeliverSectionData m_callBack;
 long m_pidArray[39];
 long m_pidArrayCount;
 //
-B2C2MPEG2Adapter *m_b2c2Adapter=new B2C2MPEG2Adapter("");
 //
 IMpeg2Data			*m_mpegSIInterface=NULL;
 IBaseFilter			*m_demux=NULL;
@@ -59,13 +58,7 @@ typedef enum
 	DATA_PIN_3
 } tDataPinIndex;
 //
-BOOL AddTSPid(int pidnum);
-HRESULT LoadGraphFile(IGraphBuilder *pGraph, const WCHAR* wszName);
-IPin *GetPin(IBaseFilter *pFilter, PIN_DIRECTION PinDir);
-HRESULT SaveGraphFile(IGraphBuilder *pGraph, WCHAR *wszPath);
-BOOL TuneCard(IB2C2MPEG2TunerCtrl2	*pB2C2FilterTunerCtrl,IB2C2MPEG2AVCtrl2	*pB2C2FilterAvCtrl,IB2C2MPEG2DataCtrl2	*pB2C2FilterDataCtrl,long Frequency,long SymbolRate,long FEC,long POL,long LNB,long Diseq,long AudioPID,long VideoPID,long LNBNumber);
 // callbacks
-UINT __stdcall VideoCB(MPEG2_VIDEO_INFO *pInfo);
 
 #if defined WIN32
 	#define MAX_ADDR_FORMAT_STR		"%02X-%02X-%02X-%02X-%02X-%02X"
@@ -103,49 +96,6 @@ BOOL CdvblibApp::InitInstance()
 	return TRUE;
 }
 
-void ResetDevice(void)
-{
-	if(m_b2c2Adapter && m_b2c2Adapter->IsInitialized())
-		m_b2c2Adapter->Release();
-}
-
-BOOL DVBInit(void)
-{
-	if(m_b2c2Adapter==NULL)
-		m_b2c2Adapter=new B2C2MPEG2Adapter("");
-	if( m_b2c2Adapter)
-	{
-		HRESULT hr;
-		if(m_b2c2Adapter->IsInitialized()==(BOOL)true)
-			return true;
-		hr=m_b2c2Adapter->Initialize();
-		if(FAILED(hr))
-			return false;
-		else
-		return true;
-	}
-	return false;	
-}
-//
-
-// setup a callback
-void SetVideoCB(LONGLONG *pointer)
-{
-	IB2C2MPEG2AVCtrl2		*pB2C2FilterAvCtrl= NULL;
-
-	if(DVBInit()==false)
-	{
-		return;
-	}
-	pB2C2FilterAvCtrl=m_b2c2Adapter->GetAvControl();
-	if(pB2C2FilterAvCtrl)
-		pB2C2FilterAvCtrl->SetCallbackForVideoMode(&VideoCB);
-}
-UINT __stdcall VideoCB(MPEG2_VIDEO_INFO *pInfo)
-{
-	return 0;
-}
-
 bool GetSectionData(IBaseFilter *filter,PID pid,TID tid,WORD *sectionCount,int tableSection,int timeout)
 {
 	
@@ -177,30 +127,6 @@ bool GetSectionData(IBaseFilter *filter,PID pid,TID tid,WORD *sectionCount,int t
 	if (SUCCEEDED(hr))
 	{
 		
-		m_sectionList->GetNumberOfSections(sectionCount);
-		return true;
-	}
- 
-	return false;
-}
-bool GetSectionDataI(PID pid,TID tid,WORD *sectionCount,int tableSection,int timeout)
-{
-	
-	IMpeg2Data		*pMPEG=m_mpegSIInterface;
-	HRESULT			hr;
-
-
-	if(AddTSPid(pid)==false)
-		return false;
-	if(timeout<1 || timeout>8000) // max. timeout 5 sec
-		timeout=1;
-	// grab table (tableSection=0) or section (tableSection!=0)
-	if(tableSection==0)
-		hr = pMPEG->GetTable(pid, tid, NULL, (DWORD)timeout, &m_sectionList);
-	else
-		hr = pMPEG->GetSection(pid, tid, NULL, (DWORD)timeout, &m_sectionList);
-	if (SUCCEEDED(hr))
-	{
 		m_sectionList->GetNumberOfSections(sectionCount);
 		return true;
 	}
@@ -274,146 +200,7 @@ BOOL GetSectionCount(IBaseFilter *filter,PID pid,TID tid,int sectionNumber,WORD 
 
 	return true;
 }
-BOOL SetCallBack(LONGLONG *pointer)
-{
-	m_callBack=(DeliverSectionData)pointer;
-	return true;
-}
-
-
 // 
-BOOL TuneCard(IB2C2MPEG2TunerCtrl2 *pB2C2FilterTunerCtrl,IB2C2MPEG2AVCtrl2 *pB2C2FilterAvCtrl,IB2C2MPEG2DataCtrl2 *pB2C2FilterDataCtrl,long Frequency,long SymbolRate,long FEC,long POL,long LNBKhz,long Diseq,long AudioPID,long VideoPID,long LNBFreq)
-{
-	
-	HRESULT hr;
-
-	if(!pB2C2FilterTunerCtrl)
-		return false;
-	
-	// Defaults.
-
-	long lSysIpPIDSize = 34;
-	long lSysTsPIDSize = 39;
-	long lSleepLength = 10;
-	//
-        
-	hr = pB2C2FilterTunerCtrl->SetFrequencyKHz(Frequency);
-	if (FAILED (hr))
-	{
-		return false;	// *** FUNCTION EXIT POINT
-	}
-        
-
-	hr = pB2C2FilterTunerCtrl->SetSymbolRate(SymbolRate);
-	if (FAILED (hr))
-	{
-		return false;	// *** FUNCTION EXIT POINT
-	}
-        
-	hr = pB2C2FilterTunerCtrl->SetLnbFrequency(LNBFreq);
-	if (FAILED (hr))
-	{
-		return false;	// *** FUNCTION EXIT POINT
-	}
-        
-	hr = pB2C2FilterTunerCtrl->SetFec(6L);
-	if (FAILED (hr))
-	{
-		return false;	// *** FUNCTION EXIT POINT
-	}
-        
-	hr = pB2C2FilterTunerCtrl->SetPolarity(POL);
-	if (FAILED (hr))
-	{
-		return false;	// *** FUNCTION EXIT POINT
-	}
-        
-	hr = pB2C2FilterTunerCtrl->SetLnbKHz(LNBKhz);
-	if (FAILED (hr))
-	{
-		return false;	// *** FUNCTION EXIT POINT
-	}
-        
-	hr = pB2C2FilterTunerCtrl->SetDiseqc(Diseq);
-	if (FAILED (hr))
-	{
-		return false;	// *** FUNCTION EXIT POINT
-	}
-			
-	hr = pB2C2FilterTunerCtrl->SetTunerStatusEx(500L);
-	if (FAILED(hr))	
-	    return false;	// *** FUNCTION EXIT POINT
-	//else
-	//	CallTunedOk(); // callback that the tuner is locked
-
-	if(AudioPID!=-1 && VideoPID!=-1)
-	{
-		hr = pB2C2FilterAvCtrl->SetAudioVideoPIDs (AudioPID, VideoPID);
-		if (FAILED (hr))
-		{
-			return false;	// *** FUNCTION EXIT POINT
-		}
-	}
-	return true;
-} // main tuning...
-
-BOOL Tune(long Frequency,long SymbolRate,long FEC,long POL,long LNB,long Diseq,long LNBNumber)
-{
-	IB2C2MPEG2DataCtrl3 	*pB2C2FilterDataCtrl = NULL;
-	IB2C2MPEG2TunerCtrl2	*pB2C2FilterTunerCtrl= NULL;
-	IB2C2MPEG2AVCtrl2		*pB2C2FilterAvCtrl= NULL;
-	BOOL					flag=false;
-	// init adapter
-
-	if(DVBInit()==false)
-	{
-		return false;
-	}
-	pB2C2FilterDataCtrl = m_b2c2Adapter->GetDataControl();
-	pB2C2FilterTunerCtrl= m_b2c2Adapter->GetTunerControl();
-	pB2C2FilterAvCtrl= m_b2c2Adapter->GetAvControl();
-
-	// Diseq = 0-6
-	// POL = 0- h / 1 -v
-	if(pB2C2FilterDataCtrl!=NULL && pB2C2FilterTunerCtrl!=NULL && pB2C2FilterAvCtrl!=NULL)
-		flag=TuneCard(pB2C2FilterTunerCtrl,pB2C2FilterAvCtrl,pB2C2FilterDataCtrl,Frequency,SymbolRate,FEC,POL,LNB,Diseq,-1L,-1L,LNBNumber);
-
-	return true;
-}
-BOOL StopSITable(void)
-{
-	IB2C2MPEG2DataCtrl3 *pB2C2FilterDataCtrl = m_b2c2Adapter->GetDataControl();
-	IMediaControl		*mediaControl=NULL;
-    HRESULT hr;
-
-	if(m_b2c2Adapter->IsInitialized()==false)
-		return false;
-
-	hr=m_b2c2Adapter->GetMediaControl(&mediaControl);
-	mediaControl->Stop();
-	
-	return true;
-}
-BOOL AddTSPid(int pidnum)
-{
-	IB2C2MPEG2DataCtrl3 *pB2C2FilterDataCtrl = m_b2c2Adapter->GetDataControl();
-    HRESULT				hr;
-	long				pidCount=1;
-	long				pids[1];
-
-	pids[0]=pidnum;
-
-	if(m_b2c2Adapter->IsInitialized()==false)
-		return false;
-	if(pB2C2FilterDataCtrl)
-	{
-		hr=pB2C2FilterDataCtrl->AddPIDsToPin(&pidCount,pids,1);// we use always pin 1 for mpeg2data
-		if(FAILED(hr))
-			return false;
-	}
-	
-	return true;
-}
 
 BOOL DeleteAllPIDs(IB2C2MPEG2DataCtrl3 *pB2C2FilterDataCtrl,long pin)
 {
@@ -438,70 +225,12 @@ BOOL DeleteAllPIDs(IB2C2MPEG2DataCtrl3 *pB2C2FilterDataCtrl,long pin)
 	
 	return true;
 }
-BOOL DeleteAllPIDsI(void)
-{
-	IB2C2MPEG2DataCtrl3 *pB2C2FilterDataCtrl = m_b2c2Adapter->GetDataControl();
-    HRESULT				hr;
-	long				pidCount=39;
-	long				pids[39];
 
-
-	if(pB2C2FilterDataCtrl)
-	{
-		do
-		{
-			hr=pB2C2FilterDataCtrl->GetTsState(NULL,NULL,&pidCount,pids);
-			if(SUCCEEDED(hr))
-			{
-				hr=pB2C2FilterDataCtrl->DeletePIDsFromPin(pidCount,pids,1);
-			} else
-				return false;
-
-		}while(pidCount>0);
-	}
-	
-	return true;
-}
 //
 // get tab without tune request
 //
 
-BOOL IsTunerLocked(void)
-{
-	IB2C2MPEG2TunerCtrl2	*pB2C2FilterTunerCtrl= NULL;
-	HRESULT					hr;
-	// init adapter
 
-	if(DVBInit()==false)
-	{
-		return false;
-	}
-	pB2C2FilterTunerCtrl= m_b2c2Adapter->GetTunerControl();
-	if(pB2C2FilterTunerCtrl)
-	{
-		hr=pB2C2FilterTunerCtrl->CheckLock();
-		if(SUCCEEDED(hr))
-			return true;
-	}
-	return false;	
-}
-BOOL PauseSITable(void)
-{
-	IB2C2MPEG2DataCtrl3 *pB2C2FilterDataCtrl = m_b2c2Adapter->GetDataControl();
-	IMediaControl		*mediaControl=NULL;
-    HRESULT hr;
-
-	if(m_b2c2Adapter->IsInitialized()==false)
-		return false;
-
-	hr=m_b2c2Adapter->GetMediaControl(&mediaControl);
-	hr=mediaControl->Pause();
-	if(SUCCEEDED(hr))
-		return true;
-	//ResetDevice();
-	
-	return false;
-}
 long SetPidToPin(IB2C2MPEG2DataCtrl3 *pB2C2FilterDataCtrl,long pin,long pid)
 {
 	long	count=1;
@@ -519,84 +248,6 @@ long SetPidToPin(IB2C2MPEG2DataCtrl3 *pB2C2FilterDataCtrl,long pin,long pid)
 	return 0;
 }
 
-BOOL RunSITable(void)
-{
-	IB2C2MPEG2DataCtrl3 *pB2C2FilterDataCtrl = m_b2c2Adapter->GetDataControl();
-	IMediaControl		*mediaControl=NULL;
-    HRESULT hr;
-
-	if(m_b2c2Adapter->IsInitialized()==false)
-		return false;
-
-	hr=m_b2c2Adapter->GetMediaControl(&mediaControl);
-	if(FAILED(hr))
-		return false;
-	hr=mediaControl->Run();
-	if(SUCCEEDED(hr))
-		return true;
-	//ResetDevice();
-	
-	return false;
-}
-BOOL ClearAllUp(void)
-{
-	IMediaControl		*mediaControl=NULL;
-    HRESULT hr;
-
-	if(m_b2c2Adapter->IsInitialized()==false)
-		return false;
-
-	hr=m_b2c2Adapter->GetMediaControl(&mediaControl);
-	if(FAILED(hr))
-		return false;
-
-	hr=mediaControl->Stop();
-	if(FAILED(hr))
-		return false;
-
-	// should be released and NULL but: who knows ? :)
-	if(m_sectionList!=NULL)
-	{
-		m_sectionList->Release();
-		m_sectionList=NULL;
-	}
-
-
-	if(m_demux!=NULL)
-	{
-		m_demux->Release();
-		m_demux=NULL;
-	}
-	if(m_mpeg2Data!=NULL)
-	{
-		m_mpeg2Data->Release();
-		m_mpeg2Data=NULL;
-	}
-
-	ResetDevice();
-	if(m_mpegSIInterface!=NULL)
-		m_mpegSIInterface->Release();
-
-	delete (void*)m_callBack;
-
-	return true;
-}
-//
-
-
-//
-void ClearAllTsPids(void)
-{
-	m_pidArrayCount=0;
-	for(int i=0;i<39;i++)
-		m_pidArray[i]=0;
-}
-BOOL AddPidToTS(long pid)
-{
-	m_pidArray[m_pidArrayCount]=pid;
-	m_pidArrayCount++;
-	return m_pidArrayCount;
-}
 
 HRESULT SetupDemuxer(IPin *pVideo,IPin *pAudio,int audioPID,int videoPID)
 {
@@ -659,39 +310,7 @@ HRESULT SetupDemuxer(IPin *pVideo,IPin *pAudio,int audioPID,int videoPID)
 	return S_OK;
 }
 //
-HRESULT LoadGraphFile(IGraphBuilder *pGraph, const WCHAR* wszName)
-{
-	MessageBox(NULL,(LPCTSTR)wszName,NULL,0);
-    IStorage *pStorage = 0;
-    if (S_OK != StgIsStorageFile(wszName))
-    {
-        return E_FAIL;
-    }
-    HRESULT hr = StgOpenStorage(wszName, 0, 
-        STGM_TRANSACTED | STGM_READ | STGM_SHARE_DENY_WRITE, 
-        0, 0, &pStorage);
-    if (FAILED(hr))
-    {
-        return hr;
-    }
-    IPersistStream *pPersistStream = 0;
-    hr = pGraph->QueryInterface(IID_IPersistStream,
-             reinterpret_cast<void**>(&pPersistStream));
-    if (SUCCEEDED(hr))
-    {
-        IStream *pStream = 0;
-        hr = pStorage->OpenStream(L"ActiveMovieGraph", 0, 
-            STGM_READ | STGM_SHARE_EXCLUSIVE, 0, &pStream);
-        if(SUCCEEDED(hr))
-        {
-            hr = pPersistStream->Load(pStream);
-            pStream->Release();
-        }
-        pPersistStream->Release();
-    }
-    pStorage->Release();
-    return hr;
-}
+
 IPin *GetPin(IBaseFilter *pFilter, PIN_DIRECTION PinDir)
 {
     BOOL       bFound = FALSE;
@@ -714,148 +333,9 @@ IPin *GetPin(IBaseFilter *pFilter, PIN_DIRECTION PinDir)
     pEnum->Release();
     return (bFound ? pPin : NULL);  
 }
-HRESULT SaveGraphFile(IGraphBuilder *pGraph, WCHAR *wszPath) 
-{
-    const WCHAR wszStreamName[] = L"ActiveMovieGraph"; 
-    HRESULT hr;
-    
-    IStorage *pStorage = NULL;
-    hr = StgCreateDocfile(
-        wszPath,
-        STGM_CREATE | STGM_TRANSACTED | STGM_READWRITE | STGM_SHARE_EXCLUSIVE,
-        0, &pStorage);
-    if(FAILED(hr)) 
-    {
-        return hr;
-    }
 
-    IStream *pStream;
-    hr = pStorage->CreateStream(
-        wszStreamName,
-        STGM_WRITE | STGM_CREATE | STGM_SHARE_EXCLUSIVE,
-        0, 0, &pStream);
-    if (FAILED(hr)) 
-    {
-        pStorage->Release();    
-        return hr;
-    }
-
-    IPersistStream *pPersist = NULL;
-    pGraph->QueryInterface(IID_IPersistStream, (void**)&pPersist);
-    hr = pPersist->Save(pStream, TRUE);
-    pStream->Release();
-    pPersist->Release();
-    if (SUCCEEDED(hr)) 
-    {
-        hr = pStorage->Commit(STGC_DEFAULT);
-    }
-    pStorage->Release();
-    return hr;
-}
-bool SetAVPids(long vPid,long aPid)
-{
-	IB2C2MPEG2AVCtrl2		*pB2C2FilterAvCtrl= NULL;
-	HRESULT					hr;
-
-	if(DVBInit()==false)
-	{
-		return false;
-	}
-	// get interface
-	pB2C2FilterAvCtrl= m_b2c2Adapter->GetAvControl();	
-	if(pB2C2FilterAvCtrl==NULL)
-		return false; // serious error
-	// set pids
-	hr=pB2C2FilterAvCtrl->SetAudioVideoPIDs(aPid,vPid);
-	if(FAILED(hr))
-		return false;
-
-	return true;
-}
 
 //
 //
 //
-bool SetupB2C2Graph(void)
-{
-	IMpeg2Data			*pMPEG = NULL; // Pointer to the Sections and Tables filter.
-	IGraphBuilder		*pGraph=NULL;
-	IMpeg2Demultiplexer *pDemux=NULL;
-	IBaseFilter			*pFilterDemux=NULL;
-	IBaseFilter			*pFilterMPEG=NULL;
-	HRESULT				hr;
-
-	// do some init here (graph,basefilters,pins...)
-	if(DVBInit()==false)
-	{
-		return false;
-	}
-
-	pGraph=m_b2c2Adapter->GetFilterGraph();
-	if(pGraph==NULL)
-		return false;
-
-	hr=CoCreateInstance(CLSID_MPEG2Demultiplexer,NULL,CLSCTX_INPROC_SERVER,IID_IBaseFilter,(LPVOID*)&pFilterDemux);
-	if(FAILED(hr))
-		return false;
-
-	m_demux=pFilterDemux;
-	
-	hr=CoCreateInstance(CLSID_MPSECTAB,NULL,CLSCTX_INPROC_SERVER,IID_IBaseFilter,(LPVOID*)&pFilterMPEG);
-	if(FAILED(hr))
-		return false;
-
-	m_mpeg2Data=pFilterMPEG;
-	
-	hr=pGraph->AddFilter(pFilterDemux,L"filter_demux");
-	if(FAILED(hr))
-		return false;
-
-	hr=pGraph->AddFilter(pFilterMPEG,L"filter_sections");
-	if(FAILED(hr))
-		return false;
-
-	hr=pFilterDemux->QueryInterface(IID_IMpeg2Demultiplexer,(void**)&pDemux);
-	if(FAILED(hr))
-		return false;
-	hr=pFilterMPEG->QueryInterface(IID_IMpeg2Data,(void**)&pMPEG);
-	if(FAILED(hr))
-		return false;
-	// save for global access the pointer
-	m_mpegSIInterface=pMPEG;
-
-	IPin *pPin;
-	AM_MEDIA_TYPE mt;
-	ZeroMemory(&mt, sizeof(AM_MEDIA_TYPE));
-	mt.majortype = MEDIATYPE_MPEG2_SECTIONS;
-	mt.subtype = MEDIASUBTYPE_MPEG2DATA;
-	// setup a pin for ts
-	hr=pDemux->CreateOutputPin(&mt,L"TS-Out",&pPin);
-	if(FAILED(hr))
-		return false;
-	// define pins
-	IPin *pinMPEG2DATAIn=GetPin(pFilterMPEG,PINDIR_INPUT);
-	IPin *pinB2C2Out=NULL;
-	IPin *pinDemuxIn=GetPin(pFilterDemux,PINDIR_INPUT);
-	hr=m_b2c2Adapter->GetTsOutPin(1,&pinB2C2Out); // always we use pin 1 of filter
-	if(FAILED(hr))
-		return false;
-//	hr=pGraph->Connect(pinB2C2Out,pinDemuxIn);
-	hr=pGraph->Render(pinB2C2Out);
-	if(FAILED(hr))
-		return false;
-//	hr=pGraph->Connect(pPin,pinMPEG2DATAIn);
-	hr=pGraph->Render(pPin);
-	if(FAILED(hr))
-		return false;
-	
-	
-	pinMPEG2DATAIn->Release();
-	pinDemuxIn->Release();
-	pPin->Release();
-
-
-
-	return true;
-}
 
