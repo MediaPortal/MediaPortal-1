c8d614a8ea4f166d5bbbcea91093b731caf813c8
corl <corl@team-mediaportal.com>
2005-02-26 21:53:18 +0000

    0391: Back goes to Home instead of MyPlugins

--- mediaportal/WindowPlugins/GUIRadio/GUIRadio.cs
+++ mediaportal/WindowPlugins/GUIRadio/GUIRadio.cs
@@ -180,7 +180,7 @@ namespace MediaPortal.GUI.Radio
 
       if (action.wID == Action.ActionType.ACTION_PREVIOUS_MENU)
       {
-        GUIWindowManager.ActivateWindow( (int)GUIWindow.Window.WINDOW_HOME);
+        GUIWindowManager.PreviousWindow();
         return;
       }
       if (action.wID==Action.ActionType.ACTION_SHOW_PLAYLIST)
