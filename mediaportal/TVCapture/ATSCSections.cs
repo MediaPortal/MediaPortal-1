41560e04781a01f8fc9ed34f3dcf2b7dc038fefd
yamp <yamp@team-mediaportal.com>
2005-05-29 18:22:03 +0000

    no message

--- mediaportal/TVCapture/ATSCSections.cs
+++ mediaportal/TVCapture/ATSCSections.cs
@@ -61,10 +61,10 @@ namespace MediaPortal.TV.Recording
 
 			
 			//get Cable Virtual Channel Table (pid=0x1FFB, table id 0xc9)
-			//Log.Write("ATSC-scan: get Cable Virtual Channel Table");
-			//GetStreamData(filter,0x1ffb, 0xc9,0,Timeout);
-			//foreach(byte[] arr in m_sectionsList)
-			//	DecodeCableVirtualChannelTable(transponder,arr);
+			Log.Write("ATSC-scan: get Cable Virtual Channel Table");
+			GetStreamData(filter,0x1ffb, 0xc9,0,Timeout);
+			foreach(byte[] arr in m_sectionsList)
+				DecodeCableVirtualChannelTable(transponder,arr);
 			
 			return transponder;
 		}
@@ -294,7 +294,131 @@ namespace MediaPortal.TV.Recording
 
 		void DecodeCableVirtualChannelTable(DVBSections.Transponder transponder,byte[] buf)
 		{
-			Log.Write("DecodeCableVirtualChannelTable()");
+			Log.Write("ATSC-scan: DecodeCableVirtualChannelTable() len={0}",buf.Length);
+			/*if (!System.IO.File.Exists("vct.dat"))
+			{
+				System.IO.FileStream stream = new System.IO.FileStream("vct.dat",System.IO.FileMode.Create,System.IO.FileAccess.Write,System.IO.FileShare.None);
+				stream.Write(buf,0,buf.Length);
+				stream.Close();
+			}*/
+
+			if (buf.Length<10) return;
+			try
+			{
+
+				// tid   
+				//  8       112-12-- -------- 16------ -------- 2-5----1 8------- 8------- 8------- 8------- 
+				// 76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210
+				//    0        1        2         3        4       5       6         7        8       9     
+				int table_id = buf[0];
+				int section_syntax_indicator = (buf[1]>>7) & 1;
+				int private_indicator = (buf[1]>>6) & 1;
+				int section_length = ((buf[1]& 0xF)<<8) + buf[2];
+				int transport_stream_id = (buf[3]<<8)+buf[4];
+				int version_number = ((buf[5]>>1)&0x1F);
+				int current_next_indicator = buf[5] & 1;
+				int section_number = buf[6];
+				int last_section_number = buf[7];
+				int protocol_version = buf[8];
+				int num_channels_in_section = buf[9];
+
+				Log.Write("atsc: channels:{0}", num_channels_in_section);
+				int start=10;
+				for (int i=0; i < num_channels_in_section;i++)
+				{
+					string shortName="unknown";
+					Log.Write("  channel:{0}", i);
+					try
+					{
+						//shortname 7*16 bits (14 bytes) in UTF-16
+						byte[] bufRev = new byte[14];
+						for (int count=0; count < 7; count++)
+						{
+							bufRev[count*2] = buf[start+count*2+1];
+							bufRev[count*2+1] = buf[start+count*2];
+						}
+						UnicodeEncoding encoding = new UnicodeEncoding();
+						shortName=encoding.GetString(bufRev,0,7*2).Trim();
+					}
+					catch(Exception ex)
+					{
+						Log.Write("{0} {1} {2}", ex.Message,ex.Source,ex.StackTrace);
+					}
+
+					Log.Write("atsc: chan:{0} name:{1}", i,shortName);
+					start+= 7*2;
+					// 4---10-- ------10 -------- 8------- 32------ -------- -------- -------- 16------ -------- 16------ -------- 2-111113 --6----- 16------ -------- 6-----10 --------
+					// 76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210|76543210
+					//    112      113      114       115      116    117      118       119     120     121       123      124      125      126      127      128      129      130
+					//     0        1        2         3        4      5        6         7       8       9        10       11       12       13       14       15       16       17 
+					
+					int major_channel    		 =((buf[start  ]&0xf)<<8) + buf[start+1];
+					int minor_channel    		 =((buf[start+1]&0xf)<<8) + buf[start+2];
+					int modulation_mode  		 = buf[start+3];
+					int carrier_frequency		 = (buf[start+4]<<24) + (buf[start+5]<<16) + (buf[start+6]<<8) + (buf[start+7]);
+					int channel_TSID         = ((buf[start+8]&0xf)<<8) + buf[start+9];
+					int program_number			 = ((buf[start+10]&0xf)<<8) + buf[start+11];
+					int ETM_location				 = ((buf[start+12]>>6)&0x3);
+					int access_controlled		 = ((buf[start+12]>>4)&0x1);
+					int hidden          		 = ((buf[start+12]>>3)&0x1);
+					int path_select     		 = ((buf[start+12]>>2)&0x1);
+					int out_of_band     		 = ((buf[start+12]>>1)&0x1);
+					int hide_guide     		   = ((buf[start+12]   )&0x1);
+					int service_type         = ((buf[start+13]   )&0x3f);
+					int source_id						 = ((buf[start+14])<<8) + buf[start+15];
+					int descriptors_length	 = ((buf[start+16]&0x3)<<8) + buf[start+17];
+
+					
+					Log.Write("atsc: chan:{0} minor:{1} major:{2} modulation:{3} freq:{4} tsid:{5} program:{6}", i,
+						major_channel,minor_channel,modulation_mode, carrier_frequency,channel_TSID, program_number);
+					Log.Write("atsc: chan:{0} etm:{1} access:{2} hidden:{3} path:{4} oub:{5} hide:{6} service:{7} source:{8}",
+						i,ETM_location,access_controlled,hidden, path_select, out_of_band,hide_guide, service_type, source_id);
+					Log.Write("atsc: chan:{0} description length:{1}", i,descriptors_length);
+					DVBSections.ChannelInfo channelInfo = new DVBSections.ChannelInfo();
+					channelInfo.minorChannel = minor_channel;
+					channelInfo.majorChannel = major_channel;
+					channelInfo.modulation   = modulation_mode;
+					channelInfo.freq         = carrier_frequency;
+					channelInfo.program_number= program_number;
+					if (service_type==0 || service_type==1|| service_type==2)
+						channelInfo.serviceType   = 1;
+					else if (service_type==3)
+						channelInfo.serviceType   = 2;
+					else 
+						channelInfo.serviceType  =3;
+					channelInfo.transportStreamID = channel_TSID;
+					channelInfo.serviceID = major_channel*1000+minor_channel;
+					channelInfo.pid_list = new ArrayList();
+
+					Log.Write("decode descriptors...");
+					start += 18;
+					int len=0;
+					while (len < descriptors_length)
+					{
+						Log.Write("  len:{0} / {1}", len,descriptors_length);
+						int descriptor_tag = buf[start+len];
+						int descriptor_len = buf[start+len+1];
+						Log.Write("  descriptor:{0:X} len:{1} {2}", descriptor_tag, descriptor_len, start);
+						switch (descriptor_tag)
+						{
+							case 0xa1:
+								DecodeServiceLocationDescriptor( buf,start+len,ref channelInfo);
+								break;
+							case 0xa0:
+								DecodeExtendedChannelNameDescriptor( buf,start+len,ref channelInfo);
+								break;
+						}
+						len += (descriptor_len+2);
+					}
+					start += descriptors_length;
+					transponder.channels.Add(channelInfo);
+				}
+				//todo decode additional descriptors
+			}
+			catch(Exception ex)
+			{
+				Log.Write("{0} {1} {2}", ex.Message,ex.Source,ex.StackTrace);
+			}
 		}
 
 		void DecodeMasterGuideTable(byte[] buf)
