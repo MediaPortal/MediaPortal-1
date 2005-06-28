c2990ea575ddb386aa6c5808698c5551bbb18a6f
leenetpoint <leenetpoint@team-mediaportal.com>
2005-06-28 22:27:12 +0000

    Initial checkin for this file, used as a place to consolidate calls to the various remote controls that are being integrated.

---
+++ mediaportal/RemotePlugins/InputDevices.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Windows.Forms;
+
+using MediaPortal.GUI.Library;
+
+namespace MediaPortal
+{
+	public sealed class InputDevices
+	{
+		#region Constructors
+
+		private InputDevices()
+		{
+		}
+
+		#endregion Constructors
+
+		#region Methods
+
+		public static void Init(/* SplashScreen splashScreen */)
+		{
+			MCE2005Remote.Init(GUIGraphicsContext.ActiveForm);
+			FireDTVRemote.Init(GUIGraphicsContext.ActiveForm);
+
+			if (HCWRemote.Enabled)
+			{
+//				if (splashScreen != null) splashScreen.SetInformation("Initializing Hauppauge remote...");
+				HCWRemote.Init();
+			}
+		}
+
+		public static void Stop()
+		{
+			MCE2005Remote.DeInit();
+			HCWRemote.DeInit();
+			FireDTVRemote.DeInit();
+		}
+
+		public static bool WndProc(ref Message msg, out Action action, out char key, out Keys keyCode)
+		{
+			action = null;
+			key = (char)0;
+			keyCode = Keys.A;
+
+			HCWRemote.WndProc(msg);
+
+			if(HIDListener.WndProc(ref msg, out action, out key, out keyCode))
+				return true;
+
+			if(MCE2005Remote.WndProc(ref msg, out action, out key, out keyCode))
+				return true;
+
+		    if(FireDTVRemote.WndProc(ref msg, out action, out  key, out keyCode))
+				return true;
+
+			return false;
+		}
+
+		#endregion Methods
+
+		#region Fields
+
+		static MCE2005Remote MCE2005Remote = new MCE2005Remote();
+		static HCWRemote HCWRemote = new HCWRemote();
+		static MediaPortal.RemoteControls.FireDTVRemote FireDTVRemote = new MediaPortal.RemoteControls.FireDTVRemote();
+
+		#endregion Fields
+	}
+}
