f2f58c13ee3f4752b4e1e4035a158e526e42a11a
yamp <yamp@team-mediaportal.com>
2005-07-17 06:25:51 +0000

    no message

--- mediaportal/WindowPlugins/GUISettings/Wizard/DVBT/GUIWizardDVBTCountry.cs
+++ mediaportal/WindowPlugins/GUISettings/Wizard/DVBT/GUIWizardDVBTCountry.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Xml;
 using MediaPortal.GUI.Library;
 namespace WindowPlugins.GUISettings.Wizard.DVBT
 {
@@ -7,6 +8,7 @@ namespace WindowPlugins.GUISettings.Wizard.DVBT
 	/// </summary>
 	public class GUIWizardDVBTCountry : GUIWindow
 	{
+		[SkinControlAttribute(24)]			protected GUIListControl listCountries=null;
 		public GUIWizardDVBTCountry()
 		{
 			GetID=(int)GUIWindow.Window.WINDOW_WIZARD_DVBT_COUNTRY;
@@ -15,6 +17,27 @@ namespace WindowPlugins.GUISettings.Wizard.DVBT
 		public override bool Init()
 		{
 			return Load (GUIGraphicsContext.Skin+@"\wizard_tvcard_dvbt_country.xml");
+		}
+		protected override void OnPageLoad()
+		{
+			base.OnPageLoad ();
+			LoadCountries();
+		}
+
+		void LoadCountries()
+		{
+			listCountries.Clear();
+			XmlDocument doc= new XmlDocument();
+			doc.Load("Tuningparameters/dvbt.xml");
+			XmlNodeList countryList=doc.DocumentElement.SelectNodes("/dvbt/country");
+			foreach (XmlNode nodeCountry in countryList)
+			{
+				string name= nodeCountry.Attributes.GetNamedItem(@"name").InnerText;
+				GUIListItem item = new GUIListItem();
+				item.IsFolder=false;
+				item.Label=name;
+				listCountries.Add(item);
+			}
 		}
 	}
 }
