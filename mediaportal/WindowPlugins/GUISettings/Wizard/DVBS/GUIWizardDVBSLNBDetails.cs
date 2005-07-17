83f0cdc3b345e4e30839a2f1d7621661abb5f5bf
yamp <yamp@team-mediaportal.com>
2005-07-17 10:33:08 +0000

    no message

--- mediaportal/WindowPlugins/GUISettings/Wizard/DVBS/GUIWizardDVBSLNBDetails.cs
+++ mediaportal/WindowPlugins/GUISettings/Wizard/DVBS/GUIWizardDVBSLNBDetails.cs
@@ -8,6 +8,26 @@ namespace WindowPlugins.GUISettings.Wizard.DVBS
 	/// </summary>
 	public class GUIWizardDVBSLNBDetails : GUIWindow
 	{
+		[SkinControlAttribute(5)]			protected GUIButtonControl btnNext=null;
+		[SkinControlAttribute(100)]		protected GUILabelControl lblLNB=null;
+		[SkinControlAttribute(40)]		protected GUICheckMarkControl cmDisEqcNone=null;
+		[SkinControlAttribute(41)]		protected GUICheckMarkControl cmDisEqcSimpleA=null;
+		[SkinControlAttribute(42)]		protected GUICheckMarkControl cmDisEqcSimpleB=null;
+		[SkinControlAttribute(43)]		protected GUICheckMarkControl cmDisEqcLevel1AA=null;
+		[SkinControlAttribute(44)]		protected GUICheckMarkControl cmDisEqcLevel1BA=null;
+		[SkinControlAttribute(45)]		protected GUICheckMarkControl cmDisEqcLevel1AB=null;
+		[SkinControlAttribute(46)]		protected GUICheckMarkControl cmDisEqcLevel1BB=null;
+
+
+		[SkinControlAttribute(50)]		protected GUICheckMarkControl cmLnb0Khz=null;
+		[SkinControlAttribute(51)]		protected GUICheckMarkControl cmLnb22Khz=null;
+		[SkinControlAttribute(52)]		protected GUICheckMarkControl cmLnb33Khz=null;
+		[SkinControlAttribute(53)]		protected GUICheckMarkControl cmLnb44Khz=null;
+		
+		[SkinControlAttribute(60)]		protected GUICheckMarkControl cmLnbBandKU=null;
+		[SkinControlAttribute(61)]		protected GUICheckMarkControl cmLnbBandC=null;
+		[SkinControlAttribute(62)]		protected GUICheckMarkControl cmLnbBandCircular=null;
+
 		public GUIWizardDVBSLNBDetails()
 		{
 			GetID=(int)GUIWindow.Window.WINDOW_WIZARD_DVBS_SELECT_DETAILS;
