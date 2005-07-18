697b650882841d1e92f0dc0df87ac9ffd3c162a7
yamp <yamp@team-mediaportal.com>
2005-07-18 10:28:20 +0000

    no message

---
+++ mediaportal/WindowPlugins/GUISettings/Wizard/Analog/GUIWizardAnalogTune.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace WindowPlugins.GUISettings.Wizard.Analog
+{
+	/// <summary>
+	/// Summary description for GUIWizardAnalogTune.
+	/// </summary>
+	public class GUIWizardAnalogTune
+	{
+		public GUIWizardAnalogTune()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
