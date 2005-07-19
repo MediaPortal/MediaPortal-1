11e1f4082c8c0636cabcf8b07e26783007baf958
yamp <yamp@team-mediaportal.com>
2005-07-19 13:46:45 +0000

    no message

---
+++ mediaportal/WindowPlugins/GUISettings/TV/GUISettingsTv.cs
@@ -0,0 +1,191 @@
+using System;
+using System.Collections;
+using MediaPortal.GUI.Library;
+using MediaPortal.Dialogs;
+using DShowNET;
+namespace WindowPlugins.GUISettings.TV
+{
+	/// <summary>
+	/// Summary description for GUISettingsTv.
+	/// </summary>
+	public class GUISettingsTv : GUIWindow
+	{
+		[SkinControlAttribute(24)]			protected GUIButtonControl btnVideoCodec=null;
+		[SkinControlAttribute(25)]			protected GUIButtonControl btnAudioCodec=null;
+		[SkinControlAttribute(26)]			protected GUIButtonControl btnVideoRenderer=null;
+		[SkinControlAttribute(28)]			protected GUIButtonControl btnAspectRatio=null;
+		[SkinControlAttribute(29)]			protected GUIButtonControl btnTimeshiftBuffer=null;
+		public GUISettingsTv()
+		{
+			GetID=(int)GUIWindow.Window.WINDOW_SETTINGS_TV;
+		}
+    
+		public override bool Init()
+		{
+			return Load (GUIGraphicsContext.Skin+@"\settings_tv.xml");
+		}
+		protected override void OnClicked(int controlId, GUIControl control, MediaPortal.GUI.Library.Action.ActionType actionType)
+		{
+			if (control==btnVideoCodec) OnVideoCodec();
+			if (control==btnAudioCodec) OnAudioCodec();
+			if (control==btnVideoRenderer) OnVideoRenderer();
+			if (control==btnAspectRatio) OnAspectRatio();
+			if (control==btnTimeshiftBuffer) OnTimeshiftBuffer();
+			base.OnClicked (controlId, control, actionType);
+		}
+		void OnVideoCodec()
+		{
+			string strVideoCodec="";
+			using (MediaPortal.Profile.Xml   xmlreader=new MediaPortal.Profile.Xml("MediaPortal.xml"))
+			{
+				strVideoCodec=xmlreader.GetValueAsString("mytv","videocodec","");
+			}
+			ArrayList availableVideoFilters = FilterHelper.GetFilters(MediaType.Video, MediaSubType.MPEG2);
+
+			GUIDialogMenu dlg=(GUIDialogMenu)GUIWindowManager.GetWindow((int)GUIWindow.Window.WINDOW_DIALOG_MENU);
+			if (dlg!=null)
+			{
+				dlg.Reset();
+				dlg.SetHeading(GUILocalizeStrings.Get(924));//Menu
+				int selected=0;
+				int count=0;
+				foreach (string codec in availableVideoFilters)
+				{
+					dlg.Add(codec);//delete
+					if (codec==strVideoCodec)
+						selected=count;
+					count++;
+				}
+				dlg.SelectedLabel=selected;
+			}
+			dlg.DoModal(GetID);
+			if (dlg.SelectedLabel<0) return;
+			using (MediaPortal.Profile.Xml   xmlwriter=new MediaPortal.Profile.Xml("MediaPortal.xml"))
+			{
+				xmlwriter.SetValue("mytv","videocodec",(string)availableVideoFilters[dlg.SelectedLabel]);
+			}
+		}
+
+		void OnAudioCodec()
+		{
+			string strAudioCodec="";
+			using (MediaPortal.Profile.Xml   xmlreader=new MediaPortal.Profile.Xml("MediaPortal.xml"))
+			{
+				strAudioCodec=xmlreader.GetValueAsString("mytv","audiocodec","");
+			}
+			ArrayList availableAudioFilters = FilterHelper.GetFilters(MediaType.Audio, MediaSubType.MPEG2_Audio);
+
+			GUIDialogMenu dlg=(GUIDialogMenu)GUIWindowManager.GetWindow((int)GUIWindow.Window.WINDOW_DIALOG_MENU);
+			if (dlg!=null)
+			{
+				dlg.Reset();
+				dlg.SetHeading(GUILocalizeStrings.Get(924));//Menu
+				int selected=0;
+				int count=0;
+				foreach (string codec in availableAudioFilters)
+				{
+					dlg.Add(codec);//delete
+					if (codec==strAudioCodec)
+						selected=count;
+					count++;
+				}
+				dlg.SelectedLabel=selected;
+			}
+			dlg.DoModal(GetID);
+			if (dlg.SelectedLabel<0) return;
+			using (MediaPortal.Profile.Xml   xmlwriter=new MediaPortal.Profile.Xml("MediaPortal.xml"))
+			{
+				xmlwriter.SetValue("mytv","audiocodec",(string)availableAudioFilters[dlg.SelectedLabel]);
+			}
+		}
+		void OnVideoRenderer()
+		{
+			int vmr9Index=0;
+			using (MediaPortal.Profile.Xml   xmlreader=new MediaPortal.Profile.Xml("MediaPortal.xml"))
+			{
+				vmr9Index= xmlreader.GetValueAsInt("mytv", "vmr9", 0);
+			}
+
+			GUIDialogMenu dlg=(GUIDialogMenu)GUIWindowManager.GetWindow((int)GUIWindow.Window.WINDOW_DIALOG_MENU);
+			if (dlg!=null)
+			{
+				dlg.Reset();
+				dlg.SetHeading(GUILocalizeStrings.Get(924));//Menu
+				dlg.Add("Video Mixing Renderer 7");
+				dlg.Add("Video Mixing Renderer 9");
+				dlg.SelectedLabel=vmr9Index;
+				dlg.DoModal(GetID);
+				if (dlg.SelectedLabel<0) return;
+				using (MediaPortal.Profile.Xml xmlwriter = new MediaPortal.Profile.Xml("MediaPortal.xml"))
+				{
+						xmlwriter.SetValue("mytv", "vmr9", vmr9Index.ToString());
+				}
+			}
+		}
+		void OnAspectRatio()
+		{
+			string[] aspectRatio = { "normal", "original", "stretch", "zoom", "letterbox", "panscan" };
+			string defaultAspectRatio ="";
+			using (MediaPortal.Profile.Xml   xmlreader=new MediaPortal.Profile.Xml("MediaPortal.xml"))
+			{
+				defaultAspectRatio = xmlreader.GetValueAsString("mytv","defaultar", "normal");
+			}
+
+			GUIDialogMenu dlg=(GUIDialogMenu)GUIWindowManager.GetWindow((int)GUIWindow.Window.WINDOW_DIALOG_MENU);
+			if (dlg!=null)
+			{
+				dlg.Reset();
+				dlg.SetHeading(GUILocalizeStrings.Get(924));//Menu
+				int selected=0;
+				for(int index = 0; index < aspectRatio.Length; index++)
+				{
+					if(aspectRatio[index].Equals(defaultAspectRatio))
+					{
+						selected=index;
+					}
+					dlg.Add(aspectRatio[index]);
+				}
+				dlg.SelectedLabel=selected;
+				dlg.DoModal(GetID);
+				if (dlg.SelectedLabel<0) return;
+				using (MediaPortal.Profile.Xml xmlwriter = new MediaPortal.Profile.Xml("MediaPortal.xml"))
+				{
+					xmlwriter.SetValue("mytv", "defaultar", aspectRatio[dlg.SelectedLabel]);
+				}
+			}
+		}
+		void OnTimeshiftBuffer()
+		{
+			int buflen=30;
+			using (MediaPortal.Profile.Xml   xmlreader=new MediaPortal.Profile.Xml("MediaPortal.xml"))
+			{
+				buflen=xmlreader.GetValueAsInt("capture", "timeshiftbuffer", 30);
+			}
+			GUIDialogMenu dlg=(GUIDialogMenu)GUIWindowManager.GetWindow((int)GUIWindow.Window.WINDOW_DIALOG_MENU);
+			if (dlg!=null)
+			{
+				dlg.Reset();
+				dlg.SetHeading(GUILocalizeStrings.Get(924));//Menu
+				int selected=0;
+				int count=0;
+				for (int i=30; i <=90; i+=30)
+				{
+					dlg.Add( String.Format("{0} mins", i.ToString() ) ) ;
+					if (i==buflen)
+					{
+						selected=count;
+					}
+					count++;
+				}
+				dlg.SelectedLabel=selected;
+				dlg.DoModal(GetID);
+				if (dlg.SelectedLabel<0) return;
+				using (MediaPortal.Profile.Xml xmlwriter = new MediaPortal.Profile.Xml("MediaPortal.xml"))
+				{
+					buflen=(dlg.SelectedLabel*30);
+					xmlwriter.SetValue("mytv", "timeshiftbuffer", buflen.ToString());
+				}
+			}
+		}
+	}
+}
