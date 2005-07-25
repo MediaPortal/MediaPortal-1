030ac108d330f28e7dd0043eb89b18438002f9de
yamp <yamp@team-mediaportal.com>
2005-07-25 06:46:54 +0000

    no message

--- mediaportal/WindowPlugins/GUISettings/Wizard/Epg/GUIWizardEpgSelect.cs
+++ mediaportal/WindowPlugins/GUISettings/Wizard/Epg/GUIWizardEpgSelect.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections;
 using System.IO;
 using System.Xml;
 using MediaPortal.Util;
+using MediaPortal.Dialogs;
 using MediaPortal.GUI.Library;
 using MediaPortal.GUI.Settings.Wizard;
+using MediaPortal.TV.Database;
+
 namespace WindowPlugins.GUISettings.Epg
 {
 	/// <summary>
@@ -12,6 +16,10 @@ namespace WindowPlugins.GUISettings.Epg
 	public class GUIWizardEpgSelect : GUIWindow
 	{
 		[SkinControlAttribute(24)]			protected GUIListControl listGrabbers=null;
+		[SkinControlAttribute(2)]				protected GUILabelControl lblLine1=null;
+		[SkinControlAttribute(3)]				protected GUILabelControl lblLine2=null;
+		[SkinControlAttribute(5)]			  protected GUIButtonControl btnNext=null;
+		[SkinControlAttribute(25)]			protected GUIButtonControl btnBack=null;
 		bool epgGrabberSelected=false;
 		public GUIWizardEpgSelect()
 		{
@@ -31,6 +39,8 @@ namespace WindowPlugins.GUISettings.Epg
 		}
 		void LoadGrabbers()
 		{
+			lblLine1.Label=GUILocalizeStrings.Get(200000);
+			lblLine2.Label=GUILocalizeStrings.Get(200001);
 			epgGrabberSelected=false;
 			listGrabbers.Clear();
 			string[] folders = System.IO.Directory.GetDirectories(@"webepg\grabbers");			
@@ -56,18 +66,35 @@ namespace WindowPlugins.GUISettings.Epg
 		{
 			if (control==listGrabbers)
 			{
-				OnGrabberSelected(listGrabbers.SelectedListItem);
+				if (!epgGrabberSelected)
+					OnGrabberSelected(listGrabbers.SelectedListItem);
+				else 
+					OnMap();
+			}
+			if (control==btnNext)
+			{
+				OnNext();
+			}
+			if (control==btnBack)
+			{
+				OnBack();
 			}
 			base.OnClicked (controlId, control, actionType);
 		}
 
-		void OnGrabberSelected(GUIListItem item)
+		void OnBack()
 		{
-			if (epgGrabberSelected) 
+			if (epgGrabberSelected)
 			{
-				OnMap();
+				epgGrabberSelected=false;
+				LoadGrabbers();
 				return;
 			}
+			GUIWindowManager.ShowPreviousWindow();
+		}
+
+		void OnGrabberSelected(GUIListItem item)
+		{
 			if (item==null) return;
 			GUIPropertyManager.SetProperty("#WizardGrabber",item.Path);
 
@@ -97,18 +124,43 @@ namespace WindowPlugins.GUISettings.Epg
 					if (nodeName==null) continue;
 					if (nodeName.Value==null) continue;
 					GUIListItem ch = new GUIListItem();
-					ch.Label=String.Format("{0}. ",listGrabbers.Count+1, nodeName.Value);
+					ch.Label=String.Format("{0}. {1}",listGrabbers.Count+1, nodeName.Value);
 					listGrabbers.Add(ch);
 				}
 			}
 
-
+			lblLine1.Label=GUILocalizeStrings.Get(200002);
+			lblLine2.Label=GUILocalizeStrings.Get(200003);
 			epgGrabberSelected=true;
 		}
-		void OnMap()
+		void OnNext()
 		{
 			GUIPropertyManager.SetProperty("#Wizard.EPG.Done","yes");
 		  GUIWizardCardsDetected.ScanNextCardType();
+		}
+		void OnMap()
+		{
+			GUIListItem item=listGrabbers.SelectedListItem;
+			if (item==null) return;
+
+			ArrayList channels = new ArrayList();
+			TVDatabase.GetChannels(ref channels);
+			GUIDialogMenu dlg=(GUIDialogMenu)GUIWindowManager.GetWindow((int)GUIWindow.Window.WINDOW_DIALOG_MENU);
+			dlg.Reset();
+			dlg.SetHeading(GUILocalizeStrings.Get(924));//Menu
+			int selected=0;
+			int count=0;
+			foreach (TVChannel chan in channels)
+			{
+				dlg.Add(chan.Name);
+				if (chan.Name==item.Label2) selected=count;
+				count++;
+			}
+			dlg.SelectedLabel=selected;
+			dlg.DoModal(GetID);
+			if (dlg.SelectedLabel<0 || dlg.SelectedLabel>=channels.Count) return;
+			TVChannel selChannel =(TVChannel)channels[dlg.SelectedLabel];
+			item.Label2=selChannel.Name;
 		}
 	}
 }
