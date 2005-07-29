32f3cab6df5af3973459732c687d8afe783160d5
agree <agree@team-mediaportal.com>
2005-07-29 08:13:58 +0000

    *** empty log message ***

--- mediaportal/Filters/MPSA/source/resource.h
+++ mediaportal/Filters/MPSA/source/resource.h
@@ -24,6 +24,7 @@
 #define IDC_EDIT6                       1008
 #define IDC_EDIT7                       1009
 #define IDC_EDIT8                       1010
+#define IDC_CHECK1                      1011
 #define VERSION_RES_LANGUAGE            0x409
 #define VERSION_RES_CHARSET             1252
 
@@ -33,7 +34,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1012
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
