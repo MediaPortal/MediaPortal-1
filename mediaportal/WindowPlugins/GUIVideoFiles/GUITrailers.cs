4179f3de7deda54e164b0ca7afd3dd477a955b53
leenetpoint <leenetpoint@team-mediaportal.com>
2005-10-10 18:41:18 +0000

    Added disclaimer

--- mediaportal/WindowPlugins/GUIVideoFiles/GUITrailers.cs
+++ mediaportal/WindowPlugins/GUIVideoFiles/GUITrailers.cs
@@ -1,3 +1,24 @@
+/* 
+ *	Copyright (C) 2005 Media Portal
+ *	http://mediaportal.sourceforge.net
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *   
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
 using System;
 using System.Drawing;
 using System.IO;
@@ -849,15 +870,20 @@ namespace MediaPortal.GUI.Video
 				Match p = Regex.Match(TempHTML,@"<img\ssrc=(?<posterurl>http://shopping.yahoo.com/video/images/muze/dvd.*.jpg)");
 				PosterUrl = p.Groups["posterurl"].Value;
 			}
-			// Download Poster
-			WebClient wc = new WebClient();
-			name = name.Replace(":","-");
-			wc.DownloadFile(PosterUrl, @"thumbs\MPTemp -"+name + ".jpg");
-			while(System.IO.File.Exists(@"thumbs\MPTemp -"+name + ".jpg")!=true)
+
+			using(WaitCursor cursor = new WaitCursor())
 			{
+				// Download Poster
+				WebClient wc = new WebClient();
+				name = name.Replace(":","-");
+				wc.DownloadFile(PosterUrl, @"thumbs\MPTemp -"+name + ".jpg");
+
+				while(System.IO.File.Exists(@"thumbs\MPTemp -"+name + ".jpg")!=true)
+					GUIWindowManager.Process();
+
+				// Display Poster
+				poster.SetFileName(@"thumbs\MPTemp -"+name + ".jpg");
 			}
-			// Display Poster
-			poster.SetFileName(@"thumbs\MPTemp -"+name + ".jpg");
 
 			// Get MoviePlot
 			int EP = TempHTML.IndexOf("<br clear=\"all\" />");
