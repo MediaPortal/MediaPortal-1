d275bfd521f10339fb96c4595608be17f99256fa
leenetpoint <leenetpoint@team-mediaportal.com>
2005-10-21 10:04:10 +0000

    *** empty log message ***

---
+++ mediaportal/Core/Controls/StackPanel.cs
@@ -0,0 +1,61 @@
+#region Copyright (C) 2005 Media Portal
+
+/* 
+ *	Copyright (C) 2005 Media Portal
+ *	http://mediaportal.sourceforge.net
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *   
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
+#endregion
+
+using System;
+
+using MediaPortal.Drawing;
+using MediaPortal.Drawing.Layouts;
+using MediaPortal.GUI.Library;
+
+namespace MediaPortal.Controls
+{
+	public class StackPanel
+	{
+		public StackPanel()
+		{
+			_layout = new StackLayout();
+
+			_group = new GUIGroup(0);
+			_group.Layout = _layout;
+		}
+
+		#region Properties
+
+		public Orientation Orientation
+		{
+			get { return _layout.Orientation; }
+			set { _layout.Orientation = value; }
+		}
+
+		#endregion Properties
+
+		#region Fields
+
+		GUIGroup					_group;
+		StackLayout					_layout;
+
+		#endregion Fields
+	}
+}
