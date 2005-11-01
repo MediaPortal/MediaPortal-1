c517d131158148f9b287ec4c6c45f9e1693d77b3
yamp <yamp@team-mediaportal.com>
2005-11-01 18:15:51 +0000

    updated CI interface for twinhan to new driver

--- mediaportal/Core/DShowNET/Twinhan.cs
+++ mediaportal/Core/DShowNET/Twinhan.cs
@@ -29,10 +29,94 @@ namespace DShowNET
 	/// </summary>
 	public class Twinhan : IksPropertyUtils
 	{
-		readonly Guid THBDA_TUNER = new Guid( "E5644CC4-17A1-4eed-BD90-74FDA1D65423");
-		readonly Guid GUID_THBDA_CMD = new Guid( "255E0082-2017-4b03-90F8-856A62CB3D67" );
-		readonly uint THBDA_IOCTL_CI_PARSER_PMT=0xaa00033c;
-		readonly uint THBDA_IOCTL_CI_INFO=0xaa0001e4;
+		#region twinhan sample app code:
+		//#define CTL_CODE( DeviceType, Function, Method, Access ) (                 \
+		//		((DeviceType) << 16) | ((Access) << 14) | ((Function) << 2) | (Method) \
+		//		)
+		//#define THBDA_IO_INDEX													0xAA00
+		//#define THBDA_IOCTL_CI_SEND_PMT                 CTL_CODE(THBDA_IO_INDEX, 206, METHOD_BUFFERED, FILE_ANY_ACCESS)
+		//#define THBDA_IOCTL_CHECK_INTERFACE             CTL_CODE(THBDA_IO_INDEX, 121, METHOD_BUFFERED, FILE_ANY_ACCESS)
+
+		//typedef struct 
+		//				{
+		//					GUID    CmdGUID;            // Private Command GUID
+		//					DWORD   dwIoControlCode;    // operation
+		//					LPVOID  lpInBuffer;         // input data buffer
+		//					DWORD   nInBufferSize;      // size of input data buffer
+		//					LPVOID  lpOutBuffer;        // output data buffer
+		//					DWORD   nOutBufferSize;     // size of output data buffer
+		//					LPDWORD lpBytesReturned;    // byte count
+		//				} THBDACMD, *PTHBDACMD;
+
+		//		BOOL CBDAFilterGraph::BDAIOControl( DWORD  dwIoControlCode,
+		//			LPVOID lpInBuffer,
+		//			DWORD  nInBufferSize,
+		//			LPVOID lpOutBuffer,
+		//			DWORD  nOutBufferSize,
+		//			LPDWORD lpBytesReturned)
+		//		{
+		//			if (!m_KsTunerPropSet)
+		//				return FALSE;
+		//
+		//			KSPROPERTY instance_data;
+		//
+		//			ULONG    ulOutBuf = 0;
+		//			ULONG    ulReturnBuf = 0;
+		//			THBDACMD THBDACmd;
+		//
+		//			THBDACmd.CmdGUID = GUID_THBDA_CMD;
+		//			THBDACmd.dwIoControlCode = dwIoControlCode;
+		//			THBDACmd.lpInBuffer = lpInBuffer;
+		//			THBDACmd.nInBufferSize = nInBufferSize;
+		//			THBDACmd.lpOutBuffer = lpOutBuffer;
+		//			THBDACmd.nOutBufferSize = nOutBufferSize;
+		//			THBDACmd.lpBytesReturned = lpBytesReturned;
+		//
+		//			HRESULT hr = m_KsTunerPropSet->Set(GUID_THBDA_TUNER, 
+		//				NULL, 
+		//				&instance_data, sizeof(instance_data),
+		//				&THBDACmd, sizeof(THBDACmd));
+		//
+		//			if (FAILED(hr))
+		//				return FALSE;
+		//			else
+		//				return TRUE;
+		//		}
+		//
+		//		BOOL CBDAFilterGraph::CheckBDAInterface()
+		//		{
+		//			BOOL bStatus = FALSE;
+		//			DWORD dwBytesReturned = 0;
+		//
+		//			bStatus = BDAIOControl( THBDA_IOCTL_CHECK_INTERFACE,
+		//				NULL,
+		//				0,
+		//				NULL,
+		//				0,
+		//				(LPDWORD)&dwBytesReturned);
+		//
+		//			return bStatus;
+		//		}
+
+		//		BOOL CBDAFilterGraph::SendCAPMT(PBYTE pBuff, BYTE byBuffSize)
+		//		{
+		//				BOOL bStatus = FALSE;
+		//				DWORD dwBytesReturned = 0;
+		//
+		//				bStatus = BDAIOControl( THBDA_IOCTL_CI_SEND_PMT,
+		//									(LPVOID)pBuff,
+		//									byBuffSize,
+		//									NULL,
+		//									0,
+		//									(LPDWORD)&dwBytesReturned);
+		//		    
+		//				return bStatus;
+		//		}
+		#endregion
+		readonly Guid THBDA_TUNER			= new Guid( "E5644CC4-17A1-4eed-BD90-74FDA1D65423");
+		readonly Guid GUID_THBDA_CMD  = new Guid( "255E0082-2017-4b03-90F8-856A62CB3D67" );
+		readonly uint THBDA_IOCTL_CI_SEND_PMT		 =0xaa000338;
+		readonly uint THBDA_IOCTL_CHECK_INTERFACE=0xaa0001e4;
 
 
 		public Twinhan(IBaseFilter filter)
@@ -48,15 +132,17 @@ namespace DShowNET
 			}
 		}
 
+		
+
 		public bool GetCIInfo()
 		{
-			//Log.Write("Twinhan get CI Info");
+			Log.Write("Twinhan: check for twinhan driver");
 			bool success=false;
 			IntPtr ptrDwBytesReturned = Marshal.AllocCoTaskMem(4);
 
 			int thbdaLen=0x28;
 			IntPtr thbdaBuf = Marshal.AllocCoTaskMem(thbdaLen);
-			Marshal.WriteInt32(thbdaBuf,0,0x255e0082);
+			Marshal.WriteInt32(thbdaBuf,0,0x255e0082);//GUID_THBDA_CMD  = new Guid( "255E0082-2017-4b03-90F8-856A62CB3D67" );
 			Marshal.WriteInt16(thbdaBuf,4,0x2017);
 			Marshal.WriteInt16(thbdaBuf,6,0x4b03);
 			Marshal.WriteByte(thbdaBuf,8,0x90);
@@ -67,7 +153,7 @@ namespace DShowNET
 			Marshal.WriteByte(thbdaBuf,13,0xcb);
 			Marshal.WriteByte(thbdaBuf,14,0x3d);
 			Marshal.WriteByte(thbdaBuf,15,0x67);
-			Marshal.WriteInt32(thbdaBuf,16,(int)THBDA_IOCTL_CI_INFO);
+			Marshal.WriteInt32(thbdaBuf,16,(int)THBDA_IOCTL_CHECK_INTERFACE);//control code
 			Marshal.WriteInt32(thbdaBuf,20,(int)IntPtr.Zero);
 			Marshal.WriteInt32(thbdaBuf,24,0);
 			Marshal.WriteInt32(thbdaBuf,28,(int)IntPtr.Zero);
@@ -81,9 +167,11 @@ namespace DShowNET
 				if (propertySet!=null) 
 				{
 					Guid propertyGuid=THBDA_TUNER;
-					int hr=propertySet.RemoteSet(ref propertyGuid,0,IntPtr.Zero,0, thbdaBuf,(uint)thbdaLen );
+
+					int hr=propertySet.RemoteSet(ref propertyGuid,0,thbdaBuf,(uint)thbdaLen, thbdaBuf,(uint)thbdaLen );
 					if (hr==0)
 					{
+						Log.WriteFile(Log.LogType.Log,"twinhan card detected");
 						success=true;
 					}
 					Marshal.ReleaseComObject(propertySet);
@@ -101,53 +189,22 @@ namespace DShowNET
 		public void SendPMT(uint videoPid, uint audioPid,byte[] PMT, int pmtLen)
 		{
 			Log.Write("Twinhan send PMT len:{0} video:0x{1:X} audio:0x{2:X}", pmtLen,videoPid,audioPid);
-			IntPtr ptrPMT = Marshal.AllocCoTaskMem(pmtLen+1);
+			IntPtr ptrPMT = Marshal.AllocCoTaskMem(pmtLen);
 			
 			if(ptrPMT==IntPtr.Zero)
 				return;
-			Marshal.Copy(PMT,0,(IntPtr)(((int)ptrPMT)+1),pmtLen);
-			Marshal.WriteByte(ptrPMT,0);
-			pmtLen+=1;
-			int lenParserPMTInfo=20;
-			IntPtr ptrPARSERPMTINFO = Marshal.AllocCoTaskMem(lenParserPMTInfo);
-			if(ptrPMT==IntPtr.Zero)
-			{
-				Marshal.FreeCoTaskMem(ptrPMT);
-				return;
-			}
-			Marshal.WriteInt32(ptrPARSERPMTINFO,0,(int)ptrPMT);
-			Marshal.WriteInt32(ptrPARSERPMTINFO,4,pmtLen);
-			Marshal.WriteInt32(ptrPARSERPMTINFO,8,(int)videoPid);
-			Marshal.WriteInt32(ptrPARSERPMTINFO,12,(int)audioPid);
-			Marshal.WriteInt32(ptrPARSERPMTINFO,16,0);//default cam
-
-			IntPtr ptrDwBytesReturned = Marshal.AllocCoTaskMem(4);
-			if(ptrDwBytesReturned==IntPtr.Zero)
-			{
-				Marshal.FreeCoTaskMem(ptrPMT);
-				Marshal.FreeCoTaskMem(ptrPARSERPMTINFO);
-				return;
-			}
-			IntPtr ksBla=Marshal.AllocCoTaskMem(0x18);
-			if(ksBla==IntPtr.Zero)
-			{
-				Marshal.FreeCoTaskMem(ptrPMT);
-				Marshal.FreeCoTaskMem(ptrPARSERPMTINFO);
-				Marshal.FreeCoTaskMem(ptrDwBytesReturned);
-				return;
-			}
+			Marshal.Copy(PMT,0,(IntPtr)(((int)ptrPMT)),pmtLen);
 		
 			int thbdaLen=0x28;
 			IntPtr thbdaBuf = Marshal.AllocCoTaskMem(thbdaLen);
 			if(thbdaBuf==IntPtr.Zero)
 			{
 				Marshal.FreeCoTaskMem(ptrPMT);
-				Marshal.FreeCoTaskMem(ptrPARSERPMTINFO);
-				Marshal.FreeCoTaskMem(ptrDwBytesReturned);
-				Marshal.FreeCoTaskMem(ksBla);
+
 				return;
 			}
 		
+			IntPtr ptrDwBytesReturned = Marshal.AllocCoTaskMem(4);
 			
 			Marshal.WriteInt32(thbdaBuf,0,0x255e0082);
 			Marshal.WriteInt16(thbdaBuf,4,0x2017);
@@ -160,9 +217,9 @@ namespace DShowNET
 			Marshal.WriteByte(thbdaBuf,13,0xcb);
 			Marshal.WriteByte(thbdaBuf,14,0x3d);
 			Marshal.WriteByte(thbdaBuf,15,0x67);
-			Marshal.WriteInt32(thbdaBuf,16,(int)THBDA_IOCTL_CI_PARSER_PMT);
-			Marshal.WriteInt32(thbdaBuf,20,(int)ptrPARSERPMTINFO);
-			Marshal.WriteInt32(thbdaBuf,24,20);
+			Marshal.WriteInt32(thbdaBuf,16,(int)THBDA_IOCTL_CI_SEND_PMT);
+			Marshal.WriteInt32(thbdaBuf,20,(int)ptrPMT);
+			Marshal.WriteInt32(thbdaBuf,24,pmtLen);
 			Marshal.WriteInt32(thbdaBuf,28,0);
 			Marshal.WriteInt32(thbdaBuf,32,0);
 			Marshal.WriteInt32(thbdaBuf,36,(int)ptrDwBytesReturned);
@@ -174,30 +231,22 @@ namespace DShowNET
 				if (propertySet!=null) 
 				{
 					Guid propertyGuid=THBDA_TUNER;
-					int hr=propertySet.RemoteSet(ref propertyGuid,0,ksBla,0x18, thbdaBuf,(uint)thbdaLen );
-					int back=Marshal.ReadInt32(ptrDwBytesReturned);
-					int ksBlaVal=Marshal.ReadInt32(ksBla);
-
+					int hr=propertySet.RemoteSet(ref propertyGuid,0,thbdaBuf,(uint)thbdaLen , thbdaBuf,(uint)thbdaLen );
 					if (hr!=0)
 					{
-						Log.Write("SetStructure() failed 0x{0:X}",hr);
+						Log.Write("twinhan: failed to send pmt 0x{0:X}",hr);
 					}
 					else
-						Log.Write("SetStructure() returned ok 0x{0:X}",hr);
+						Log.Write("twinhan: send pmt returned ok  0x{0:X}",hr);
 					Marshal.ReleaseComObject(propertySet);
-
 				}
 				Marshal.ReleaseComObject(pin);
 			}
 
 
-			Marshal.FreeCoTaskMem(thbdaBuf);
-			Marshal.FreeCoTaskMem(ptrDwBytesReturned);
-			Marshal.FreeCoTaskMem(ptrPARSERPMTINFO);
 			Marshal.FreeCoTaskMem(ptrPMT);
-			Marshal.FreeCoTaskMem(ksBla);
-
-
+			Marshal.FreeCoTaskMem(ptrDwBytesReturned);
+			Marshal.FreeCoTaskMem(thbdaBuf);
 		}
 
 		protected override void SetStructure(Guid guidPropSet, uint propId, System.Type structureType, object structValue)
