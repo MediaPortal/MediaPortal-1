a3ea52a755d95481dff87b6d7473d7e434b1377f
james <james@team-mediaportal.com>
2005-11-18 21:45:59 +0000

    WebEPG.exe moved

--- mediaportal/xbmc/bin/Release/PostBuild.bat
+++ mediaportal/xbmc/bin/Release/PostBuild.bat
@@ -92,9 +92,8 @@ copy ..\..\..\WebEPG\WebEPG\bin\Release\WebEPG.dll .
 copy ..\..\..\Utils\bin\Release\Utils.dll .
 
 copy ..\..\..\WebEPG\WebEPG\bin\Release\WebEPG.dll WebEPG\
-copy ..\..\..\WebEPG\WebEPG-xmltv\bin\Release\WebEPG-xmltv.exe WebEPG\WebEPG.exe
+copy ..\..\..\WebEPG\WebEPG-xmltv\bin\Release\WebEPG-xmltv.exe WebEPG.exe
 copy ..\..\..\WebEPG\WebEPG-conf\bin\Release\WebEPG-conf.exe WebEPG\
 copy ..\..\..\WebEPG\WebEPG-channels\bin\Release\WebEPG-channels.exe WebEPG\
-copy ..\..\..\Utils\bin\Release\Utils.dll WebEPG\
 
 @if exist postbuild2.bat call postbuild2.bat
