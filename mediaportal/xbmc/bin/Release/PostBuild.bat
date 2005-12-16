956426de9a4a242368a3ae2b4ae1cc0a39f284e8
mpod_sf <mpod_sf@team-mediaportal.com>
2005-12-16 22:58:01 +0000

    *** empty log message ***

--- mediaportal/xbmc/bin/Release/PostBuild.bat
+++ mediaportal/xbmc/bin/Release/PostBuild.bat
@@ -20,6 +20,7 @@ del *.ax
 
 copy ..\..\..\RemotePlugins\HCWHelper\HCWHelper\bin\Release\HCWHelper.exe .
 copy ..\..\..\RemotePlugins\HCWHelper\HCWHelper\bin\Release\NetHelper.dll .
+if exist NetHelper.pdb del NetHelper.pdb
 copy ..\..\..\lame_enc.dll .
 copy ..\..\..\MPSA.ax .
 copy ..\..\..\TsFileSource.ax .
