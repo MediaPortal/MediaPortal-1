ea5304bd3e24d4e39020212972e04dabdb661b2c
mpod_sf <mpod_sf@team-mediaportal.com>
2005-12-23 10:54:31 +0000

    *** empty log message ***

--- mediaportal/Configuration/Sections/TVRecording.cs
+++ mediaportal/Configuration/Sections/TVRecording.cs
@@ -43,8 +43,6 @@ namespace MediaPortal.Configuration.Sections
     private System.Windows.Forms.CheckBox cbDeleteWatchedShows;
     private System.Windows.Forms.CheckBox cbAddRecordingsToMovie;
     private GroupBox groupBox2;
-    private Label label41;
-    private Label label40;
     private TextBox textBoxMovies;
     private Label label39;
     private Label label38;
@@ -54,6 +52,12 @@ namespace MediaPortal.Configuration.Sections
     private Label labelSampleSeries;
     private Label label8;
     private Label label7;
+    private Label label5;
+    private Label label10;
+    private Label label13;
+    private Label label12;
+    private Label label11;
+    private Label label9;
     private System.ComponentModel.IContainer components = null;
 
     public TVRecording()
@@ -92,7 +96,6 @@ namespace MediaPortal.Configuration.Sections
     /// </summary>
     private void InitializeComponent()
     {
-      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TVRecording));
       this.groupBox1 = new MediaPortal.UserInterface.Controls.MPGroupBox();
       this.cbAddRecordingsToMovie = new System.Windows.Forms.CheckBox();
       this.endTextBox = new System.Windows.Forms.TextBox();
@@ -104,13 +107,17 @@ namespace MediaPortal.Configuration.Sections
       this.cbDeleteWatchedShows = new System.Windows.Forms.CheckBox();
       this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
       this.groupBox2 = new System.Windows.Forms.GroupBox();
+      this.label13 = new System.Windows.Forms.Label();
+      this.label12 = new System.Windows.Forms.Label();
+      this.label11 = new System.Windows.Forms.Label();
+      this.label10 = new System.Windows.Forms.Label();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label9 = new System.Windows.Forms.Label();
       this.label7 = new System.Windows.Forms.Label();
       this.labelSampleSeries = new System.Windows.Forms.Label();
       this.label8 = new System.Windows.Forms.Label();
       this.labelSampleMovies = new System.Windows.Forms.Label();
       this.label6 = new System.Windows.Forms.Label();
-      this.label41 = new System.Windows.Forms.Label();
-      this.label40 = new System.Windows.Forms.Label();
       this.textBoxMovies = new System.Windows.Forms.TextBox();
       this.label39 = new System.Windows.Forms.Label();
       this.label38 = new System.Windows.Forms.Label();
@@ -134,7 +141,7 @@ namespace MediaPortal.Configuration.Sections
       this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
       this.groupBox1.Location = new System.Drawing.Point(0, 0);
       this.groupBox1.Name = "groupBox1";
-      this.groupBox1.Size = new System.Drawing.Size(472, 104);
+      this.groupBox1.Size = new System.Drawing.Size(472, 112);
       this.groupBox1.TabIndex = 0;
       this.groupBox1.TabStop = false;
       this.groupBox1.Text = "Settings";
@@ -142,7 +149,7 @@ namespace MediaPortal.Configuration.Sections
       // cbAddRecordingsToMovie
       // 
       this.cbAddRecordingsToMovie.FlatStyle = System.Windows.Forms.FlatStyle.System;
-      this.cbAddRecordingsToMovie.Location = new System.Drawing.Point(272, 76);
+      this.cbAddRecordingsToMovie.Location = new System.Drawing.Point(272, 80);
       this.cbAddRecordingsToMovie.Name = "cbAddRecordingsToMovie";
       this.cbAddRecordingsToMovie.Size = new System.Drawing.Size(184, 16);
       this.cbAddRecordingsToMovie.TabIndex = 7;
@@ -207,7 +214,7 @@ namespace MediaPortal.Configuration.Sections
       // cbDeleteWatchedShows
       // 
       this.cbDeleteWatchedShows.FlatStyle = System.Windows.Forms.FlatStyle.System;
-      this.cbDeleteWatchedShows.Location = new System.Drawing.Point(16, 76);
+      this.cbDeleteWatchedShows.Location = new System.Drawing.Point(16, 80);
       this.cbDeleteWatchedShows.Name = "cbDeleteWatchedShows";
       this.cbDeleteWatchedShows.Size = new System.Drawing.Size(232, 16);
       this.cbDeleteWatchedShows.TabIndex = 6;
@@ -215,122 +222,191 @@ namespace MediaPortal.Configuration.Sections
       // 
       // groupBox2
       // 
+      this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.groupBox2.Controls.Add(this.label13);
+      this.groupBox2.Controls.Add(this.label12);
+      this.groupBox2.Controls.Add(this.label11);
+      this.groupBox2.Controls.Add(this.label10);
+      this.groupBox2.Controls.Add(this.label5);
+      this.groupBox2.Controls.Add(this.label9);
       this.groupBox2.Controls.Add(this.label7);
       this.groupBox2.Controls.Add(this.labelSampleSeries);
       this.groupBox2.Controls.Add(this.label8);
       this.groupBox2.Controls.Add(this.labelSampleMovies);
       this.groupBox2.Controls.Add(this.label6);
-      this.groupBox2.Controls.Add(this.label41);
-      this.groupBox2.Controls.Add(this.label40);
       this.groupBox2.Controls.Add(this.textBoxMovies);
       this.groupBox2.Controls.Add(this.label39);
       this.groupBox2.Controls.Add(this.label38);
       this.groupBox2.Controls.Add(this.textBoxSeries);
       this.groupBox2.FlatStyle = System.Windows.Forms.FlatStyle.System;
-      this.groupBox2.Location = new System.Drawing.Point(0, 112);
+      this.groupBox2.Location = new System.Drawing.Point(0, 120);
       this.groupBox2.Name = "groupBox2";
-      this.groupBox2.Size = new System.Drawing.Size(472, 296);
-      this.groupBox2.TabIndex = 63;
+      this.groupBox2.Size = new System.Drawing.Size(472, 288);
+      this.groupBox2.TabIndex = 1;
       this.groupBox2.TabStop = false;
-      this.groupBox2.Text = "Filenames";
+      this.groupBox2.Text = "Custom Paths and Filenames";
+      // 
+      // label13
+      // 
+      this.label13.AutoSize = true;
+      this.label13.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.label13.Location = new System.Drawing.Point(388, 24);
+      this.label13.Name = "label13";
+      this.label13.Size = new System.Drawing.Size(13, 130);
+      this.label13.TabIndex = 16;
+      this.label13.Text = "=\r\n=\r\n=\r\n=\r\n=\r\n=\r\n=\r\n=\r\n=\r\n=";
+      // 
+      // label12
+      // 
+      this.label12.AutoSize = true;
+      this.label12.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.label12.Location = new System.Drawing.Point(400, 24);
+      this.label12.Name = "label12";
+      this.label12.Size = new System.Drawing.Size(66, 130);
+      this.label12.TabIndex = 15;
+      this.label12.Text = "start day\r\nstart month\r\nstart year\r\nstart hours\r\nstart minutes\r\nend day\r\nend mont" +
+          "h\r\nend year\r\nend hours\r\nend minutes";
+      // 
+      // label11
+      // 
+      this.label11.AutoSize = true;
+      this.label11.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.label11.Location = new System.Drawing.Point(311, 24);
+      this.label11.Name = "label11";
+      this.label11.Size = new System.Drawing.Size(72, 130);
+      this.label11.TabIndex = 14;
+      this.label11.Text = "%startday%\r\n%startmonth%\r\n%startyear%\r\n%starthh%\r\n%startmm%\r\n%endday%\r\n%endmonth%" +
+          "\r\n%endyear%\r\n%endhh%\r\n%endmm%";
+      this.label11.TextAlign = System.Drawing.ContentAlignment.TopRight;
+      // 
+      // label10
+      // 
+      this.label10.AutoSize = true;
+      this.label10.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.label10.Location = new System.Drawing.Point(229, 24);
+      this.label10.Name = "label10";
+      this.label10.Size = new System.Drawing.Size(82, 130);
+      this.label10.TabIndex = 13;
+      this.label10.Text = "channel name\r\ntitle\r\nepisode name\r\ngenre\r\nseries number\r\nepisode number\r\nepisode " +
+          "part\r\ndate\r\nstart time\r\nend time";
+      // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.label5.Location = new System.Drawing.Point(217, 24);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(13, 130);
+      this.label5.TabIndex = 11;
+      this.label5.Text = "=\r\n=\r\n=\r\n=\r\n=\r\n=\r\n=\r\n=\r\n=\r\n=";
+      // 
+      // label9
+      // 
+      this.label9.AutoSize = true;
+      this.label9.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.label9.Location = new System.Drawing.Point(152, 24);
+      this.label9.Name = "label9";
+      this.label9.Size = new System.Drawing.Size(61, 130);
+      this.label9.TabIndex = 12;
+      this.label9.Text = "%channel%\r\n%title%\r\n%name%\r\n%genre%\r\n%series%\r\n%episode%\r\n%part%\r\n%date%\r\n%start%" +
+          "\r\n%end%";
+      this.label9.TextAlign = System.Drawing.ContentAlignment.TopRight;
       // 
       // label7
       // 
       this.label7.AutoSize = true;
-      this.label7.Location = new System.Drawing.Point(336, 24);
+      this.label7.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.label7.Location = new System.Drawing.Point(16, 37);
       this.label7.Name = "label7";
-      this.label7.Size = new System.Drawing.Size(118, 130);
-      this.label7.TabIndex = 70;
-      this.label7.Text = "Leave empty for classic\r\nnaming. Use block-\r\nquotes [] to specify\r\noptional field" +
-          "s.\r\n\r\nNote: A recording is\r\nbeing considered as\r\na movie, if the re-\r\ncording ty" +
-          "pe is manual\r\nor single recording.";
+      this.label7.Size = new System.Drawing.Size(134, 104);
+      this.label7.TabIndex = 0;
+      this.label7.Text = "Leave empty for classic\r\nnaming. Use blockquotes\r\n[ ] to specify optional fields." +
+          "\r\n\r\nNote: A recording is being\r\nconsidered as a movie, if\r\nthe recording type is" +
+          "\r\nmanual or single recording.";
       // 
       // labelSampleSeries
       // 
+      this.labelSampleSeries.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.labelSampleSeries.FlatStyle = System.Windows.Forms.FlatStyle.System;
       this.labelSampleSeries.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-      this.labelSampleSeries.Location = new System.Drawing.Point(64, 264);
+      this.labelSampleSeries.Location = new System.Drawing.Point(66, 256);
       this.labelSampleSeries.Name = "labelSampleSeries";
-      this.labelSampleSeries.Size = new System.Drawing.Size(392, 26);
-      this.labelSampleSeries.TabIndex = 69;
-      this.labelSampleSeries.Text = "line1\r\nline2";
+      this.labelSampleSeries.Size = new System.Drawing.Size(392, 16);
+      this.labelSampleSeries.TabIndex = 10;
+      this.labelSampleSeries.Text = "line1";
       // 
       // label8
       // 
       this.label8.AutoSize = true;
-      this.label8.Location = new System.Drawing.Point(16, 264);
+      this.label8.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.label8.Location = new System.Drawing.Point(16, 256);
       this.label8.Name = "label8";
       this.label8.Size = new System.Drawing.Size(45, 13);
-      this.label8.TabIndex = 68;
+      this.label8.TabIndex = 9;
       this.label8.Text = "Sample:";
       // 
       // labelSampleMovies
       // 
+      this.labelSampleMovies.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.labelSampleMovies.FlatStyle = System.Windows.Forms.FlatStyle.System;
       this.labelSampleMovies.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-      this.labelSampleMovies.Location = new System.Drawing.Point(64, 200);
+      this.labelSampleMovies.Location = new System.Drawing.Point(66, 200);
       this.labelSampleMovies.Name = "labelSampleMovies";
-      this.labelSampleMovies.Size = new System.Drawing.Size(392, 26);
-      this.labelSampleMovies.TabIndex = 67;
-      this.labelSampleMovies.Text = "line1\r\nline2";
+      this.labelSampleMovies.Size = new System.Drawing.Size(392, 16);
+      this.labelSampleMovies.TabIndex = 6;
+      this.labelSampleMovies.Text = "line1";
       // 
       // label6
       // 
       this.label6.AutoSize = true;
+      this.label6.FlatStyle = System.Windows.Forms.FlatStyle.System;
       this.label6.Location = new System.Drawing.Point(16, 200);
       this.label6.Name = "label6";
       this.label6.Size = new System.Drawing.Size(45, 13);
-      this.label6.TabIndex = 66;
+      this.label6.TabIndex = 5;
       this.label6.Text = "Sample:";
-      // 
-      // label41
-      // 
-      this.label41.AutoSize = true;
-      this.label41.Location = new System.Drawing.Point(176, 24);
-      this.label41.Name = "label41";
-      this.label41.Size = new System.Drawing.Size(136, 130);
-      this.label41.TabIndex = 63;
-      this.label41.Text = resources.GetString("label41.Text");
-      // 
-      // label40
-      // 
-      this.label40.AutoSize = true;
-      this.label40.Location = new System.Drawing.Point(16, 24);
-      this.label40.Name = "label40";
-      this.label40.Size = new System.Drawing.Size(147, 130);
-      this.label40.TabIndex = 62;
-      this.label40.Text = resources.GetString("label40.Text");
       // 
       // textBoxMovies
       // 
+      this.textBoxMovies.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
       this.textBoxMovies.Location = new System.Drawing.Point(64, 168);
       this.textBoxMovies.Name = "textBoxMovies";
       this.textBoxMovies.Size = new System.Drawing.Size(392, 20);
-      this.textBoxMovies.TabIndex = 61;
+      this.textBoxMovies.TabIndex = 4;
       this.textBoxMovies.TextChanged += new System.EventHandler(this.textBoxMovies_TextChanged);
       // 
       // label39
       // 
       this.label39.AutoSize = true;
-      this.label39.Location = new System.Drawing.Point(16, 232);
+      this.label39.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.label39.Location = new System.Drawing.Point(16, 227);
       this.label39.Name = "label39";
       this.label39.Size = new System.Drawing.Size(39, 13);
-      this.label39.TabIndex = 60;
+      this.label39.TabIndex = 7;
       this.label39.Text = "Series:";
       // 
       // label38
       // 
       this.label38.AutoSize = true;
-      this.label38.Location = new System.Drawing.Point(16, 168);
+      this.label38.FlatStyle = System.Windows.Forms.FlatStyle.System;
+      this.label38.Location = new System.Drawing.Point(16, 171);
       this.label38.Name = "label38";
       this.label38.Size = new System.Drawing.Size(44, 13);
-      this.label38.TabIndex = 59;
+      this.label38.TabIndex = 3;
       this.label38.Text = "Movies:";
       // 
       // textBoxSeries
       // 
-      this.textBoxSeries.Location = new System.Drawing.Point(64, 232);
+      this.textBoxSeries.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                  | System.Windows.Forms.AnchorStyles.Right)));
+      this.textBoxSeries.Location = new System.Drawing.Point(64, 224);
       this.textBoxSeries.Name = "textBoxSeries";
       this.textBoxSeries.Size = new System.Drawing.Size(392, 20);
-      this.textBoxSeries.TabIndex = 58;
+      this.textBoxSeries.TabIndex = 8;
       this.textBoxSeries.TextChanged += new System.EventHandler(this.textBoxSeries_TextChanged);
       // 
       // TVRecording
--- mediaportal/Core/Util/Util.cs
+++ mediaportal/Core/Util/Util.cs
@@ -1082,6 +1082,7 @@ namespace MediaPortal.Util
       if (strText == null) return String.Empty;
       if (strText.Length == 0) return String.Empty;
       string strFName = strText.Replace('*', '_');
+      strFName = strFName.Replace(':', '_');
       strFName = strFName.Replace('?', '_');
       strFName = strFName.Replace('\"', '_');
       strFName = strFName.Replace('<', '_'); ;
