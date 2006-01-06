55f247f8112eb0b0a053e2f67e349c5f31cc60d2
systay <systay@team-mediaportal.com>
2006-01-06 16:02:42 +0000

    Added PlayListPlayerTest

---
+++ mediaportal/MediaPortal.Tests/Core/Playlists/PlayListPlayerTest.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using MediaPortal.Playlists;
+
+namespace MediaPortal.Tests.Core.Playlists
+{
+  [TestFixture]
+  public class PlayListPlayerTest : NonStaticPlayListPlayer.IPlayer
+  {
+    [Test]
+    public void InsertItemButNotStartPlayingGivesNull()
+    {
+      NonStaticPlayListPlayer player = new NonStaticPlayListPlayer();
+      player.CurrentPlaylistType = PlayListType.PLAYLIST_MUSIC;
+      PlayList playlist = player.GetPlaylist(PlayListType.PLAYLIST_MUSIC);
+      PlayListItem item1 = new PlayListItem();
+      playlist.Add(item1);
+      Assert.IsNull(player.GetCurrentItem());
+    }
+
+    [Test]
+    public void PlayMovesCurrentToItem()
+    {
+      NonStaticPlayListPlayer player = new NonStaticPlayListPlayer();
+      player.g_Player = this; //fake g_Player
+      player.CurrentPlaylistType = PlayListType.PLAYLIST_MUSIC;
+      PlayList playlist = player.GetPlaylist(PlayListType.PLAYLIST_MUSIC);
+      PlayListItem item1 = new PlayListItem();
+      playlist.Add(item1);
+      player.PlayNext();
+      Assert.AreEqual(item1, player.GetCurrentItem());
+      Assert.IsTrue(hasPlayBeenCalled);
+    }
+
+    [Test]
+    public void GetNextReturnsFileName()
+    {
+      NonStaticPlayListPlayer player = new NonStaticPlayListPlayer();
+      player.CurrentPlaylistType = PlayListType.PLAYLIST_MUSIC;
+      PlayList playlist = player.GetPlaylist(PlayListType.PLAYLIST_MUSIC);
+      PlayListItem item1 = new PlayListItem("apa", "c:\\apa.mp3");
+      playlist.Add(item1);
+      Assert.AreEqual("c:\\apa.mp3", player.GetNext());
+
+    }
+
+    #region IPlayer Members
+
+    private bool hasPlayBeenCalled = false;
+
+    public bool Playing
+    {
+      get { return true; }
+    }
+
+    public void Release()
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
+    public bool Play(string strFile)
+    {
+      hasPlayBeenCalled = true;
+      return true;
+    }
+
+    public void Stop()
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
+    public void SeekAsolutePercentage(int iPercentage)
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
+    public double Duration
+    {
+      get { throw new Exception("The method or operation is not implemented."); }
+    }
+
+    public double CurrentPosition
+    {
+      get { throw new Exception("The method or operation is not implemented."); }
+    }
+
+    public void SeekAbsolute(double dTime)
+    {
+      throw new Exception("The method or operation is not implemented.");
+    }
+
+    public bool HasVideo
+    {
+      get { throw new Exception("The method or operation is not implemented."); }
+    }
+
+    #endregion
+  }
+}
