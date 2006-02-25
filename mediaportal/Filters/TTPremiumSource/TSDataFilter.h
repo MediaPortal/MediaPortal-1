f40e0e3fc3417cd7178c68c8a5601f1bc129a7d1
draktheas <draktheas@team-mediaportal.com>
2006-02-25 20:04:26 +0000

    Techo Trend Premium Source filter code

---
+++ mediaportal/Filters/TTPremiumSource/TSDataFilter.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "DVBTSFilter.h"
+#include "Lock.h"
+#include <vector>
+#include <map>
+#include <queue>
+
+class CTTPremiumOutputPin;
+
+class TSDataFilter : public CDVBTSFilter
+{
+public:
+	TSDataFilter(int bufferSize);
+	virtual ~TSDataFilter();
+
+	void Start();
+	void Stop();
+	bool IsRunning() { return m_isRunning; }
+	int RefCount() { return m_refCount; }
+	
+	int Copy(BYTE *buff, int maxLen);
+
+	void AddRef(CTTPremiumOutputPin *pPin);
+	void RemoveRef(CTTPremiumOutputPin *pPin);
+
+protected:
+	friend class CTTPremiumOutputPin;
+
+	virtual void OnDataArrival(BYTE* Buff, int len);
+  HRESULT SendData(CTTPremiumOutputPin *outputPin, BYTE* buff, int len);
+
+	int						m_refCount;
+	bool					m_isRunning;
+	int						m_bufferSize;
+
+	Lock					    *m_lock;
+  std::vector<CTTPremiumOutputPin *> m_watchers;
+};
