17c6aa8da6daf6b3d433793b7ec9c1f0ad5a6937
dman_lfc <dman_lfc@team-mediaportal.com>
2006-04-01 19:50:18 +0000

    Supress excessive logging

--- mediaportal/Core/guilib/GUITextureManager.cs
+++ mediaportal/Core/guilib/GUITextureManager.cs
@@ -424,7 +424,7 @@ namespace MediaPortal.GUI.Library
             width = info2.Width;
             height = info2.Height;
 
-            Log.Write("Texturemanager loaded temporay:{0} {1}x{2} format:{3}",
+            //Log.Write("Texturemanager loaded temporay:{0} {1}x{2} format:{3}",
               fileName, width, height, info2.Format);
           }
         }
