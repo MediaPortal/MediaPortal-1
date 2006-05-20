2e808323e98be7856e6ad0fbd95233105b09d0e9
mpod_sf <mpod_sf@team-mediaportal.com>
2006-05-20 07:55:55 +0000

    formatting

--- mediaportal/Dialogs/Dialogs/VirtualKeyboard.cs
+++ mediaportal/Dialogs/Dialogs/VirtualKeyboard.cs
@@ -1,5 +1,7 @@
+#region Copyright (C) 2005-2006 Team MediaPortal
+
 /* 
- *	Copyright (C) 2005 Team MediaPortal
+ *	Copyright (C) 2005-2006 Team MediaPortal
  *	http://www.team-mediaportal.com
  *
  *  This Program is free software; you can redistribute it and/or modify
@@ -18,6 +20,9 @@
  *  http://www.gnu.org/copyleft/gpl.html
  *
  */
+
+#endregion
+
 using System;
 using System.Drawing;
 using System.Windows.Forms;
@@ -709,7 +714,7 @@ namespace MediaPortal.Dialogs
     }
     void Close()
     {
-        _isVisible = false;
+      _isVisible = false;
     }
 
     public void DoModal(int dwParentId)
