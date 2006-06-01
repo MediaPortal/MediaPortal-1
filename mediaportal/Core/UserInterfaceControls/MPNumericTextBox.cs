f4bf61c3221038797767c690929dc09032db9d25
mpod_sf <mpod_sf@team-mediaportal.com>
2006-06-01 10:37:36 +0000

    NumericTextBox became MPNumericTextBox and is part of UserInterfaceControls now

--- mediaportal/Core/UserInterfaceControls/MPNumericTextBox.cs
+++ mediaportal/Core/UserInterfaceControls/MPNumericTextBox.cs
@@ -1,3 +1,5 @@
+#region Copyright (C) 2005-2006 Team MediaPortal
+
 /* 
  *	Copyright (C) 2005-2006 Team MediaPortal
  *	http://www.team-mediaportal.com
@@ -19,149 +21,78 @@
  *
  */
 
-
+#endregion
 
 using System;
-
 using System.Windows.Forms;
-
 using System.Globalization;
 
-
-
-namespace Yeti.Controls
-
+namespace MediaPortal.UserInterface.Controls
 {
-
 	/// <summary>
-
 	/// Define a TextBox that allow only integer numbers.
-
 	/// </summary>
-
-	public class NumericTextBox : TextBox
-
+	public class MPNumericTextBox : System.Windows.Forms.TextBox
 	{
-
-		public NumericTextBox()
-
+    public MPNumericTextBox()
       :base()
-
 		{
-
 		}
 
-
-
     protected override void OnKeyPress(KeyPressEventArgs e)
-
     {
-
       base.OnKeyPress(e);
-
       if ( (!e.Handled) && ( "1234567890\b".IndexOf(e.KeyChar) < 0) )
-
       {
-
         Yeti.Sys.Win32.MessageBeep(Yeti.Sys.BeepType.SimpleBeep);
-
         e.Handled = true;
-
       }
-
     }
-
-
 
 	  public event EventHandler FormatError;
-
     public event EventHandler FormatValid;
 
-
-
     protected virtual void OnFormatError(EventArgs e)
-
     {
-
       if (FormatError != null)
-
       {
-
         FormatError(this, e);
-
       }
-
     }
-
-
 
     protected virtual void OnFormatValid(EventArgs e)
-
     {
-
       if (FormatValid != null)
-
       {
-
         FormatValid(this, e);
-
       }
-
     }
-
-
 
     protected override void OnTextChanged(EventArgs e)
-
     {
-
       try
-
       {
-
         int.Parse(this.Text, NumberStyles.Integer);
-
         OnFormatValid(e);
-
       }
-
       catch
-
       {
-
         OnFormatError(e);
-
       }
-
       base.OnTextChanged (e);
-
     }
-
-
 
     public int Value
-
     {
-
       get
-
       {
-
         return int.Parse(this.Text, NumberStyles.Integer);
-
       }
-
       set
-
       {
-
         this.Text = value.ToString();
-
       }
-
     }
-
   }
-
 }
 
