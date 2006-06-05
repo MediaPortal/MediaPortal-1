1af5b19bae344683219c84502c6ce643c7e0013a
mpod_sf <mpod_sf@team-mediaportal.com>
2006-06-05 17:49:22 +0000

    added: rows can be dragged to change order

--- mediaportal/Core/UserInterfaceControls/MPListView.cs
+++ mediaportal/Core/UserInterfaceControls/MPListView.cs
@@ -24,29 +24,185 @@
 #endregion
 
 using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections;
 
 namespace MediaPortal.UserInterface.Controls
 {
-	/// <summary>
-	/// Summary description for ListView.
-	/// </summary>
-	public class MPListView : System.Windows.Forms.ListView
-	{
-		protected override void WndProc(ref System.Windows.Forms.Message m)
-		{
-			const int WM_PAINT = 0xf ;
+  /// <summary>
+  /// Summary description for ListView.
+  /// </summary>
+  public class MPListView : System.Windows.Forms.ListView
+  {
 
-			switch(m.Msg)
-			{
-				case WM_PAINT:
-					if(this.View == System.Windows.Forms.View.Details && this.Columns.Count > 0)
-					{
-						this.Columns[this.Columns.Count - 1].Width = -2 ;
-					}
-					break ;
-			}
+    private const string REORDER = "Reorder";
+    private bool allowRowReorder = true;
 
-			base.WndProc (ref m);
-		}
-	}
+    public bool AllowRowReorder
+    {
+      get
+      {
+        return this.allowRowReorder;
+      }
+      set
+      {
+        this.allowRowReorder = value;
+        base.AllowDrop = value;
+      }
+    }
+
+    public new SortOrder Sorting
+    {
+      get
+      {
+        return SortOrder.None;
+      }
+      set
+      {
+        base.Sorting = SortOrder.None;
+      }
+    }
+
+    public MPListView()
+      : base()
+    {
+      this.AllowRowReorder = true;
+    }
+
+    //protected override void WndProc(ref System.Windows.Forms.Message m)
+    //{
+    //  const int WM_PAINT = 0xf ;
+
+    //  switch(m.Msg)
+    //  {
+    //    case WM_PAINT:
+    //      if(this.View == System.Windows.Forms.View.Details && this.Columns.Count > 0)
+    //      {
+    //        this.Columns[this.Columns.Count - 1].Width = -2 ;
+    //      }
+    //      break ;
+    //  }
+
+    //  base.WndProc (ref m);
+    //}
+
+    protected override void OnItemDrag(ItemDragEventArgs e)
+    {
+      if (!this.AllowRowReorder)
+      {
+        return;
+      }
+      base.DoDragDrop(REORDER, DragDropEffects.Move);
+      base.OnItemDrag(e);
+    }
+
+    protected override void OnDragEnter(DragEventArgs e)
+    {
+      if (!this.AllowRowReorder)
+      {
+        e.Effect = DragDropEffects.None;
+        return;
+      }
+      if (!e.Data.GetDataPresent(DataFormats.Text))
+      {
+        e.Effect = DragDropEffects.None;
+        return;
+      }
+      String text = (String)e.Data.GetData(REORDER.GetType());
+      if (text.CompareTo(REORDER) == 0)
+      {
+        e.Effect = DragDropEffects.Move;
+      }
+      else
+      {
+        e.Effect = DragDropEffects.None;
+      }
+      base.OnDragEnter(e);
+    }
+
+    protected override void OnDragOver(DragEventArgs e)
+    {
+      if (!this.AllowRowReorder)
+      {
+        e.Effect = DragDropEffects.None;
+        return;
+      }
+      if (!e.Data.GetDataPresent(DataFormats.Text))
+      {
+        e.Effect = DragDropEffects.None;
+        return;
+      }
+      Point cp = base.PointToClient(new Point(e.X, e.Y));
+      ListViewItem hoverItem = base.GetItemAt(cp.X, cp.Y);
+      if (hoverItem == null)
+      {
+        e.Effect = DragDropEffects.None;
+        return;
+      }
+      foreach (ListViewItem moveItem in base.SelectedItems)
+      {
+        if (moveItem.Index == hoverItem.Index)
+        {
+          e.Effect = DragDropEffects.None;
+          hoverItem.EnsureVisible();
+          return;
+        }
+      }
+      base.OnDragOver(e);
+      String text = (String)e.Data.GetData(REORDER.GetType());
+      if (text.CompareTo(REORDER) == 0)
+      {
+        e.Effect = DragDropEffects.Move;
+        hoverItem.EnsureVisible();
+      }
+      else
+      {
+        e.Effect = DragDropEffects.None;
+      }
+    }
+
+    protected override void OnDragDrop(DragEventArgs e)
+    {
+      if (!this.AllowRowReorder)
+      {
+        return;
+      }
+      if (base.SelectedItems.Count == 0)
+      {
+        return;
+      }
+      Point cp = base.PointToClient(new Point(e.X, e.Y));
+      ListViewItem dragToItem = base.GetItemAt(cp.X, cp.Y);
+      if (dragToItem == null)
+      {
+        return;
+      }
+      BeginUpdate();
+      int dropIndex = dragToItem.Index;
+      if (dropIndex > base.SelectedItems[0].Index)
+      {
+        dropIndex++;
+      }
+      ArrayList insertItems =
+        new ArrayList(base.SelectedItems.Count);
+      foreach (ListViewItem item in base.SelectedItems)
+      {
+        insertItems.Add(item.Clone());
+      }
+      for (int i = insertItems.Count - 1; i >= 0; i--)
+      {
+        ListViewItem insertItem =
+         (ListViewItem)insertItems[i];
+        base.Items.Insert(dropIndex, insertItem);
+      }
+      foreach (ListViewItem removeItem in base.SelectedItems)
+      {
+        base.Items.Remove(removeItem);
+      }
+      base.OnDragDrop(e);
+      EndUpdate();
+    }
+
+  }
 }
