7b3e7b2298860de471a45558cfc0ce7d28ef698a
mr_hipp <mr_hipp@team-mediaportal.com>
2006-06-12 08:34:10 +0000

    Removed saving of timeshift state, because this is a state of the tv-card and thus could not be a global setting.

--- mediaportal/WindowPlugins/GUITV/GUITVHome.cs
+++ mediaportal/WindowPlugins/GUITV/GUITVHome.cs
@@ -58,7 +58,7 @@ namespace MediaPortal.GUI.TV
     };
 
     static bool _isTvOn = true;
-    static bool _isTimeShifting = true;
+    static bool _isTimeShifting = false;
     static ChannelNavigator m_navigator = new ChannelNavigator();
 
     DateTime _updateTimer = DateTime.Now;
@@ -101,7 +101,6 @@ namespace MediaPortal.GUI.TV
       {
         m_navigator.LoadSettings(xmlreader);
         _isTvOn = xmlreader.GetValueAsBool("mytv", "tvon", true);
-        _isTimeShifting = xmlreader.GetValueAsBool("mytv", "timeshifting", true);
         _autoTurnOnTv = xmlreader.GetValueAsBool("mytv", "autoturnontv", false);
 
         string strValue = xmlreader.GetValueAsString("mytv", "defaultar", "normal");
@@ -120,7 +119,6 @@ namespace MediaPortal.GUI.TV
       {
         m_navigator.SaveSettings(xmlwriter);
         xmlwriter.SetValueAsBool("mytv", "tvon", _isTvOn);
-        xmlwriter.SetValueAsBool("mytv", "timeshifting", _isTimeShifting);
       }
     }
     #endregion
@@ -448,9 +446,7 @@ namespace MediaPortal.GUI.TV
         //turn timeshifting off 
         _isTimeShifting = !Recorder.IsTimeShifting();
         Log.Write("tv home timeshift onoff:{0}", _isTimeShifting);
-        SaveSettings();
         ViewChannelAndCheck(Navigator.CurrentChannel);
-
         _isTimeShifting = Recorder.IsTimeShifting();
 
         UpdateStateOfButtons();
