03543bbe7227e0211a853529d33271b415f29c11
rtv_sf <github@rjtv.de>
2006-06-28 21:52:15 +0000

    added reset button for steps

--- mediaportal/WindowPlugins/GUISettings/General/GUISettingsSkipSteps.cs
+++ mediaportal/WindowPlugins/GUISettings/General/GUISettingsSkipSteps.cs
@@ -33,33 +33,53 @@ using MediaPortal.Player;
 
 namespace WindowPlugins.GUISettings
 {
-	public class GUISettingsSkipSteps : GUIWindow
-	{
-		[SkinControlAttribute(2)]    protected GUICheckMarkControl checkMarkButtonStep1 = null;
-    [SkinControlAttribute(3)]    protected GUICheckMarkControl checkMarkButtonStep2 = null;
-    [SkinControlAttribute(4)]    protected GUICheckMarkControl checkMarkButtonStep3 = null;
-    [SkinControlAttribute(5)]    protected GUICheckMarkControl checkMarkButtonStep4 = null;
-    [SkinControlAttribute(6)]    protected GUICheckMarkControl checkMarkButtonStep5 = null;
-    [SkinControlAttribute(7)]    protected GUICheckMarkControl checkMarkButtonStep6 = null;
-    [SkinControlAttribute(8)]    protected GUICheckMarkControl checkMarkButtonStep7 = null;
-    [SkinControlAttribute(9)]    protected GUICheckMarkControl checkMarkButtonStep8 = null;
-    [SkinControlAttribute(10)]   protected GUICheckMarkControl checkMarkButtonStep9 = null;
-    [SkinControlAttribute(11)]   protected GUICheckMarkControl checkMarkButtonStep10 = null;
-    [SkinControlAttribute(12)]   protected GUICheckMarkControl checkMarkButtonStep11 = null;
-    [SkinControlAttribute(13)]   protected GUICheckMarkControl checkMarkButtonStep12 = null;
-    [SkinControlAttribute(14)]   protected GUICheckMarkControl checkMarkButtonStep13 = null;
-    [SkinControlAttribute(15)]   protected GUICheckMarkControl checkMarkButtonStep14 = null;
-    [SkinControlAttribute(16)]   protected GUICheckMarkControl checkMarkButtonStep15 = null;
-    [SkinControlAttribute(17)]   protected GUICheckMarkControl checkMarkButtonStep16 = null;
+  public class GUISettingsSkipSteps : GUIWindow
+  {
+    #region Skin elements
+    [SkinControlAttribute(2)]
+    protected GUICheckMarkControl checkMarkButtonStep1 = null;
+    [SkinControlAttribute(3)]
+    protected GUICheckMarkControl checkMarkButtonStep2 = null;
+    [SkinControlAttribute(4)]
+    protected GUICheckMarkControl checkMarkButtonStep3 = null;
+    [SkinControlAttribute(5)]
+    protected GUICheckMarkControl checkMarkButtonStep4 = null;
+    [SkinControlAttribute(6)]
+    protected GUICheckMarkControl checkMarkButtonStep5 = null;
+    [SkinControlAttribute(7)]
+    protected GUICheckMarkControl checkMarkButtonStep6 = null;
+    [SkinControlAttribute(8)]
+    protected GUICheckMarkControl checkMarkButtonStep7 = null;
+    [SkinControlAttribute(9)]
+    protected GUICheckMarkControl checkMarkButtonStep8 = null;
+    [SkinControlAttribute(10)]
+    protected GUICheckMarkControl checkMarkButtonStep9 = null;
+    [SkinControlAttribute(11)]
+    protected GUICheckMarkControl checkMarkButtonStep10 = null;
+    [SkinControlAttribute(12)]
+    protected GUICheckMarkControl checkMarkButtonStep11 = null;
+    [SkinControlAttribute(13)]
+    protected GUICheckMarkControl checkMarkButtonStep12 = null;
+    [SkinControlAttribute(14)]
+    protected GUICheckMarkControl checkMarkButtonStep13 = null;
+    [SkinControlAttribute(15)]
+    protected GUICheckMarkControl checkMarkButtonStep14 = null;
+    [SkinControlAttribute(16)]
+    protected GUICheckMarkControl checkMarkButtonStep15 = null;
+    [SkinControlAttribute(17)]
+    protected GUICheckMarkControl checkMarkButtonStep16 = null;
+    [SkinControlAttribute(18)]
+    protected GUIButtonControl buttonReset = null;
+    #endregion
 
     public GUISettingsSkipSteps()
-		{
-			GetID=(int)GUIWindow.Window.WINDOW_SETTINGS_SKIPSTEPS;
-		}
-    
-		public override bool Init()
-		{
-			return Load (GUIGraphicsContext.Skin+@"\settingsSkipSteps.xml");      
+    {
+      GetID = (int)GUIWindow.Window.WINDOW_SETTINGS_SKIPSTEPS;
+    }
+
+    public override bool Init()
+    {
+      return Load(GUIGraphicsContext.Skin + @"\settingsSkipSteps.xml");
     }
 
     protected override void OnPageLoad()
@@ -132,14 +152,29 @@ namespace WindowPlugins.GUISettings
       Log.Write("GUISkipSteps: {0}", "reset g_player settings");
     }
 
-		protected override void OnClicked(int controlId, GUIControl control, MediaPortal.GUI.Library.Action.ActionType actionType)
-		{
-      if ( control == checkMarkButtonStep1 )
+    protected override void OnClicked( int controlId, GUIControl control, MediaPortal.GUI.Library.Action.ActionType actionType )
+    {
+      if ( control == buttonReset )
       {
-        //Log.Write("DEBUG - GUISkipSteps: {0}", "checkMarkButtonStep1.onclick");
+        checkMarkButtonStep1.Selected = false;
+        checkMarkButtonStep2.Selected = true;
+        checkMarkButtonStep3.Selected = true;
+        checkMarkButtonStep4.Selected = false;
+        checkMarkButtonStep5.Selected = true;
+        checkMarkButtonStep6.Selected = true;
+        checkMarkButtonStep7.Selected = true;
+        checkMarkButtonStep8.Selected = false;
+        checkMarkButtonStep9.Selected = true;
+        checkMarkButtonStep10.Selected = true;
+        checkMarkButtonStep11.Selected = true;
+        checkMarkButtonStep12.Selected = false;
+        checkMarkButtonStep13.Selected = true;
+        checkMarkButtonStep14.Selected = false;
+        checkMarkButtonStep15.Selected = true;
+        checkMarkButtonStep16.Selected = false;
       }
-			base.OnClicked (controlId, control, actionType);
-		}
-    
-	}
+      base.OnClicked(controlId, control, actionType);
+    }
+
+  }
 }
