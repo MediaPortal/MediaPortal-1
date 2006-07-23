67f5348491659c6cb5c3aae11600b8ed1cb3d22e
agree <agree@team-mediaportal.com>
2006-07-23 11:22:35 +0000

    First simple MPNG version.
    There are not much comments and not much doc in yet ;)
    
    greets from agree

---
+++ MediaPortalNG/app.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Data;
+using System.Xml;
+using System.Configuration;
+
+namespace MediaPortal
+{
+
+    public partial class app : Application
+    {
+
+        void AppStartup(object sender, StartupEventArgs args)
+        {
+            Core mpCore = new Core();
+        }
+ 
+    }
+}
