c05c469b777b35038f92ce352675239125fae9b7
james <james@team-mediaportal.com>
2006-09-24 19:49:57 +0000

    Making use of System.IO.Path Added GetSubFolder and IsSet

--- mediaportal/Core/Util/Config.cs
+++ mediaportal/Core/Util/Config.cs
@@ -25,202 +25,209 @@ using System.Xml;
 
 namespace MediaPortal.Util
 {
-    public class Config
+  public class Config
+  {
+    private static Dictionary<Dir, string> directories;
+
+    public enum Dir
     {
-        private static Dictionary<Dir, string> directories;
+      // Path holding Path Information
+      Base,
+      Log,
+      Skin,
+      Language,
+      Database,
+      Plugins,
+      Thumbs,
+      Cache,
+      Weather,
+      CustomInputDevice,
+      Config
+    }
 
-        public enum Dir
-        {
-            // Path holding Path Information
-            Base,
-            Log,
-            Skin,
-            Language,
-            Database,
-            Plugins,
-            Thumbs,
-            Cache,
-            Weather,
-            CustomInputDevice,
-            Config
-        }
-
-        /// <summary>
-        /// Private constructor. Singleton. Do not allow any instance of this class.
-        /// </summary>
-        ///
-        /*
+    /// <summary>
+    /// Private constructor. Singleton. Do not allow any instance of this class.
+    /// </summary>
+    ///
+    /*
 private Config()
 {
 }
 */
-        static Config()
-        {
-            directories = new Dictionary<Dir, string>();
-            LoadDirs(AppDomain.CurrentDomain.BaseDirectory);
-        }
-
-        /// <summary>
-        /// Read the Directory Configuration from the Config File.
-        /// First we look for the file in MyDocuments of the logged on user. If file is not there or invalid, 
-        /// we use the one from the MediaPortal InstallPath.
-        /// </summary>
-        private static void LoadDirs(string startuppath)
-        {
-            Set(Dir.Base, startuppath + @"\");
-            if (!ReadConfig(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Team MediaPortal\"))
-            {
-                if (!ReadConfig(Get(Dir.Base)))
-                {
-                    LoadDefaultDirs();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Load the Path information from the Config File into the dictionary
-        /// </summary>
-        /// <returns></returns>
-        private static bool ReadConfig(string configDir)
-        {
-            // Make sure the file exists before we try to do any processing
-            string strFileName = configDir + "MediaPortalDirs.xml";
-            if (File.Exists(strFileName))
-            {
-                try
-                {
-                    XmlDocument xml = new XmlDocument();
-                    xml.Load(strFileName);
-                    if (xml.DocumentElement == null)
-                    {
-                        return false;
-                    }
-                    XmlNodeList dirList = xml.DocumentElement.SelectNodes("/Config/Dir");
-                    if (dirList == null || dirList.Count == 0)
-                    {
-                        return false;
-                    }
-                    foreach (XmlNode nodeDir in dirList)
-                    {
-                        if (nodeDir.Attributes != null)
-                        {
-                            XmlNode dirId = nodeDir.Attributes.GetNamedItem("id");
-                            if (dirId != null && dirId.InnerText != null && dirId.InnerText.Length > 0)
-                            {
-                                XmlNode path = nodeDir.SelectSingleNode("Path");
-                                if (path != null)
-                                {
-                                    string strPath = path.InnerText;
-                                    // Check to see, if the location was specified with an absolute or relative path.
-                                    // In case of relative path, prefix it with the startuppath                 
-                                    if (!Path.IsPathRooted(path.InnerText))
-                                    {
-                                        strPath = Get(Dir.Base) + path.InnerText;
-                                    }
-                                    // See if we got a slash at the end. If not add one.
-                                    if (!strPath.EndsWith(@"\"))
-                                    {
-                                        strPath += @"\";
-                                    }
-                                    try
-                                    {
-                                        Set((Dir) Enum.Parse(typeof(Dir), dirId.InnerText), strPath);
-                                    }
-                                    catch (Exception)
-                                    {
-                                        return false;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    return true;
-                }
-                catch (Exception)
-                {
-                    return false;
-                }
-            }
-            return false;
-        }
-
-        private static void LoadDefaultDirs()
-        {
-            string baseDir = Get(Dir.Base);
-            Set(Dir.Cache, baseDir + @"cache\");
-            Set(Dir.Config, baseDir);
-            Set(Dir.CustomInputDevice, baseDir + @"InputDeviceMappings\custom\");
-            Set(Dir.Database, baseDir + @"database\");
-            Set(Dir.Language, baseDir + @"language\");
-            Set(Dir.Log, baseDir + @"log\");
-            Set(Dir.Plugins, baseDir + @"plugins\");
-            Set(Dir.Skin, baseDir + @"skin\");
-            Set(Dir.Thumbs, baseDir + @"thumbs\");
-            Set(Dir.Weather, baseDir + @"weather\");
-        }
-
-
-        //TODO: Make Get method private, when all external references are replaced by a call to GetFolder or GetFile
-        /// <summary>
-        /// Returns the complete path for the specified MP directory.
-        /// </summary>
-        /// <param name="path">A <see cref="Dir"/> value, indicating the directory to return</param>
-        /// <returns>a string containing the complete path, with trailing backslash</returns>
-        [Obsolete("This method is obsolete and will disappear in the future.  Use GetFolder(Dir path) or GetFile(Dir path, string fileName) instead.",false)]
-        public static string Get(Dir path)
-        {
-            string returnVal = "";
-            if (directories.TryGetValue(path, out returnVal))
-            {
-                return returnVal;
-            }
-            else
-            {
-                return "";
-            }
-        }
-
-        /// <summary>
-        /// Returns the complete path for the specified file in the specified MP directory.
-        /// </summary>
-        /// <param name="directory">A <see cref="Dir"/> value, indicating the directory where the file should be located</param>
-        /// <param name="fileName">The name of the file for which to return the complete path.</param>
-        /// <returns>A string containing the complete path.</returns>
-        public static string GetFile(Dir directory, string fileName)
-        {
-            string dirName = Get(directory);
-            if (!dirName.EndsWith(@"\"))
-            {
-                dirName += @"\";
-            }
-            if (fileName.StartsWith(@"\"))
-            {
-                fileName = fileName.Substring(1);
-            }
-            return dirName + fileName;
-        }
-
-        /// <summary>
-        /// Returns the complete path for the specified MP directory.
-        /// </summary>
-        /// <param name="directory">A <see cref="Dir"/> value, indicating the directory to return</param>
-        /// <returns>a string containing the complete path, without trailing backslash</returns>
-        public static string GetFolder(Dir directory)
-        {
-            string dirName = Get(directory);
-            if (dirName.EndsWith(@"\"))
-                return dirName.Substring(0, dirName.Length - 1);
-            return dirName;
-        }
-
-        private static void Set(Dir path, string value)
-        {
-            value = value.Replace('/', '\\');
-            value = value.Replace(@"\\", @"\");
-            if (!value.EndsWith(@"\"))
-                value += '\\';
-            directories[path]= value;
-        }
-
+    static Config()
+    {
+      directories = new Dictionary<Dir, string>();
+      LoadDirs(AppDomain.CurrentDomain.BaseDirectory);
     }
+
+    /// <summary>
+    /// Read the Directory Configuration from the Config File.
+    /// First we look for the file in MyDocuments of the logged on user. If file is not there or invalid, 
+    /// we use the one from the MediaPortal InstallPath.
+    /// </summary>
+    private static void LoadDirs(string startuppath)
+    {
+      Set(Dir.Base, startuppath + @"\");
+      if (!ReadConfig(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\Team MediaPortal\"))
+      {
+        if (!ReadConfig(Get(Dir.Base)))
+        {
+          LoadDefaultDirs();
+        }
+      }
+    }
+
+    /// <summary>
+    /// Load the Path information from the Config File into the dictionary
+    /// </summary>
+    /// <returns></returns>
+    private static bool ReadConfig(string configDir)
+    {
+      // Make sure the file exists before we try to do any processing
+      string strFileName = configDir + "MediaPortalDirs.xml";
+      if (File.Exists(strFileName))
+      {
+        try
+        {
+          XmlDocument xml = new XmlDocument();
+          xml.Load(strFileName);
+          if (xml.DocumentElement == null)
+          {
+            return false;
+          }
+          XmlNodeList dirList = xml.DocumentElement.SelectNodes("/Config/Dir");
+          if (dirList == null || dirList.Count == 0)
+          {
+            return false;
+          }
+          foreach (XmlNode nodeDir in dirList)
+          {
+            if (nodeDir.Attributes != null)
+            {
+              XmlNode dirId = nodeDir.Attributes.GetNamedItem("id");
+              if (dirId != null && dirId.InnerText != null && dirId.InnerText.Length > 0)
+              {
+                XmlNode path = nodeDir.SelectSingleNode("Path");
+                if (path != null)
+                {
+                  string strPath = path.InnerText;
+                  // Check to see, if the location was specified with an absolute or relative path.
+                  // In case of relative path, prefix it with the startuppath                 
+                  if (!Path.IsPathRooted(path.InnerText))
+                  {
+                    strPath = Get(Dir.Base) + path.InnerText;
+                  }
+                  // See if we got a slash at the end. If not add one.
+                  if (!strPath.EndsWith(@"\"))
+                  {
+                    strPath += @"\";
+                  }
+                  try
+                  {
+                    Set((Dir)Enum.Parse(typeof(Dir), dirId.InnerText), strPath);
+                  }
+                  catch (Exception)
+                  {
+                    return false;
+                  }
+                }
+              }
+            }
+          }
+          return true;
+        }
+        catch (Exception)
+        {
+          return false;
+        }
+      }
+      return false;
+    }
+
+    private static void LoadDefaultDirs()
+    {
+      string baseDir = Get(Dir.Base);
+      Set(Dir.Cache, baseDir + @"cache\");
+      Set(Dir.Config, baseDir);
+      Set(Dir.CustomInputDevice, baseDir + @"InputDeviceMappings\custom\");
+      Set(Dir.Database, baseDir + @"database\");
+      Set(Dir.Language, baseDir + @"language\");
+      Set(Dir.Log, baseDir + @"log\");
+      Set(Dir.Plugins, baseDir + @"plugins\");
+      Set(Dir.Skin, baseDir + @"skin\");
+      Set(Dir.Thumbs, baseDir + @"thumbs\");
+      Set(Dir.Weather, baseDir + @"weather\");
+    }
+
+
+    //TODO: Make Get method private, when all external references are replaced by a call to GetFolder or GetFile
+    /// <summary>
+    /// Returns the complete path for the specified MP directory.
+    /// </summary>
+    /// <param name="path">A <see cref="Dir"/> value, indicating the directory to return</param>
+    /// <returns>a string containing the complete path, with trailing backslash</returns>
+    [Obsolete("This method is obsolete and will disappear in the future.  Use GetFolder(Dir path) or GetFile(Dir path, string fileName) instead.", false)]
+    public static string Get(Dir path)
+    {
+      string returnVal = "";
+      if (directories.TryGetValue(path, out returnVal))
+      {
+        return returnVal;
+      }
+      else
+      {
+        return "";
+      }
+    }
+
+    /// <summary>
+    /// Returns the complete path for the specified file in the specified MP directory.
+    /// </summary>
+    /// <param name="directory">A <see cref="Dir"/> value, indicating the directory where the file should be located</param>
+    /// <param name="fileName">The name of the file for which to return the complete path.</param>
+    /// <returns>A string containing the complete path.</returns>
+    public static string GetFile(Dir directory, string fileName)
+    {
+      return Path.Combine(Get(directory), fileName);
+    }
+
+    /// <summary>
+    /// Returns the complete path for the specified MP directory.
+    /// </summary>
+    /// <param name="directory">A <see cref="Dir"/> value, indicating the directory to return</param>
+    /// <returns>a string containing the complete path, without trailing backslash</returns>
+    public static string GetFolder(Dir directory)
+    {
+      return Path.GetDirectoryName(Get(directory));
+    }
+
+    /// <summary>
+    /// Returns the complete path for the specified sub directory in the specified MP directory
+    /// </summary>
+    /// <param name="directory">A <see cref="Dir"/> value, indicating the directory to return</param>
+    /// <returns>a string containing the complete path, without trailing backslash</returns>
+    public static string GetSubFolder(Dir directory, string subDirectory)
+    {
+      return Path.Combine(Get(directory), subDirectory);
+    }
+
+    /// <summary>
+    /// Checks if a path is set for the specified MP directory.
+    /// </summary>
+    /// <param name="path">A <see cref="Dir"/> value, indicating the directory to return</param>
+    /// <returns>a bool indicating if the directory is set</returns>
+    public static bool IsSet(Dir path)
+    {
+      return directories.ContainsKey(path);
+    }
+
+    private static void Set(Dir path, string value)
+    {
+      if (!Path.IsPathRooted(value) && IsSet(Dir.Base))
+        directories[path] = Path.Combine(Get(Dir.Base), value);
+      else
+        directories[path] = Path.GetFullPath(value);
+    }
+
+  }
 }
