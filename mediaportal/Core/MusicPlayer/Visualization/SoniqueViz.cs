26fb5f8ce31d5abde10dd768f910010b73938848
hwahrmann < hwahrmann@team-mediaportal.com>
2006-10-05 14:19:02 +0000

    Changed the Fading in Sonique Visualisations, so that colors appear correct now

--- mediaportal/Core/MusicPlayer/Visualization/SoniqueViz.cs
+++ mediaportal/Core/MusicPlayer/Visualization/SoniqueViz.cs
@@ -34,193 +34,194 @@ using MediaPortal.GUI.Library;
 
 using MediaPortal.Player;
 
-namespace MediaPortal.Visualization 
+namespace MediaPortal.Visualization
 {
-    public class SoniqueViz : VisualizationBase
+  public class SoniqueViz : VisualizationBase
+  {
+    private int VisChannel = 0;
+
+    public SoniqueViz(VisualizationInfo vizPluginInfo, VisualizationWindow vizCtrl)
+      : base(vizPluginInfo, vizCtrl)
     {
-        private int VisChannel = 0;
-
-        public SoniqueViz(VisualizationInfo vizPluginInfo, VisualizationWindow vizCtrl)
-            : base(vizPluginInfo, vizCtrl)
-        {
-            Log.Info("Visualization Manager: Creating Sonique visualization...");
-            BassVis.BASS_SONIQUEVIS_DestroyFakeSoniqueWnd();
-            BassVis.BASS_SONIQUEVIS_CreateFakeSoniqueWnd();
-            Log.Info("Visualization Manager: Sonique visualization created.");
-        }
-
-        public override bool Initialize()
-        {
-            try
-            {
-                Log.Info("Visualization Manager: Initializing Sonique visualization engine...");
-
-                if (VizPluginInfo == null)
-                {
-                    Log.Error("Visualization Manager: Sonique visualization engine initialization failed! Reason:{0}",
-                        "Missing or invalid VisualizationInfo object.");
-
-                    return false;
-                }
-
-                bool result = SetOutputContext(VisualizationWindow.OutputContextType);
-                _Initialized = result && VisChannel != 0;
-            }
-
-            catch (Exception ex)
-            {
-                Log.Error("Visualization Manager: Sonique visualization engine initialization failed with the following exception {0}", ex);
-                return false;
-            }
-
-            return Initialized;
-        }
-
-        public override bool InitializePreview()
-        {
-            base.InitializePreview();
-            return Initialize();
-        }
-
-        public override void Dispose()
-        {
-            base.Dispose();
-            Close();
-        }
-
-        public override int RenderVisualization()
-        {
-            IntPtr hdc = IntPtr.Zero;
-            Graphics destGraphics = null;
-
-            try
-            {
-                if (VisualizationWindow == null || !VisualizationWindow.Visible)
-                    return 0;
-
-
-                if (VisualizationWindow.InvokeRequired)
-                {
-                    ThreadSafeRenderDelegate d = new ThreadSafeRenderDelegate(RenderVisualization);
-                    VisualizationWindow.Invoke(d);
-                    return 0;
-                }
-
-                int stream = 0;
-
-                if (Bass != null)
-                    stream = (int)Bass.GetCurrentStream();
-
-                if (VisualizationWindow.OutputContextType == OutputContextType.DeviceContext)
-                    hdc = VisualizationWindow.CompatibleDC;
-
-                else if (VisualizationWindow.OutputContextType == OutputContextType.WindowHandle)
-                {
-                    destGraphics = Graphics.FromHwnd(VisualizationWindow.CompatibleDC);
-                    hdc = destGraphics.GetHdc();
-                }
-
-                else
-                    return -1;
-
-                bool result = BassVis.BASS_SONIQUEVIS_Render(VisChannel, stream, hdc);
-
-                if (!result)
-                {
-                    int code = Un4seen.Bass.Bass.BASS_ErrorGetCode();
-
-                    if (code != 0)
-                    {
-                        //Console.WriteLine("BASS_SONIQUEVIS_Render() error: ");
-                    }
-                }
-            }
-
-            catch (Exception)
-            {
-                //Console.WriteLine("Sonique viz caused an exception: {0}", ex.Message);
-            }
-
-            finally
-            {
-                if (destGraphics != null)
-                {
-                    if (hdc != IntPtr.Zero)
-                        destGraphics.ReleaseHdc(hdc);
-
-                    destGraphics.Dispose();
-                }
-            }
-
-            return 1;
-        }
-
-        public override bool Close()
-        {
-            try
-            {
-                if (VisChannel != 0)
-                {
-                    BassVis.BASS_SONIQUEVIS_DestroyFakeSoniqueWnd();
-                    BassVis.BASS_SONIQUEVIS_Free(VisChannel);
-                    VisChannel = 0;
-                }
-
-                return true;
-            }
-
-            catch (Exception ex)
-            {
-                Console.WriteLine("Visualization Manager: Sonique Close caused an exception: {0}", ex.Message);
-                return false;
-            }
-        }
-
-        public override bool WindowChanged(VisualizationWindow vizWindow)
-        {
-            base.WindowChanged(vizWindow);
-
-            if (vizWindow == null)
-                return false;
-
-            bool result = SetOutputContext(VisualizationWindow.OutputContextType);
-            return result; 
-        }
-
-        public override bool WindowSizeChanged(Size newSize)
-        {
-            bool result = SetOutputContext(VisualizationWindow.OutputContextType);
-            return result;
-        }
-
-        public override bool SetOutputContext(VisualizationBase.OutputContextType outputType)
-        {
-            if (VisualizationWindow == null)
-                return false;
-
-            // If width or height are 0 the call to BASS_SONIQUEVIS_CreateVis will fail.  
-            // If width or height are 1 the window is in transition so we can ignore it.
-            if (VisualizationWindow.Width <= 1 || VisualizationWindow.Height <= 1)
-                return false;
-
-            if (VizPluginInfo == null || VizPluginInfo.FilePath.Length == 0 || !File.Exists(VizPluginInfo.FilePath))
-                return false;
-
-            string vizPath = VizPluginInfo.FilePath;
-            string configFile = Path.Combine(Path.GetDirectoryName(vizPath), "vis.ini");
-
-            if (VisChannel != 0)
-            {
-                bool result;
-                result = BassVis.BASS_SONIQUEVIS_Free(VisChannel);
-                VisChannel = 0;
-
-                BassVis.BASS_SONIQUEVIS_DestroyFakeSoniqueWnd();
-            }
-
-            BassVis.BASS_SONIQUEVIS_CreateFakeSoniqueWnd();
-            VisChannel = BassVis.BASS_SONIQUEVIS_CreateVis(vizPath, configFile, BASSVISCreate.BASS_VIS_DEFAULT, VisualizationWindow.Width, VisualizationWindow.Height);
-            return VisChannel != 0;
-        }
+      Log.Info("Visualization Manager: Creating Sonique visualization...");
+      BassVis.BASS_SONIQUEVIS_DestroyFakeSoniqueWnd();
+      BassVis.BASS_SONIQUEVIS_CreateFakeSoniqueWnd();
+      Log.Info("Visualization Manager: Sonique visualization created.");
     }
+
+    public override bool Initialize()
+    {
+      try
+      {
+        Log.Info("Visualization Manager: Initializing Sonique visualization engine...");
+
+        if (VizPluginInfo == null)
+        {
+          Log.Error("Visualization Manager: Sonique visualization engine initialization failed! Reason:{0}",
+              "Missing or invalid VisualizationInfo object.");
+
+          return false;
+        }
+
+        BassVis.BASS_VIS_SetConfig(BASSVISConfig.BASS_VIS_CONFIG_SONIQUE_SLOWFADE, 0);
+        bool result = SetOutputContext(VisualizationWindow.OutputContextType);
+        _Initialized = result && VisChannel != 0;
+      }
+
+      catch (Exception ex)
+      {
+        Log.Error("Visualization Manager: Sonique visualization engine initialization failed with the following exception {0}", ex);
+        return false;
+      }
+
+      return Initialized;
+    }
+
+    public override bool InitializePreview()
+    {
+      base.InitializePreview();
+      return Initialize();
+    }
+
+    public override void Dispose()
+    {
+      base.Dispose();
+      Close();
+    }
+
+    public override int RenderVisualization()
+    {
+      IntPtr hdc = IntPtr.Zero;
+      Graphics destGraphics = null;
+
+      try
+      {
+        if (VisualizationWindow == null || !VisualizationWindow.Visible)
+          return 0;
+
+
+        if (VisualizationWindow.InvokeRequired)
+        {
+          ThreadSafeRenderDelegate d = new ThreadSafeRenderDelegate(RenderVisualization);
+          VisualizationWindow.Invoke(d);
+          return 0;
+        }
+
+        int stream = 0;
+
+        if (Bass != null)
+          stream = (int)Bass.GetCurrentStream();
+
+        if (VisualizationWindow.OutputContextType == OutputContextType.DeviceContext)
+          hdc = VisualizationWindow.CompatibleDC;
+
+        else if (VisualizationWindow.OutputContextType == OutputContextType.WindowHandle)
+        {
+          destGraphics = Graphics.FromHwnd(VisualizationWindow.CompatibleDC);
+          hdc = destGraphics.GetHdc();
+        }
+
+        else
+          return -1;
+
+        bool result = BassVis.BASS_SONIQUEVIS_Render(VisChannel, stream, hdc);
+
+        if (!result)
+        {
+          int code = Un4seen.Bass.Bass.BASS_ErrorGetCode();
+
+          if (code != 0)
+          {
+            //Console.WriteLine("BASS_SONIQUEVIS_Render() error: ");
+          }
+        }
+      }
+
+      catch (Exception)
+      {
+        //Console.WriteLine("Sonique viz caused an exception: {0}", ex.Message);
+      }
+
+      finally
+      {
+        if (destGraphics != null)
+        {
+          if (hdc != IntPtr.Zero)
+            destGraphics.ReleaseHdc(hdc);
+
+          destGraphics.Dispose();
+        }
+      }
+
+      return 1;
+    }
+
+    public override bool Close()
+    {
+      try
+      {
+        if (VisChannel != 0)
+        {
+          BassVis.BASS_SONIQUEVIS_DestroyFakeSoniqueWnd();
+          BassVis.BASS_SONIQUEVIS_Free(VisChannel);
+          VisChannel = 0;
+        }
+
+        return true;
+      }
+
+      catch (Exception ex)
+      {
+        Console.WriteLine("Visualization Manager: Sonique Close caused an exception: {0}", ex.Message);
+        return false;
+      }
+    }
+
+    public override bool WindowChanged(VisualizationWindow vizWindow)
+    {
+      base.WindowChanged(vizWindow);
+
+      if (vizWindow == null)
+        return false;
+
+      bool result = SetOutputContext(VisualizationWindow.OutputContextType);
+      return result;
+    }
+
+    public override bool WindowSizeChanged(Size newSize)
+    {
+      bool result = SetOutputContext(VisualizationWindow.OutputContextType);
+      return result;
+    }
+
+    public override bool SetOutputContext(VisualizationBase.OutputContextType outputType)
+    {
+      if (VisualizationWindow == null)
+        return false;
+
+      // If width or height are 0 the call to BASS_SONIQUEVIS_CreateVis will fail.  
+      // If width or height are 1 the window is in transition so we can ignore it.
+      if (VisualizationWindow.Width <= 1 || VisualizationWindow.Height <= 1)
+        return false;
+
+      if (VizPluginInfo == null || VizPluginInfo.FilePath.Length == 0 || !File.Exists(VizPluginInfo.FilePath))
+        return false;
+
+      string vizPath = VizPluginInfo.FilePath;
+      string configFile = Path.Combine(Path.GetDirectoryName(vizPath), "vis.ini");
+
+      if (VisChannel != 0)
+      {
+        bool result;
+        result = BassVis.BASS_SONIQUEVIS_Free(VisChannel);
+        VisChannel = 0;
+
+        BassVis.BASS_SONIQUEVIS_DestroyFakeSoniqueWnd();
+      }
+
+      BassVis.BASS_SONIQUEVIS_CreateFakeSoniqueWnd();
+      VisChannel = BassVis.BASS_SONIQUEVIS_CreateVis(vizPath, configFile, BASSVISCreate.BASS_VIS_DEFAULT, VisualizationWindow.Width, VisualizationWindow.Height);
+      return VisChannel != 0;
+    }
+  }
 }
