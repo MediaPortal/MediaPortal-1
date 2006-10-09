d8c42f470080faba61f3a3bb9561433b7df4e54e
tourettes <tourettes@team-mediaportal.com>
2006-10-09 06:10:36 +0000

    Code cleanup

--- mediaportal/Filters/SubTrans/source/SubTransform.h
+++ mediaportal/Filters/SubTrans/source/SubTransform.h
@@ -28,7 +28,6 @@
 #include <streams.h>
 #include <initguid.h>
 
-#include "ITSFileSource.h"
 #include "dvbsubs\dvbsubs.h"
 #include "SubdecoderObserver.h"
 
@@ -43,8 +42,6 @@ typedef __int64 int64_t;
 // {591AB987-9689-4c07-846D-0006D5DD2BFD}
 DEFINE_GUID(CLSID_SubTransform, 
 	0x591ab987, 0x9689, 0x4c07, 0x84, 0x6d, 0x0, 0x6, 0xd5, 0xdd, 0x2b, 0xfd);
-
-struct __declspec(uuid("559E6E81-FAC4-4EBC-9530-662DAA27EDC2")) ITSFileSource;
 
 DECLARE_INTERFACE_( IStreamAnalyzer, IUnknown )
 {
@@ -65,6 +62,7 @@ public:
   HRESULT DecideBufferSize( IMemAllocator *pAlloc, ALLOCATOR_PROPERTIES *pProp );
   HRESULT GetMediaType( int iPosition, CMediaType *pMediaType );
   HRESULT Transform( IMediaSample *pIn, IMediaSample *pOut );
+  HRESULT SetMediaType( PIN_DIRECTION direction, const CMediaType *pmt );
 
 	STDMETHODIMP Run( REFERENCE_TIME tStart );
 	STDMETHODIMP Pause();
@@ -76,21 +74,16 @@ public:
   CBasePin * GetPin( int n );
   int GetPinCount();
 
-  HRESULT SetMediaType(PIN_DIRECTION direction, const CMediaType *pmt);
-
-  static CUnknown * WINAPI CreateInstance(LPUNKNOWN pUnk, HRESULT *pHr);
+  static CUnknown * WINAPI CreateInstance( LPUNKNOWN pUnk, HRESULT *pHr );
 
 	void CSubTransform::GetVideoInfoParameters(
-  const VIDEOINFOHEADER *pvih,	// Pointer to the format header
-  BYTE  * const pbData,			// Pointer to first address in buffer
-  DWORD *pdwWidth,				// Returns the width in pixels
-  DWORD *pdwHeight,				// Returns the height in pixels
-  LONG  *plStrideInBytes,			// Add this to a row to get the new row down
-  BYTE **ppbTop,					// Pointer to first byte in top row of pixels
-  bool bYuv );
-
-	
-	HRESULT ConnectToTSFileSource();
+        const VIDEOINFOHEADER *pvih,    // Pointer to the format header
+        BYTE  * const pbData,			      // Pointer to first address in buffer
+        DWORD *pdwWidth,				        // Returns the width in pixels
+        DWORD *pdwHeight,				        // Returns the height in pixels
+        LONG  *plStrideInBytes,			    // Add this to a row to get the new row down
+        BYTE **ppbTop,					        // Pointer to first byte in top row of pixels
+        bool bYuv );
 
 	bool IsValidYUY2( const CMediaType *pmt );
 	bool IsValidUYVY( const CMediaType *pmt );
@@ -105,6 +98,7 @@ public:
 	void Notify();
 
 private:
+  
   HRESULT ProcessFrameUYVY( BYTE *pbInput, BYTE *pbOutput, long *pcbByte );
   HRESULT ProcessFrameYUY2( BYTE *pbInput, BYTE *pbOutput, long *pcbByte );
 	
@@ -116,29 +110,26 @@ private:
 	CSubtitleInputPin*	m_pSubtitlePin;
 	CAudioInputPin*		  m_pAudioPin;
 	
-	CDVBSubDecoder*		m_pSubDecoder;
+	CDVBSubDecoder*		  m_pSubDecoder;
 
-	CCritSec			m_Lock;				// Main renderer critical section
+	CCritSec			m_Lock;				    // Main renderer critical section
   CCritSec			m_ReceiveLock;		// Sublock for received samples
 
-	unsigned char*		m_curSubtitleData;//[720*576*3];
-	ULONGLONG			m_NextSubtitlePTS;
-	ULONGLONG			m_curSubtitlePTS;
-	ULONGLONG			m_ShowDuration;
+	unsigned char*  m_curSubtitleData;//[720*576*3];
+	CSubtitle*      m_pSubtitle;
 
-	ULONGLONG			m_curPTS; 
-	ULONGLONG			m_firstPTS;
-	ULONGLONG			m_PTSdiff;
-
-	CSubtitle*			m_pSubtitle;
+  ULONGLONG m_NextSubtitlePTS;
+	ULONGLONG m_curSubtitlePTS;
+	ULONGLONG m_ShowDuration;
+	ULONGLONG m_curPTS; 
+	ULONGLONG m_firstPTS;
+	ULONGLONG m_PTSdiff;
 
   HBITMAP				m_DibsSub; // dibsection for to-be-scaled subtitle bitmap
   HDC					  m_DC;
   void *				m_pDibBits;
   HGDIOBJ				m_OldObject;
 
-	bool				m_bRenderCurrentSubtitle;
-	bool				m_bSubtitleDiscarded;
-
-	CComQIPtr<ITSFileSource> m_pTSFileSource;
+	bool m_bRenderCurrentSubtitle;
+	bool m_bSubtitleDiscarded;
 };
