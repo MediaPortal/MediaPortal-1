128aaeb37374b90b01802b7e1b03a86d87bab6b4
Broceliande <Broceliande@team-mediaportal.com>
2006-11-06 20:52:49 +0000

    uploaded missing assembly

--- TvEngine3/TVLibrary/Plugins/ConflictsManager/Properties/AssemblyInfo.cs
+++ TvEngine3/TVLibrary/Plugins/ConflictsManager/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("ConflictsManager Plugin")]
+[assembly: AssemblyTitle("ConflictsManager plugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Team Mediaportal")]
