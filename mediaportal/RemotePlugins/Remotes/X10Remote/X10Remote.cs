5c932d85e84f0279702a7097334e3d1544f7ad63
diehard2 <diehard2@team-mediaportal.com>
2006-12-22 01:04:05 +0000

    fixes double click on resume problem w/ x10 remotes.

--- mediaportal/RemotePlugins/Remotes/X10Remote/X10Remote.cs
+++ mediaportal/RemotePlugins/Remotes/X10Remote/X10Remote.cs
@@ -28,6 +28,10 @@ using MediaPortal.GUI.Library;
 using MediaPortal.Util;
 using MediaPortal.Player;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using X10;
+
 
 namespace MediaPortal.InputDevices
 {
@@ -36,7 +40,12 @@ namespace MediaPortal.InputDevices
   /// </summary>
   public class X10Remote
   {
-    X10RemoteForm _x10Form = null;
+
+    X10.X10Interface X10Inter = null;
+    IConnectionPointContainer icpc = null;
+    IConnectionPoint icp = null;
+    X10Sink X10Sink = null;
+    int cookie = 0;
     InputHandler _inputHandler = null;
     bool _controlEnabled = false;
     bool _logVerbose = false;
@@ -47,27 +56,18 @@ namespace MediaPortal.InputDevices
     int _x10Channel = 0;
    
     //This struct stores information needed to tell whether a key is a repeat (bug in X10 after standby)
-    public struct repeatpreventer
-    {
-      public string command;
-      public DateTime time;
-
-      public int span()
-      {
-        TimeSpan span = DateTime.Now - time;
-        return span.Milliseconds;
-      }
-    };
-
-    repeatpreventer preventdoublepress;
+    
+    
 
     public X10Remote()
     {
+      
     }
 
-    public void Init(IntPtr hwnd)
+    public void Init()
     {
-      preventdoublepress = new repeatpreventer();
+      //IX10Interface test = new IX10Interface();
+      
       using (MediaPortal.Profile.Settings xmlreader = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "MediaPortal.xml")))
       {
         _controlEnabled = xmlreader.GetValueAsBool("remote", "X10", false);
@@ -110,19 +110,23 @@ namespace MediaPortal.InputDevices
           else
             Log.Info("X10Remote: Start Other");
         }
-      }
-      if (_x10Form == null)
-      {
         try
         {
-          _x10Form = new X10RemoteForm(new AxX10._DIX10InterfaceEvents_X10CommandEventHandler(this.IX10_X10Command));
+          if (X10Inter == null)
+          {
+            X10Inter = new X10Interface();
+            X10Sink = new X10Sink(_inputHandler, _logVerbose);
+            icpc = (IConnectionPointContainer)X10Inter;
+            Guid IID_InterfaceEvents = typeof(_DIX10InterfaceEvents).GUID;
+            icpc.FindConnectionPoint(ref IID_InterfaceEvents, out icp);
+            icp.Advise(X10Sink, out cookie);
+          }
         }
         catch (System.Runtime.InteropServices.COMException)
         {
-          _controlEnabled = false;
-          Log.Info("X10Remote: Can't initialize");
         }
       }
+      
     }
 
     public void DeInit()
@@ -130,71 +134,14 @@ namespace MediaPortal.InputDevices
       if (!_controlEnabled)
         return;
       
-      if (_x10Form != null)
-      {
-        _x10Form.Close();
-        _x10Form.Dispose();
-        _x10Form = null;
-      }
-
+      
       _inputHandler = null;
 
       if (_logVerbose)
         Log.Info("X10Remote: Stop");
     }
 
-    public void IX10_X10Command(object sender, AxX10._DIX10InterfaceEvents_X10CommandEvent e)
-    {
-      if (_logVerbose)
-      {
-        Log.Info("X10Remote: Command Start --------------------------------------------");
-        Log.Info("X10Remote: e            = {0}", e.ToString());
-        Log.Info("X10Remote: bszCommand   = {0}", e.bszCommand.ToString());
-        Log.Info("X10Remote: eCommand     = {0} - {1}", (int)Enum.Parse(typeof(X10.EX10Command), e.eCommand.ToString()), e.eCommand.ToString());
-        Log.Info("X10Remote: eCommandType = {0}", e.eCommandType.ToString());
-        Log.Info("X10Remote: eKeyState    = {0}", e.eKeyState.ToString());
-        Log.Info("X10Remote: lAddress     = {0}", e.lAddress.ToString());
-        Log.Info("X10Remote: lSequence    = {0}", e.lSequence.ToString());
-        Log.Info("X10Remote: varTimestamp = {0}", e.varTimestamp.ToString());
-        Log.Info("X10Remote: Command End ----------------------------------------------");
-     }
-
-      if (e.eKeyState.ToString() == "X10KEY_ON" || e.eKeyState.ToString() == "X10KEY_REPEAT")
-      {
-        if (_x10UseChannelControl && (e.lAddress != _x10Channel))
-        {
-          return;
-        }
-
-        //Resuming from standby leads to double key presses. This is difficult to track down, 
-        //but trivial to account for. It is unclear whether this an MP problem or a X10 problem
-
-        if ((e.eCommand.ToString() == preventdoublepress.command))
-        {
-          if (preventdoublepress.span() < 150)
-          {
-            Log.Info("X10 debug - Caught double press - {0} ms", preventdoublepress.span());
-            return;
-          }
-          else
-          {
-            Log.Info("X10 debug - Missed double press - {0} ms", preventdoublepress.span());
-          }
-
-        }
-
-        if (_inputHandler.MapAction((int)Enum.Parse(typeof(X10.EX10Command), e.eCommand.ToString())))
-        {
-          if (_logVerbose) Log.Info("X10Remote: Action mapped");
-          preventdoublepress.command = e.eCommand.ToString();
-          preventdoublepress.time = DateTime.Now;
-        }
-        else
-        {
-          if (_logVerbose) Log.Info("X10Remote: Action not mapped");
-        }
-      }
-    }
+   
 
   }
 }
