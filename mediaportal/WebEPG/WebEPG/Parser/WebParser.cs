156f69f8f69b4b942b7bb8b7847f7a6d860d18fd
james <james@team-mediaportal.com>
2007-02-11 14:22:31 +0000

    Added comments

--- mediaportal/WebEPG/WebEPG/Parser/WebParser.cs
+++ mediaportal/WebEPG/WebEPG/Parser/WebParser.cs
@@ -46,30 +46,49 @@ namespace MediaPortal.WebEPG.Parser
     #endregion
 
     #region Constructors/Destructors
+    /// <summary>
+    /// Initializes a new instance of the <see cref="WebParser"/> class.
+    /// </summary>
+    /// <param name="webTemplate">The web template.</param>
     public WebParser(WebParserTemplate webTemplate)
     {
+      // Store the template
       _template = webTemplate;
+
+      // Get default template -> currently only a default is supported
+      // In the future template per channel ID can be added.
       HtmlParserTemplate listingTemplate = _template.GetTemplate("default");
       _listingPreference = _template.GetPreference("default");
+
+      // Create dictionary for month strings
       Dictionary<string, int> monthsDict = null;
       if (_template.months != null)
       {
+        // template contains months list -> load into dictionary
         monthsDict = new Dictionary<string, int>();
         for (int i = 0; i < _template.months.Length; i++)
         {
           monthsDict.Add(_template.months[i], i + 1); ;
         }
       }
-      _listingParser = new HtmlParser(listingTemplate, typeof(ProgramData), monthsDict); //_template.months);
-      _sublinkParser = null;
 
+
+      // create new Html Parser using default template
+      _listingParser = new HtmlParser(listingTemplate, typeof(ProgramData), monthsDict);
+
+      // setup sublink parser if template and config exists
+      _sublinkParser = null;
       if (_template.sublinks != null && _template.sublinks.Count > 0)
       {
+        // Load first sublink template -> only one supported now
+        // future support for multiple sublinks possible
         SublinkInfo sublink = _template.sublinks[0];
         HtmlParserTemplate sublinkTemplate = _template.GetTemplate(sublink.template);
         _sublinkPreference = _template.GetPreference(sublinkTemplate.Name);
+
         if (sublinkTemplate != null)
         {
+          // create sublink parser using template
           _sublinkParser = new HtmlParser(sublinkTemplate, typeof(ProgramData));
           _sublinkMatch = sublink.search;
           _sublinkRequest = sublink.url;
@@ -79,15 +98,22 @@ namespace MediaPortal.WebEPG.Parser
     #endregion
 
     #region Public Methods
+    /// <summary>
+    /// Gets the data from a linked page
+    /// </summary>
+    /// <param name="data">ref to ProgramData</param>
+    /// <returns>true if data was correctly parsed otherwise false</returns>
     public bool GetLinkedData(ref ProgramData data)
     {
+      // check if the Data has a sublink request set
       if (data.SublinkRequest != null)
       {
+        // find template matches
         int count = _sublinkParser.ParseUrl(data.SublinkRequest);
 
         if (count > 0)
         {
-          // get first match
+          // get first match -> only the first match is supported for sublink templates
           ProgramData subdata = (ProgramData)_sublinkParser.GetData(0);
           if (subdata != null)
           {
@@ -104,6 +130,11 @@ namespace MediaPortal.WebEPG.Parser
     #endregion
 
     #region IParser Implementations
+    /// <summary>
+    /// Parses a site for a given URL.
+    /// </summary>
+    /// <param name="site">The request for the site to be parsed</param>
+    /// <returns>number of instances of the tempate found</returns>
     public int ParseUrl(HTTPRequest site)
     {
       if (_sublinkParser != null && _sublinkRequest == null)
@@ -111,8 +142,16 @@ namespace MediaPortal.WebEPG.Parser
       return _listingParser.ParseUrl(site);
     }
 
+    /// <summary>
+    /// Gets the data.
+    /// </summary>
+    /// <param name="index">The index.</param>
+    /// <returns></returns>
     public IParserData GetData(int index)
     {
+      // Perform any search and remove requests
+      // Searches can search over the whole data 
+      // optionally removing text so it will not be parsed with the main data
       ProgramData searchData = null;
       if (_template.searchList != null && _template.searchList.Count > 0)
       {
@@ -126,12 +165,20 @@ namespace MediaPortal.WebEPG.Parser
         }
       }
 
+      // Get the parsed data at index
       ProgramData data = ((ProgramData)_listingParser.GetData(index));
       if (data != null)
       {
+        // Set the data preference -> important for merging data (eg data from sublink page)
         data.Preference = new DataPreference(_listingPreference);
+
+        // If search data exists merge.
         if (searchData != null)
           data.Merge(searchData);
+
+        // If there is a sublink parser, check for a matching sublink
+        // sublink is not parsed here, because that may not be required 
+        // the URL for the sublink will be built and stored for future use see GetLinkedData()
         if (_sublinkParser != null)
         {
           HTTPRequest sublinkRequest = new HTTPRequest(_sublinkRequest);
