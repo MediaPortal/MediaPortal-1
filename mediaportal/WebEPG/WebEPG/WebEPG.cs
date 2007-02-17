3935268296444c33067198b6519bfe10cf740f5d
james <james@team-mediaportal.com>
2007-02-17 07:31:59 +0000

    Version 2.4.2

--- mediaportal/WebEPG/WebEPG/WebEPG.cs
+++ mediaportal/WebEPG/WebEPG/WebEPG.cs
@@ -97,7 +97,7 @@ namespace MediaPortal.EPG
       // get logging service
       _log = GlobalServiceProvider.Get<ILog>();
 
-      _log.Info(LogType.WebEPG, "WebEPG: Version 2.4.1");
+      _log.Info(LogType.WebEPG, "WebEPG: Version 2.4.2");
       // set config directories and files.
       _configFile = configFile; 
       _xmltvDirectory = xmltvDirectory;
