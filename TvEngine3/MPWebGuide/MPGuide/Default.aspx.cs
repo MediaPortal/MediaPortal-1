4c3f4a28c9b44a6be96230fd2b794d506e203271
yamp <yamp@team-mediaportal.com>
2007-03-01 06:38:23 +0000

    MP Web guide for tv server

---
+++ TvEngine3/MPWebGuide/MPGuide/Default.aspx.cs
@@ -0,0 +1,284 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using TvDatabase;
+using TvLibrary;
+using TvLibrary.Interfaces;
+using TvLibrary.Channels;
+using Gentle.Common;
+using Gentle.Framework;
+
+public partial class _Default : System.Web.UI.Page
+{
+  const int PIX_PER_MINUTE = 5;
+  protected void Page_Load(object sender, EventArgs e)
+  {
+    if (!Page.IsPostBack)
+    {
+      UpdateGuide();
+    }
+  }
+  void UpdateGuide()
+  {
+    SqlBuilder sb = new SqlBuilder(StatementType.Select, typeof(Channel));
+    sb.AddOrderByField(true, "sortOrder");
+    SqlStatement stmt = sb.GetStatement(true);
+    IList channels = ObjectFactory.GetCollection(typeof(Channel), stmt.Execute());
+    List<Channel> tvChannels = new List<Channel>();
+
+    foreach (Channel channel in channels)
+    {
+      if (channel.IsTv && channel.VisibleInGuide)
+      {
+        tvChannels.Add(channel);
+      }
+    }
+    UpdateGuide(tvChannels);
+  }
+  void UpdateGuide(List<Channel> tvChannels)
+  {
+    DateTime now;
+    if (Session["currentTime"] == null)
+    {
+      now = DateTime.Now;
+      Session["currentTime"] = now;
+    }
+    now = (DateTime)Session["currentTime"];
+
+    int min = now.Minute;
+    if (min < 30) min = 0;
+    else min = 30;
+    now = now.AddMinutes(-now.Minute + min);
+    now = now.AddSeconds(-now.Second);
+    now = now.AddMilliseconds(-now.Millisecond);
+
+
+    DateTime dt = new DateTime(2000, 1, 1, 0, 0, 0);
+    DateTime dtEnd = dt.AddDays(1);
+    while (dt < dtEnd)
+    {
+      dropDownTime.Items.Add(dt.ToShortTimeString());
+      if (dt.Hour == now.Hour && dt.Minute == now.Minute) dropDownTime.SelectedIndex = dropDownTime.Items.Count - 1;
+      dt = dt.AddMinutes(30);
+    }
+    DateTime dateNow = DateTime.Now;
+    DateTime dateEnd = now.AddDays(14);
+    while (dateNow < dateEnd)
+    {
+      dropDownDate.Items.Add(dateNow.ToLongDateString());
+      if (dt.Date == now.Date) dropDownDate.SelectedIndex = dropDownDate.Items.Count - 1;
+      dateNow = dateNow.AddDays(1);
+    }
+    spanClock.InnerText = DateTime.Now.ToShortTimeString();
+    TvBusinessLayer layer = new TvBusinessLayer();
+
+
+    DateTime end = now.AddHours(2);
+    Dictionary<int, List<Program>> programs = layer.GetProgramsForAllChannels(now, end, tvChannels);
+
+    List<Program> headers = new List<Program>();
+    Channel ch = new Channel(now.ToShortDateString(), false, true, 0, DateTime.MinValue, false, DateTime.MinValue, 0, false, "", false);
+    headers.Add(new Program(1, now.AddMinutes(0), now.AddMinutes(30), now.ToShortTimeString(), "", "", false));
+    headers.Add(new Program(1, now.AddMinutes(30), now.AddMinutes(60), now.AddMinutes(30).ToShortTimeString(), "", "", false));
+    headers.Add(new Program(1, now.AddMinutes(60), now.AddMinutes(90), now.AddMinutes(60).ToShortTimeString(), "", "", false));
+    headers.Add(new Program(1, now.AddMinutes(90), now.AddMinutes(120), now.AddMinutes(90).ToShortTimeString(), "", "", false));
+    ShowGuideForChannel(0, now, end, ch, headers);
+    int count = 0;
+    foreach (Channel channel in tvChannels)
+    {
+      if (programs.ContainsKey(channel.IdChannel))
+      {
+        ShowGuideForChannel(count + 1, now, end, channel, programs[channel.IdChannel]);
+        count++;
+      }
+      //if (count > 15) break;
+    }
+  }
+
+  void ShowGuideForChannel(int nr, DateTime startTime, DateTime endTime, Channel channel, List<Program> programs)
+  {
+    HtmlTableRow row = new HtmlTableRow();
+    AddChannelRow(nr, startTime, endTime, channel, row, programs);
+    tableGuide.Rows.Add(row);
+  }
+  void AddChannelRow(int nr, DateTime now, DateTime end, Channel channel, HtmlTableRow row, List<Program> programs)
+  {
+    HtmlTableCell cellBase = new HtmlTableCell();
+    cellBase.Width = "167";
+    cellBase.BgColor = "#0d4798";
+
+    HtmlTable subTable = new HtmlTable();
+    subTable.Width = "100%";
+
+    HtmlTableRow subTableRow = new HtmlTableRow();
+    HtmlTableCell td1 = new HtmlTableCell();
+    HtmlTableCell td2 = new HtmlTableCell();
+    td1.Width = "30";
+    td2.Align = "left";
+    if (nr == 0)
+    {
+      td1.InnerHtml = "&nbsp";
+      td2.InnerHtml = String.Format("<nobr><span class=\"guide_title_text\">{0}</span></nobr>", channel.Name);
+    }
+    else
+    {
+      td1.InnerHtml = String.Format("<span class=\"grid_channel_num\"  style=\"cursor: pointer\">{0}</span>", nr);
+      td2.InnerHtml = String.Format("<nobr><A class=grid_channel style=\"CURSOR: pointer\" href=\"ShowEpg.aspx?id={0}\">{1}</A></nobr>", channel.IdChannel, channel.Name);
+    }
+    subTableRow.Cells.Add(td1);
+    subTableRow.Cells.Add(td2);
+    subTable.Rows.Add(subTableRow);
+
+    cellBase.Controls.Add(subTable);
+
+    row.Cells.Add(cellBase);
+    AddPrograms(nr, now, end, row, programs);
+  }
+
+  void AddPrograms(int nr, DateTime now, DateTime end, HtmlTableRow row, List<Program> programs)
+  {
+    //695 = 120min = 24x5min
+    //
+    int span = 0;
+    TimeSpan left = new TimeSpan();
+    int count = 0;
+    int cellCount = 0;
+    foreach (Program program in programs)
+    {
+      DateTime startTime = program.StartTime;
+      if (startTime < now) startTime = now;
+      DateTime endTime = program.EndTime;
+      startTime = startTime - left;
+      if (endTime > end) endTime = end;
+      TimeSpan ts = endTime - startTime;
+      HtmlTableCell cellBase = new HtmlTableCell();
+      if (DateTime.Now >= program.StartTime && DateTime.Now <= program.EndTime && nr > 0)
+      {
+        cellBase.Attributes.Add("class", "grid_kids");
+      }
+      else
+      {
+        cellBase.Attributes.Add("class", "grid_default");
+      }
+      cellBase.Attributes.Add("title", String.Format("{0} {1}-{2}\r\n{3}", program.Title, program.StartTime.ToShortTimeString(), program.EndTime.ToShortTimeString(), program.Description));
+      cellBase.ColSpan = (int)((ts.TotalMinutes) / 5.0f);
+      if (count == programs.Count - 1)
+        cellBase.ColSpan = 24 - span;
+      left = ts;
+      left -= new TimeSpan(0, cellBase.ColSpan * 5, 0);
+      count++;
+      if (cellBase.ColSpan == 0) continue;
+
+      HtmlTable subTable = new HtmlTable();
+      subTable.CellSpacing = 0;
+      subTable.CellPadding = 0;
+      subTable.Width = "100%";
+      subTable.Border = 0;
+
+      HtmlTableRow subRow = new HtmlTableRow();
+      subRow.Style.Add("height", "22");
+      HtmlTableCell td1 = new HtmlTableCell();
+      HtmlTableCell td2 = new HtmlTableCell();
+      if (program.StartTime < now)
+      {
+        td1.InnerHtml = "<img height=\"25\" src=\"images/leftcontinue.gif\" width=\"12\">";
+      }
+      else
+      {
+        if (cellCount == 0)
+          td1.InnerHtml = "<img height=\"25\" src=\"images/leftblock.gif\" width=\"12\">";
+        else
+          td1.InnerHtml = "<img height=\"25\" src=\"images/leftblock.gif\" width=\"2\">";
+      }
+      cellCount++;
+      td2.VAlign = "center";
+      td2.Width = "100%";
+      int length = (int)(((float)cellBase.ColSpan) * 4f);
+      if (length > program.Title.Length) length = program.Title.Length;
+      string title = "";
+      if (length > 3)
+      {
+        if (length != program.Title.Length)
+          title = program.Title.Substring(0, length - 3) + "...";
+        else
+          title = program.Title.Substring(0, length);
+      }
+      span += cellBase.ColSpan;
+      //title = ".";
+      if (nr == 0)
+      {
+        td2.InnerHtml = String.Format("<nobr>&nbsp;<A class=guide_title_text>{0}</A></nobr>", title);
+      }
+      else
+      {
+        td2.InnerHtml = String.Format("<nobr>&nbsp;<A class=white>{0}</A></nobr>", title);
+      }
+      subRow.Cells.Add(td1);
+      subRow.Cells.Add(td2);
+      if (program.EndTime > end)
+      {
+        HtmlTableCell td3 = new HtmlTableCell();
+        td3.InnerHtml = "<img height=\"25\" src=\"images/rightcontinue.gif\" width=\"12\">";
+        subRow.Cells.Add(td3);
+      }
+
+      subTable.Rows.Add(subRow);
+
+      cellBase.Controls.Add(subTable);
+      row.Cells.Add(cellBase);
+    }
+  }
+  protected void idForward_Click(object sender, EventArgs e)
+  {
+    DateTime now;
+    if (Session["currentTime"] == null)
+    {
+      now = DateTime.Now;
+      Session["currentTime"] = now;
+    }
+    now = (DateTime)Session["currentTime"];
+    now = now.AddMinutes(30);
+    Session["currentTime"] = now;
+    UpdateGuide();
+  }
+  protected void idBack_Click(object sender, EventArgs e)
+  {
+    DateTime now;
+    if (Session["currentTime"] == null)
+    {
+      now = DateTime.Now;
+      Session["currentTime"] = now;
+    }
+    now = (DateTime)Session["currentTime"];
+    now = now.AddMinutes(-30);
+    Session["currentTime"] = now;
+    UpdateGuide();
+  }
+  protected void dropDownTime_SelectedIndexChanged(object sender, EventArgs e)
+  {
+    DateTime now = (DateTime)Session["currentTime"];
+    now = now.AddHours(-now.Hour);
+    now = now.AddMinutes(-now.Minute);
+    now=now.AddMinutes(dropDownTime.SelectedIndex * 30);
+    Session["currentTime"] = now;
+    UpdateGuide();
+
+  }
+  protected void dropDownDate_SelectedIndexChanged(object sender, EventArgs e)
+  {
+    DateTime now = (DateTime)Session["currentTime"];
+    DateTime dateNow = DateTime.Now;
+    dateNow=dateNow.AddDays(dropDownDate.SelectedIndex);
+    now = new DateTime(dateNow.Year, dateNow.Month, dateNow.Day, now.Hour, now.Minute, 0);
+    Session["currentTime"] = now;
+    UpdateGuide();
+
+  }
+}
---
+++ TvEngine3/MPWebGuide/MPGuide/script/script.js
@@ -0,0 +1,342 @@
+﻿// JScript File
+
+		var swapping=false;
+		function handleButton(obj,on,css)
+		{
+		   
+		    //info.innerHTML=obj.name + " - " + on + " - " + obj.style.visibility;
+		    if(!on)
+			{
+			
+				if(outTimer==null || outItem_name!=obj)
+				{
+					outItem_name=obj;
+					//oUPnP.addToLog("handleMouseItem (" + item_type + " - " + which_event + " - " + item_name + " - " + window.event.srcElement.tagName );
+					outTimer=window.setTimeout("doSwap('" + obj + "','" + on + "','" + css + "')",75);
+				}
+			}
+			else
+			{
+			    if(outTimer!=null && outItem_name==obj)
+				{
+					//oUPnP.addToLog("cleared timer");
+					window.clearTimeout(outTimer);
+					outItem_name="";
+				}
+				
+		            inTimer=window.setTimeout("doSwap('" + obj + "','true','" + css + "')",1);
+		       
+		      }
+		}
+		
+		
+        function showWait()
+        {
+            try
+            {
+                wait_div.style.display="block";
+            }
+             catch(e){
+             try
+             {
+                parent.wait_div.style.display="block";
+             }
+             catch(e){}
+             }
+        }
+        function hideWait()
+        {
+            try
+            {
+                wait_div.style.display="none";
+            }
+             catch(e){
+             try
+             {
+                parent.wait_div.style.display="none";
+             }
+             catch(e){}
+             }
+        }
+		
+		var outTimer=null;
+		var inTimer=null;
+	    var outItem_name="";
+		
+		function doSwap(obj,on,css)
+		{
+		//alert(obj);
+		    if(!swapping)
+		    {
+		        swapping=true;
+		        try{
+		            //eval('document.all.td_' + obj).style.filter="progid:DXImageTransform.Microsoft.Fade(duration=.2)";
+		            //eval('document.all.td_' + obj).filters[0].Apply();
+		        }catch(e){}
+		        if (on=='true')
+		        {
+		            try{eval('document.all.text_' + obj).className=css + '_text_on';}catch(e){}
+		            try{eval('document.all.over_image_' + obj).style.visibility='visible';}catch(e){}
+		        }
+		        else
+		        {
+		            try{eval('document.all.over_image_' + obj).style.visibility='hidden';}catch(e){}
+		            try{eval('document.all.text_' + obj).className=css + '_text_off';}catch(e){}
+		            
+		        }
+		         try{
+		        //eval('document.all.td_' + obj).filters[0].Play();
+		        }catch(e){}
+		        swapping=false;
+		     }
+		}
+
+
+function highLight(obj, on)
+{
+    if(on)
+    {
+        obj.style.backgroundImage='url(/images/cell_over.png)';
+    }
+    else
+    {
+        obj.style.backgroundImage='none';
+    }
+}
+
+
+//page clicks
+
+function loadInfo(type,args)
+{
+
+    info_row.style.display="block";
+    info_row.style.visibility="visible";
+    info_close_button.style.display="block";
+    if(type=="music")
+    {
+        document.all.info_frame.style.height="225px";
+        document.all.info_frame.src="info_music.aspx?" + args
+       
+    }
+    else if(type=="recorded")
+    {
+    document.all.info_frame.style.height="155px";
+        document.all.info_frame.src='info_tv.aspx?type=recorded&id=' + args;
+    }
+    else if(type=="scheduled")
+    {
+        document.all.info_frame.style.height="155px";
+        document.all.info_frame.src='info_tv.aspx?type=scheduled&id=' + args;
+    }
+    else if(type=="new_recording")
+    {
+        document.all.info_frame.style.height="155px";
+        document.all.info_frame.src='info_tv_edit.aspx?type=new_recording';
+    }
+    else if(type=="entry")
+    {
+        document.all.info_frame.style.height="155px";
+        document.all.info_frame.src='info_tv.aspx?type=entry&id=' + args;
+    }
+    else if(type=="picture")
+    {
+        document.all.info_frame.style.height="225px";
+        //alert(URLEncode(args));
+        document.all.info_frame.src='info_picture.aspx?path=' + args; //URLEncode(args);
+    }
+    else if(type=="video")
+    {
+        document.all.info_frame.style.height="165px";
+        //alert(URLEncode(args));
+        document.all.info_frame.src='info_video.aspx?path=' + args; //URLEncode(args);
+    }
+    //window.setTimeout("document.all.info_frame.style.visibility='visible';",50);
+     resizeScroller();
+}
+
+	
+	function closeInfo()
+	{
+	    if(info_row.style.display=="block")
+	    {
+	        info_row.style.visibility="hidden";
+	        //history.back();
+	        document.all.info_frame.src='/blank.htm';
+	        document.all.info_frame.style.height="0px";
+	        info_close_button.style.display="none";
+		    //
+    		
+		    resizeScroller();
+		    
+		 }
+	}
+
+
+
+//draggable stuff
+
+var oActiveSel="";
+	var topZ = 1;
+	function fxPoint(oPoint){
+	if(event.srcElement.tagName!="SELECT"){
+		oPoint=event.srcElement;
+		//oLab1.innerText="Point 1: " + oPoint.tagName;
+		}
+	}
+	
+	function fxTrapSelect(){
+		event.returnValue=false;
+	}
+	
+	function fxSetCapture(oPoint){
+		var cmp=oPoint.componentFromPoint(event.clientX,event.clientY);
+		if (cmp.indexOf("scrollbar")==-1){
+		if(event.y < oPoint.offsetTop+35){
+			topZ++;
+			if(oPoint.style.position!='absolute')
+			{
+			    oPoint.style.position='absolute';
+			    oPoint.style.left=oPoint.offsetLeft-oPoint.offsetWidth/2;
+			}
+			oPoint.style.zIndex=topZ;
+			oPoint.setCapture(true);
+			oPoint.dx=event.clientX - oPoint.offsetLeft;// + oPoint.offsetWidth;
+			oPoint.dy=event.clientY - oPoint.offsetTop;
+			
+			oPoint.isMoving=true;
+			if(oActiveSel){
+				oLatitude.style.display="none";
+				oLongitude.style.display="none";
+			}
+		}}
+	}
+		
+	function fxMove(oPoint){
+		//alert(oPoint);
+		if(oPoint.isMoving){
+			
+			offsetleft=0;
+			offsettop=0;
+			offsetright=0;
+			offsetbottom=0;
+		
+			var iNewX=event.x - oPoint.dx;
+			var iNewY=event.y - oPoint.dy;
+			
+			if (iNewX+oPoint.offsetWidth+25>document.body.offsetWidth )
+			{
+			    iNewX=document.body.offsetWidth-oPoint.offsetWidth-25;
+			}
+			if (iNewX<0){iNewX=0;}
+			if (iNewY+oPoint.offsetHeight+5>document.body.offsetHeight )
+			{
+			    iNewY=document.body.offsetHeight-oPoint.offsetHeight-5;
+			}
+			if (iNewY<0){iNewY=0;}
+			
+			oPoint.style.left=iNewX;
+			oPoint.style.top=iNewY;
+		}
+	}
+	function fxReleaseCapture(oPoint){
+		//oPoint.style.zIndex=10
+		if(oActiveSel){
+			oActiveSel.selectedIndex=iLastIndex;
+			fxCalcPoints(oActiveSel);
+		}
+		
+		document.releaseCapture();
+		oPoint.isMoving=false;
+	}
+	
+	
+// ====================================================================
+//       URLEncode and URLDecode functions
+//
+// Copyright Albion Research Ltd. 2002
+// http://www.albionresearch.com/
+//
+// You may copy these functions providing that 
+// (a) you leave this copyright notice intact, and 
+// (b) if you use these functions on a publicly accessible
+//     web site you include a credit somewhere on the web site 
+//     with a link back to http://www.albionresarch.com/
+//
+// If you find or fix any bugs, please let us know at albionresearch.com
+//
+// SpecialThanks to Neelesh Thakur for being the first to
+// report a bug in URLDecode() - now fixed 2003-02-19.
+// ====================================================================
+function URLEncode(val)
+{
+	// The Javascript escape and unescape functions do not correspond
+	// with what browsers actually do...
+	var SAFECHARS = "0123456789" +					// Numeric
+					"ABCDEFGHIJKLMNOPQRSTUVWXYZ" +	// Alphabetic
+					"abcdefghijklmnopqrstuvwxyz" +
+					"-_.!~*'()";					// RFC2396 Mark characters
+	var HEX = "0123456789ABCDEF";
+
+	var plaintext = val;
+	var encoded = "";
+	for (var i = 0; i < plaintext.length; i++ ) {
+		var ch = plaintext.charAt(i);
+	    if (ch == " ") {
+		    encoded += "+";				// x-www-urlencoded, rather than %20
+		} else if (SAFECHARS.indexOf(ch) != -1) {
+		    encoded += ch;
+		} else {
+		    var charCode = ch.charCodeAt(0);
+			if (charCode > 255) {
+			    alert( "Unicode Character '" 
+                        + ch 
+                        + "' cannot be encoded using standard URL encoding.\n" +
+				          "(URL encoding only supports 8-bit characters.)\n" +
+						  "A space (+) will be substituted." );
+				encoded += "+";
+			} else {
+				encoded += "%";
+				encoded += HEX.charAt((charCode >> 4) & 0xF);
+				encoded += HEX.charAt(charCode & 0xF);
+			}
+		}
+	} // for
+
+	//document.URLForm.F2.value = encoded;
+	return encoded;
+};
+
+function URLDecode( )
+{
+   // Replace + with ' '
+   // Replace %xx with equivalent character
+   // Put [ERROR] in output if %xx is invalid.
+   var HEXCHARS = "0123456789ABCDEFabcdef"; 
+   var encoded = document.URLForm.F2.value;
+   var plaintext = "";
+   var i = 0;
+   while (i < encoded.length) {
+       var ch = encoded.charAt(i);
+	   if (ch == "+") {
+	       plaintext += " ";
+		   i++;
+	   } else if (ch == "%") {
+			if (i < (encoded.length-2) 
+					&& HEXCHARS.indexOf(encoded.charAt(i+1)) != -1 
+					&& HEXCHARS.indexOf(encoded.charAt(i+2)) != -1 ) {
+				plaintext += unescape( encoded.substr(i,3) );
+				i += 3;
+			} else {
+				alert( 'Bad escape combination near ...' + encoded.substr(i) );
+				plaintext += "%[ERROR]";
+				i++;
+			}
+		} else {
+		   plaintext += ch;
+		   i++;
+		}
+	} // while
+   document.URLForm.F1.value = plaintext;
+   return false;
+};
