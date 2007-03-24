0f50b84468f11cb36ecf96a72e5bdf86bbd4094d
infinite-loop <infinityloop@team-mediaportal.com>
2007-03-24 12:57:09 +0000

    example of Homescreen (created with MS Expression Blend) - listbox is scroll-able - buttons are click-able - all Buttons have MouseOver and IsPressed effects - demonstrates how to use triggers for certain events i.e --- display hoover images only when mouse is over a listbutton --- pageload animation

---
+++ Infinity/mytv/Homescreen/InfinityStart.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+
+namespace MediaPortal
+{
+	public partial class InfinityStart
+	{
+		public InfinityStart()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
