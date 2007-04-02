d72036098e63cb3688a3bf0c75b7084389826ec3
joedalton <joedalton@team-mediaportal.com>
2007-04-02 21:47:30 +0000

    Fixed test plugin Step 1 to new project structure

---
+++ Infinity/ProjectInfinity.Interface/Core.cs
@@ -0,0 +1,190 @@
+using System;
+using System.ComponentModel;
+using System.Windows;
+using ProjectInfinity.Messaging;
+using ProjectInfinity.Messaging.SystemMessages;
+using ProjectInfinity.Navigation;
+using ProjectInfinity.Plugins;
+using ProjectInfinity.Themes;
+
+namespace ProjectInfinity
+{
+  /// <summary>
+  /// This is the main ProjectInfinity aplication
+  /// </summary>
+  public class Core : Application
+  {
+    #region Messages
+
+    /// <summary>
+    /// Occurs when ProjectInfinity is starting up.
+    /// </summary>
+    [MessagePublication(typeof (Startup))]
+    public new event EventHandler Startup;
+
+    /// <summary>
+    /// Occurs when the ProjectInfinity core has finished starting up and is ready to show the
+    /// main screen.
+    /// </summary>
+    [MessagePublication(typeof (StartupComplete))]
+    public event EventHandler StartupComplete;
+
+    /// <summary>
+    /// <para>Occurs when the operating system is about to end the users' session (and thus shut down ProjectInfinity)</para>
+    /// <para>The passed <see cref="SessionEndingCancelEventArgs"/> contain the reason why the session is ending.</para>
+    /// </summary>
+    /// <remarks>This is a cancelable event.</remarks>
+    [MessagePublication(typeof (SessionEnding))]
+    public new event SessionEndingCancelEventHandler SessionEnding;
+
+    /// <summary>
+    /// Occurs when ProjectInfinity is about to shutdown.
+    /// </summary>
+    /// <remarks>This is a cancelable event.</remarks>
+    [MessagePublication(typeof (BeforeShutdown))]
+    public event CancelEventHandler BeforeShutdown;
+
+    [MessagePublication(typeof (Shutdown))]
+    public new event EventHandler Shutdown;
+
+    [MessagePublication(typeof (ShutdownComplete))]
+    public event EventHandler ShutdownComplete;
+
+    /// <summary>
+    /// Occurs when ProjectInfinity becomes the foreground application.
+    /// </summary>
+    [MessagePublication(typeof (Activated))]
+    public new event EventHandler Activated;
+
+    /// <summary>
+    /// Occurs when ProjectInfinity stops being the foreground application.
+    /// </summary>
+    [MessagePublication(typeof (Deactivated))]
+    public new event EventHandler Deactivated;
+
+    #endregion
+
+    public Core()
+    {
+      //Get the messagebroker and register ourselfs to it.
+      //The messagebroker will inspect all our public events an look for the
+      //MessagePublicationAttribute on them.  It will automatically add its eventhandler
+      //to all such events it finds.
+      IMessageBroker msgBroker = ServiceScope.Get<IMessageBroker>();
+      msgBroker.Register(this);
+    }
+
+    private void DoStart()
+    {
+      //notify our own subscribers (through the message broker)
+      OnStartup(new EventArgs());
+
+      //Start the plugins
+      ServiceScope.Get<IPluginManager>().StartAll();
+      ServiceScope.Get<IThemeManager>().SetDefaultTheme();
+      INavigationService navigation = ServiceScope.Get<INavigationService>();
+
+      navigation.Closing += new CancelEventHandler(mainWindow_Closing);
+      try
+      {
+        OnStartupComplete(EventArgs.Empty);
+
+
+        //navigation.Navigate(startupUri);
+        Run(navigation.GetWindow());
+        OnShutdown(EventArgs.Empty);
+      }
+      finally
+      {
+        navigation.Closing -= new CancelEventHandler(mainWindow_Closing);
+      }
+      OnShutdownComplete(EventArgs.Empty);
+    }
+
+    protected virtual void OnStartup(EventArgs e)
+    {
+      if (Startup != null)
+      {
+        Startup(this, e);
+      }
+    }
+
+    #region Message Sending
+
+    protected virtual void OnStartupComplete(EventArgs e)
+    {
+      if (StartupComplete != null)
+      {
+        StartupComplete(this, e);
+      }
+    }
+
+    protected virtual void OnBeforeShutdown(CancelEventArgs e)
+    {
+      if (BeforeShutdown != null)
+      {
+        BeforeShutdown(this, e);
+      }
+    }
+
+    protected virtual void OnShutdown(EventArgs e)
+    {
+      if (Shutdown != null)
+      {
+        Shutdown(this, e);
+      }
+    }
+
+    protected virtual void OnShutdownComplete(EventArgs e)
+    {
+      if (ShutdownComplete != null)
+      {
+        ShutdownComplete(this, e);
+      }
+    }
+
+    protected override void OnSessionEnding(SessionEndingCancelEventArgs e)
+    {
+      base.OnSessionEnding(e);
+      if (SessionEnding != null)
+      {
+        SessionEnding(this, e);
+      }
+    }
+
+    protected override void OnActivated(EventArgs e)
+    {
+      base.OnActivated(e);
+      if (Activated != null)
+      {
+        Activated(this, e);
+      }
+    }
+
+    protected override void OnDeactivated(EventArgs e)
+    {
+      base.OnDeactivated(e);
+      if (Deactivated != null)
+      {
+        Deactivated(this, e);
+      }
+    }
+
+    #endregion
+
+    #region Event Handling
+
+    private void mainWindow_Closing(object sender, CancelEventArgs e)
+    {
+      OnBeforeShutdown(e);
+    }
+
+    #endregion
+
+    public static void Start()
+    {
+      Core core = new Core();
+      core.DoStart();
+    }
+  }
+}
