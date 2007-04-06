84c6108575ee089d80bad397e3d68670aedc6b9f
gemx <gemx@team-mediaportal.com>
2007-04-06 18:01:15 +0000

    Fixed upgrading of database

--- TvEngine3/TVLibrary/SetupTv/upgradedatabase.sql
+++ TvEngine3/TVLibrary/SetupTv/upgradedatabase.sql
@@ -1,6 +1,14 @@
 USE TvLibrary
 GO
-ALTER TABLE Recording ADD stopTime int NOT NULL DEFAULT  0
+ALTER TABLE Program ADD originalAirDate datetime NOT NULL DEFAULT  0
+GO
+ALTER TABLE Program ADD seriesNum varchar(200) NOT NULL DEFAULT ''
+GO
+ALTER TABLE Program ADD episodeNum varchar(200) NOT NULL DEFAULT ''
+GO
+ALTER TABLE Program ADD starRating int NOT NULL DEFAULT 0
+GO
+ALTER TABLE Program ADD classification varchar(200) NOT NULL DEFAULT ''
 GO
 delete from version
 GO
--- TvEngine3/TVLibrary/SetupTv/upgrademysqldatabase.sql
+++ TvEngine3/TVLibrary/SetupTv/upgrademysqldatabase.sql
@@ -1,6 +1,14 @@
 USE TvLibrary;
 #
-ALTER TABLE `recording` ADD COLUMN `stopTime` INTEGER NOT NULL DEFAULT 0;
+ALTER TABLE `Program` ADD `originalAirDate` datetime NOT NULL DEFAULT  0;
+#
+ALTER TABLE `Program` ADD `seriesNum` varchar(200) NOT NULL DEFAULT '';
+#
+ALTER TABLE `Program` ADD `episodeNum` varchar(200) NOT NULL DEFAULT '';
+#
+ALTER TABLE `Program` ADD `starRating` int NOT NULL DEFAULT 0;
+#
+ALTER TABLE `Program` ADD `classification` varchar(200) NOT NULL DEFAULT '';
 #
 UPDATE `version` SET `versionNumber`=25;
 #
