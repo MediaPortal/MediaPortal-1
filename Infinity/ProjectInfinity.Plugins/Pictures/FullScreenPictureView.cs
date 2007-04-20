d028182a341007c1aad4aa40013b249f08874319
joedalton <joedalton@team-mediaportal.com>
2007-04-20 18:36:47 +0000

    Reworked myPictures UI

--- Infinity/ProjectInfinity.Plugins/Pictures/FullScreenPictureView.cs
+++ Infinity/ProjectInfinity.Plugins/Pictures/FullScreenPictureView.cs
@@ -1,38 +1,11 @@
-using System.IO;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Markup;
-using System.Windows.Media;
+using ProjectInfinity.Controls;
 
 namespace ProjectInfinity.Pictures
 {
-  internal class FullScreenPictureView : Page
+  internal class FullScreenPictureView : View
   {
     public FullScreenPictureView(object model)
     {
-      string fileName = @"skin\default\MyPictures\MyPicturesFullScreen_resources.xaml";
-      if (File.Exists(fileName))
-      {
-        using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
-        {
-          if (fileStream.CanRead)
-          {
-            Resources = (ResourceDictionary) XamlReader.Load(fileStream);
-          }
-        }
-      }
-      Background = Application.Current.Resources["backGroundBrush"] as Brush;
-      fileName = @"skin\default\MyPictures\MyPicturesFullScreen.xaml";
-      if (File.Exists(fileName))
-      {
-        using (FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
-        {
-          if (fileStream.CanRead)
-          {
-            Content = XamlReader.Load(fileStream);
-          }
-        }
-      }
       DataContext = model;
     }
   }
