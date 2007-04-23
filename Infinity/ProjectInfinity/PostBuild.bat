53336ce3030edd062ef440177ced680d79f43c22
james <james@team-mediaportal.com>
2007-04-23 05:12:39 +0000

    Copy debug file .pdb also

--- Infinity/ProjectInfinity/PostBuild.bat
+++ Infinity/ProjectInfinity/PostBuild.bat
@@ -5,10 +5,10 @@ REM Copy All new files from base
 xcopy %1\ProjectInfinity.Base\*.* . /E /R /Y /D
 
 REM Controls
-xcopy /y %1\ProjectInfinity.Controls\bin\%2\ProjectInfinity.Controls.dll .
+xcopy /y %1\ProjectInfinity.Controls\bin\%2\ProjectInfinity.Controls.* .
 
 REM Utilities
-xcopy /y %1\ProjectInfinity.Utilities\bin\%2\ProjectInfinity.Utilities.dll .
+xcopy /y %1\ProjectInfinity.Utilities\bin\%2\ProjectInfinity.Utilities.* .
 
 REM Copy Plugins
 REM MyTv
@@ -16,13 +16,13 @@ xcopy /y %1\mytv\MyTv\bin\%2\*.dll .
 xcopy /y %1\mytv\MyTv\bin\%2\gentle.config .
 
 REM Menu
-xcopy /y %1\Plugins\Menu\bin\%2\Menu.dll Plugins\Menu\
+xcopy /y %1\Plugins\Menu\bin\%2\Menu.* Plugins\Menu\
 
 REM MyPictures
-xcopy /y %1\Plugins\MyPictures\bin\%2\MyPictures.dll Plugins\MyPictures\
+xcopy /y %1\Plugins\MyPictures\bin\%2\MyPictures.* Plugins\MyPictures\
 
 REM MyVideos
-xcopy /y %1\Plugins\MyVideos\bin\%2\MyVideos.dll .
+xcopy /y %1\Plugins\MyVideos\bin\%2\MyVideos.* .
 
 REM MyWeather
-xcopy /y %1\Plugins\MyWeather\bin\%2\MyWeather.dll .
+xcopy /y %1\Plugins\MyWeather\bin\%2\MyWeather.* .
