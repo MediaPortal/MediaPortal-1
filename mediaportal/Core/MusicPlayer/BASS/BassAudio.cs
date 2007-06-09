f4c23fa99e16747f9a45156372de64c9aa1ba306
hwahrmann < hwahrmann@team-mediaportal.com>
2007-06-09 17:19:38 +0000

    Change Volume for ASIO Channel to 0.5 as some users reported problems

--- mediaportal/Core/MusicPlayer/BASS/BassAudio.cs
+++ mediaportal/Core/MusicPlayer/BASS/BassAudio.cs
@@ -1581,7 +1581,7 @@ namespace MediaPortal.Player
             _asioHandler.Pan = _asioBalance;
 
             // Volume always set to 1.0 as it causes stuttering otherwise
-            _asioHandler.Volume = 1.0;
+            _asioHandler.Volume = 0.5;
 
             // Set the Sample Rate from the strea
             _asioHandler.SampleRate = (double)info.freq;
