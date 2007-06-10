7d54e3aa94818e40d880e1ba4e552492614a4aae
diehard2 <diehard2@team-mediaportal.com>
2007-06-10 18:23:09 +0000

    Updated the batch file to build from the vc# express solution

--- mediaportal/Build VCExpress MP.bat
+++ mediaportal/Build VCExpress MP.bat
@@ -1,7 +1,5 @@
-::  rebuild and time MP build
-
-time /t > buildtime.txt
+::   MP VC express solution build
 
 "%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\vcsexpress.exe" /build Release MediaPortal.VC#Express.sln
+xbmc\postbuild.bat
 
-time /t > > buildtime.txt
