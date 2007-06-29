2c177d08bc7c1d51d9cd354692da6cc445e3a6f9
dman_lfc <dman_lfc@team-mediaportal.com>
2007-06-29 20:55:19 +0000

    fixes MPEG-2 stream pin detection thus allowing devices such as the Adaptec VideoOh! to work.

--- TvEngine3/TVLibrary/TVLibrary/Implementations/Analog/Graphs/Analog/TvCardAnalogBase.cs
+++ TvEngine3/TVLibrary/TVLibrary/Implementations/Analog/Graphs/Analog/TvCardAnalogBase.cs
@@ -977,7 +977,8 @@ namespace TvLibrary.Implementations.Analog
           if (fetchedMedia != 1) break;
           if (media[0].majorType == mediaType)
           {
-            if (media[0].subType == mediaSubtype || mediaSubtype == MediaSubType.Null)
+            Log.Log.WriteFile("analog: FindCapturePin major:{0}", media[0].majorType);
+            if (media[0].subType == mediaSubtype || media[0].subType == MediaSubType.Mpeg2Program)
             {
               //it does... we're done
               _pinCapture = pins[0];
@@ -987,6 +988,7 @@ namespace TvLibrary.Implementations.Analog
               DsUtils.FreeAMMediaType(media[0]);
               return;
             }
+            Log.Log.WriteFile("analog: FindCapturePin subtype:{0}", media[0].subType);
           }
           DsUtils.FreeAMMediaType(media[0]);
         }
