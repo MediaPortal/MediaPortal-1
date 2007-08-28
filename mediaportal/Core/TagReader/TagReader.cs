687d90ddb73ba85fb9c76b144dc22664d6f7217a
hwahrmann < hwahrmann@team-mediaportal.com>
2007-08-28 11:39:16 +0000

    Introducing taglib-sharp as Music Tagreader.

--- mediaportal/Core/TagReader/TagReader.cs
+++ mediaportal/Core/TagReader/TagReader.cs
@@ -30,6 +30,7 @@ using System.Drawing;
 using MediaPortal.GUI.Library;
 using MediaPortal.Util;
 using MediaPortal.Configuration;
+using TagLib;
 
 namespace MediaPortal.TagReader
 {
@@ -43,6 +44,7 @@ namespace MediaPortal.TagReader
   public class TagReader
   {
     static ArrayList m_readers = new ArrayList();
+    static bool _useTaglibSharp = false;
 
     /// <summary>
     /// Constructor
@@ -50,6 +52,13 @@ namespace MediaPortal.TagReader
     /// </summary>
     static TagReader()
     {
+      using (MediaPortal.Profile.Settings xmlreader = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "MediaPortal.xml")))
+      {
+        _useTaglibSharp = xmlreader.GetValueAsBool("musicfiles", "usetaglibsharp", false);
+      }
+
+      if (_useTaglibSharp)
+        return;
 
       Log.Info("TagReader: Loading tag reader plugins");
       string[] strFiles = System.IO.Directory.GetFiles(Config.GetSubFolder(Config.Dir.Plugins, "tagreaders"), "*.dll");
@@ -100,6 +109,12 @@ namespace MediaPortal.TagReader
     /// </returns>
     static public MusicTag ReadTag(string strFile)
     {
+
+
+      // Use Taglib-Sharp to read the Music Tag
+      if (_useTaglibSharp)
+        return ReadTags(strFile);
+
       ITag reader = null;
       int prio = -1;
       if (strFile == null) return null;
@@ -122,7 +137,7 @@ namespace MediaPortal.TagReader
           {
             MusicTag musicTag = new MusicTag();
             if (reader.Read(strFile))
-            {             
+            {
               musicTag.Album = reader.Album;
               musicTag.Artist = reader.Artist;
               musicTag.AlbumArtist = reader.AlbumArtist;
@@ -185,6 +200,118 @@ namespace MediaPortal.TagReader
       MusicTag tag = new MusicTag();
       tag.Title = System.IO.Path.GetFileNameWithoutExtension(strFile);
       return tag;
+    }
+
+    static private MusicTag ReadTags(string strFile)
+    {
+      if (!IsAudio(strFile))
+        return null;
+
+      try
+      {
+        TagLib.File tag = TagLib.File.Create(strFile);
+        MusicTag musictag = new MusicTag();
+        string[] artists = tag.Tag.Performers;
+        if (artists.Length > 0)
+          musictag.Artist = artists[0];
+        musictag.Album = tag.Tag.Album;
+        string[] albumartists = tag.Tag.AlbumArtists;
+        if (albumartists.Length > 0)
+          musictag.AlbumArtist = albumartists[0];
+        musictag.BitRate = tag.Properties.AudioBitrate;
+        musictag.Comment = tag.Tag.Comment;
+        string[] composer = tag.Tag.Composers;
+        if (composer.Length > 0)
+          musictag.Composer = composer[0];
+        IPicture[] pics = new IPicture[] { };
+        pics = tag.Tag.Pictures;
+        if (pics.Length > 0)
+          musictag.CoverArtImageBytes = pics[0].Data.Data;
+        musictag.Duration = (int)tag.Properties.Duration.TotalSeconds;
+        musictag.FileName = strFile;
+        musictag.FileType = tag.MimeType;
+        string[] genre = tag.Tag.Genres;
+        if (genre.Length > 0)
+          musictag.Genre = genre[0];
+        string lyrics = tag.Tag.Lyrics;
+        if (lyrics == null)
+          musictag.Lyrics = "";
+        else
+          musictag.Lyrics = lyrics;
+        musictag.Title = tag.Tag.Title;
+        musictag.Track = (int)tag.Tag.Track;
+        musictag.Year = (int)tag.Tag.Year;
+
+        if (tag.MimeType == "taglib/mp3")
+        {
+          // Handle the Rating, which comes from the POPM frame
+          TagLib.Id3v2.Tag id32_tag = tag.GetTag(TagLib.TagTypes.Id3v2) as TagLib.Id3v2.Tag;
+          if (id32_tag != null)
+          {
+            TagLib.Id3v2.PopularimeterFrame popm;
+            foreach (TagLib.Id3v2.Frame frame in id32_tag)
+            {
+              popm = frame as TagLib.Id3v2.PopularimeterFrame;
+              if (popm != null)
+              {
+                int rating = popm.Rating;
+                int i = 0;
+                if (rating > 205)
+                  i = 5;
+                else if (rating > 154)
+                  i = 4;
+                else if (rating > 104)
+                  i = 3;
+                else if (rating > 53)
+                  i = 2;
+                else if (rating > 0)
+                  i = 1;
+                musictag.Rating = i;
+              }
+            }
+          }
+        }
+
+        // if we didn't get a title, use the Filename without extension to prevent the file to appear as "unknown"
+        if (musictag.Title == "")
+          musictag.Title = System.IO.Path.GetFileNameWithoutExtension(strFile);
+
+        return musictag;
+      }
+      catch (UnsupportedFormatException)
+      {
+        Log.Warn("Tagreader: Unsupported File Format {0}", strFile);
+      }
+      catch (Exception ex)
+      {
+        Log.Error("TagReader: Exception reading file {0}. {1}", strFile, ex.Message);
+      }
+      return null;
+    }
+
+    static private bool IsAudio(string fileName)
+    {
+      string ext = System.IO.Path.GetExtension(fileName).ToLower();
+
+      switch (ext)
+      {
+        case ".ape":
+        case ".flac":
+        case ".mp3":
+        case ".ogg":
+        case ".wv":
+        case ".wav":
+        case ".wma":
+        case ".mp4":
+        case ".m4a":
+        case ".m4p":
+        case ".mpc":
+        case ".mp+":
+        case ".mpp":
+          return true;
+      }
+
+      return false;
     }
   }
 }
