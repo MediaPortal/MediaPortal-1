8c8f4edb3458ca0bc0275d225dbc1cc34e75665e
dukus <dukusi@gmail.com>
2007-09-12 18:56:25 +0000

    Background changed of Start form thx to MadsRH

--- mediaportal/MPInstaller/StartForm.Designer.cs
+++ mediaportal/MPInstaller/StartForm.Designer.cs
@@ -38,31 +38,34 @@ namespace MediaPortal.MPInstaller
           // 
           // button1
           // 
-          this.button1.Location = new System.Drawing.Point(171, 21);
+          this.button1.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+          this.button1.Location = new System.Drawing.Point(279, 95);
           this.button1.Name = "button1";
-          this.button1.Size = new System.Drawing.Size(109, 32);
+          this.button1.Size = new System.Drawing.Size(137, 32);
           this.button1.TabIndex = 0;
-          this.button1.Text = "Create extension";
+          this.button1.Text = "&Create extension";
           this.button1.UseVisualStyleBackColor = true;
           this.button1.Click += new System.EventHandler(this.button1_Click);
           // 
           // button2
           // 
-          this.button2.Location = new System.Drawing.Point(171, 71);
+          this.button2.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+          this.button2.Location = new System.Drawing.Point(279, 145);
           this.button2.Name = "button2";
-          this.button2.Size = new System.Drawing.Size(109, 32);
+          this.button2.Size = new System.Drawing.Size(137, 32);
           this.button2.TabIndex = 1;
-          this.button2.Text = "Install extension";
+          this.button2.Text = "&Install extension";
           this.button2.UseVisualStyleBackColor = true;
           this.button2.Click += new System.EventHandler(this.button2_Click);
           // 
           // button3
           // 
-          this.button3.Location = new System.Drawing.Point(171, 121);
+          this.button3.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+          this.button3.Location = new System.Drawing.Point(279, 195);
           this.button3.Name = "button3";
-          this.button3.Size = new System.Drawing.Size(109, 32);
+          this.button3.Size = new System.Drawing.Size(137, 32);
           this.button3.TabIndex = 2;
-          this.button3.Text = "Extension manager";
+          this.button3.Text = "Extension &manager";
           this.button3.UseVisualStyleBackColor = true;
           this.button3.Click += new System.EventHandler(this.button3_Click);
           // 
@@ -72,11 +75,12 @@ namespace MediaPortal.MPInstaller
           // 
           // button4
           // 
-          this.button4.Location = new System.Drawing.Point(171, 172);
+          this.button4.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+          this.button4.Location = new System.Drawing.Point(279, 246);
           this.button4.Name = "button4";
-          this.button4.Size = new System.Drawing.Size(109, 32);
+          this.button4.Size = new System.Drawing.Size(137, 32);
           this.button4.TabIndex = 3;
-          this.button4.Text = "Check for updates";
+          this.button4.Text = "&Check for updates";
           this.button4.UseVisualStyleBackColor = true;
           this.button4.Click += new System.EventHandler(this.button4_Click_1);
           // 
@@ -86,7 +90,7 @@ namespace MediaPortal.MPInstaller
           this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
           this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
           this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-          this.ClientSize = new System.Drawing.Size(292, 249);
+          this.ClientSize = new System.Drawing.Size(694, 373);
           this.Controls.Add(this.button4);
           this.Controls.Add(this.button3);
           this.Controls.Add(this.button2);
