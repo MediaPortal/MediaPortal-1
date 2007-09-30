57ec0d39d664ea68fff9dcfaa88a4705e725d199
micheloe <micheloe@team-mediaportal.com>
2007-09-30 16:54:18 +0000

    Only clear temp. playlists when wer're playing back music videos

--- mediaportal/Core/Player/g_player.cs
+++ mediaportal/Core/Player/g_player.cs
@@ -661,11 +661,14 @@ namespace MediaPortal.Player
         GUIMessage msgRadio = new GUIMessage(GUIMessage.MessageType.GUI_MSG_RECORDER_STOP_RADIO, 0, 0, 0, 0, 0, null);
         GUIWindowManager.SendMessage(msgRadio);
         PlayListType currentList = PlayListPlayer.SingletonPlayer.CurrentPlaylistType;
-        // Clear any temp. playlists before starting playback
-        if (currentList == PlayListType.PLAYLIST_MUSIC_TEMP || currentList == PlayListType.PLAYLIST_VIDEO_TEMP)
+        if (isMusicVideo)
         {
-          PlayListPlayer.SingletonPlayer.GetPlaylist(currentList).Clear();
-          PlayListPlayer.SingletonPlayer.Reset();
+          // Clear any temp. playlists before starting playback
+          if (currentList == PlayListType.PLAYLIST_MUSIC_TEMP || currentList == PlayListType.PLAYLIST_VIDEO_TEMP)
+          {
+            PlayListPlayer.SingletonPlayer.GetPlaylist(currentList).Clear();
+            PlayListPlayer.SingletonPlayer.Reset();
+          }
         }
         //stop timeshifting tv
         //GUIMessage msgTv = new GUIMessage(GUIMessage.MessageType.GUI_MSG_RECORDER_STOP_TIMESHIFT, 0, 0, 0, 0, 0, null);
