73f1422ded1b46bd8499063510b572eed613d86c
hwahrmann < hwahrmann@team-mediaportal.com>
2007-10-03 13:57:15 +0000

    Artists were added multiple times by mistake

--- mediaportal/Databases/Music/MusicDatabase.Updates.cs
+++ mediaportal/Databases/Music/MusicDatabase.Updates.cs
@@ -682,7 +682,7 @@ namespace MediaPortal.Music.Database
         string[] artists = strArtist.Split(new char[] { ';', '|' });
         foreach (string artist in artists)
         {
-          strSQL = String.Format("select idArtist from artist where strArtist = '{0}'", strArtist.Trim());
+          strSQL = String.Format("select idArtist from artist where strArtist = '{0}'", artist.Trim());
           if (MusicDatabase.DirectExecute(strSQL).Rows.Count < 1)
           {
             // Insert the Artist
@@ -750,7 +750,7 @@ namespace MediaPortal.Music.Database
         string[] genres = strGenre.Split(new char[] { ';', '|' });
         foreach (string genre in genres)
         {
-          strSQL = String.Format("select idGenre from genre where upper(strGenre) = '{0}'", genre.Trim().ToUpperInvariant());
+          strSQL = String.Format("select idGenre from genre where upper(strGenre) = '{0}'", genre.Trim());
           if (MusicDatabase.DirectExecute(strSQL).Rows.Count < 1)
           {
             // Insert the Genre
