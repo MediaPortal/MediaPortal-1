f6b6d7e49a15b0e794d824b099ec64eed89887cc
gemx <gemx@team-mediaportal.com>
2008-01-24 15:51:42 +0000

    Added the sources for the modified TsWriter filter as "NewTsWriter"

---
+++ TvEngine3/Filters/sources/NewTsWriter/source/Section.cpp
@@ -0,0 +1,70 @@
+/* 
+ *	Copyright (C) 2006-2008 Team MediaPortal
+ *	http://www.team-mediaportal.com
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *   
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+#include <windows.h>
+#include "Section.h"
+#pragma warning(disable : 4995)
+
+CSection::CSection(void)
+{
+	Data=new byte[MAX_SECTION_LENGTH*5];
+  Reset();
+}
+
+CSection::~CSection(void)
+{
+	delete[] Data;
+}
+
+void CSection::Reset()
+{
+  TableId=-1;
+  Version=-1;
+  SectionNumber=-1;
+  SectionLength=-1;
+  SectionSyntaxIndicator=-1;
+  CurrentNextIndicator=-1;
+  BufferPos=0;
+  SectionLength=0;
+  TransportId=0;
+  LastSectionNumber=0;
+
+}
+
+bool CSection::DecodeHeader()
+{
+	if (BufferPos<8) return false;
+	TableId=Data[0];
+	SectionSyntaxIndicator = (Data[1]>>7) & 1;
+	SectionLength=((Data[1] & 0xF) << 8) + Data[2];
+	TransportId=(Data[3]<<8)+Data[4];
+	CurrentNextIndicator=Data[5] & 1;
+	Version = ((Data[5]>>1)&0x1F);
+	SectionNumber=Data[6];
+	LastSectionNumber=Data[7];	
+	return true;
+}
+
+bool CSection::SectionComplete()
+{
+	if (!DecodeHeader())
+		return false;
+	return (BufferPos>=SectionLength);
+}
---
+++ TvEngine3/Filters/sources/NewTsWriter/source/TimeShifting.h
@@ -0,0 +1,196 @@
+/* 
+ *	Copyright (C) 2006-2008 Team MediaPortal
+ *	http://www.team-mediaportal.com
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *   
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+#pragma once
+#include "multiplexer.h"
+#include "multifilewriter.h"
+#include "criticalsection.h"
+#include "entercriticalsection.h"
+#include "tsheader.h"
+#include "adaptionfield.h"
+#include "pcr.h"
+#include "PcrRefClock.h"
+#include <vector>
+#include <map>
+using namespace std;
+using namespace Mediaportal;
+
+
+//* enum which specified the timeshifting mode 
+enum TimeShiftingMode
+{
+    ProgramStream=0,
+    TransportStream=1
+};
+//* enum which specified the pid type 
+enum PidType
+{
+  Video=0,
+  Audio=1,
+  Other=2
+};
+
+typedef struct stLastPtsDtsRecord
+{
+	CPcr pts;
+	CPcr dts;
+} LastPtsDtsRecord;
+
+// {89459BF6-D00E-4d28-928E-9DA8F76B6D3A}
+DEFINE_GUID(IID_ITsTimeshifting,0x89459bf6, 0xd00e, 0x4d28, 0x92, 0x8e, 0x9d, 0xa8, 0xf7, 0x6b, 0x6d, 0x3a);
+
+// video anayzer interface
+DECLARE_INTERFACE_(ITsTimeshifting, IUnknown)
+{
+	STDMETHOD(SetPcrPid)(THIS_ int pcrPid)PURE;
+	STDMETHOD(AddStream)(THIS_ int pid, int serviceType, char* language)PURE;
+	STDMETHOD(AddStreamWithDescriptor)(THIS_ int pid, const byte* descriptor_data, int descriptor_length, bool isAC3, bool isMpeg1, bool isMpeg2)PURE;	 
+	STDMETHOD(RemoveStream)(THIS_ int pid)PURE;
+	
+  STDMETHOD(SetTimeShiftingFileName)(THIS_ char* pszFileName)PURE;
+  STDMETHOD(Start)(THIS_ )PURE;
+  STDMETHOD(Stop)(THIS_ )PURE;
+  STDMETHOD(Reset)(THIS_ )PURE;
+	STDMETHOD(GetBufferSize) (THIS_ long * size) PURE;
+//	STDMETHOD(GetCurrentTSFile) (THIS_ FileWriter* fileWriter) PURE;
+	STDMETHOD(GetNumbFilesAdded) (THIS_ WORD *numbAdd) PURE;
+	STDMETHOD(GetNumbFilesRemoved) (THIS_ WORD *numbRem) PURE;
+	STDMETHOD(GetCurrentFileId) (THIS_ WORD *fileID) PURE;
+	STDMETHOD(GetMinTSFiles) (THIS_ WORD *minFiles) PURE;
+	STDMETHOD(SetMinTSFiles) (THIS_ WORD minFiles) PURE;
+	STDMETHOD(GetMaxTSFiles) (THIS_ WORD *maxFiles) PURE;
+	STDMETHOD(SetMaxTSFiles) (THIS_ WORD maxFiles) PURE;
+	STDMETHOD(GetMaxTSFileSize) (THIS_ __int64 *maxSize) PURE;
+	STDMETHOD(SetMaxTSFileSize) (THIS_ __int64 maxSize) PURE;
+	STDMETHOD(GetChunkReserve) (THIS_ __int64 *chunkSize) PURE;
+	STDMETHOD(SetChunkReserve) (THIS_ __int64 chunkSize) PURE;
+	STDMETHOD(GetFileBufferSize) (THIS_ __int64 *lpllsize) PURE;
+	STDMETHOD(SetMode) (THIS_ int mode) PURE;
+	STDMETHOD(GetMode) (THIS_ int *mode) PURE;
+	STDMETHOD(SetPmtPid) (THIS_ int pmtPid) PURE;
+	STDMETHOD(Pause) (THIS_ BYTE onOff) PURE;
+};
+
+//** timeshifting class
+class CTimeShifting: public CUnknown, public ITsTimeshifting, public IFileWriter
+{
+public:
+	struct PidInfo
+	{
+		int  realPid;
+		int  fakePid;
+		int  serviceType;
+		bool seenStart;
+		char language[255];
+		byte descriptor_data[255]; // if descriptor_valid then this contains the original descriptor from the PMT
+		bool descriptor_valid;
+		int  ContintuityCounter;
+
+		PidInfo(){
+			descriptor_valid = false;
+			memset(descriptor_data, 0xFF, sizeof(descriptor_data));
+		}
+	};
+	CTimeShifting(LPUNKNOWN pUnk, HRESULT *phr);
+	~CTimeShifting(void);
+  DECLARE_IUNKNOWN
+	
+	STDMETHODIMP SetPcrPid(int pcrPid);
+	STDMETHODIMP AddStream(int pid, int serviceType, char* language);
+	STDMETHODIMP AddStreamWithDescriptor(int pid, const byte* descriptor_data, int descriptor_length, bool isAC3, bool isMpeg1, bool isMpeg2);
+	STDMETHODIMP RemoveStream(int pid);
+	STDMETHODIMP SetTimeShiftingFileName(char* pszFileName);
+	STDMETHODIMP Start();
+	STDMETHODIMP Stop();
+	STDMETHODIMP Reset();
+
+	STDMETHODIMP GetBufferSize( long * size) ;
+//	STDMETHODIMP GetCurrentTSFile( FileWriter* fileWriter) ;
+	STDMETHODIMP GetNumbFilesAdded( WORD *numbAdd) ;
+	STDMETHODIMP GetNumbFilesRemoved( WORD *numbRem) ;
+	STDMETHODIMP GetCurrentFileId( WORD *fileID) ;
+	STDMETHODIMP GetMinTSFiles( WORD *minFiles) ;
+	STDMETHODIMP SetMinTSFiles( WORD minFiles) ;
+	STDMETHODIMP GetMaxTSFiles( WORD *maxFiles) ;
+	STDMETHODIMP SetMaxTSFiles( WORD maxFiles) ;
+	STDMETHODIMP GetMaxTSFileSize( __int64 *maxSize) ;
+	STDMETHODIMP SetMaxTSFileSize( __int64 maxSize) ;
+	STDMETHODIMP GetChunkReserve( __int64 *chunkSize) ;
+	STDMETHODIMP SetChunkReserve( __int64 chunkSize) ;
+	STDMETHODIMP GetFileBufferSize( __int64 *lpllsize) ;
+	STDMETHODIMP SetMode(int mode) ;
+	STDMETHODIMP GetMode(int *mode) ;
+	STDMETHODIMP SetPmtPid(int pmtPid);
+	STDMETHODIMP Pause( BYTE onOff) ;
+	void OnTsPacket(byte* tsPacket);
+	void Write(byte* buffer, int len);
+
+private:  
+  void Flush();
+	void WriteTs(byte* tsPacket);
+  void WriteFakePAT();  
+  void WriteFakePMT();
+
+  void PatchPcr(byte* tsPacket,CTsHeader& header);
+  void PatchPtsDts(byte* tsPacket,CTsHeader& header,CPcr& startPcr);
+
+	MultiFileWriterParam m_params;
+  TimeShiftingMode     m_timeShiftMode;
+	CMultiplexer         m_multiPlexer;
+	bool				         m_bTimeShifting;
+	char				         m_szFileName[2048];
+	MultiFileWriter*     m_pTimeShiftFile;
+	CCriticalSection     m_section;
+  int                  m_pmtPid;
+  int                  m_pcrPid;
+	vector<PidInfo>			 m_vecPids;
+	typedef vector<PidInfo>::iterator itvecPids;
+	bool								 m_bSeenAudioStart;
+	bool								 m_bSeenVideoStart;
+	int									 m_iPmtContinuityCounter;
+	int									 m_iPatContinuityCounter;
+  
+  BOOL            m_bPaused;
+	CPcr            m_startPcr;
+	CPcr            m_highestPcr;
+  bool            m_bDetermineNewStartPcr;
+	bool		        m_bStartPcrFound;
+  int             m_iPacketCounter;
+	int			        m_iPatVersion;
+	int			        m_iPmtVersion;
+  byte*           m_pWriteBuffer;
+  int             m_iWriteBufferPos;
+  CTsHeader       m_tsHeader;
+  CAdaptionField  m_adaptionField;
+  CPcr            m_prevPcr;
+  CPcr            m_pcrHole;
+  CPcr            m_backwardsPcrHole;
+  CPcr            m_pcrDuration;
+  bool            m_bPCRRollover;
+  bool            m_bIgnoreNextPcrJump;
+  FILE*           m_fDump;
+
+  vector<char*>   m_tsQueue;
+  bool            m_bClearTsQueue;
+  unsigned long   m_TsPacketCount;
+  CPcrRefClock*	  rclock;
+  map<unsigned short,LastPtsDtsRecord> m_mapLastPtsDts;
+  typedef map<unsigned short,LastPtsDtsRecord>::iterator imapLastPtsDts;
+};
