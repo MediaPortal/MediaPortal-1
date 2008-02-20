419fb5c878830426857db99fb37fcb2a1d71f1f8
gemx <gemx@team-mediaportal.com>
2008-02-20 18:34:27 +0000

    - Reverted the last changes - DeployVersionSVN is now always built

--- TvEngine3/TVLibrary/Build Deploy Release.bat
+++ TvEngine3/TVLibrary/Build Deploy Release.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-IF EXIST DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe GOTO BUILT
+rem IF EXIST DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe GOTO BUILT
 
 "%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release DeployVersionSVN\DeployVersionSVN.sln
 
--- TvEngine3/TVLibrary/DirectShowLib/AssemblyInfo.cs
+++ TvEngine3/TVLibrary/DirectShowLib/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿/*
+﻿#region Copyright
+/*
 DirectShowLib - Provide access to DirectShow interfaces via .NET
 Copyright (C) 2007
 http://sourceforge.net/projects/directshownet/
@@ -17,7 +18,7 @@ You should have received a copy of the GNU Lesser General Public
 License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
+#endregion
 
 
 using System;
