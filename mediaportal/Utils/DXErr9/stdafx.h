a3adbacb777d41bab7d7be51b75149de6e6fdb10
diehard2 <diehard2@team-mediaportal.com>
2008-03-17 17:14:40 +0000

    Updated dxerr9 project and dll

--- mediaportal/Utils/DXErr9/stdafx.h
+++ mediaportal/Utils/DXErr9/stdafx.h
@@ -8,7 +8,6 @@
 
 #define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
 // Windows Header Files:
-#include <windows.h>
 #include <comutil.h>
 #include <dxerr9.h>
-// TODO: reference additional headers your program requires here
+
