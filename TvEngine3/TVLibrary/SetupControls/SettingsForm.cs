a09ee5322fd867933f9ab6a64c9403af62bc48cf
chefkoch <chefkoch@team-mediaportal.com>
2008-03-19 22:09:48 +0000

    code cleanup use .designer file for windows forms designer code

--- TvEngine3/TVLibrary/SetupControls/SettingsForm.cs
+++ TvEngine3/TVLibrary/SetupControls/SettingsForm.cs
@@ -34,57 +34,41 @@ using System.Net;
 using System.Net.Sockets;
 using MediaPortal.UserInterface.Controls;
 
-
-
 namespace SetupTv
 {
-  /// <summary>
-  /// Summary description for Settings.
-  /// </summary>
-  public class SettingsForm : Form
+  public partial class SettingsForm : Form
   {
-
     protected MPButton cancelButton;
     protected MPButton okButton;
+    protected MPButton applyButton;
+    protected MPButton buttonViewLog;
     protected MPBeveledLine beveledLine1;
     protected TreeView sectionTree;
     protected Panel holderPanel;
     protected MPGradientLabel headerLabel;
     protected SectionSettings _previousSection = null;
+    protected static Hashtable settingSections = new Hashtable();
 
-    //
-    // Hashtable where we store each added tree node/section for faster access
-    //
+    /// <summary>
+    /// Hashtable where we store each added tree node/section for faster access
+    /// </summary>
     public static Hashtable SettingSections
     {
       get { return settingSections; }
     }
 
-    protected static Hashtable settingSections = new Hashtable();
-    protected MPButton buttonViewLog;
-    private System.ComponentModel.IContainer components;
-    protected MPButton applyButton;
-    //private System.ComponentModel.IContainer components;
 
     public SettingsForm()
     {
 
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="section"></param>
+
     public virtual void AddSection(SectionSettings section)
     {
       AddChildSection(null, section);
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="parentSection"></param>
-    /// <param name="section"></param>
     public virtual void AddChildSection(SectionSettings parentSection, SectionSettings section)
     {
       //
@@ -116,171 +100,7 @@ namespace SetupTv
 
       //treeNode.EnsureVisible();
     }
-
-    /// <summary>
-    /// Clean up any resources being used.
-    /// </summary>
-    protected override void Dispose(bool disposing)
-    {
-      if (disposing)
-      {
-        /*if(components != null)
-				{
-					components.Dispose();
-				}*/
-      }
-      base.Dispose(disposing);
-    }
-
-    #region Windows Form Designer generated code
-
-    /// <summary>
-    /// Required method for Designer support - do not modify
-    /// the contents of this method with the code editor.
-    /// </summary>
-    private void InitializeComponent()
-    {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
-			this.sectionTree = new System.Windows.Forms.TreeView();
-			this.cancelButton = new MediaPortal.UserInterface.Controls.MPButton();
-			this.okButton = new MediaPortal.UserInterface.Controls.MPButton();
-			this.headerLabel = new MediaPortal.UserInterface.Controls.MPGradientLabel();
-			this.holderPanel = new System.Windows.Forms.Panel();
-			this.beveledLine1 = new MediaPortal.UserInterface.Controls.MPBeveledLine();
-			this.applyButton = new MediaPortal.UserInterface.Controls.MPButton();
-			this.buttonViewLog = new MediaPortal.UserInterface.Controls.MPButton();
-			this.SuspendLayout();
-			// 
-			// sectionTree
-			// 
-			this.sectionTree.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-									| System.Windows.Forms.AnchorStyles.Left)));
-			this.sectionTree.FullRowSelect = true;
-			this.sectionTree.HideSelection = false;
-			this.sectionTree.HotTracking = true;
-			this.sectionTree.Indent = 19;
-			this.sectionTree.ItemHeight = 16;
-			this.sectionTree.Location = new System.Drawing.Point(16, 16);
-			this.sectionTree.Name = "sectionTree";
-			this.sectionTree.Size = new System.Drawing.Size(184, 460);
-			this.sectionTree.TabIndex = 3;
-			this.sectionTree.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.sectionTree_AfterSelect);
-			this.sectionTree.BeforeSelect += new System.Windows.Forms.TreeViewCancelEventHandler(this.sectionTree_BeforeSelect);
-			// 
-			// cancelButton
-			// 
-			this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.cancelButton.Location = new System.Drawing.Point(520, 499);
-			this.cancelButton.Name = "cancelButton";
-			this.cancelButton.Size = new System.Drawing.Size(75, 23);
-			this.cancelButton.TabIndex = 1;
-			this.cancelButton.Text = "&Cancel";
-			this.cancelButton.UseVisualStyleBackColor = true;
-			this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
-			// 
-			// okButton
-			// 
-			this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.okButton.Location = new System.Drawing.Point(441, 499);
-			this.okButton.Name = "okButton";
-			this.okButton.Size = new System.Drawing.Size(75, 23);
-			this.okButton.TabIndex = 0;
-			this.okButton.Text = "&OK";
-			this.okButton.UseVisualStyleBackColor = true;
-			this.okButton.Click += new System.EventHandler(this.okButton_Click);
-			// 
-			// headerLabel
-			// 
-			this.headerLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-									| System.Windows.Forms.AnchorStyles.Right)));
-			this.headerLabel.Caption = "";
-			this.headerLabel.FirstColor = System.Drawing.SystemColors.InactiveCaption;
-			this.headerLabel.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.headerLabel.LastColor = System.Drawing.Color.WhiteSmoke;
-			this.headerLabel.Location = new System.Drawing.Point(216, 16);
-			this.headerLabel.Name = "headerLabel";
-			this.headerLabel.PaddingLeft = 2;
-			this.headerLabel.Size = new System.Drawing.Size(472, 24);
-			this.headerLabel.TabIndex = 4;
-			this.headerLabel.TabStop = false;
-			this.headerLabel.TextColor = System.Drawing.Color.WhiteSmoke;
-			this.headerLabel.TextFont = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			// 
-			// holderPanel
-			// 
-			this.holderPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-									| System.Windows.Forms.AnchorStyles.Left)
-									| System.Windows.Forms.AnchorStyles.Right)));
-			this.holderPanel.AutoScroll = true;
-			this.holderPanel.BackColor = System.Drawing.SystemColors.Control;
-			this.holderPanel.Location = new System.Drawing.Point(216, 48);
-			this.holderPanel.Name = "holderPanel";
-			this.holderPanel.Size = new System.Drawing.Size(472, 428);
-			this.holderPanel.TabIndex = 5;
-			this.holderPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.holderPanel_Paint);
-			// 
-			// beveledLine1
-			// 
-			this.beveledLine1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-									| System.Windows.Forms.AnchorStyles.Right)));
-			this.beveledLine1.Location = new System.Drawing.Point(8, 489);
-			this.beveledLine1.Name = "beveledLine1";
-			this.beveledLine1.Size = new System.Drawing.Size(688, 2);
-			this.beveledLine1.TabIndex = 6;
-			this.beveledLine1.TabStop = false;
-			// 
-			// applyButton
-			// 
-			this.applyButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.applyButton.Location = new System.Drawing.Point(361, 499);
-			this.applyButton.Name = "applyButton";
-			this.applyButton.Size = new System.Drawing.Size(75, 23);
-			this.applyButton.TabIndex = 7;
-			this.applyButton.TabStop = false;
-			this.applyButton.Text = "&Apply";
-			this.applyButton.UseVisualStyleBackColor = true;
-			this.applyButton.Visible = false;
-			this.applyButton.Click += new System.EventHandler(this.applyButton_Click);
-			// 
-			// buttonViewLog
-			// 
-			this.buttonViewLog.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonViewLog.Location = new System.Drawing.Point(600, 499);
-			this.buttonViewLog.Name = "buttonViewLog";
-			this.buttonViewLog.Size = new System.Drawing.Size(96, 23);
-			this.buttonViewLog.TabIndex = 2;
-			this.buttonViewLog.Text = "View &log files";
-			this.buttonViewLog.UseVisualStyleBackColor = true;
-			this.buttonViewLog.Click += new System.EventHandler(this.buttonViewLog_Click);
-			// 
-			// SettingsForm
-			// 
-			this.AcceptButton = this.okButton;
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.AutoScroll = true;
-			this.CancelButton = this.cancelButton;
-			this.ClientSize = new System.Drawing.Size(704, 530);
-			this.Controls.Add(this.buttonViewLog);
-			this.Controls.Add(this.applyButton);
-			this.Controls.Add(this.beveledLine1);
-			this.Controls.Add(this.holderPanel);
-			this.Controls.Add(this.headerLabel);
-			this.Controls.Add(this.okButton);
-			this.Controls.Add(this.cancelButton);
-			this.Controls.Add(this.sectionTree);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.Name = "SettingsForm";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Settings";
-			this.Closed += new System.EventHandler(this.SettingsForm_Closed);			
-			this.Load += new System.EventHandler(this.SettingsForm_Load);
-			this.ResumeLayout(false);
-
-    }
-
-    #endregion
-
+    
     public virtual void sectionTree_BeforeSelect(object sender, TreeViewCancelEventArgs e)
     {
       SectionTreeNode treeNode = e.Node as SectionTreeNode;
@@ -291,11 +111,6 @@ namespace SetupTv
       }
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
     public virtual void sectionTree_AfterSelect(object sender, TreeViewEventArgs e)
     {
       SectionTreeNode treeNode = e.Node as SectionTreeNode;
@@ -309,56 +124,39 @@ namespace SetupTv
       }
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="section"></param>
     public virtual bool ActivateSection(SectionSettings section)
     {
       return true;
     }
+
 
     public virtual void SettingsForm_Closed(object sender, EventArgs e)
     {
 
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
     public virtual void SettingsForm_Load(object sender, EventArgs e)
     {
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="currentNode"></param>
     public virtual void LoadSectionSettings(TreeNode currentNode)
     {
     }
 
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="currentNode"></param>
     public virtual void SaveSectionSettings(TreeNode currentNode)
     {
     }
+
+    public virtual void SaveAllSettings()
+    {
+    }
+
 
     public virtual void cancelButton_Click(object sender, EventArgs e)
     {
     }
 
     public virtual void okButton_Click(object sender, EventArgs e)
-    {
-    }
-
-
-
-    public virtual void SaveAllSettings()
     {
     }
 
@@ -370,10 +168,10 @@ namespace SetupTv
     {
     }
 
+
     private void holderPanel_Paint(object sender, PaintEventArgs e)
     {
 
-    }		
-
+    }
   }
 }
