d1ab9433a43ac805b5da4e86d937aa2226e08c17
chefkoch <chefkoch@team-mediaportal.com>
2008-03-21 13:41:49 +0000

    added the possibility to build the nsis installer out of this batch file

--- mediaportal/Build Deploy Release.bat
+++ mediaportal/Build Deploy Release.bat
@@ -11,3 +11,11 @@ DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe /svn=%CD%
 "%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release MediaPortal.sln
 
 DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe /svn=%CD% /revert
+
+EXIT
+rem these commands are necessary to get the svn revision, to enable them just remove the EXIT one line above
+DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe /svn=%CD% /GetVersion
+IF NOT EXIST version.txt EXIT
+SET /p version=<version.txt
+DEL version.txt
+"%ProgramFiles%\NSIS\makensis.exe" /DVER_BUILD=%version% Setup\setup.nsi
