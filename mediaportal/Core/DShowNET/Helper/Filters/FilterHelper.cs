a2742cf3b9d20e95eb1b3f59c36325a923fe87ca
tourettes <tourettes@team-mediaportal.com>
2008-04-11 12:45:31 +0000

    Added support for Cyberlink PowerDVD8 DVD Navigator

--- mediaportal/Core/DShowNET/Helper/Filters/FilterHelper.cs
+++ mediaportal/Core/DShowNET/Helper/Filters/FilterHelper.cs
@@ -141,7 +141,8 @@ namespace DShowNET.Helper
         String.Compare(filter.Name, "CyberLink DVD Navigator", true) == 0 ||
         String.Compare(filter.Name, "Cyberlink DVD Navigator (ATI)", true) == 0 ||
         String.Compare(filter.Name, "Cyberlink DVD Navigator (PDVD6)", true) == 0 ||
-        String.Compare(filter.Name, "CyberLink DVD Navigator (PDVD7)", true) == 0)
+        String.Compare(filter.Name, "Cyberlink DVD Navigator (PDVD7)", true) == 0 ||
+        String.Compare(filter.Name, "CyberLink DVD Navigator (PDVD8)", true) == 0)
         {
           navigators.Add(filter.Name);
         }
