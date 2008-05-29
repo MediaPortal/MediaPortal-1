2c96d85e849ea22e09da849c24a57015ff6a072f
rtv_sf <github@rjtv.de>
2008-05-29 20:35:52 +0000

    Threaded Timer checks, Added repeating loop, Added MP's DXUtil implementation for reference

--- Tools/TimerTest/FormMain.Designer.cs
+++ Tools/TimerTest/FormMain.Designer.cs
@@ -32,11 +32,17 @@
       this.listBoxResults = new System.Windows.Forms.ListBox();
       this.buttonClose = new System.Windows.Forms.Button();
       this.checkBoxClearValues = new System.Windows.Forms.CheckBox();
+      this.lblMaxDesc = new System.Windows.Forms.Label();
+      this.lblMaxAccurary = new System.Windows.Forms.Label();
+      this.numLoopCount = new System.Windows.Forms.NumericUpDown();
+      this.lblLoopCountDesc = new System.Windows.Forms.Label();
+      ((System.ComponentModel.ISupportInitialize)(this.numLoopCount)).BeginInit();
       this.SuspendLayout();
       // 
       // buttonStart
       // 
-      this.buttonStart.Location = new System.Drawing.Point(251, 235);
+      this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.buttonStart.Location = new System.Drawing.Point(321, 496);
       this.buttonStart.Name = "buttonStart";
       this.buttonStart.Size = new System.Drawing.Size(75, 23);
       this.buttonStart.TabIndex = 0;
@@ -51,14 +57,15 @@
                   | System.Windows.Forms.AnchorStyles.Right)));
       this.listBoxResults.BorderStyle = System.Windows.Forms.BorderStyle.None;
       this.listBoxResults.FormattingEnabled = true;
-      this.listBoxResults.Location = new System.Drawing.Point(12, 12);
+      this.listBoxResults.Location = new System.Drawing.Point(12, 28);
       this.listBoxResults.Name = "listBoxResults";
-      this.listBoxResults.Size = new System.Drawing.Size(395, 208);
+      this.listBoxResults.Size = new System.Drawing.Size(465, 455);
       this.listBoxResults.TabIndex = 1;
       // 
       // buttonClose
       // 
-      this.buttonClose.Location = new System.Drawing.Point(332, 235);
+      this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.buttonClose.Location = new System.Drawing.Point(402, 496);
       this.buttonClose.Name = "buttonClose";
       this.buttonClose.Size = new System.Drawing.Size(75, 23);
       this.buttonClose.TabIndex = 2;
@@ -68,25 +75,84 @@
       // 
       // checkBoxClearValues
       // 
+      this.checkBoxClearValues.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
       this.checkBoxClearValues.AutoSize = true;
-      this.checkBoxClearValues.Location = new System.Drawing.Point(12, 239);
+      this.checkBoxClearValues.Checked = true;
+      this.checkBoxClearValues.CheckState = System.Windows.Forms.CheckState.Checked;
+      this.checkBoxClearValues.Location = new System.Drawing.Point(12, 500);
       this.checkBoxClearValues.Name = "checkBoxClearValues";
       this.checkBoxClearValues.Size = new System.Drawing.Size(101, 17);
       this.checkBoxClearValues.TabIndex = 3;
       this.checkBoxClearValues.Text = "Clear old values";
       this.checkBoxClearValues.UseVisualStyleBackColor = true;
       // 
+      // lblMaxDesc
+      // 
+      this.lblMaxDesc.AutoSize = true;
+      this.lblMaxDesc.Location = new System.Drawing.Point(9, 9);
+      this.lblMaxDesc.Name = "lblMaxDesc";
+      this.lblMaxDesc.Size = new System.Drawing.Size(101, 13);
+      this.lblMaxDesc.TabIndex = 4;
+      this.lblMaxDesc.Text = "Maximum accuracy:";
+      this.lblMaxDesc.Visible = false;
+      // 
+      // lblMaxAccurary
+      // 
+      this.lblMaxAccurary.AutoSize = true;
+      this.lblMaxAccurary.Location = new System.Drawing.Point(116, 9);
+      this.lblMaxAccurary.Name = "lblMaxAccurary";
+      this.lblMaxAccurary.Size = new System.Drawing.Size(0, 13);
+      this.lblMaxAccurary.TabIndex = 5;
+      // 
+      // numLoopCount
+      // 
+      this.numLoopCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+      this.numLoopCount.Location = new System.Drawing.Point(119, 499);
+      this.numLoopCount.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+      this.numLoopCount.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+      this.numLoopCount.Name = "numLoopCount";
+      this.numLoopCount.Size = new System.Drawing.Size(64, 20);
+      this.numLoopCount.TabIndex = 6;
+      this.numLoopCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+      this.numLoopCount.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+      // 
+      // lblLoopCountDesc
+      // 
+      this.lblLoopCountDesc.AutoSize = true;
+      this.lblLoopCountDesc.Location = new System.Drawing.Point(189, 501);
+      this.lblLoopCountDesc.Name = "lblLoopCountDesc";
+      this.lblLoopCountDesc.Size = new System.Drawing.Size(101, 13);
+      this.lblLoopCountDesc.TabIndex = 7;
+      this.lblLoopCountDesc.Text = "Run this many times";
+      // 
       // FormMain
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(419, 270);
+      this.ClientSize = new System.Drawing.Size(489, 531);
+      this.Controls.Add(this.lblLoopCountDesc);
+      this.Controls.Add(this.numLoopCount);
+      this.Controls.Add(this.lblMaxAccurary);
+      this.Controls.Add(this.lblMaxDesc);
       this.Controls.Add(this.checkBoxClearValues);
       this.Controls.Add(this.buttonClose);
       this.Controls.Add(this.listBoxResults);
       this.Controls.Add(this.buttonStart);
       this.Name = "FormMain";
       this.Text = "TimerTest";
+      ((System.ComponentModel.ISupportInitialize)(this.numLoopCount)).EndInit();
       this.ResumeLayout(false);
       this.PerformLayout();
 
@@ -98,6 +164,10 @@
     private System.Windows.Forms.ListBox listBoxResults;
     private System.Windows.Forms.Button buttonClose;
     private System.Windows.Forms.CheckBox checkBoxClearValues;
+    private System.Windows.Forms.Label lblMaxDesc;
+    private System.Windows.Forms.Label lblMaxAccurary;
+    private System.Windows.Forms.NumericUpDown numLoopCount;
+    private System.Windows.Forms.Label lblLoopCountDesc;
   }
 }
 
--- Tools/TimerTest/FormMain.cs
+++ Tools/TimerTest/FormMain.cs
@@ -2,11 +2,15 @@ using System;
 using System.ComponentModel;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Threading;
+using System.Globalization;
 
 namespace TimerTest
 {
   public partial class FormMain : Form
   {
+    #region DDL imports
+
     [System.Security.SuppressUnmanagedCodeSecurity]
     [DllImport("kernel32.dll")]
     static extern IntPtr GetCurrentThread();
@@ -23,9 +27,18 @@ namespace TimerTest
     [DllImport("kernel32")]
     private static extern bool QueryPerformanceCounter(ref long PerformanceCount);
 
+    #endregion
+
+    protected delegate void MethodListViewItems(string aItem);
+    protected delegate void MethodListViewClearer();
+
+    private double fMaxAccuracy = 1;
+    private object ExecLock = null;
+
     public FormMain()
     {
       InitializeComponent();
+      ExecLock = new object();
     }
 
     private void buttonClose_Click(object sender, EventArgs e)
@@ -35,16 +48,42 @@ namespace TimerTest
 
     private void buttonStart_Click(object sender, EventArgs e)
     {
-      if (checkBoxClearValues.Checked)
-        listBoxResults.Items.Clear();
-      GetTimerFrequency();
+      Invoke(new MethodListViewClearer(ThreadedListClearer));
+      Thread ExecutionThread = new Thread(new ThreadStart(GetTimerFrequencyThread));
+      ExecutionThread.IsBackground = true;
+      ExecutionThread.Start();
     }
 
-    unsafe private void GetTimerFrequency()
+    unsafe private void GetTimerFrequencyThread()
     {
-      for (int i = 0; i < Environment.ProcessorCount; i++)
+      try
       {
-        SetThreadAffinityMask(GetCurrentThread(), new IntPtr(1 << i));
+        for (int j = 0 ; j < numLoopCount.Value ; j++)
+        {
+          lock (ExecLock)
+          {
+            for (short i = 0 ; i < Environment.ProcessorCount ; i++)
+            {
+              Thread TimerThread = new Thread(new ParameterizedThreadStart(CheckTimer));
+              TimerThread.Start((object)i);
+              TimerThread.IsBackground = true;
+            }
+            SetThreadAffinityMask(GetCurrentThread(), new IntPtr(0));
+          }
+        }
+      }
+      catch (ThreadAbortException) { }
+    }
+
+    unsafe private void CheckTimer(object aCpuNo)
+    {
+      try
+      {
+        short currentCpu = (short)aCpuNo;
+        SetThreadAffinityMask(GetCurrentThread(), new IntPtr(1 << currentCpu));
+
+        float MpAccuracy = DXUtil.Timer(DirectXTimer.GetAbsoluteTime);
+        MpAccuracy = DXUtil.Timer(DirectXTimer.GetAbsoluteTime) - MpAccuracy;
 
         long TicksPerSec = 0;
         if (!QueryPerformanceFrequency(ref TicksPerSec))
@@ -53,9 +92,43 @@ namespace TimerTest
           return;
         }
 
-        listBoxResults.Items.Add(string.Format("CPU {0}: Ticks per second: {1}", i, TicksPerSec));
+        long TempTime = 0;
+        double StartTime = 0;
+
+        QueryPerformanceCounter(ref TempTime);
+        // we neglect the overhead of these assignments in between..
+        StartTime = TempTime;
+        QueryPerformanceCounter(ref TempTime);
+
+        // The timespan between two timer results in ms
+        double SysAccuracyMs = ((TempTime - StartTime) / TicksPerSec) * 1000;
+        string SysAccuracyResult = SysAccuracyMs.ToString("N5");
+
+        if (SysAccuracyMs < fMaxAccuracy)
+          fMaxAccuracy = SysAccuracyMs;
+
+        string ResultString = string.Format("CPU {0}: Ticks per second: {1} / MP accuracy: {2} / Approx. accurary in ms: {3}", currentCpu, TicksPerSec, MpAccuracy, SysAccuracyResult);
+
+        // Let the GUI thread add our result to the listbox
+        Invoke(new MethodListViewItems(ThreadedListItemAdder), new object[] { ResultString });
+        Thread.Sleep(0);
       }
-      SetThreadAffinityMask(GetCurrentThread(), new IntPtr(0));
+      catch (ThreadAbortException) { }
+    }
+
+    private void ThreadedListItemAdder(string aItem)
+    {
+      listBoxResults.BeginUpdate();
+      listBoxResults.Items.Add(aItem);
+      lblMaxAccurary.Text = fMaxAccuracy.ToString("N9");
+      lblMaxDesc.Visible = lblMaxAccurary.Text.Length > 0;
+      listBoxResults.EndUpdate();
+    }
+
+    private void ThreadedListClearer()
+    {
+      if (checkBoxClearValues.Checked)
+        listBoxResults.Items.Clear();
     }
   }
 }
