25f677fd541a76c3720b5bd208cdfefda12c8c38
chefkoch <chefkoch@team-mediaportal.com>
2008-07-24 19:00:41 +0000

    fixed some paths

--- TvEngine3/TVLibrary/Build Deploy Release.bat
+++ TvEngine3/TVLibrary/Build Deploy Release.bat
@@ -5,11 +5,11 @@ echo -= TV Server / Client plugin : Build Deploy Release.bat =-
 
 echo.
 echo Building DeployVersionSVN...
-"%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release "..\Tools\Script & Batch tools\DeployVersionSVN\DeployVersionSVN.sln" > build.log
+"%ProgramFiles%\Microsoft Visual Studio 8\Common7\IDE\devenv.com" /rebuild Release "..\..\Tools\Script & Batch tools\DeployVersionSVN\DeployVersionSVN.sln" > build.log
 
 echo.
 echo Writing SVN revision assemblies...
-"..\Tools\Script & Batch tools\DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe" /svn=%CD% >> build.log
+"..\..\Tools\Script & Batch tools\DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe" /svn=%CD% >> build.log
 
 echo.
 echo Building TV Server...
@@ -20,11 +20,11 @@ echo Building TV Client plugin...
 
 echo.
 echo Reverting assemblies...
-"..\Tools\Script & Batch tools\DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe" /svn=%CD% /revert >> build.log
+"..\..\Tools\Script & Batch tools\DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe" /svn=%CD% /revert >> build.log
 
 echo.
 echo Building Installer...
-"..\Tools\Script & Batch tools\DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe" /svn=%CD% /GetVersion >> build.log
+"..\..\Tools\Script & Batch tools\DeployVersionSVN\DeployVersionSVN\bin\Release\DeployVersionSVN.exe" /svn=%CD% /GetVersion >> build.log
 IF NOT EXIST version.txt EXIT >> build.log
 SET /p version=<version.txt >> build.log
 DEL version.txt >> build.log
