ac795d5cceab30ce6c8a0f4eddff8405e87b9486
rtv_sf <github@rjtv.de>
2008-10-25 09:27:33 +0000

    fixed EVR autodetection

--- mediaportal/Configuration/Sections/FiltersVideoRendererConfig.cs
+++ mediaportal/Configuration/Sections/FiltersVideoRendererConfig.cs
@@ -5,22 +5,21 @@ using System.Drawing;
 using System.Data;
 using System.Text;
 using System.Windows.Forms;
+using MediaPortal.ServiceImplementations;
 
 namespace MediaPortal.Configuration.Sections
 {
   public partial class FiltersVideoRenderer : MediaPortal.Configuration.SectionSettings
   {
-
     bool _init = false;
 
     public FiltersVideoRenderer()
-      :
-      this("Video Renderer Settings")
+      : this("Video Renderer Settings")
     {
     }
+
     public FiltersVideoRenderer(string name)
-      :
-      base(name)
+      : base(name)
     {
       InitializeComponent();
     }
@@ -28,6 +27,7 @@ namespace MediaPortal.Configuration.Sections
     public override void OnSectionActivated()
     {
       base.OnSectionActivated();
+
       if (_init == false)
       {
         using (MediaPortal.Profile.Settings xmlreader = new MediaPortal.Profile.Settings(Config.GetFile(Config.Dir.Config, "MediaPortal.xml")))
@@ -38,28 +38,22 @@ namespace MediaPortal.Configuration.Sections
           mpVMR9FilterMethod.Text = xmlreader.GetValueAsString("general", "dx9filteringmode", "Gaussian Quad Filtering"); // http://msdn2.microsoft.com/en-us/library/ms788066.aspx
           checkBoxVMRWebStreams.Checked = xmlreader.GetValueAsBool("general", "usevrm9forwebstreams", true);
           checkBoxDecimateMask.Checked = xmlreader.GetValueAsBool("general", "dx9decimatemask", false); // http://msdn2.microsoft.com/en-us/library/ms787452(VS.85).aspx
-          //EVR - VMR9 selection
-          OsDetection.OSVersionInfo os = new OsDetection.OperatingSystemVersion();
-          int ver = (os.OSMajorVersion * 10) + os.OSMinorVersion;
-          bool UseEVR;
-          string ValueEVR = xmlreader.GetValueAsString("general", "useevr", "---");
-          switch (ValueEVR)
+
+          bool ValueEVR = false;
+
+          try
           {
-            case "---":
-              UseEVR = ver >= 60 ? true : false;
-              break;
-            default:
-              UseEVR = Convert.ToBoolean(ValueEVR);
-              break;
+            //EVR - VMR9 selection
+            OsDetection.OSVersionInfo os = new OsDetection.OperatingSystemVersion();
+            int ver = (os.OSMajorVersion * 10) + os.OSMinorVersion;
+            ValueEVR = ver >= 60 ? true : false;
           }
-          if (UseEVR)
+          catch (Exception ex)    
           {
-            radioButtonEVR.Checked = true;
+            Log.Error("FilterVideoRendererConfig: Os detection unsuccessful - {0}", ex.Message);
           }
-          else
-          {
-            radioButtonVMR9.Checked = true;
-          }
+
+          radioButtonEVR.Checked = xmlreader.GetValueAsBool("general", "useEVRenderer", ValueEVR);
         }
         _init = true;
       }
@@ -75,7 +69,7 @@ namespace MediaPortal.Configuration.Sections
         xmlwriter.SetValue("general", "dx9filteringmode", mpVMR9FilterMethod.Text);
         xmlwriter.SetValueAsBool("general", "usevrm9forwebstreams", checkBoxVMRWebStreams.Checked);
         xmlwriter.SetValueAsBool("general", "dx9decimatemask", checkBoxDecimateMask.Checked);
-        xmlwriter.SetValueAsBool("general", "useevr", radioButtonEVR.Checked);
+        xmlwriter.SetValueAsBool("general", "useEVRenderer", radioButtonEVR.Checked);
       }
     }
 
