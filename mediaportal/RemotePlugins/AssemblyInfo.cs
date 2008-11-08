8f7ac5a2436885d0b98ab807def06d20ef0c5d82
rtv_sf <github@rjtv.de>
2008-11-08 15:22:46 +0000

    reverted 20753 only

--- mediaportal/RemotePlugins/AssemblyInfo.cs
+++ mediaportal/RemotePlugins/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.3.*")]
-[assembly: AssemblyFileVersion("0.9.3.0")]
+[assembly: AssemblyVersion("0.9.4.*")]
+[assembly: AssemblyFileVersion("0.9.4.0")]
