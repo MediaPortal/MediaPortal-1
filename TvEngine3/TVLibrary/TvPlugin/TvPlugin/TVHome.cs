2a7c7f503f5f2c79d945c0e1925286c60762b042
Bavarian <Bavarian@team-mediaportal.com>
2008-11-09 00:54:15 +0000

    fixing mantis bug 1671: OSD on TV recording has wrong caption - change to "recording"

--- TvEngine3/TVLibrary/TvPlugin/TvPlugin/TVHome.cs
+++ TvEngine3/TVLibrary/TvPlugin/TvPlugin/TVHome.cs
@@ -2332,7 +2332,7 @@ namespace TvPlugin
         GUIPropertyManager.SetProperty("#TV.Record.percent1", ((int)percentLivePoint).ToString());
         GUIPropertyManager.SetProperty("#TV.Record.percent2", "0");
         GUIPropertyManager.SetProperty("#TV.Record.percent3", "0");
-        GUIPropertyManager.SetProperty("#TV.View.channel", TvRecorded.ActiveRecording().ReferencedChannel().DisplayName + " (" + GUILocalizeStrings.Get(949) + ")");
+        GUIPropertyManager.SetProperty("#TV.View.channel", TvRecorded.ActiveRecording().ReferencedChannel().DisplayName + " (" + GUILocalizeStrings.Get(682) + ")");
         GUIPropertyManager.SetProperty("#TV.View.title", g_Player.currentTitle);
         GUIPropertyManager.SetProperty("#TV.View.description", g_Player.currentDescription);
 
