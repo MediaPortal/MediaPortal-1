da747876218795b7eb6f8961c8b8efba5f419841
flokel <flokel@team-mediaportal.com>
2008-12-15 12:55:52 +0000

    - minor spelling/optical change

--- TvEngine3/TVLibrary/SetupTv/Dialogs/FormEditCard.Designer.cs
+++ TvEngine3/TVLibrary/SetupTv/Dialogs/FormEditCard.Designer.cs
@@ -35,13 +35,13 @@ namespace SetupTv.Sections
       this.numericUpDownDecryptLimit = new System.Windows.Forms.NumericUpDown();
       this.mpButtonCancel = new MediaPortal.UserInterface.Controls.MPButton();
       this.groupBox1 = new System.Windows.Forms.GroupBox();
+      this.label5 = new MediaPortal.UserInterface.Controls.MPLabel();
+      this.ComboBoxCamType = new MediaPortal.UserInterface.Controls.MPComboBox();
       this.checkBoxCAMenabled = new System.Windows.Forms.CheckBox();
       this.groupBox2 = new System.Windows.Forms.GroupBox();
       this.checkBoxAllowEpgGrab = new System.Windows.Forms.CheckBox();
       this.checkBoxPreloadCard = new System.Windows.Forms.CheckBox();
       this.groupBox3 = new System.Windows.Forms.GroupBox();
-      this.ComboBoxCamType = new MediaPortal.UserInterface.Controls.MPComboBox();
-      this.label5 = new MediaPortal.UserInterface.Controls.MPLabel();
       ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDecryptLimit)).BeginInit();
       this.groupBox1.SuspendLayout();
       this.groupBox2.SuspendLayout();
@@ -50,9 +50,9 @@ namespace SetupTv.Sections
       // 
       // label1
       // 
-      this.label1.Location = new System.Drawing.Point(3, 48);
+      this.label1.Location = new System.Drawing.Point(15, 57);
       this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(297, 58);
+      this.label1.Size = new System.Drawing.Size(289, 58);
       this.label1.TabIndex = 0;
       this.label1.Text = "If your card has a CAM module then specify the number of channels this CAM can de" +
           "code simultaneously. \r\n\r\nSetting the value to 0 will disable the limit completel" +
@@ -61,7 +61,7 @@ namespace SetupTv.Sections
       // label3
       // 
       this.label3.AutoSize = true;
-      this.label3.Location = new System.Drawing.Point(3, 116);
+      this.label3.Location = new System.Drawing.Point(15, 125);
       this.label3.Name = "label3";
       this.label3.Size = new System.Drawing.Size(111, 13);
       this.label3.TabIndex = 2;
@@ -70,7 +70,7 @@ namespace SetupTv.Sections
       // label4
       // 
       this.label4.AutoSize = true;
-      this.label4.Location = new System.Drawing.Point(175, 116);
+      this.label4.Location = new System.Drawing.Point(176, 125);
       this.label4.Name = "label4";
       this.label4.Size = new System.Drawing.Size(121, 13);
       this.label4.TabIndex = 4;
@@ -89,9 +89,9 @@ namespace SetupTv.Sections
       // 
       // numericUpDownDecryptLimit
       // 
-      this.numericUpDownDecryptLimit.Location = new System.Drawing.Point(120, 114);
+      this.numericUpDownDecryptLimit.Location = new System.Drawing.Point(132, 123);
       this.numericUpDownDecryptLimit.Name = "numericUpDownDecryptLimit";
-      this.numericUpDownDecryptLimit.Size = new System.Drawing.Size(49, 20);
+      this.numericUpDownDecryptLimit.Size = new System.Drawing.Size(38, 20);
       this.numericUpDownDecryptLimit.TabIndex = 5;
       this.numericUpDownDecryptLimit.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
       this.numericUpDownDecryptLimit.Value = new decimal(new int[] {
@@ -123,27 +123,48 @@ namespace SetupTv.Sections
       this.groupBox1.Controls.Add(this.numericUpDownDecryptLimit);
       this.groupBox1.Location = new System.Drawing.Point(12, 12);
       this.groupBox1.Name = "groupBox1";
-      this.groupBox1.Size = new System.Drawing.Size(310, 174);
+      this.groupBox1.Size = new System.Drawing.Size(310, 189);
       this.groupBox1.TabIndex = 7;
       this.groupBox1.TabStop = false;
       this.groupBox1.Text = "CAM Setup";
       // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Location = new System.Drawing.Point(59, 152);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(67, 13);
+      this.label5.TabIndex = 8;
+      this.label5.Text = "CAM model :";
+      // 
+      // ComboBoxCamType
+      // 
+      this.ComboBoxCamType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.ComboBoxCamType.FormattingEnabled = true;
+      this.ComboBoxCamType.Items.AddRange(new object[] {
+            "default",
+            "Astoncrypt 2"});
+      this.ComboBoxCamType.Location = new System.Drawing.Point(132, 149);
+      this.ComboBoxCamType.Name = "ComboBoxCamType";
+      this.ComboBoxCamType.Size = new System.Drawing.Size(103, 21);
+      this.ComboBoxCamType.TabIndex = 7;
+      // 
       // checkBoxCAMenabled
       // 
       this.checkBoxCAMenabled.AutoSize = true;
-      this.checkBoxCAMenabled.Location = new System.Drawing.Point(6, 19);
+      this.checkBoxCAMenabled.Location = new System.Drawing.Point(18, 28);
       this.checkBoxCAMenabled.Name = "checkBoxCAMenabled";
       this.checkBoxCAMenabled.RightToLeft = System.Windows.Forms.RightToLeft.No;
-      this.checkBoxCAMenabled.Size = new System.Drawing.Size(210, 17);
+      this.checkBoxCAMenabled.Size = new System.Drawing.Size(207, 17);
       this.checkBoxCAMenabled.TabIndex = 6;
-      this.checkBoxCAMenabled.Text = "CAM enabled and present for this card.";
+      this.checkBoxCAMenabled.Text = "CAM enabled and present for this card";
       this.checkBoxCAMenabled.UseVisualStyleBackColor = true;
       this.checkBoxCAMenabled.CheckedChanged += new System.EventHandler(this.checkBoxCAMenabled_CheckedChanged);
       // 
       // groupBox2
       // 
       this.groupBox2.Controls.Add(this.checkBoxAllowEpgGrab);
-      this.groupBox2.Location = new System.Drawing.Point(12, 192);
+      this.groupBox2.Location = new System.Drawing.Point(12, 207);
       this.groupBox2.Name = "groupBox2";
       this.groupBox2.Size = new System.Drawing.Size(310, 44);
       this.groupBox2.TabIndex = 8;
@@ -153,7 +174,7 @@ namespace SetupTv.Sections
       // checkBoxAllowEpgGrab
       // 
       this.checkBoxAllowEpgGrab.AutoSize = true;
-      this.checkBoxAllowEpgGrab.Location = new System.Drawing.Point(6, 19);
+      this.checkBoxAllowEpgGrab.Location = new System.Drawing.Point(18, 21);
       this.checkBoxAllowEpgGrab.Name = "checkBoxAllowEpgGrab";
       this.checkBoxAllowEpgGrab.RightToLeft = System.Windows.Forms.RightToLeft.No;
       this.checkBoxAllowEpgGrab.Size = new System.Drawing.Size(231, 17);
@@ -164,7 +185,7 @@ namespace SetupTv.Sections
       // checkBoxPreloadCard
       // 
       this.checkBoxPreloadCard.AutoSize = true;
-      this.checkBoxPreloadCard.Location = new System.Drawing.Point(6, 19);
+      this.checkBoxPreloadCard.Location = new System.Drawing.Point(18, 21);
       this.checkBoxPreloadCard.Name = "checkBoxPreloadCard";
       this.checkBoxPreloadCard.RightToLeft = System.Windows.Forms.RightToLeft.No;
       this.checkBoxPreloadCard.Size = new System.Drawing.Size(171, 17);
@@ -175,33 +196,12 @@ namespace SetupTv.Sections
       // groupBox3
       // 
       this.groupBox3.Controls.Add(this.checkBoxPreloadCard);
-      this.groupBox3.Location = new System.Drawing.Point(12, 242);
+      this.groupBox3.Location = new System.Drawing.Point(12, 257);
       this.groupBox3.Name = "groupBox3";
       this.groupBox3.Size = new System.Drawing.Size(310, 44);
       this.groupBox3.TabIndex = 9;
       this.groupBox3.TabStop = false;
       this.groupBox3.Text = "Preload Card";
-      // 
-      // ComboBoxCamType
-      // 
-      this.ComboBoxCamType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-      this.ComboBoxCamType.FormattingEnabled = true;
-      this.ComboBoxCamType.Items.AddRange(new object[] {
-            "default",
-            "Astoncrypt 2"});
-      this.ComboBoxCamType.Location = new System.Drawing.Point(120, 140);
-      this.ComboBoxCamType.Name = "ComboBoxCamType";
-      this.ComboBoxCamType.Size = new System.Drawing.Size(103, 21);
-      this.ComboBoxCamType.TabIndex = 7;
-      // 
-      // label5
-      // 
-      this.label5.AutoSize = true;
-      this.label5.Location = new System.Drawing.Point(47, 143);
-      this.label5.Name = "label5";
-      this.label5.Size = new System.Drawing.Size(67, 13);
-      this.label5.TabIndex = 8;
-      this.label5.Text = "CAM model :";
       // 
       // FormEditCard
       // 
