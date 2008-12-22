4fe7c73be631bbae6178f824168aacdbfe97b0e3
rtv_sf <github@rjtv.de>
2008-12-22 13:51:03 +0000

    tagged final 1.0 release

--- TvEngine3/TVLibrary/SetupTv/39_upgrade_mysql_database.sql
+++
@@ -1,6 +0,0 @@
-USE %TvLibrary%;
-
-ALTER TABLE "card"
- ADD COLUMN "preload" bit(1) NOT NULL;
-
-UPDATE "Version" SET "versionNumber"=39;
--- mp-xaml-experiment/ProjectInfinity.Core/Messaging/CancelMessageHandler.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace ProjectInfinity.Messaging
-{
-  [Serializable]
-  public delegate void CancelMessageHandler<TMessage>(TMessage e) where TMessage : CancelMessage;
-}
