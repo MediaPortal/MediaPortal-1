1267629d1b2c657bfd377b97478ba8ff44b99e6e
tontsa <tontsa@team-mediaportal.com>
2009-01-18 14:06:01 +0000

    more video properties for MediaInfoWrapper

--- mediaportal/Core/Player/MediaInfoWrapper.cs
+++ mediaportal/Core/Player/MediaInfoWrapper.cs
@@ -24,9 +24,20 @@ namespace MediaPortal.Player
     private int _audiochannels = 0;
     private string _aspectRatio = "";
     private string _videoCodec = "";
+    private string _scanType = "";
+    private bool _isXVID = false; // mpeg4 asp
+    private bool _isH264 = false; // mpeg4 avc h264/x264
+    private bool _isMP1V = false; // mpeg1 video (VCD)
+    private bool _isMP2V = false; // mpeg2 video
+    private bool _is720P = false; // is 1280x720 video
+    private bool _is1080P = false; // is 1980x1080 video, progressive
+    private bool _is1080I = false; // is 1920x1080 video, interlaced
+    private bool _isInterlaced = false; // is interlaced
+    private bool _isHDTV = false; // is HDTV resolution
+    private bool _isSDTV = false; // is SDTV resolution
     private bool _isAC3 = false;  // AC3
     private bool _isMP3 = false; // MPEG-1 Audio layer 3
-    private bool _isMP2 = false; // MPEG-1 Audio layer 2
+    private bool _isMP2A = false; // MPEG-1 Audio layer 2
     private bool _isDTS = false; // DTS
     private bool _isOGG = false; // OGG VORBIS
     private bool _isAAC = false;  //AAC
@@ -58,7 +69,8 @@ namespace MediaPortal.Player
         providerNumber.NumberDecimalSeparator = ".";
 
         double.TryParse(_mI.Get(StreamKind.Video, 0, "FrameRate"), NumberStyles.AllowDecimalPoint, providerNumber, out _framerate);
-        _videoCodec = _mI.Get(StreamKind.Video, 0, "Codec"); 
+        _videoCodec = _mI.Get(StreamKind.Video, 0, "Codec").ToLower();
+        _scanType = _mI.Get(StreamKind.Video, 0, "ScanType").ToLower();
         Int32.TryParse(_mI.Get(StreamKind.Video, 0, "Width"), NumberStyles.Integer, providerNumber, out _width);
         Int32.TryParse(_mI.Get(StreamKind.Video, 0, "Height"), NumberStyles.Integer, providerNumber, out _height);
         Int32.TryParse(_mI.Get(StreamKind.Audio, 0, "Audiochannels"), NumberStyles.Integer, providerNumber, out _audiochannels);
@@ -66,29 +78,70 @@ namespace MediaPortal.Player
 
         _aspectRatio = _mI.Get(StreamKind.Video, 0, "AspectRatio/String");
 
-        string codec = _mI.Get(StreamKind.Audio, 0, "Codec/String").ToLower();;
+        string codec = _mI.Get(StreamKind.Audio, 0, "Codec/String").ToLower();
+
+        _isInterlaced = (_scanType.IndexOf("interlaced") > -1);
+        if (_height >= 720)
+        {
+            _isHDTV = true;
+        }
+        else
+        {
+            _isSDTV = true;
+        }
+
+        if (_width == 1280 && _height == 720 && _scanType.IndexOf("progressive") > 1)
+            _is720P = true;
+
+        if (_width == 1920 && _height == 1080 && _scanType.IndexOf("progressive") > 1)
+            _is1080P = true;
+
+        if (_width == 1920 && _height == 1080 && _scanType.IndexOf("interlaced") > 1)
+            _is1080I = true;
+
+        _isXVID = (_videoCodec.IndexOf("xvid") > -1);
+        _isH264 = (_videoCodec.IndexOf("avc") > -1);
+        _isMP1V = (_videoCodec.IndexOf("mpeg-1v") > -1);
+        _isMP2V = (_videoCodec.IndexOf("mpeg-2v") > -1);
 
         _isAC3 = (codec.IndexOf("ac3") > -1);
-        _isMP3 = (codec.IndexOf("mpeg-1 audio layer 3") > -1);
-        _isMP2 = (codec.IndexOf("mpeg-1 audio layer 2") > -1);
-        _isDTS = (codec.IndexOf("dts") > -1);
-        _isOGG = (codec.IndexOf("ogg") > -1); 
-        _isAAC = (codec.IndexOf("aac") > -1);
+        _isMP3  = (codec.IndexOf("mpeg-1 audio layer 3") > -1);
+        _isMP2A = (codec.IndexOf("mpeg-1 audio layer 2") > -1);
+        _isDTS  = (codec.IndexOf("dts") > -1);
+        _isOGG  = (codec.IndexOf("ogg") > -1); 
+        _isAAC  = (codec.IndexOf("aac") > -1);
 
         Log.Info("MediaInfoWrapper.MediaInfoWrapper: inspecting media : {0}", strFile);
         Log.Info("MediaInfoWrapper.MediaInfoWrapper: FrameRate : {0}", _framerate);
+        Log.Info("MediaInfoWrapper.MediaInfoWrapper: VideoCodec : {0}", _videoCodec);
+        if(_isXVID)
+            Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsXVID: {0}", _isXVID);
+        if (_isH264)
+            Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsH264: {0}", _isH264);
+        if (_isMP1V)
+            Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsMP1V: {0}", _isMP1V);
+        if (_isMP2V)
+            Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsMP2V: {0}", _isMP2V);
+        Log.Info("MediaInfoWrapper.MediaInfoWrapper: Scan type : {0}", _scanType);
+        Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsInterlaced: {0}", _isInterlaced);
         Log.Info("MediaInfoWrapper.MediaInfoWrapper: Width : {0}", _width);
         Log.Info("MediaInfoWrapper.MediaInfoWrapper: Height : {0}", _height);
         Log.Info("MediaInfoWrapper.MediaInfoWrapper: Audiochannels : {0}", _audiochannels);
         Log.Info("MediaInfoWrapper.MediaInfoWrapper: Audiorate : {0}", _audiorate);
         Log.Info("MediaInfoWrapper.MediaInfoWrapper: AspectRatio : {0}", _aspectRatio);
-        Log.Info("MediaInfoWrapper.MediaInfoWrapper: Codec : {0}", codec);
-        Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsAC3 : {0}", _isAC3);
-        Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsMP3 : {0}", _isMP3);
-        Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsMP2 : {0}", _isMP2);
-        Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsDTS : {0}", _isDTS);
-        Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsOGG : {0}", _isOGG);
-        Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsAAC : {0}", _isAAC);
+        Log.Info("MediaInfoWrapper.MediaInfoWrapper: AudioCodec : {0}", codec);
+        if(_isAC3)
+            Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsAC3 : {0}", _isAC3);
+        if(_isMP3)
+            Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsMP3 : {0}", _isMP3);
+        if(_isMP2A)
+            Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsMP2A: {0}", _isMP2A);
+        if(_isDTS)
+            Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsDTS : {0}", _isDTS);
+        if(_isOGG)
+            Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsOGG : {0}", _isOGG);
+        if(_isAAC)
+            Log.Info("MediaInfoWrapper.MediaInfoWrapper: IsAAC : {0}", _isAAC);
 
 
       }
@@ -125,6 +178,45 @@ namespace MediaPortal.Player
     {
       get { return _framerate; }
     }
+    
+    public bool IsXVID
+    {
+      get { return _isXVID; }
+    }
+    public bool IsH264
+    {
+       get { return _isH264; }
+    }
+
+    public bool IsMP1V
+    {
+     get { return _isMP1V; }
+    }
+    
+    public bool IsMP2V
+    {
+      get { return _isMP2V; }
+    }
+
+    public bool Is720P
+    {
+      get { return _is720P; }
+    }
+
+    public bool Is1080P
+    {
+      get { return _is1080P; }
+    }
+
+    public bool Is1080I
+    {
+       get { return _is1080I; }
+    }
+
+    public bool IsInterlaced
+    {
+       get { return _isInterlaced; }
+    }
 
     public int Width
     {
@@ -160,9 +252,9 @@ namespace MediaPortal.Player
       get { return _isMP3; }
     }
 
-    public bool IsMP2
+    public bool IsMP2A
     {
-      get { return _isMP2; }
+      get { return _isMP2A; }
     }
 
     public bool IsDTS
