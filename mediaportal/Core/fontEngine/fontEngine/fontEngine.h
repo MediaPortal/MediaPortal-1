8a153a1aea0fc833b4dc1ad9c5aec61e52eb3320
tourettes <tourettes@team-mediaportal.com>
2009-02-19 19:50:11 +0000

    Fixed Mantis issue: 0001998: Threading issue with texture cache

--- mediaportal/Core/fontEngine/fontEngine/fontEngine.h
+++ mediaportal/Core/fontEngine/fontEngine/fontEngine.h
@@ -1,3 +1,24 @@
+/* 
+ *	Copyright (C) 2005-2008 Team MediaPortal
+ *	http://www.team-mediaportal.com
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *   
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *   
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  http://www.gnu.org/copyleft/gpl.html
+ *
+ */
+
 // The following ifdef block is the standard way of creating macros which make exporting 
 // from a DLL simpler. All files within this DLL are compiled with the FONTENGINE_EXPORTS
 // symbol defined on the command line. this symbol should not be defined on any project
@@ -24,9 +45,8 @@ void FontEngineSetCoordinate(int fontNumber, int index, int subindex, float fVal
 void FontEngineDrawText3D(int fontNumber, void* text, int xposStart, int yposStart, DWORD intColor,int maxWidth);
 void FontEnginePresent3D(int fontNumber);
 void FontEngineSetTexture(void* texture);
-void FontEngineDrawSurface(int fx, int fy, int nw, int nh, 
-						   int dstX, int dstY, int dstWidth, int dstHeight,
-						   void* surface);
+void FontEngineDrawSurface( int fx, int fy, int nw, int nh, 
+                            int dstX, int dstY, int dstWidth, int dstHeight,void* surface);
 
 void PrintStatistics();
 void Cleanup();
