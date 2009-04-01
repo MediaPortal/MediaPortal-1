7b0dc3d8633c5d8170da641ea5b2817e87f957a1
rtv_sf <github@rjtv.de>
2009-04-01 18:19:02 +0000

    Fixes for mantis #2067: Animations of default skin do not provide a smooth experience - handle render exceptions (e.g. race conditons in gui controls)

--- mediaportal/Core/guilib/GUITextScrollUpControl.cs
+++ mediaportal/Core/guilib/GUITextScrollUpControl.cs
@@ -108,90 +108,99 @@ namespace MediaPortal.GUI.Library
 
     public override void Render(float timePassed)
     {
-      _invalidate = false;
-      // Nothing visibile - save CPU cycles
-      if (null == _font)
+      try
       {
-        base.Render(timePassed);
-        return;
-      }
-      if (GUIGraphicsContext.EditMode == false)
-      {
-        if (!IsVisible)
+        _invalidate = false;
+        // Nothing visibile - save CPU cycles
+        if (null == _font)
         {
           base.Render(timePassed);
           return;
         }
-      }
-
-      int dwPosY = _positionY;
-
-      _timeElapsed += timePassed;
-      if (_frameLimiter < GUIGraphicsContext.MaxFPS)
-        _frameLimiter++;
-      else
-        _frameLimiter = 1;
-
-      if (_containsProperty)
-      {
-        string strText = GUIPropertyManager.Parse(_property);
-
-        strText = strText.Replace("\\r", "\r");
-        if (strText != _previousProperty)
+        if (GUIGraphicsContext.EditMode == false)
         {
-          // Reset the scrolling position - e.g. if we switch in TV Guide between various items
-          _yPositionScroll = 0;
-          _scrollOffset = 0.0f;
-          _frameLimiter = 1;
-          _offset = 0;
-          _listItems.Clear();
-
-          _previousProperty = strText;
-          SetText(strText);
-        }
-      }
-
-      if (GUIGraphicsContext.graphics != null)
-      {
-        _offset = 0;
-      }
-      if (_listItems.Count > _itemsPerPage)
-      {
-        // rest before we start scrolling
-        if ((int)_timeElapsed > _scrollStartDelay)
-        {
-          _invalidate = true;
-          // apply user scroll speed setting. 1 = slowest / 10 = fastest
-          //int userSpeed = 11 - GUIGraphicsContext.ScrollSpeedVertical;           //  10 - 1
-
-          if (_frameLimiter % (6 - GUIGraphicsContext.ScrollSpeedVertical) == 0)
-            _yPositionScroll++;
-          //_yPositionScroll = _yPositionScroll + GUIGraphicsContext.ScrollSpeedVertical;
-
-          dwPosY -= (int)(_yPositionScroll - _scrollOffset);
-          // Log.Debug("*** _frameLimiter: {0}, dwPosY: {1}, _scrollOffset: {2}", _frameLimiter, dwPosY, _scrollOffset);
-
-          if (_positionY - dwPosY >= _itemHeight)
+          if (!IsVisible)
           {
-            // one line has been scrolled away entirely
-            dwPosY += _itemHeight;
-            _scrollOffset += _itemHeight;
-            _offset++;
-            if (_offset >= _listItems.Count)
+            base.Render(timePassed);
+            return;
+          }
+        }
+
+        int dwPosY = _positionY;
+
+        _timeElapsed += timePassed;
+        if (_frameLimiter < GUIGraphicsContext.MaxFPS)
+          _frameLimiter++;
+        else
+          _frameLimiter = 1;
+
+        if (_containsProperty)
+        {
+          string strText = GUIPropertyManager.Parse(_property);
+
+          strText = strText.Replace("\\r", "\r");
+          if (strText != _previousProperty)
+          {
+            // Reset the scrolling position - e.g. if we switch in TV Guide between various items
+            _yPositionScroll = 0;
+            _scrollOffset = 0.0f;
+            _frameLimiter = 1;
+            _offset = 0;
+            _listItems.Clear();
+
+            _previousProperty = strText;
+            SetText(strText);
+          }
+        }
+
+        if (GUIGraphicsContext.graphics != null)
+        {
+          _offset = 0;
+        }
+        if (_listItems.Count > _itemsPerPage)
+        {
+          // rest before we start scrolling
+          if ((int)_timeElapsed > _scrollStartDelay)
+          {
+            _invalidate = true;
+            // apply user scroll speed setting. 1 = slowest / 10 = fastest
+            //int userSpeed = 11 - GUIGraphicsContext.ScrollSpeedVertical;           //  10 - 1
+
+            if (_frameLimiter % (6 - GUIGraphicsContext.ScrollSpeedVertical) == 0)
+              _yPositionScroll++;
+            //_yPositionScroll = _yPositionScroll + GUIGraphicsContext.ScrollSpeedVertical;
+
+            dwPosY -= (int)(_yPositionScroll - _scrollOffset);
+            // Log.Debug("*** _frameLimiter: {0}, dwPosY: {1}, _scrollOffset: {2}", _frameLimiter, dwPosY, _scrollOffset);
+
+            if (_positionY - dwPosY >= _itemHeight)
             {
-              // restart with the first line
-              if (Seperator.Length > 0)
+              // one line has been scrolled away entirely
+              dwPosY += _itemHeight;
+              _scrollOffset += _itemHeight;
+              _offset++;
+              if (_offset >= _listItems.Count)
               {
-                if (_offset >= _listItems.Count + 1)
+                // restart with the first line
+                if (Seperator.Length > 0)
+                {
+                  if (_offset >= _listItems.Count + 1)
+                  {
+                    _offset = 0;
+                  }
+                }
+                else
                 {
                   _offset = 0;
                 }
               }
-              else
-              {
-                _offset = 0;
-              }
             }
+          }
+          else
+          {
+            _scrollOffset = 0.0f;
+            _frameLimiter = 1;
+            _offset = 0;
           }
         }
         else
@@ -200,145 +209,143 @@ namespace MediaPortal.GUI.Library
           _frameLimiter = 1;
           _offset = 0;
         }
-      }
-      else
-      {
-        _scrollOffset = 0.0f;
-        _frameLimiter = 1;
-        _offset = 0;
-      }
 
-      Viewport oldviewport = GUIGraphicsContext.DX9Device.Viewport;
-      if (GUIGraphicsContext.graphics != null)
-      {
-        GUIGraphicsContext.graphics.SetClip(new Rectangle(_positionX + GUIGraphicsContext.OffsetX,
-                                                          _positionY + GUIGraphicsContext.OffsetY, _width,
-                                                          _itemsPerPage * _itemHeight));
-      }
-      else
-      {
-        if (_width < 1)
+        Viewport oldviewport = GUIGraphicsContext.DX9Device.Viewport;
+        if (GUIGraphicsContext.graphics != null)
         {
-          base.Render(timePassed);
-          return;
+          GUIGraphicsContext.graphics.SetClip(new Rectangle(_positionX + GUIGraphicsContext.OffsetX,
+                                                            _positionY + GUIGraphicsContext.OffsetY, _width,
+                                                            _itemsPerPage * _itemHeight));
         }
-        if (_height < 1)
+        else
         {
-          base.Render(timePassed);
-          return;
-        }
-
-        Viewport newviewport = new Viewport();
-        newviewport.X = _positionX + GUIGraphicsContext.OffsetX;
-        newviewport.Y = _positionY + GUIGraphicsContext.OffsetY;
-        newviewport.Width = _width;
-        newviewport.Height = _height;
-        newviewport.MinZ = 0.0f;
-        newviewport.MaxZ = 1.0f;
-        GUIGraphicsContext.DX9Device.Viewport = newviewport;
-      }
-      long color = _textColor;
-      if (Dimmed)
-      {
-        color &= DimColor;
-      }
-      for (int i = 0; i < 1 + _itemsPerPage; i++)
-      {
-        // render each line
-        int dwPosX = _positionX;
-        int iItem = i + _offset;
-        int iMaxItems = _listItems.Count;
-        if (_listItems.Count > _itemsPerPage && Seperator.Length > 0)
-        {
-          iMaxItems++;
-        }
-
-        if (iItem >= iMaxItems)
-        {
-          if (iMaxItems > _itemsPerPage)
+          if (_width < 1)
           {
-            iItem -= iMaxItems;
+            base.Render(timePassed);
+            return;
           }
-          else
+          if (_height < 1)
           {
-            break;
-          }
-        }
-
-        if (iItem >= 0 && iItem < iMaxItems)
-        {
-          // render item
-          string strLabel1 = "", strLabel2 = "";
-          if (iItem < _listItems.Count)
-          {
-            GUIListItem item = (GUIListItem)_listItems[iItem];
-            strLabel1 = item.Label;
-            strLabel2 = item.Label2;
-          }
-          else
-          {
-            strLabel1 = Seperator;
+            base.Render(timePassed);
+            return;
           }
 
-          int ixoff = _xOffset;
-          int ioffy = 2;
-          GUIGraphicsContext.ScaleVertical(ref ioffy);
-          GUIGraphicsContext.ScaleHorizontal(ref ixoff);
-          string wszText1 = String.Format("{0}", strLabel1);
-          int dMaxWidth = _width + ixoff;
-          float x = dwPosX;
-          if (strLabel2.Length > 0)
+          Viewport newviewport = new Viewport();
+          newviewport.X = _positionX + GUIGraphicsContext.OffsetX;
+          newviewport.Y = _positionY + GUIGraphicsContext.OffsetY;
+          newviewport.Width = _width;
+          newviewport.Height = _height;
+          newviewport.MinZ = 0.0f;
+          newviewport.MaxZ = 1.0f;
+          GUIGraphicsContext.DX9Device.Viewport = newviewport;
+        }
+        long color = _textColor;
+        if (Dimmed)
+        {
+          color &= DimColor;
+        }
+        for (int i = 0; i < 1 + _itemsPerPage; i++)
+        {
+          // render each line
+          int dwPosX = _positionX;
+          int iItem = i + _offset;
+          int iMaxItems = _listItems.Count;
+          if (_listItems.Count > _itemsPerPage && Seperator.Length > 0)
           {
-            string wszText2;
-            float fTextWidth = 0, fTextHeight = 0;
-            wszText2 = String.Format("{0}", strLabel2);
-            _font.GetTextExtent(wszText2.Trim(), ref fTextWidth, ref fTextHeight);
-            dMaxWidth -= (int)(fTextWidth);
+            iMaxItems++;
+          }
+
+          if (iItem >= iMaxItems)
+          {
+            if (iMaxItems > _itemsPerPage)
+            {
+              iItem -= iMaxItems;
+            }
+            else
+            {
+              break;
+            }
+          }
+
+          if (iItem >= 0 && iItem < iMaxItems)
+          {
+            // render item
+            string strLabel1 = "", strLabel2 = "";
+            if (iItem < _listItems.Count)
+            {
+              GUIListItem item = (GUIListItem)_listItems[iItem];
+              strLabel1 = item.Label;
+              strLabel2 = item.Label2;
+            }
+            else
+            {
+              strLabel1 = Seperator;
+            }
+
+            int ixoff = _xOffset;
+            int ioffy = 2;
+            GUIGraphicsContext.ScaleVertical(ref ioffy);
+            GUIGraphicsContext.ScaleHorizontal(ref ixoff);
+            string wszText1 = String.Format("{0}", strLabel1);
+            int dMaxWidth = _width + ixoff;
+            float x = dwPosX;
+            if (strLabel2.Length > 0)
+            {
+              string wszText2;
+              float fTextWidth = 0, fTextHeight = 0;
+              wszText2 = String.Format("{0}", strLabel2);
+              _font.GetTextExtent(wszText2.Trim(), ref fTextWidth, ref fTextHeight);
+              dMaxWidth -= (int)(fTextWidth);
+
+              switch (_textAlignment)
+              {
+                case Alignment.ALIGN_LEFT:
+                case Alignment.ALIGN_CENTER:
+                  x = dwPosX + dMaxWidth;
+                  break;
+
+                case Alignment.ALIGN_RIGHT:
+                  x = dwPosX + dMaxWidth + _width;
+                  break;
+              }
+
+              _font.DrawTextWidth(x, (float)dwPosY + ioffy, _textColor, wszText2.Trim(), fTextWidth, _textAlignment);
+            }
 
             switch (_textAlignment)
             {
-              case Alignment.ALIGN_LEFT:
               case Alignment.ALIGN_CENTER:
-                x = dwPosX + dMaxWidth;
+              case Alignment.ALIGN_LEFT:
+                x = dwPosX;
                 break;
 
               case Alignment.ALIGN_RIGHT:
-                x = dwPosX + dMaxWidth + _width;
+                x = dwPosX + _width;
                 break;
             }
+            _font.DrawTextWidth(x, (float)dwPosY + ioffy, _textColor, wszText1.Trim(), (float)dMaxWidth, _textAlignment);
 
-            _font.DrawTextWidth(x, (float)dwPosY + ioffy, _textColor, wszText2.Trim(), fTextWidth, _textAlignment);
+            //            Log.Info("dw _positionY, dwPosY, _yPositionScroll, _scrollOffset: {0} {1} {2} {3}", _positionY, dwPosY, _yPositionScroll, _scrollOffset);
+            //            Log.Info("dw wszText1.Trim() {0}", wszText1.Trim());
+
+            dwPosY += _itemHeight;
           }
-
-          switch (_textAlignment)
-          {
-            case Alignment.ALIGN_CENTER:
-            case Alignment.ALIGN_LEFT:
-              x = dwPosX;
-              break;
-
-            case Alignment.ALIGN_RIGHT:
-              x = dwPosX + _width;
-              break;
-          }
-          _font.DrawTextWidth(x, (float)dwPosY + ioffy, _textColor, wszText1.Trim(), (float)dMaxWidth, _textAlignment);
-
-          //            Log.Info("dw _positionY, dwPosY, _yPositionScroll, _scrollOffset: {0} {1} {2} {3}", _positionY, dwPosY, _yPositionScroll, _scrollOffset);
-          //            Log.Info("dw wszText1.Trim() {0}", wszText1.Trim());
-
-          dwPosY += _itemHeight;
         }
-      }
 
-      if (GUIGraphicsContext.graphics != null)
-      {
-        GUIGraphicsContext.graphics.SetClip(new Rectangle(0, 0, GUIGraphicsContext.Width, GUIGraphicsContext.Height));
+        if (GUIGraphicsContext.graphics != null)
+        {
+          GUIGraphicsContext.graphics.SetClip(new Rectangle(0, 0, GUIGraphicsContext.Width, GUIGraphicsContext.Height));
+        }
+        else
+        {
+          GUIGraphicsContext.DX9Device.Viewport = oldviewport;
+        }
+        base.Render(timePassed);
       }
-      else
+      catch (Exception ex)
       {
-        GUIGraphicsContext.DX9Device.Viewport = oldviewport;
+        Log.Error("GUITextScrollUpControl: Error during the render process - maybe a threading issue. {0}", ex.ToString());
       }
-      base.Render(timePassed);
     }
 
     public override bool OnMessage(GUIMessage message)
