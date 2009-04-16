16c544d50db0b87bcc7df4998bf8d60a0f1dda96
chemelli <chemelli@team-mediaportal.com>
2009-04-16 10:27:24 +0000

    Removed VC2005 redist check, now all projects are on VS2008

--- Tools/MediaPortal.DeployTool/Sections/InstallDlg.cs
+++ Tools/MediaPortal.DeployTool/Sections/InstallDlg.cs
@@ -169,7 +169,6 @@ namespace MediaPortal.DeployTool.Sections
       if (InstallationProperties.Instance["InstallType"] != "download_only")
         AddPackageToListView(new OldPackageChecker());
       AddPackageToListView(new DirectX9Checker());
-      AddPackageToListView(new VCRedist2005Checker());
       AddPackageToListView(new VCRedist2008Checker());
       AddPackageToListView(new WindowsMediaPlayerChecker());
       switch (InstallationProperties.Instance["InstallType"])
