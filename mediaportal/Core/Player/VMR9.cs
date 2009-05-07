6a9d9a60eae80f2effac0c91d298b895d5e20add
tourettes <tourettes@team-mediaportal.com>
2009-05-07 15:07:54 +0000

    Increased rendering stats update resolution. Related to 0002070: EVR renderer doesn't provide fps information.

--- mediaportal/Core/Player/VMR9.cs
+++ mediaportal/Core/Player/VMR9.cs
@@ -559,10 +559,11 @@ namespace MediaPortal.Player
 
       TimeSpan ts = DateTime.Now - _repaintTimer;
       int frames = FrameCounter;
-      if (ts.TotalMilliseconds >= 5000 
+      if (ts.TotalMilliseconds >= 1750 
         || (currentVmr9State == Vmr9PlayState.Repaint && FrameCounter > 0)
         || g_Player.Paused ) // in paused state frames aren't rendered so we need "force" the GUI drawing here
       {
+        _repaintTimer = DateTime.Now;
         GUIGraphicsContext.Vmr9FPS = ((float) (frames*1000))/((float) ts.TotalMilliseconds);
         //Log.Info("VMR9Helper:frames:{0} fps:{1} time:{2}", frames, GUIGraphicsContext.Vmr9FPS,ts.TotalMilliseconds);
         FrameCounter = 0;
@@ -578,7 +579,6 @@ namespace MediaPortal.Player
             Log.Debug("_qualityInterface is null!");
           }
         }
-        _repaintTimer = DateTime.Now;
       }
 
       if (currentVmr9State == Vmr9PlayState.Repaint && frames > 0)
