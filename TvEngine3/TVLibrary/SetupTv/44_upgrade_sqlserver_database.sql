6c5ddd838b8a56b441e540e75d2aa156ed9c56b1
chemelli <chemelli@team-mediaportal.com>
2009-06-15 21:50:01 +0000

    Additional fix for mantis #0002166: Add EpisodeName and EpisodePart to program db table
    MSSQL script was failing to upgrade DB

--- TvEngine3/TVLibrary/SetupTv/44_upgrade_sqlserver_database.sql
+++ TvEngine3/TVLibrary/SetupTv/44_upgrade_sqlserver_database.sql
@@ -2,8 +2,8 @@
 GO
 
 ALTER TABLE Program
-  ADD episodeName varchar(MAX) NOT NULL, 
-	  episodePart varchar(MAX) NOT NULL
+  ADD episodeName varchar(MAX) NOT NULL CONSTRAINT [DF_Program_EpisodeName] DEFAULT '',
+      episodePart varchar(MAX) NOT NULL CONSTRAINT [DF_Program_EpisodePart] DEFAULT ''
 GO
 
 UPDATE Version SET versionNumber=44
