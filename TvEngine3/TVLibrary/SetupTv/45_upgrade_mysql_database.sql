4f3994c320ee2702a4514ddf1e7ce75819138dd6
rtv_sf <github@rjtv.de>
2009-06-22 21:06:20 +0000

    Fix for mantis #2288: Database sometimes logs slow queries - added index on Program table

--- TvEngine3/TVLibrary/SetupTv/45_upgrade_mysql_database.sql
+++ TvEngine3/TVLibrary/SetupTv/45_upgrade_mysql_database.sql
@@ -1,7 +1,10 @@
 ﻿USE %TvLibrary%;
 
 ALTER TABLE "Recording"
- MODIFY COLUMN "fileName" VARCHAR(260) NOT NULL,
- ADD INDEX "IDX_Recording_Filename"("fileName");
+  MODIFY COLUMN "fileName" VARCHAR(260) NOT NULL,
+  ADD INDEX "IDX_Recording_Filename"("fileName");
+
+ALTER TABLE "Program" 
+  ADD INDEX "IDX_Program_Notify"("notify");
 
 UPDATE "Version" SET "versionNumber"=45;
--- TvEngine3/TVLibrary/SetupTv/45_upgrade_sqlserver_database.sql
+++ TvEngine3/TVLibrary/SetupTv/45_upgrade_sqlserver_database.sql
@@ -7,6 +7,8 @@ GO
 
 CREATE INDEX IDX_Recording_Filename ON Recording(fileName)
 GO
+CREATE INDEX IDX_Program_Notify ON Program(notify)
+GO
 
 UPDATE Version SET versionNumber=45
 GO
