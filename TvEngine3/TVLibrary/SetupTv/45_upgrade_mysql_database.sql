8ac921a44c33ecdebf4caa4708fb271c7ed483fc
rtv_sf <github@rjtv.de>
2009-06-22 20:49:08 +0000

    Fix for mantis #2288: Database sometimes logs slow queries - added index on Recording table - reduced unnecessary large field size for the filenames as Windows doesn't allow more than 257 chars effectively

---
+++ TvEngine3/TVLibrary/SetupTv/45_upgrade_mysql_database.sql
@@ -0,0 +1,7 @@
+﻿USE %TvLibrary%;
+
+ALTER TABLE "Recording"
+ MODIFY COLUMN "fileName" VARCHAR(260) NOT NULL,
+ ADD INDEX "IDX_Recording_Filename"("fileName");
+
+UPDATE "Version" SET "versionNumber"=45;
---
+++ TvEngine3/TVLibrary/SetupTv/45_upgrade_sqlserver_database.sql
@@ -0,0 +1,12 @@
+﻿use %TvLibrary%
+GO
+
+ALTER TABLE Recording
+  ALTER COLUMN fileName VARCHAR(260) NOT NULL
+GO
+
+CREATE INDEX IDX_Recording_Filename ON Recording(fileName)
+GO
+
+UPDATE Version SET versionNumber=45
+GO
