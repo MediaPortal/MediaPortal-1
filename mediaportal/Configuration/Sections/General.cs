c2c2c9673713b42cba5972c95010f55f5380d698
rtv_sf <github@rjtv.de>
2009-06-26 14:32:21 +0000

    Fixed log level wasn't set to debug correctly.

--- mediaportal/Configuration/Sections/General.cs
+++ mediaportal/Configuration/Sections/General.cs
@@ -78,7 +78,7 @@ namespace MediaPortal.Configuration.Sections
       InitializeComponent();
     }
 
-    private string loglevel = "2"; // 1= error, 2 = info, 3 = debug
+    private string loglevel = "3"; // 1= error, 2 = info, 3 = debug
     private int screennumber = 0; // 0 is the primary screen
 
     private string[][] sectionEntries = new string[][]
@@ -183,7 +183,7 @@ namespace MediaPortal.Configuration.Sections
                                                                          bool.Parse(currentSection[2])));
         }
 
-        loglevel = xmlreader.GetValueAsString("general", "loglevel", "2"); // set loglevel to info
+        loglevel = xmlreader.GetValueAsString("general", "loglevel", "3"); // set loglevel to debug
         cbDebug.SelectedIndex = Convert.ToInt16(loglevel);
         screennumber = xmlreader.GetValueAsInt("screenselector", "screennumber", 0);
 
