c251e91d557152c9c4dbc5f7defe36064f1ae9bc
dman_lfc <dman_lfc@team-mediaportal.com>
2009-07-27 19:12:46 +0000

    GenPix updates for debug driver test

--- TvEngine3/TVLibrary/TVLibrary/Implementations/DVB/DisEqC/GenPixBDA.cs
+++ TvEngine3/TVLibrary/TVLibrary/Implementations/DVB/DisEqC/GenPixBDA.cs
@@ -19,6 +19,7 @@
  *
  */
 using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using DirectShowLib;
 using DirectShowLib.BDA;
@@ -49,6 +50,33 @@ namespace TvLibrary.Implementations.DVB
       SEC_MINI_A = 0x00,
       SEC_MINI_B = 0x01
     }
+    #endregion
+
+    #region structs
+    public unsafe struct DISEQC_COMMAND
+    {
+      public byte Framing;
+      public byte Address;
+      public byte Command;
+      public byte Data0;
+      public byte Data1;
+      public byte Data2;
+      public byte MessageLength;
+      public static int GetSize()
+      {
+        return sizeof(DISEQC_COMMAND);
+      }
+    }
+
+    /*
+    [StructLayout(LayoutKind.Sequential), ComVisible(true)]
+    public unsafe struct DISEQC_COMMAND
+    {
+      [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)]
+      public byte[] ucMessage;
+      public byte ucMessageLength;
+    }*/
+
     #endregion
 
     /// <summary>
@@ -127,6 +155,7 @@ namespace TvLibrary.Implementations.DVB
       for (int i = 0; i < length; ++i)
         str += String.Format("0x{0:X} ", Marshal.ReadByte(_ptrDiseqc, i));
       Log.Log.Debug("GenPix: get diseqc returned: {0} len: {1}", str, length);
+
       //get tunning parameters for DiSEqC message
       //bit 0	(1)	: 0=low band, 1 = hi band
       //bit 1 (2) : 0=vertical, 1 = horizontal
@@ -140,25 +169,66 @@ namespace TvLibrary.Implementations.DVB
       int antennaNr = BandTypeConverter.GetAntennaNr(channel);
       bool hiBand = BandTypeConverter.IsHiBand(channel, parameters);
       bool isHorizontal = ((channel.Polarisation == Polarisation.LinearH) || (channel.Polarisation == Polarisation.CircularL));
+      //byte cmd = 0xf0;
       byte cmd = 0xf0;
       cmd |= (byte)(hiBand ? 1 : 0);
       cmd |= (byte)((isHorizontal) ? 2 : 0);
       cmd |= (byte)((antennaNr - 1) << 2);
-      ulong diseqc = 0xE0103800;//currently committed switches only. i.e. ports 1-4
-      diseqc += cmd;
+      
+      //ulong diseqc = 0xE0103800;//currently committed switches only. i.e. ports 1-4
+      //diseqc += cmd;
+
+      DISEQC_COMMAND DiseqcCommand = new DISEQC_COMMAND();
+      /*
+        DiseqcCommand.ucMessage[0] = 0xE0;//framing byte
+        DiseqcCommand.ucMessage[1] = 0x10;//address byte
+        DiseqcCommand.ucMessage[2] = 0x38;//command byte
+        DiseqcCommand.ucMessage[3] = cmd;//data byte (port group 0)
+        DiseqcCommand.ucMessage[4] = 0;//Need not fill this up
+        DiseqcCommand.ucMessage[5] = 0;//Need not fill this up
+        DiseqcCommand.ucMessageLength = 4;//Number of Valid bytes in the Command.
+
+      */
+
+      DiseqcCommand.Framing = 0xE0;
+      DiseqcCommand.Address = 0x10;
+      DiseqcCommand.Command = 0x38;
+      DiseqcCommand.Data0 = cmd;
+      DiseqcCommand.Data1 = 0;
+      DiseqcCommand.Data2 = 0;
+      DiseqcCommand.MessageLength = 4;
+      
+      int len = Marshal.SizeOf(DiseqcCommand);
+      //for (int i = 0; i < len; ++i)
+      //  Marshal.WriteByte(_ptrDiseqc, i, 0);
+
       //write the diseqc command to memory
       Log.Log.Debug("GenPix: Write diseqc command");
-      Marshal.WriteByte(_ptrDiseqc, 0, (byte)((diseqc >> 24) & 0xff));//framing byte
+      //Marshal.StructureToPtr(DiseqcCommand, _ptrDiseqc, true); 
+      
+      Marshal.WriteByte(_ptrDiseqc, 0, (byte)DiseqcCommand.Framing);//framing byte
+      Marshal.WriteByte(_ptrDiseqc, 1, (byte)DiseqcCommand.Address);//address byte
+      Marshal.WriteByte(_ptrDiseqc, 2, (byte)DiseqcCommand.Command);//command byte
+      Marshal.WriteByte(_ptrDiseqc, 3, (byte)DiseqcCommand.Data0);//data byte (port group 0)
+      Marshal.WriteByte(_ptrDiseqc, 4, (byte)DiseqcCommand.Data1);//Need not fill this up
+      Marshal.WriteByte(_ptrDiseqc, 5, (byte)DiseqcCommand.Data2);//Need not fill this up
+      Marshal.WriteInt16(_ptrDiseqc, 6, (byte)DiseqcCommand.MessageLength);//Number of Valid bytes in the Command.
+
+      /*Marshal.WriteByte(_ptrDiseqc, 0, (byte)((diseqc >> 24) & 0xff));//framing byte
       Marshal.WriteByte(_ptrDiseqc, 1, (byte)((diseqc >> 16) & 0xff));//address byte
       Marshal.WriteByte(_ptrDiseqc, 2, (byte)((diseqc >> 8) & 0xff));//command byte
       Marshal.WriteByte(_ptrDiseqc, 3, (byte)(diseqc & 0xff));//data byte (port group 0)
+      */
+
       //check the command
       string txt = "";
-      for (int i = 0; i < 4; ++i)
+      //int len = DISEQC_COMMAND.GetSize();
+
+      for (int i = 0; i < len; ++i)
         txt += String.Format("0x{0:X} ", Marshal.ReadByte(_ptrDiseqc, i));
-      Log.Log.Debug("GenPix: SendDiseq: {0}", txt);
+      Log.Log.Debug("GenPix: SendDiseq: {0} legth: {1}", txt, len);
       //set it to the tuner pin
-      int hr = _propertySet.Set(BdaTunerExtentionProperties, (int)BdaTunerExtension.KSPROPERTY_BDA_DISEQC, _ptrTempInstance, 188, _ptrDiseqc, 4);
+      int hr = _propertySet.Set(BdaTunerExtentionProperties, (int)BdaTunerExtension.KSPROPERTY_BDA_DISEQC, _ptrTempInstance, 32, _ptrDiseqc, len);
       if (hr != 0)
       {
         Log.Log.Info("GenPix: SendDiseqCommand returned: 0x{0:X} - {1}", hr, HResult.GetDXErrorDescription(hr));
