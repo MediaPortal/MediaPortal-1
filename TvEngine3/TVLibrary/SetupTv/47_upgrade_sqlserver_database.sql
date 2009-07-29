133a280a6c8d795b8b0e5f3bd34175b6db0d673e
chemelli <chemelli@team-mediaportal.com>
2009-07-29 22:00:47 +0000

    Fix for mantis #2370: Improved episode handling / display - thx hkjensen! - changed MSSQL script

--- TvEngine3/TVLibrary/SetupTv/47_upgrade_sqlserver_database.sql
+++ TvEngine3/TVLibrary/SetupTv/47_upgrade_sqlserver_database.sql
@@ -2,9 +2,9 @@ use %TvLibrary%
 GO
 
 ALTER TABLE Recording
-  ADD seriesNum varchar(200) NOT NULL CONSTRAINT [DF_Recording_SeriesNum] DEFAULT ''
-  ADD episodeNum varchar(200) NOT NULL CONSTRAINT [DF_Recording_EpisodeNum] DEFAULT ''
-  ADD episodePart varchar(MAX) NOT NULL CONSTRAINT [DF_Recording_EpisodePart] DEFAULT ''
+  ADD seriesNum varchar(200) NOT NULL CONSTRAINT [DF_Recording_SeriesNum] DEFAULT '',
+      episodeNum varchar(200) NOT NULL CONSTRAINT [DF_Recording_EpisodeNum] DEFAULT '',
+      episodePart varchar(MAX) NOT NULL CONSTRAINT [DF_Recording_EpisodePart] DEFAULT ''
   
 UPDATE Version SET versionNumber=47
 GO
