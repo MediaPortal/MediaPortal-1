b60c6ef665a7db0ef440fc652680210ad483f66a
manfred-mp <manfred@mbnet.fi>
2009-08-30 08:50:36 +0000

    Clean-up

--- MP-II/Source/Main/MediaPortal.Client/postbuild.bat
+++
@@ -1,6 +0,0 @@
-REM %1 = Solution directory
-REM %2 = Target directory
-
-echo Solution directory = %1, target directory = %2
-xcopy %1\Base\Client\*.* %2 /E /Y /D
-copy %1\Ui\Players\DShowHelper\release\DShowHelper.dll %2
