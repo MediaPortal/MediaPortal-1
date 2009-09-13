def9f7312a214fa08530a26f16ecbfc9981c681b
MisterD81 <misterd81-mp@yahoo.de>
2009-09-13 12:58:05 +0000

    First part of #2127: Update Live555 library - UsageEnvironment (only comment and formatting changes)

--- DirectShowFilters/LiveMedia555/UsageEnvironment/UsageEnvironment_version.hh
+++ DirectShowFilters/LiveMedia555/UsageEnvironment/UsageEnvironment_version.hh
@@ -1,10 +1,10 @@
 // Version information for the "UsageEnvironment" library
-// Copyright (c) 1996-2007 Live Networks, Inc.  All rights reserved.
+// Copyright (c) 1996-2009 Live Networks, Inc.  All rights reserved.
 
 #ifndef _USAGEENVIRONMENT_VERSION_HH
 #define _USAGEENVIRONMENT_VERSION_HH
 
-#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2007.01.17"
-#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1168992000
+#define USAGEENVIRONMENT_LIBRARY_VERSION_STRING	"2009.09.04"
+#define USAGEENVIRONMENT_LIBRARY_VERSION_INT		1252022400
 
 #endif
--- DirectShowFilters/LiveMedia555/UsageEnvironment/strDup.hh
+++ DirectShowFilters/LiveMedia555/UsageEnvironment/strDup.hh
@@ -11,13 +11,13 @@ more details.
 
 You should have received a copy of the GNU Lesser General Public License
 along with this library; if not, write to the Free Software Foundation, Inc.,
-59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 **********/
 
 #ifndef _STRDUP_HH
 #define _STRDUP_HH
 
-// Copyright (c) 1996-2003 Live Networks, Inc.  All rights reserved.
+// Copyright (c) 1996-2009 Live Networks, Inc.  All rights reserved.
 // A C++ equivalent to the standard C routine "strdup()".
 // This generates a char* that can be deleted using "delete[]"
 // Header
