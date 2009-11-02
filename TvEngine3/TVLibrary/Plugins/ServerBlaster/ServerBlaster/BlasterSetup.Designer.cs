36ac0e629a49b90eb012edb1c26db9e3ef569672
chemelli <chemelli@team-mediaportal.com>
2009-11-02 16:59:18 +0000

    Fixed mantis #0002092: ServerBlaster does not work in Windows Vista
    - Disabled MCE portion of ServerBlaster under Vista and newer [ reworked ComboBox1SelectedIndexChanged() ]
    - Renamed objects to be intuitive
    - Fixed layout

--- TvEngine3/TVLibrary/Plugins/ServerBlaster/ServerBlaster/BlasterSetup.Designer.cs
+++ TvEngine3/TVLibrary/Plugins/ServerBlaster/ServerBlaster/BlasterSetup.Designer.cs
@@ -28,189 +28,189 @@ namespace SetupTv.Sections
     /// </summary>
     private void InitializeComponent()
     {
-    	this.tabControl1 = new System.Windows.Forms.TabControl();
-    	this.tabPage1 = new System.Windows.Forms.TabPage();
-    	this.mpLabel1 = new MediaPortal.UserInterface.Controls.MPLabel();
-    	this.checkSendSelect = new System.Windows.Forms.CheckBox();
-    	this.checkBox1 = new System.Windows.Forms.CheckBox();
-    	this.comboBox4 = new System.Windows.Forms.ComboBox();
-    	this.comboBox3 = new System.Windows.Forms.ComboBox();
-    	this.comboBox2 = new System.Windows.Forms.ComboBox();
-    	this.label4 = new System.Windows.Forms.Label();
-    	this.label3 = new System.Windows.Forms.Label();
-    	this.label2 = new System.Windows.Forms.Label();
-    	this.label1 = new System.Windows.Forms.Label();
-    	this.comboBox1 = new System.Windows.Forms.ComboBox();
-    	this.tabControl1.SuspendLayout();
-    	this.tabPage1.SuspendLayout();
-    	this.SuspendLayout();
-    	// 
-    	// tabControl1
-    	// 
-    	this.tabControl1.AccessibleRole = System.Windows.Forms.AccessibleRole.HelpBalloon;
-    	this.tabControl1.Controls.Add(this.tabPage1);
-    	this.tabControl1.Location = new System.Drawing.Point(3, 3);
-    	this.tabControl1.Name = "tabControl1";
-    	this.tabControl1.SelectedIndex = 0;
-    	this.tabControl1.Size = new System.Drawing.Size(574, 308);
-    	this.tabControl1.TabIndex = 8;
-    	// 
-    	// tabPage1
-    	// 
-    	this.tabPage1.Controls.Add(this.mpLabel1);
-    	this.tabPage1.Controls.Add(this.checkSendSelect);
-    	this.tabPage1.Controls.Add(this.checkBox1);
-    	this.tabPage1.Controls.Add(this.comboBox4);
-    	this.tabPage1.Controls.Add(this.comboBox3);
-    	this.tabPage1.Controls.Add(this.comboBox2);
-    	this.tabPage1.Controls.Add(this.label4);
-    	this.tabPage1.Controls.Add(this.label3);
-    	this.tabPage1.Controls.Add(this.label2);
-    	this.tabPage1.Controls.Add(this.label1);
-    	this.tabPage1.Controls.Add(this.comboBox1);
-    	this.tabPage1.Location = new System.Drawing.Point(4, 22);
-    	this.tabPage1.Name = "tabPage1";
-    	this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-    	this.tabPage1.Size = new System.Drawing.Size(566, 282);
-    	this.tabPage1.TabIndex = 0;
-    	this.tabPage1.Text = "General Setup";
-    	this.tabPage1.UseVisualStyleBackColor = true;
-    	// 
-    	// mpLabel1
-    	// 
-    	this.mpLabel1.Location = new System.Drawing.Point(133, 85);
-    	this.mpLabel1.Name = "mpLabel1";
-    	this.mpLabel1.Size = new System.Drawing.Size(251, 43);
-    	this.mpLabel1.TabIndex = 20;
-    	this.mpLabel1.Text = "To configure the Hauppauge IR Blaster, use the original Hauppauge IR configuratio" +
-    	"n software.";
-    	// 
-    	// checkSendSelect
-    	// 
-    	this.checkSendSelect.AutoSize = true;
-    	this.checkSendSelect.Location = new System.Drawing.Point(20, 195);
-    	this.checkSendSelect.Name = "checkSendSelect";
-    	this.checkSendSelect.Size = new System.Drawing.Size(203, 17);
-    	this.checkSendSelect.TabIndex = 19;
-    	this.checkSendSelect.Text = "Send SELECT after channel numbers";
-    	this.checkSendSelect.UseVisualStyleBackColor = true;
-    	// 
-    	// checkBox1
-    	// 
-    	this.checkBox1.AutoSize = true;
-    	this.checkBox1.Location = new System.Drawing.Point(20, 172);
-    	this.checkBox1.Name = "checkBox1";
-    	this.checkBox1.Size = new System.Drawing.Size(129, 17);
-    	this.checkBox1.TabIndex = 18;
-    	this.checkBox1.Text = "Use extended logging";
-    	this.checkBox1.UseVisualStyleBackColor = true;
-    	// 
-    	// comboBox4
-    	// 
-    	this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-    	this.comboBox4.FormattingEnabled = true;
-    	this.comboBox4.Location = new System.Drawing.Point(133, 134);
-    	this.comboBox4.Name = "comboBox4";
-    	this.comboBox4.Size = new System.Drawing.Size(210, 21);
-    	this.comboBox4.TabIndex = 17;
-    	// 
-    	// comboBox3
-    	// 
-    	this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-    	this.comboBox3.FormattingEnabled = true;
-    	this.comboBox3.Location = new System.Drawing.Point(133, 107);
-    	this.comboBox3.Name = "comboBox3";
-    	this.comboBox3.Size = new System.Drawing.Size(210, 21);
-    	this.comboBox3.TabIndex = 16;
-    	// 
-    	// comboBox2
-    	// 
-    	this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-    	this.comboBox2.FormattingEnabled = true;
-    	this.comboBox2.Items.AddRange(new object[] {
-    	    	    	"Fast",
-    	    	    	"Medium",
-    	    	    	"Slow"});
-    	this.comboBox2.Location = new System.Drawing.Point(133, 82);
-    	this.comboBox2.Name = "comboBox2";
-    	this.comboBox2.Size = new System.Drawing.Size(121, 21);
-    	this.comboBox2.TabIndex = 13;
-    	// 
-    	// label4
-    	// 
-    	this.label4.AutoSize = true;
-    	this.label4.Location = new System.Drawing.Point(17, 137);
-    	this.label4.Name = "label4";
-    	this.label4.Size = new System.Drawing.Size(103, 13);
-    	this.label4.TabIndex = 12;
-    	this.label4.Text = "Card Using Blaster 2";
-    	// 
-    	// label3
-    	// 
-    	this.label3.AutoSize = true;
-    	this.label3.Location = new System.Drawing.Point(17, 110);
-    	this.label3.Name = "label3";
-    	this.label3.Size = new System.Drawing.Size(103, 13);
-    	this.label3.TabIndex = 11;
-    	this.label3.Text = "Card Using Blaster 1";
-    	// 
-    	// label2
-    	// 
-    	this.label2.AutoSize = true;
-    	this.label2.Location = new System.Drawing.Point(17, 85);
-    	this.label2.Name = "label2";
-    	this.label2.Size = new System.Drawing.Size(73, 13);
-    	this.label2.TabIndex = 10;
-    	this.label2.Text = "Blaster Speed";
-    	// 
-    	// label1
-    	// 
-    	this.label1.AutoSize = true;
-    	this.label1.Location = new System.Drawing.Point(17, 58);
-    	this.label1.Name = "label1";
-    	this.label1.Size = new System.Drawing.Size(66, 13);
-    	this.label1.TabIndex = 9;
-    	this.label1.Text = "Blaster Type";
-    	// 
-    	// comboBox1
-    	// 
-    	this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-    	this.comboBox1.FormattingEnabled = true;
-    	this.comboBox1.Items.AddRange(new object[] {
-    	    	    	"Microsoft ",
-    	    	    	"SMK",
-    	    	    	"Hauppauge"});
-    	this.comboBox1.Location = new System.Drawing.Point(133, 55);
-    	this.comboBox1.Name = "comboBox1";
-    	this.comboBox1.Size = new System.Drawing.Size(236, 21);
-    	this.comboBox1.TabIndex = 8;
-    	this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
-    	// 
-    	// BlasterSetup
-    	// 
-    	this.Controls.Add(this.tabControl1);
-    	this.Name = "BlasterSetup";
-    	this.Size = new System.Drawing.Size(590, 325);
-    	this.tabControl1.ResumeLayout(false);
-    	this.tabPage1.ResumeLayout(false);
-    	this.tabPage1.PerformLayout();
-    	this.ResumeLayout(false);
+      this.tabControl1 = new System.Windows.Forms.TabControl();
+      this.tabPage1 = new System.Windows.Forms.TabPage();
+      this.mpLabelAdditionalNotes = new MediaPortal.UserInterface.Controls.MPLabel();
+      this.checkSendSelect = new System.Windows.Forms.CheckBox();
+      this.checkBoxExtLog = new System.Windows.Forms.CheckBox();
+      this.comboBoxBlaster2 = new System.Windows.Forms.ComboBox();
+      this.comboBoxBlaster1 = new System.Windows.Forms.ComboBox();
+      this.comboBoxSpeed = new System.Windows.Forms.ComboBox();
+      this.labelUseBlaster2 = new System.Windows.Forms.Label();
+      this.labelUseBlaster1 = new System.Windows.Forms.Label();
+      this.labelBlasterSpeed = new System.Windows.Forms.Label();
+      this.labelBlasterType = new System.Windows.Forms.Label();
+      this.comboBoxType = new System.Windows.Forms.ComboBox();
+      this.tabControl1.SuspendLayout();
+      this.tabPage1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // tabControl1
+      // 
+      this.tabControl1.AccessibleRole = System.Windows.Forms.AccessibleRole.HelpBalloon;
+      this.tabControl1.Controls.Add(this.tabPage1);
+      this.tabControl1.Location = new System.Drawing.Point(3, 3);
+      this.tabControl1.Name = "tabControl1";
+      this.tabControl1.SelectedIndex = 0;
+      this.tabControl1.Size = new System.Drawing.Size(476, 308);
+      this.tabControl1.TabIndex = 8;
+      // 
+      // tabPage1
+      // 
+      this.tabPage1.Controls.Add(this.mpLabelAdditionalNotes);
+      this.tabPage1.Controls.Add(this.checkSendSelect);
+      this.tabPage1.Controls.Add(this.checkBoxExtLog);
+      this.tabPage1.Controls.Add(this.comboBoxBlaster2);
+      this.tabPage1.Controls.Add(this.comboBoxBlaster1);
+      this.tabPage1.Controls.Add(this.comboBoxSpeed);
+      this.tabPage1.Controls.Add(this.labelUseBlaster2);
+      this.tabPage1.Controls.Add(this.labelUseBlaster1);
+      this.tabPage1.Controls.Add(this.labelBlasterSpeed);
+      this.tabPage1.Controls.Add(this.labelBlasterType);
+      this.tabPage1.Controls.Add(this.comboBoxType);
+      this.tabPage1.Location = new System.Drawing.Point(4, 22);
+      this.tabPage1.Name = "tabPage1";
+      this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage1.Size = new System.Drawing.Size(468, 282);
+      this.tabPage1.TabIndex = 0;
+      this.tabPage1.Text = "General Setup";
+      this.tabPage1.UseVisualStyleBackColor = true;
+      // 
+      // mpLabelAdditionalNotes
+      // 
+      this.mpLabelAdditionalNotes.Location = new System.Drawing.Point(130, 85);
+      this.mpLabelAdditionalNotes.Name = "mpLabelAdditionalNotes";
+      this.mpLabelAdditionalNotes.Size = new System.Drawing.Size(251, 43);
+      this.mpLabelAdditionalNotes.TabIndex = 20;
+      this.mpLabelAdditionalNotes.Text = "Additional notes";
+      // 
+      // checkSendSelect
+      // 
+      this.checkSendSelect.AutoSize = true;
+      this.checkSendSelect.Location = new System.Drawing.Point(20, 195);
+      this.checkSendSelect.Name = "checkSendSelect";
+      this.checkSendSelect.Size = new System.Drawing.Size(203, 17);
+      this.checkSendSelect.TabIndex = 19;
+      this.checkSendSelect.Text = "Send SELECT after channel numbers";
+      this.checkSendSelect.UseVisualStyleBackColor = true;
+      // 
+      // checkBoxExtLog
+      // 
+      this.checkBoxExtLog.AutoSize = true;
+      this.checkBoxExtLog.Location = new System.Drawing.Point(20, 172);
+      this.checkBoxExtLog.Name = "checkBoxExtLog";
+      this.checkBoxExtLog.Size = new System.Drawing.Size(129, 17);
+      this.checkBoxExtLog.TabIndex = 18;
+      this.checkBoxExtLog.Text = "Use extended logging";
+      this.checkBoxExtLog.UseVisualStyleBackColor = true;
+      // 
+      // comboBoxBlaster2
+      // 
+      this.comboBoxBlaster2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.comboBoxBlaster2.FormattingEnabled = true;
+      this.comboBoxBlaster2.Location = new System.Drawing.Point(133, 134);
+      this.comboBoxBlaster2.Name = "comboBoxBlaster2";
+      this.comboBoxBlaster2.Size = new System.Drawing.Size(210, 21);
+      this.comboBoxBlaster2.TabIndex = 17;
+      // 
+      // comboBoxBlaster1
+      // 
+      this.comboBoxBlaster1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.comboBoxBlaster1.FormattingEnabled = true;
+      this.comboBoxBlaster1.Location = new System.Drawing.Point(133, 107);
+      this.comboBoxBlaster1.Name = "comboBoxBlaster1";
+      this.comboBoxBlaster1.Size = new System.Drawing.Size(210, 21);
+      this.comboBoxBlaster1.TabIndex = 16;
+      // 
+      // comboBoxSpeed
+      // 
+      this.comboBoxSpeed.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.comboBoxSpeed.FormattingEnabled = true;
+      this.comboBoxSpeed.Items.AddRange(new object[] {
+            "Fast",
+            "Medium",
+            "Slow"});
+      this.comboBoxSpeed.Location = new System.Drawing.Point(133, 82);
+      this.comboBoxSpeed.Name = "comboBoxSpeed";
+      this.comboBoxSpeed.Size = new System.Drawing.Size(121, 21);
+      this.comboBoxSpeed.TabIndex = 13;
+      // 
+      // labelUseBlaster2
+      // 
+      this.labelUseBlaster2.AutoSize = true;
+      this.labelUseBlaster2.Location = new System.Drawing.Point(17, 137);
+      this.labelUseBlaster2.Name = "labelUseBlaster2";
+      this.labelUseBlaster2.Size = new System.Drawing.Size(103, 13);
+      this.labelUseBlaster2.TabIndex = 12;
+      this.labelUseBlaster2.Text = "Card Using Blaster 2";
+      // 
+      // labelUseBlaster1
+      // 
+      this.labelUseBlaster1.AutoSize = true;
+      this.labelUseBlaster1.Location = new System.Drawing.Point(17, 110);
+      this.labelUseBlaster1.Name = "labelUseBlaster1";
+      this.labelUseBlaster1.Size = new System.Drawing.Size(103, 13);
+      this.labelUseBlaster1.TabIndex = 11;
+      this.labelUseBlaster1.Text = "Card Using Blaster 1";
+      // 
+      // labelBlasterSpeed
+      // 
+      this.labelBlasterSpeed.AutoSize = true;
+      this.labelBlasterSpeed.Location = new System.Drawing.Point(17, 85);
+      this.labelBlasterSpeed.Name = "labelBlasterSpeed";
+      this.labelBlasterSpeed.Size = new System.Drawing.Size(73, 13);
+      this.labelBlasterSpeed.TabIndex = 10;
+      this.labelBlasterSpeed.Text = "Blaster Speed";
+      // 
+      // labelBlasterType
+      // 
+      this.labelBlasterType.AutoSize = true;
+      this.labelBlasterType.Location = new System.Drawing.Point(17, 58);
+      this.labelBlasterType.Name = "labelBlasterType";
+      this.labelBlasterType.Size = new System.Drawing.Size(66, 13);
+      this.labelBlasterType.TabIndex = 9;
+      this.labelBlasterType.Text = "Blaster Type";
+      // 
+      // comboBoxType
+      // 
+      this.comboBoxType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+      this.comboBoxType.FormattingEnabled = true;
+      this.comboBoxType.Items.AddRange(new object[] {
+            "Microsoft ",
+            "SMK",
+            "Hauppauge"});
+      this.comboBoxType.Location = new System.Drawing.Point(133, 55);
+      this.comboBoxType.Name = "comboBoxType";
+      this.comboBoxType.Size = new System.Drawing.Size(236, 21);
+      this.comboBoxType.TabIndex = 8;
+      this.comboBoxType.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
+      // 
+      // BlasterSetup
+      // 
+      this.Controls.Add(this.tabControl1);
+      this.Name = "BlasterSetup";
+      this.Size = new System.Drawing.Size(483, 316);
+      this.tabControl1.ResumeLayout(false);
+      this.tabPage1.ResumeLayout(false);
+      this.tabPage1.PerformLayout();
+      this.ResumeLayout(false);
+
     }
-    private MediaPortal.UserInterface.Controls.MPLabel mpLabel1;
+    private MediaPortal.UserInterface.Controls.MPLabel mpLabelAdditionalNotes;
 
     #endregion
 
     private System.Windows.Forms.TabControl tabControl1;
     private System.Windows.Forms.TabPage tabPage1;
-    private System.Windows.Forms.ComboBox comboBox2;
-    private System.Windows.Forms.Label label4;
-    private System.Windows.Forms.Label label3;
-    private System.Windows.Forms.Label label2;
-    private System.Windows.Forms.Label label1;
-    private System.Windows.Forms.ComboBox comboBox1;
-    private System.Windows.Forms.ComboBox comboBox3;
-    private System.Windows.Forms.ComboBox comboBox4;
-    private System.Windows.Forms.CheckBox checkBox1;
+    private System.Windows.Forms.ComboBox comboBoxSpeed;
+    private System.Windows.Forms.Label labelUseBlaster2;
+    private System.Windows.Forms.Label labelUseBlaster1;
+    private System.Windows.Forms.Label labelBlasterSpeed;
+    private System.Windows.Forms.Label labelBlasterType;
+    private System.Windows.Forms.ComboBox comboBoxType;
+    private System.Windows.Forms.ComboBox comboBoxBlaster1;
+    private System.Windows.Forms.ComboBox comboBoxBlaster2;
+    private System.Windows.Forms.CheckBox checkBoxExtLog;
     private System.Windows.Forms.CheckBox checkSendSelect;
 
   }
--- TvEngine3/TVLibrary/Plugins/ServerBlaster/ServerBlaster/BlasterSetup.cs
+++ TvEngine3/TVLibrary/Plugins/ServerBlaster/ServerBlaster/BlasterSetup.cs
@@ -15,19 +15,19 @@ namespace SetupTv.Sections
     {
       TvBusinessLayer layer = new TvBusinessLayer();
       Setting setting = layer.GetSetting("SrvBlasterType");
-      setting.Value = comboBox1.SelectedIndex.ToString();
+      setting.Value = comboBoxType.SelectedIndex.ToString();
       setting.Persist();
       setting = layer.GetSetting("SrvBlasterSpeed");
-      setting.Value = comboBox2.SelectedIndex.ToString();
+      setting.Value = comboBoxSpeed.SelectedIndex.ToString();
       setting.Persist();
       setting = layer.GetSetting("SrvBlaster1Card");
-      setting.Value = comboBox3.SelectedIndex.ToString();
+      setting.Value = comboBoxBlaster1.SelectedIndex.ToString();
       setting.Persist();
       setting = layer.GetSetting("SrvBlaster2Card");
-      setting.Value = comboBox4.SelectedIndex.ToString();
+      setting.Value = comboBoxBlaster2.SelectedIndex.ToString();
       setting.Persist();
       setting = layer.GetSetting("SrvBlasterLog");
-      setting.Value = Convert.ToString(checkBox1.Checked);
+      setting.Value = Convert.ToString(checkBoxExtLog.Checked);
       setting.Persist();
       setting = layer.GetSetting("SrvBlasterSendSelect");
       setting.Value = Convert.ToString(checkSendSelect.Checked);
@@ -41,53 +41,82 @@ namespace SetupTv.Sections
 
 
       TvBusinessLayer layer = new TvBusinessLayer();
-      comboBox1.SelectedIndex = Convert.ToInt16(layer.GetSetting("SrvBlasterType", "0").Value);
-      comboBox2.SelectedIndex = Convert.ToInt16(layer.GetSetting("SrvBlasterSpeed", "0").Value);
-      comboBox3.Items.Clear();
-      comboBox4.Items.Clear();
-      comboBox3.Items.Add("None");
-      comboBox4.Items.Add("None");
+      comboBoxType.SelectedIndex = Convert.ToInt16(layer.GetSetting("SrvBlasterType", "0").Value);
+      comboBoxSpeed.SelectedIndex = Convert.ToInt16(layer.GetSetting("SrvBlasterSpeed", "0").Value);
+      comboBoxBlaster1.Items.Clear();
+      comboBoxBlaster2.Items.Clear();
+      comboBoxBlaster1.Items.Add("None");
+      comboBoxBlaster2.Items.Add("None");
       for (int i = 0; i < layer.Cards.Count; ++i)
       {
         Card card = layer.Cards[i];
-        comboBox3.Items.Add(card.Name);
-        comboBox4.Items.Add(card.Name);
+        comboBoxBlaster1.Items.Add(card.Name);
+        comboBoxBlaster2.Items.Add(card.Name);
       }
-      Log.WriteFile("CB1Size {0}, CB2Size {1}, BT1 {2}, BT2 {3}", comboBox3.Items.Count, comboBox3.Items.Count, Convert.ToInt16(layer.GetSetting("SrvBlaster1Card", "0").Value), Convert.ToInt16(layer.GetSetting("SrvBlaster2Card", "0").Value));
-      comboBox3.SelectedIndex = Convert.ToInt16(layer.GetSetting("SrvBlaster1Card", "0").Value);
-      comboBox4.SelectedIndex = Convert.ToInt16(layer.GetSetting("SrvBlaster2Card", "0").Value);
-      checkBox1.Checked = (layer.GetSetting("SrvBlasterLog").Value == "True");
+      Log.WriteFile("CB1Size {0}, CB2Size {1}, BT1 {2}, BT2 {3}", comboBoxBlaster1.Items.Count, comboBoxBlaster1.Items.Count, Convert.ToInt16(layer.GetSetting("SrvBlaster1Card", "0").Value), Convert.ToInt16(layer.GetSetting("SrvBlaster2Card", "0").Value));
+      comboBoxBlaster1.SelectedIndex = Convert.ToInt16(layer.GetSetting("SrvBlaster1Card", "0").Value);
+      comboBoxBlaster2.SelectedIndex = Convert.ToInt16(layer.GetSetting("SrvBlaster2Card", "0").Value);
+      checkBoxExtLog.Checked = (layer.GetSetting("SrvBlasterLog").Value == "True");
       checkSendSelect.Checked = (layer.GetSetting("SrvBlasterSendSelect").Value == "True");
     }
 
     void ComboBox1SelectedIndexChanged(object sender, EventArgs e)
     {
-    	switch (comboBox1.SelectedIndex)
-      	{
-        	case 0:
-    		case 1:
-    			comboBox2.Visible=true;
-    			comboBox3.Visible=true;
-    			comboBox4.Visible=true;
-    			label2.Visible=true;
-    			label3.Visible=true;
-    			label4.Visible=true;
-    			checkSendSelect.Visible=true;
-    			mpLabel1.Visible=false;
-    			break;
-        	
-        	case 2: // Hauppauge blasting
-    			comboBox2.Visible=false;
-    			comboBox3.Visible=false;
-    			comboBox4.Visible=false;
-    			label2.Visible=false;
-    			label3.Visible=false;
-    			label4.Visible=false;
-    			checkSendSelect.Visible=false;
-    			mpLabel1.Visible=true;
-    			break;  
-        	default: break;
-      	}
+      bool enabled;
+
+      switch (comboBoxType.SelectedIndex)
+      {
+        case 0: // Microsoft
+          int osver = (OSInfo.OSInfo.OSMajorVersion * 10) + OSInfo.OSInfo.OSMinorVersion;
+          if (osver >= 60)
+          {
+            enabled = false;
+            checkBoxExtLog.Visible = false;
+            mpLabelAdditionalNotes.Text =
+              "Because of an architecture change in the driver handling, MCE blasting is no more available under Vista and newer operating systems";
+          }
+          else
+          {
+            enabled = true;
+          }
+          break;
+
+        case 1: // SMK
+          enabled = true;
+          break;
+
+        case 2: // Hauppauge blasting
+          enabled = false;
+          checkBoxExtLog.Visible = true;
+          mpLabelAdditionalNotes.Text = "To configure the Hauppauge IR Blaster, use the original Hauppauge IR configuration software.";
+          break;
+        default:
+          enabled = false;
+          break;
+      }
+      if (enabled)
+      {
+        comboBoxSpeed.Visible = true;
+        comboBoxBlaster1.Visible = true;
+        comboBoxBlaster2.Visible = true;
+        labelBlasterSpeed.Visible = true;
+        labelUseBlaster1.Visible = true;
+        labelUseBlaster2.Visible = true;
+        checkBoxExtLog.Visible = true;
+        checkSendSelect.Visible = true;
+        mpLabelAdditionalNotes.Visible = false;
+      }
+      else
+      {
+        comboBoxSpeed.Visible = false;
+        comboBoxBlaster1.Visible = false;
+        comboBoxBlaster2.Visible = false;
+        labelBlasterSpeed.Visible = false;
+        labelUseBlaster1.Visible = false;
+        labelUseBlaster2.Visible = false;
+        checkSendSelect.Visible = false;
+        mpLabelAdditionalNotes.Visible = true;
+      }
     }
   }
 }
