ac31ef9132380e226ce30c7f060b67962c530c08
rtv_sf <github@rjtv.de>
2009-11-23 20:35:34 +0000

    fix for mantis #2556: TVguide speedup + datamodel changes. - added named defaults for SQL Server script to avoid random naming

--- TvEngine3/TVLibrary/SetupTv/49_upgrade_sqlserver_database.sql
+++ TvEngine3/TVLibrary/SetupTv/49_upgrade_sqlserver_database.sql
@@ -2,25 +2,24 @@ use %TvLibrary%
 GO
 
 ALTER TABLE Recording
-  ADD isRecording bit NOT NULL DEFAULT 0,
-      idSchedule int NOT NULL DEFAULT 0      
-        
+  ADD isRecording bit NOT NULL CONSTRAINT [DF_Recording_isRecording] DEFAULT 0,
+      idSchedule int NOT NULL CONSTRAINT [DF_Recording_idSchedule] DEFAULT 0         
 GO
 
 DROP INDEX IDX_Program_Notify ON Program
 DROP INDEX IX_Program_EPG_Lookup ON Program 
-
 GO
 
 ALTER TABLE Program
-  ADD state int NOT NULL DEFAULT 0
+  ADD state int NOT NULL CONSTRAINT [DF_Program_state] DEFAULT 0 
+GO
 
 ALTER TABLE Program
-  DROP COLUMN notify
+  DROP COLUMN notify 
+GO
 
 ALTER TABLE Schedule
-  ADD idParentSchedule int NOT NULL DEFAULT 0
-  
+  ADD idParentSchedule int NOT NULL CONSTRAINT [DF_Schedule_idParentSchedule] DEFAULT 0   
 GO
 
 CREATE UNIQUE INDEX IX_Program_EPG_Lookup ON Program 
@@ -42,8 +41,8 @@ INCLUDE
     state,
     parentalRating
 )
-CREATE INDEX IDX_Program_State ON Program(state)
+CREATE INDEX IDX_Program_State ON Program(state) 
 GO
 
-UPDATE Version SET versionNumber=49
+UPDATE Version SET versionNumber=49 
 GO
