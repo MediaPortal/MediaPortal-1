cccaae42acb2196f8daed1c774bd6d3891bd0b7b
rtv_sf <github@rjtv.de>
2009-11-23 20:39:22 +0000

    fix for mantis #2556: TVguide speedup + datamodel changes. - cleanup of mysql script

--- TvEngine3/TVLibrary/SetupTv/49_upgrade_mysql_database.sql
+++ TvEngine3/TVLibrary/SetupTv/49_upgrade_mysql_database.sql
@@ -1,12 +1,12 @@
 USE %TvLibrary%;
 
-ALTER TABLE Recording ADD COLUMN isRecording bit DEFAULT 0 NOT NULL;
-ALTER TABLE Recording ADD COLUMN idSchedule int DEFAULT 0 NOT NULL;
+ALTER TABLE "Recording" ADD COLUMN "isRecording" BIT NOT NULL DEFAULT 0; 
+ALTER TABLE "Recording" ADD COLUMN "idSchedule" INT NOT NULL DEFAULT 0; 
 
-ALTER TABLE Program ADD COLUMN state int DEFAULT 0 NOT NULL, DROP COLUMN notify;
-ALTER TABLE Program ADD INDEX IDX_Program_State(state);
+ALTER TABLE "Program" ADD COLUMN "state" INT NOT NULL DEFAULT 0; 
+ALTER TABLE "Program" DROP COLUMN "notify"; 
+ALTER TABLE "Program" ADD INDEX IDX_Program_State(state); 
 
-
-ALTER TABLE Schedule ADD COLUMN idParentSchedule int DEFAULT 0 NOT NULL;
+ALTER TABLE "Schedule" ADD COLUMN "idParentSchedule" INT NOT NULL DEFAULT 0; 
 
 UPDATE Version SET versionNumber=49;
