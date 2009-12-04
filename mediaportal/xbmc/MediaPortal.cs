495c53f885d77db249e2d8d80001bea9f67d1299
mr_hipp <mr_hipp@team-mediaportal.com>
2009-12-04 06:59:26 +0000

    0002547: Volume control malfunctions after standby

--- mediaportal/xbmc/MediaPortal.cs
+++ mediaportal/xbmc/MediaPortal.cs
@@ -1228,6 +1228,10 @@ public class MediaPortalApp : D3DApp, IRender
         return;
       }
 
+      // Dispose the volume handle to get a fresh handle to the mixer
+      // Otherwise volume does not work for ATI HDMI devices after standby
+      VolumeHandler.Dispose();
+
       _onResumeRunning = true;
 
       ReOpenDBs();
