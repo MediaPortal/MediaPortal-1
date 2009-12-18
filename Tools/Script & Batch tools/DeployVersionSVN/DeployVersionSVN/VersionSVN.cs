6bca074d6946b9979c4ea98f516294a86603f766
chemelli <chemelli@team-mediaportal.com>
2009-12-18 19:06:28 +0000

    Getting version as "Last committed at revision" instead of "Updated to revision"

--- Tools/Script & Batch tools/DeployVersionSVN/DeployVersionSVN/VersionSVN.cs
+++ Tools/Script & Batch tools/DeployVersionSVN/DeployVersionSVN/VersionSVN.cs
@@ -54,7 +54,7 @@ namespace DeployVersionSVN
         {
           string svn = proc.StandardOutput.ReadToEnd();
 
-          Regex tortoiseRegex = new Regex("Update.+ (?<version>[0-9]+)");
+          Regex tortoiseRegex = new Regex("Last committed at revision.+ (?<version>[0-9]+)");
 
           string ver = tortoiseRegex.Match(svn).Groups["version"].Value;
           if (String.IsNullOrEmpty(ver))
