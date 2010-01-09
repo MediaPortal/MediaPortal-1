bd381a9a78d96bac8c4a5bdcb952bedba5f11886
Albert78 <albert@team-mediaportal.com>
2010-01-09 07:37:58 +0000

    New branch for performance rework

--- TvEngine3/TVLibrary/SetupTv/42_upgrade_mysql_database.sql
+++
@@ -1,6 +0,0 @@
-USE %TvLibrary%;
-
-ALTER TABLE "TuningDetail"
- ADD COLUMN "isVCRSignal" bit(1) NOT NULL;
-
-UPDATE "Version" SET "versionNumber"=42;
--- TvEngine3/TVLibrary/SetupTv/44_upgrade_mysql_database.sql
+++
@@ -1,7 +0,0 @@
-﻿USE %TvLibrary%;
-
-ALTER TABLE "Program"
- ADD COLUMN "episodeName" text NOT NULL,
- ADD COLUMN "episodePart" text NOT NULL;
-
-UPDATE "Version" SET "versionNumber"=44;
