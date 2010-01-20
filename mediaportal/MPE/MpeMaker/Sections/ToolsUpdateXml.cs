81255ad0afaa4ccdf464ba6d9ce7656ea51a3cc4
chefkoch <chefkoch@team-mediaportal.com>
2010-01-20 23:54:30 +0000

    disabled buttons, if textBoxes are empty set InitialDirectory when opening saveFileDialog

--- mediaportal/MPE/MpeMaker/Sections/ToolsUpdateXml.cs
+++ mediaportal/MPE/MpeMaker/Sections/ToolsUpdateXml.cs
@@ -42,14 +42,63 @@ namespace MpeMaker.Sections
       InitializeComponent();
     }
 
+    #region Browse buttons
+
     private void button1_Click(object sender, EventArgs e)
     {
-      saveFileDialog1.FileName = textBox1.Text;
-      if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+      SetupSaveFileDialog(textBox1.Text);
+      if (saveFileDialog.ShowDialog() == DialogResult.OK)
       {
-        textBox1.Text = saveFileDialog1.FileName;
+        textBox1.Text = saveFileDialog.FileName;
       }
     }
+
+    private void btn_browse1_Click(object sender, EventArgs e)
+    {
+      SetupSaveFileDialog(txt_list1.Text);
+      if (saveFileDialog.ShowDialog() == DialogResult.OK)
+      {
+        txt_list1.Text = saveFileDialog.FileName;
+      }
+    }
+
+    private void btn_browse2_Click(object sender, EventArgs e)
+    {
+      SetupSaveFileDialog(txt_list2.Text);
+      if (saveFileDialog.ShowDialog() == DialogResult.OK)
+      {
+        txt_list2.Text = saveFileDialog.FileName;
+      }
+    }
+
+    private void SetupSaveFileDialog(string oldPath)
+    {
+      saveFileDialog.FileName = oldPath;
+      if (String.IsNullOrEmpty(oldPath)) return;
+
+      string directory;
+      string file = Path.GetFileName(oldPath);
+
+      // set initial dir to old one
+      directory = Path.GetDirectoryName(oldPath);
+      if (Directory.Exists(directory))
+      {
+        saveFileDialog.InitialDirectory = directory;
+        saveFileDialog.FileName = file;
+        return;
+      }
+      
+      // set initial dir to the xmp2 package dir
+      directory = Path.GetFileName(Package.ProjectSettings.ProjectFilename);
+      if (Directory.Exists(directory))
+      {
+        saveFileDialog.InitialDirectory = directory;
+        saveFileDialog.FileName = file;
+        return;
+      }
+    }
+
+    #endregion
 
     /// <summary>
     /// Handles the Click event of the btn_gen control.
@@ -69,34 +118,19 @@ namespace MpeMaker.Sections
     public void Set(PackageClass pak)
     {
       Package = pak;
+
       loading = true;
       textBox1.Text = Package.ProjectSettings.UpdatePath1;
       txt_list1.Text = Package.ProjectSettings.UpdatePath2;
       txt_list2.Text = Package.ProjectSettings.UpdatePath3;
       loading = false;
+
+      textBox_TextChanged(null, null);
     }
 
     public PackageClass Get()
     {
       throw new NotImplementedException();
-    }
-
-    private void btn_browse1_Click(object sender, EventArgs e)
-    {
-      saveFileDialog1.FileName = txt_list1.Text;
-      if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-      {
-        txt_list1.Text = saveFileDialog1.FileName;
-      }
-    }
-
-    private void btn_browse2_Click(object sender, EventArgs e)
-    {
-      saveFileDialog1.FileName = txt_list2.Text;
-      if (saveFileDialog1.ShowDialog() == DialogResult.OK)
-      {
-        txt_list2.Text = saveFileDialog1.FileName;
-      }
     }
 
     private void add_list_Click(object sender, EventArgs e)
@@ -112,13 +146,16 @@ namespace MpeMaker.Sections
       list.Save(xmlFile);
     }
 
-    private void textBox1_TextChanged(object sender, EventArgs e)
+    private void textBox_TextChanged(object sender, EventArgs e)
     {
-      if (loading)
-        return;
+      if (loading) return;
+
       Package.ProjectSettings.UpdatePath1 = textBox1.Text;
       Package.ProjectSettings.UpdatePath2 = txt_list1.Text;
       Package.ProjectSettings.UpdatePath3 = txt_list2.Text;
+
+      btn_gen.Enabled = !String.IsNullOrEmpty(textBox1.Text);
+      add_list.Enabled = !(String.IsNullOrEmpty(txt_list1.Text) || String.IsNullOrEmpty(txt_list2.Text));
     }
   }
 }
