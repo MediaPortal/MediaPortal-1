646f94f48399e3187e30aa61ebe9c53bd2bbcfdc
chemelli <chemelli@team-mediaportal.com>
2010-08-06 19:45:49 +0000

    Automate bin building

---
+++ Build dshowhelper - Owlsroost.bat
@@ -0,0 +1,28 @@
+@ECHO OFF
+
+REM detect if BUILD_TYPE should be release or debug
+if not %1!==Debug! goto RELEASE
+:DEBUG
+set BUILD_TYPE=Debug
+goto START
+:RELEASE
+set BUILD_TYPE=Release
+goto START
+
+
+:START
+REM Select program path based on current machine environment
+set progpath=%ProgramFiles%
+if not "%ProgramFiles(x86)%".=="". set progpath=%ProgramFiles(x86)%
+
+
+echo.
+echo -= dshowhelper - Owlsroost =-
+echo -= build mode: %BUILD_TYPE% =-
+echo. > build.log
+echo.
+
+
+echo.
+echo Building dshowhelper - Owlsroost...
+"%progpath%\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com" ".\dshowhelper - Owlsroost\dshowhelper.sln" /Build >> build.log
