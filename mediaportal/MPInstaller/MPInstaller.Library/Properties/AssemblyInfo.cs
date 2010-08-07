b106236e8d7bc0f242dd4ef062ee1b640c73fc45
chemelli <chemelli@team-mediaportal.com>
2010-08-07 11:47:25 +0000

    Updated AssemblyInfo to 1.1.1

--- mediaportal/MPInstaller/MPInstaller.Library/Properties/AssemblyInfo.cs
+++ mediaportal/MPInstaller/MPInstaller.Library/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.1.1.*")]
+[assembly: AssemblyFileVersion("1.1.1.0")]
