5c1acbd834ddd451c5069c792383c4f37a0d6c03
chemelli <chemelli@team-mediaportal.com>
2010-08-07 11:43:02 +0000

    Updated AssemblyInfo to 1.2.0

--- mediaportal/MPE/MpeInstaller/Properties/AssemblyInfo.cs
+++ mediaportal/MPE/MpeInstaller/Properties/AssemblyInfo.cs
@@ -47,6 +47,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.*")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 [assembly: ComVisibleAttribute(true)]
--- mediaportal/MPInstaller/MPInstaller.Library/Properties/AssemblyInfo.cs
+++ mediaportal/MPInstaller/MPInstaller.Library/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.*")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
