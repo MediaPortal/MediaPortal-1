9f18c7b9c98c267465d9288ff0113832078ebed3
chefkoch <chefkoch@team-mediaportal.com>
2010-09-12 10:41:42 +0000

    moved common build commands into BuildInit.bat

--- Build dshowhelper.bat
+++ Build dshowhelper.bat
@@ -1,28 +1,10 @@
 @ECHO OFF
 
-REM detect if BUILD_TYPE should be release or debug
-if not %1!==Debug! goto RELEASE
-:DEBUG
-set BUILD_TYPE=Debug
-goto START
-:RELEASE
-set BUILD_TYPE=Release
-goto START
+set project=dshowhelper - Owlsroost
+set log=build.log
 
-
-:START
-REM Select program path based on current machine environment
-set progpath=%ProgramFiles%
-if not "%ProgramFiles(x86)%".=="". set progpath=%ProgramFiles(x86)%
-
-
-echo.
-echo -= dshowhelper - Owlsroost =-
-echo -= build mode: %BUILD_TYPE% =-
-echo. > build.log
-echo.
-
+call BuildInit.bat %1
 
 echo.
 echo Building dshowhelper - Owlsroost...
-"%progpath%\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com" ".\dshowhelper\dshowhelper.sln" /Rebuild "%BUILD_TYPE%" >> build.log
+"%progpath%\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com" ".\dshowhelper\dshowhelper.sln" /Rebuild "%BUILD_TYPE%" >> %log%
