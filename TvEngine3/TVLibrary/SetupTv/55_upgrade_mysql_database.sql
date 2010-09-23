c644ced8e5ea0f677e9f8337cb02eb55c11592db
ronilse <ronilse67@gmail.com>
2010-09-23 17:05:33 +0000

    Fix for MySQL update script(55), just a missing ",".

--- TvEngine3/TVLibrary/SetupTv/55_upgrade_mysql_database.sql
+++ TvEngine3/TVLibrary/SetupTv/55_upgrade_mysql_database.sql
@@ -2,7 +2,7 @@ USE %TvLibrary%;
 
 CREATE TABLE "PendingDeletion" (
   "idPendingDeletion" int(11) NOT NULL auto_increment,  
-  "fileName" varchar(260) NOT NULL 
+  "fileName" varchar(260) NOT NULL, 
   PRIMARY KEY  ("idPendingDeletion")
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
