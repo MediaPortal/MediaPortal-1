72454750085378705789310b805b86224fde5629
chemelli <chemelli@team-mediaportal.com>
2010-09-26 12:29:49 +0000

    Removed old gfx

--- mediaportal/Configuration/ConfigSplashScreen.cs
+++ mediaportal/Configuration/ConfigSplashScreen.cs
@@ -290,7 +290,7 @@ namespace MediaPortal.Configuration
         // SplashForm
         // 
         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        this.BackgroundImage = global::MediaPortal.Configuration.Properties.Resources.mplogo1;
+        this.BackgroundImage = global::MediaPortal.Configuration.Properties.Resources.mplogo;
         this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
         this.ClientSize = new System.Drawing.Size(400, 172);
         this.Controls.Add(this.panel1);
