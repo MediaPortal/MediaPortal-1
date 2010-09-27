86207a6b5171a71a101bb620e11503910d034254
chefkoch <chefkoch@team-mediaportal.com>
2010-09-27 23:34:09 +0000

    added more build scripts and fixed one

---
+++ Build/VS2008_Rebuild_Debug_Win7RefreshRateHelper.bat
@@ -0,0 +1,3 @@
+@echo off
+
+call "VS2008_Rebuild_Release_Win7RefreshRateHelper.bat" Debug
---
+++ Build/VS2008_Rebuild_Release_Win7RefreshRateHelper.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+rem build init
+set project=Win7RefreshRateHelper
+call BuildInit.bat %1
+
+rem build
+echo.
+echo Building %project%
+"%progpath%\Microsoft Visual Studio 9.0\Common7\IDE\devenv.com" "..\mediaportal\Core.cpp\Win7RefreshRateHelper\Win7RefreshRateHelper.sln" /Rebuild "%BUILD_TYPE%" >> %log%
