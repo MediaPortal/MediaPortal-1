1370e367e111c3f770b1988a2b6b35fb79cb8e1b
tourettes <tourettes@team-mediaportal.com>
2010-09-27 18:59:59 +0000

    updated version

--- DirectShowFilters/MPAudioRenderer/source/Settings.cpp
+++ DirectShowFilters/MPAudioRenderer/source/Settings.cpp
@@ -39,7 +39,7 @@ AudioRendererSettings::AudioRendererSettings() :
   m_wWASAPIPreferredDeviceId(NULL)
 {
   LogRotate();
-  Log("MP Audio Renderer - v0.90");
+  Log("MP Audio Renderer - v0.91");
 
   LoadSettingsFromRegistry();
 }
