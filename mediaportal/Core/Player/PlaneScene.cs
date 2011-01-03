0d7008385e6c1193da5809b293d53a9c64810580
tourettes <tourettes@team-mediaportal.com>
2011-01-03 20:59:17 +0000

    Reverting fix for: 0003128: No video preview in MyVideos in some situations.
    Was causing some side effecs

--- mediaportal/Core/Player/PlaneScene.cs
+++ mediaportal/Core/Player/PlaneScene.cs
@@ -375,12 +375,19 @@ namespace MediaPortal.Player
         }
         else
         {
-          // We're in preview mode. Check if a VideoWindow is defined or 
-          // video/tv preview window is enable
-          Rectangle rect = GUIGraphicsContext.VideoWindow;
-          if (((rect.Height < 1) && (rect.Width < 1)) || (!GUIGraphicsContext.Overlay))
+          // we're in preview mode. Check if we are in the tv module
+
+          if (!InTv)
           {
-            return false;
+            //we are not in the my tv module
+            //then check if a VideoWindow is defined or video/tv preview window is enable
+            Rectangle rect = GUIGraphicsContext.VideoWindow;
+            //BAV: todo -> remove Overlay check -> should no longer be needed
+            //if (((rect.Height < 1) && (rect.Width < 1)) || (!GUIGraphicsContext.Overlay)) return false; //not enabled, dont show tv
+            if ((rect.Height < 1) && (rect.Width < 1))
+            {
+              return false;
+            }
           }
         }
 
