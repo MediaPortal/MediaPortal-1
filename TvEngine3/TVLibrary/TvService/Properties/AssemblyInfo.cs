7d261b5e3765e23844c4f4ebc03524d2e439d2cc
blackbird99 <MediaPortal@blackbird99.demon.co.uk>
2011-02-23 21:35:37 +0000

    AssemblyInfo updated to 1.1.3

--- TvEngine3/TVLibrary/TvService/Properties/AssemblyInfo.cs
+++ TvEngine3/TVLibrary/TvService/Properties/AssemblyInfo.cs
@@ -53,5 +53,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("1.1.2.*")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.3.*")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
--- mediaportal/MPInstaller/MPInstaller.Library/Properties/AssemblyInfo.cs
+++ mediaportal/MPInstaller/MPInstaller.Library/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 
-[assembly: AssemblyVersion("1.1.2.*")]
-[assembly: AssemblyFileVersion("1.1.2.0")]
+[assembly: AssemblyVersion("1.1.3.*")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
