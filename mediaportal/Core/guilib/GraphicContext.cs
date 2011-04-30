5ccecc289527b0e5598b38becc3ac9699d1a21aa
Jamesonuk <Jameson_uk@team-mediaportal.com>
2011-04-30 08:39:23 +0000

    Mantis 3292 - Removed redundant window IDs WINDOW_RECORDEDTVGENRE and WINDOW_RECORDEDTVCHANNEL

--- mediaportal/Core/guilib/GraphicContext.cs
+++ mediaportal/Core/guilib/GraphicContext.cs
@@ -1498,14 +1498,11 @@ namespace MediaPortal.GUI.Library
         case (int)GUIWindow.Window.WINDOW_TVFULLSCREEN:
         case (int)GUIWindow.Window.WINDOW_TVGUIDE:
         case (int)GUIWindow.Window.WINDOW_RECORDEDTV:
-        case (int)GUIWindow.Window.WINDOW_RECORDEDTVCHANNEL:
-        case (int)GUIWindow.Window.WINDOW_RECORDEDTVGENRE:
         case (int)GUIWindow.Window.WINDOW_SCHEDULER:
         case (int)GUIWindow.Window.WINDOW_SEARCHTV:
         case (int)GUIWindow.Window.WINDOW_TELETEXT:
         case (int)GUIWindow.Window.WINDOW_FULLSCREEN_TELETEXT:
         case (int)GUIWindow.Window.WINDOW_TV_SCHEDULER_PRIORITIES:
-        case (int)GUIWindow.Window.WINDOW_TV_CONFLICTS:
         case (int)GUIWindow.Window.WINDOW_TV_NO_SIGNAL:
         case (int)GUIWindow.Window.WINDOW_TV_PROGRAM_INFO:
           return true;
