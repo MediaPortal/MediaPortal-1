0cc98c3543dda07df48a1cf76da76a328ebdfb67
gibman <gibman.dk@gmail.com>
2011-07-03 20:11:37 +0000

    3605: tvguide does not remember previous program when returning. reverted : additional fix for fullscreen TV not remembering the last program.

--- TvEngine3/TVLibrary/TvPlugin/TvPlugin/TvGuideBase.cs
+++ TvEngine3/TVLibrary/TvPlugin/TvPlugin/TvGuideBase.cs
@@ -725,8 +725,7 @@ namespace TvPlugin
               UpdateChannelCount();
 
               bool isPreviousWindowTvGuideRelated = (message.Param1 == (int)Window.WINDOW_TV_PROGRAM_INFO ||
-                                                     message.Param1 == (int)Window.WINDOW_VIDEO_INFO ||
-                                                     message.Param1 == (int)Window.WINDOW_TVFULLSCREEN);
+                                                     message.Param1 == (int)Window.WINDOW_VIDEO_INFO);
 
               if (!isPreviousWindowTvGuideRelated)
               {
@@ -887,6 +886,7 @@ namespace TvPlugin
             {
               OnSelectItem(true);
               Update(false);
+              SetFocus();
             }
             else if (_cursorY == 0)
             {
