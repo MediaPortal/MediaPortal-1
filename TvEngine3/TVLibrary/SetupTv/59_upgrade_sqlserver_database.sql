3cb237b2eeff063b4b719bd82c1a13be080eb35c
Jamesonuk <Jameson_uk@team-mediaportal.com>
2011-08-04 19:07:17 +0000

    Mantis 3333 - Update for SQL Server

--- TvEngine3/TVLibrary/SetupTv/59_upgrade_sqlserver_database.sql
+++ TvEngine3/TVLibrary/SetupTv/59_upgrade_sqlserver_database.sql
@@ -1,7 +1,13 @@
 use %TvLibrary%
 GO
 
-ALTER TABLE Card ALTER COLUMN stopgraph DEFAULT ((1))
+alter table Card drop constraint DF_Card_stopgraph
+GO
+
+
+ALTER TABLE Card
+  ADD CONSTRAINT DF_Card_stopgraph
+  DEFAULT (1) FOR stopgraph
 GO
 
 UPDATE Version SET versionNumber=59
