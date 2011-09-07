8acd7e9e6033e14c3b7246156496e2a3deb8d6d5
Panayotis Kouvarakis <pkouvar-github@yahoo.gr>
2011-09-07 16:38:41 +0300

    Changed DeployVersionSVN to DeployVersionGIT
    Initial changes to use git version instead of SVN version

---
+++ Tools/Script & Batch tools/DeployVersionGIT/DeployVersionGIT/Program.cs
@@ -0,0 +1,121 @@
+#region Copyright (C) 2005-2011 Team MediaPortal
+
+// Copyright (C) 2005-2011 Team MediaPortal
+// http://www.team-mediaportal.com
+// 
+// MediaPortal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MediaPortal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
+using System.IO;
+using MediaPortal.Utilities.CommandLine;
+
+namespace DeployVersionGIT
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+
+      string version;
+      int versionInt;
+      string fullVersion;
+
+      ICommandLineOptions argsOptions = new CommandLineOptions();
+
+      try
+      {
+        CommandLine.Parse(args, ref argsOptions);
+      }
+      catch (ArgumentException)
+      {
+        argsOptions.DisplayOptions();
+        Environment.Exit(0);
+      }
+
+      CommandLineOptions options = argsOptions as CommandLineOptions;
+
+      if (!options.IsOption(CommandLineOptions.Option.git))
+      {
+        argsOptions.DisplayOptions();
+        Environment.Exit(0);
+      }
+      string directory = options.GetOption(CommandLineOptions.Option.git);
+
+      if (options.IsOption(CommandLineOptions.Option.UpdateCopyright))
+      {
+        string copyrightText = options.GetOption(CommandLineOptions.Option.UpdateCopyright);
+        Console.WriteLine("Writing Copyright text: " + copyrightText);
+
+        AssemblyUpdate copyrightUpdate = new AssemblyUpdate(copyrightText, AssemblyUpdate.UpdateMode.Copyright);
+        copyrightUpdate.UpdateAll(directory);
+
+        Environment.Exit(0);
+      }
+
+      if (options.IsOption(CommandLineOptions.Option.GetVersion))
+      {
+        VersionGIT git = new VersionGIT();
+        bool versionExists = git.ReadVersion(directory);
+
+        if (File.Exists("version.txt"))
+        {
+          File.Delete("version.txt");
+        }
+        if (!versionExists)
+        {
+          Console.WriteLine("Local GIT not up to date");
+          Environment.Exit(0);
+        }
+
+        version = git.GetVersion();
+        fullVersion = git.GetFullVersion();
+
+        TextWriter write = new StreamWriter("version.txt");
+        write.Write(fullVersion);
+        write.Close();
+        Int32.TryParse(version, out versionInt);
+        Environment.Exit(versionInt);
+      }
+
+      if (options.IsOption(CommandLineOptions.Option.revert))
+      {
+        version = "0";
+        fullVersion = "";
+
+        Console.WriteLine("Reverting to build 0");
+      }
+      else
+      {
+        VersionGIT git = new VersionGIT();
+        bool versionExists = git.ReadVersion(directory);
+
+        if (!versionExists)
+        {
+          Console.WriteLine("Local GIT not up to date");
+          Environment.Exit(0);
+        }
+
+        version = git.GetVersion();
+        fullVersion = git.GetFullVersion();
+
+        Console.WriteLine("GIT Version: " + fullVersion);
+      }
+
+      AssemblyUpdate update = new AssemblyUpdate(version, fullVersion);
+      update.UpdateAll(directory);
+    }
+  }
+}
---
+++ Tools/Script & Batch tools/DeployVersionGIT/DeployVersionGIT/VersionGIT.cs
@@ -0,0 +1,107 @@
+#region Copyright (C) 2005-2011 Team MediaPortal
+
+// Copyright (C) 2005-2011 Team MediaPortal
+// http://www.team-mediaportal.com
+// 
+// MediaPortal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MediaPortal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DeployVersionGIT
+{
+  public class VersionGIT
+  {
+    private string _version;
+    private string _fullVersion;
+
+    public bool ReadVersion(string directory)
+    {
+      FileInfo file = new FileInfo(Environment.GetEnvironmentVariable("ProgramFiles") + @"\Git\bin\git.exe");
+
+      ProcessStartInfo procInfo = new ProcessStartInfo();
+      procInfo.RedirectStandardOutput = true;
+      procInfo.UseShellExecute = false;
+      procInfo.Arguments = String.Format("--git-dir=\"{0}\\.git\" --no-pager describe", directory);
+      procInfo.FileName = file.FullName;
+
+      Console.WriteLine("Running : {0}", file.FullName);
+
+      if (file.Exists)
+      {
+        // Start process
+        Process proc = Process.Start(procInfo);
+
+        // Get process output
+        if (proc != null)
+        {
+          string git = proc.StandardOutput.ReadToEnd();
+
+          Regex tortoiseRegex = new Regex(@"^.*-(?<version>[0-9]+)-[0-9a-f]{8}$");
+
+          string ver = tortoiseRegex.Match(git).Groups["version"].Value;
+          if (String.IsNullOrEmpty(ver))
+          {
+            Console.WriteLine("Unable to determine GIT version.");
+            return false;
+          }
+          _version = ver;
+          _fullVersion = git;
+        }
+        else
+        {
+          return false;
+        }
+
+        // Get branchname
+        procInfo.Arguments = String.Format("--git-dir=\"{0}\\.git\" --no-pager branch --no-color", directory);
+        proc = Process.Start(procInfo);
+        if (proc != null)
+        {
+          string git = proc.StandardOutput.ReadToEnd();
+
+          Regex tortoiseRegex = new Regex(@"^[*]\s*(?<branch>.*)$");
+
+          string branch = tortoiseRegex.Match(git).Groups["branch"].Value;
+          if (String.IsNullOrEmpty(branch))
+          {
+            Console.WriteLine("Unable to determine GIT branch.");
+          }
+          else
+          {
+            _fullVersion = _fullVersion + " (" + branch + " branch)";
+          }
+        }
+        return true;
+      }
+
+      Console.WriteLine("git.exe not found!");
+      return false;
+    }
+
+    public string GetVersion()
+    {
+      return _version;
+    }
+
+    public string GetFullVersion()
+    {
+      return _fullVersion;
+    }
+  }
+}
