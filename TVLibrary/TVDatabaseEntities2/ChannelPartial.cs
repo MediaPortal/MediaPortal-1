1fd9f4e78d6b209cf8e758faadecd0c4c6dde6c5
gibman <gibman.dk@gmail.com>
2011-09-29 09:40:37 +0000

    TVDatabaseEntities

---
+++ TVLibrary/TVDatabaseEntities2/ChannelPartial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TVDatabaseEntities
+{
+  public partial class Channel
+  {
+    public override string ToString()
+    {
+      return displayName;
+    }
+  }
+}
---
+++ TVLibrary/TVDatabaseEntities2/ProgramPartial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace TVDatabaseEntities
+{
+  /*[MetadataType (typeof (BookingMetadata))]
+public partial class Booking
+{
+ // This is your custom partial class     
+}
+
+public class BookingMetadata
+{
+ [Required] [StringLength(15)]
+ public object ClientName { get; set; }
+
+ [Range(1, 20)]
+ public object NumberOfGuests { get; set; }
+
+ [Required] [DataType(DataType.Date)]
+ public object ArrivalDate { get; set; }
+}
+*/
+
+  public class ProgramMetadata
+  {
+    [ProgramAttribute("Title", 1)]
+    public string title { get; set; }
+
+    [ProgramAttribute("Description", 2)]
+    public string description { get; set; }
+
+    [ProgramAttribute("SeriesNum", 3)]
+    public int seriesNum { get; set; }
+
+    [ProgramAttribute("EpisodeNum", 4)]
+    public int episodeNum { get; set; }
+
+    [ProgramAttribute("Classification", 5)]
+    public int classification { get; set; }
+
+    [ProgramAttribute("StarRating", 6)]
+    public int starRating { get; set; }
+
+    [ProgramAttribute("ParentalRating", 7)]
+    public int parentalRating { get; set; }
+
+    [ProgramAttribute("EpisodeName", 8)]
+    public string episodeName { get; set; }
+
+    [ProgramAttribute("EpisodePart", 9)]
+    public int episodePart { get; set; }    
+  }
+
+  [MetadataType(typeof(ProgramMetadata))]
+  public partial class Program
+  {
+   
+  }
+}
