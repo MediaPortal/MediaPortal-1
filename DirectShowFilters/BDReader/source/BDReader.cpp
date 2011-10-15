e42a7dbc9263141f340d178aa4a54b576e415e00
tourettes <tourettes@team-mediaportal.com>
2011-10-15 17:34:13 +0000

    Reduce dropped frame amount on clip boundaries

--- DirectShowFilters/BDReader/source/BDReader.cpp
+++ DirectShowFilters/BDReader/source/BDReader.cpp
@@ -1148,19 +1148,16 @@ void CBDReaderFilter::DeliverBeginFlush()
   if (m_pVideoPin && m_pVideoPin->IsConnected())
   {
     m_pVideoPin->DeliverBeginFlush();
-    m_pVideoPin->Stop();
   }
 
   if (m_pAudioPin && m_pAudioPin->IsConnected())
   {
     m_pAudioPin->DeliverBeginFlush();
-    m_pAudioPin->Stop();
   }
 
   if (m_pSubtitlePin && m_pSubtitlePin->IsConnected())
   {
     m_pSubtitlePin->DeliverBeginFlush();
-    m_pSubtitlePin->Stop();
   }
 }
 
