6fabd38d91a0a599cdb772fa1ad534251e3d7b24
tourettes <tourettes@team-mediaportal.com>
2011-10-17 10:19:43 +0000

    Update time conversion macros in BDReader as well

--- DirectShowFilters/BDReader/source/BDReader.cpp
+++ DirectShowFilters/BDReader/source/BDReader.cpp
@@ -41,8 +41,8 @@
 // For more details for memory leak detection see the alloctracing.h header
 #include "..\..\alloctracing.h"
 
-#define CONVERT_90KHz_DS(x) (REFERENCE_TIME)(x * (1000.0 / 9.0))
-#define CONVERT_DS_90KHz(x) (REFERENCE_TIME)(x / (1000.0 / 9.0))
+#define CONVERT_DS_90KHz(x) (REFERENCE_TIME)((x * (111 +1 / 9))
+#define CONVERT_90KHz_DS(x) (REFERENCE_TIME)((x * 111) + (x/9))
 
 extern void SetThreadName(DWORD dwThreadID, char* threadName);
 extern void LogDebug(const char *fmt, ...);
