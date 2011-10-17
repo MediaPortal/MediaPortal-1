dd5226862b294d0a9608435e3f767cbe00fc333e
tourettes <tourettes@team-mediaportal.com>
2011-10-17 10:35:58 +0000

    Revering BDReader.cpp - time conversion changes break Alpha and Omega BD

--- DirectShowFilters/BDReader/source/BDReader.cpp
+++ DirectShowFilters/BDReader/source/BDReader.cpp
@@ -41,8 +41,13 @@
 // For more details for memory leak detection see the alloctracing.h header
 #include "..\..\alloctracing.h"
 
-#define CONVERT_DS_90KHz(x) (REFERENCE_TIME)((x * (111 +1 / 9)))
-#define CONVERT_90KHz_DS(x) (REFERENCE_TIME)((x * 111) + (x / 9))
+// TODO - check why these break Alpha & Omega BD
+
+//#define CONVERT_DS_90KHz(x) (REFERENCE_TIME)((x * (111 +1 / 9)))
+//#define CONVERT_90KHz_DS(x) (REFERENCE_TIME)((x * 111) + (x / 9))
+
+#define CONVERT_90KHz_DS(x) (REFERENCE_TIME)(x * (1000.0 / 9.0))
+#define CONVERT_DS_90KHz(x) (REFERENCE_TIME)(x / (1000.0 / 9.0))
 
 extern void SetThreadName(DWORD dwThreadID, char* threadName);
 extern void LogDebug(const char *fmt, ...);
