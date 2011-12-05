b7079925e73dbc109a4bf01a018d553895cae20e
SilentException <mitja.skuver@gmail.com>
2011-12-05 23:00:07 +0100

    Mantis 0003687: improved logging of System.Data.SQLite version, code cleanup (mostly deleted comments and unused stuff) + some formatting

--- mediaportal/Databases/SQLiteException.cs
+++ mediaportal/Databases/SQLiteException.cs
@@ -27,23 +27,6 @@ namespace SQLite.NET
   /// </summary>
   public class SQLiteException : Exception
   {
-    private SQLiteClient.SqliteError errorCode;
-
-    // Methods
     public SQLiteException(string message) : base(message) {}
-
-
-    // Properties
-    public SQLiteException(string message, SQLiteClient.SqliteError code) : base(message)
-    {
-      this.errorCode = code;
-    }
-
-
-    // Fields
-    public SQLiteClient.SqliteError ErrorCode
-    {
-      get { return this.errorCode; }
-    }
   }
 }
