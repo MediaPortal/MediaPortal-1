94b6a26f0dceefd6e67a53fe04bdc7ae1d410ef1
mm1352000 <music_man1352000@yahoo.com.au>
2011-12-25 16:47:59 +1300

    Minor: re-order/update usings.

--- TvEngine3/TVLibrary/TVLibrary/Implementations/DVB/DisEqC/Omicom.cs
+++ TvEngine3/TVLibrary/TVLibrary/Implementations/DVB/DisEqC/Omicom.cs
@@ -20,12 +20,11 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Runtime.InteropServices;
+using System.Text;
 using DirectShowLib;
-using TvLibrary.Channels;
 using DirectShowLib.BDA;
+using TvLibrary.Channels;
 
 namespace TvLibrary.Implementations.DVB
 {
