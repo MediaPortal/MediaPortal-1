d60fb84af2c3990e2a84b92d22a51add92399be0
blackbird99 <MediaPortal@blackbird99.demon.co.uk>
2011-12-28 23:44:00 +0000

    Seeking Implemented

--- DirectShowFilters/BDReader/source/Clip.cpp
+++ DirectShowFilters/BDReader/source/Clip.cpp
@@ -151,6 +151,11 @@ Packet* CClip::ReturnNextVideoPacket(REFERENCE_TIME playlistOffset)
         ret->bNewClip = true;
         firstVideo=false;
         ret->pmt = CreateMediaType(m_videoPmt);
+        if (clipReset)
+        {
+          ret->bResuming = true;
+          clipReset = false;
+        }
       }
 
       if (ret->rtStart > videoPlaybackPosition) videoPlaybackPosition = ret->rtStart;
