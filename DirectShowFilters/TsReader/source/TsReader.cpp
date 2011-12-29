cdc132c2b49c715c677e759a30277cd9b7b4f10d
owlsroost <tony@owlsroost.org.uk>
2011-12-29 16:00:46 +0000

    Bug fix for 'no sound after zapping' problem discussed here - http://forum.team-mediaportal.com/bugreports-74/1-2-2-tv-sound-problems-103877/
    Version raised to v0.4.15

--- DirectShowFilters/TsReader/source/TsReader.cpp
+++ DirectShowFilters/TsReader/source/TsReader.cpp
@@ -170,7 +170,7 @@ CTsReaderFilter::CTsReaderFilter(IUnknown *pUnk, HRESULT *phr):
   TCHAR filename[1024];
   GetLogFile(filename);
   ::DeleteFile(filename);
-  LogDebug("--------------- v0.4.15 XXX -------------------");
+  LogDebug("--------------- v0.4.15 -------------------");
 
   m_fileReader=NULL;
   m_fileDuration=NULL;
