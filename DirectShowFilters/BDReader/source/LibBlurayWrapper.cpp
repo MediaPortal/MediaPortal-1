cf5f9983a383e99066cc16f81a7bd7389b5af6c6
tourettes <tourettes@team-mediaportal.com>
2011-12-29 19:18:31 +0200

    Updated bluray.h - it was not updated when the bluray.dll was updated previously Added logging for new BD events

--- DirectShowFilters/BDReader/source/LibBlurayWrapper.cpp
+++ DirectShowFilters/BDReader/source/LibBlurayWrapper.cpp
@@ -951,6 +951,16 @@ void CLibBlurayWrapper::LogEvent(const BD_EVENT& pEvent, bool pIgnoreNoneEvent)
   case BD_EVENT_STILL_TIME:
     //LogDebug("    BD_EVENT_STILL_TIME - %d", pEvent.param);
     break;
+  case BD_EVENT_SOUND_EFFECT:
+    //LogDebug("    BD_EVENT_SOUND_EFFECT - %d", pEvent.param);
+    break;
+  case BD_EVENT_POPUP:
+    LogDebug("    BD_EVENT_POPUP - %d", pEvent.param);
+    break;
+  case BD_EVENT_MENU:
+    LogDebug("    BD_EVENT_MENU - %d", pEvent.param);
+    break;
+
   default:
     LogDebug("    ERROR - no event!");
   }
