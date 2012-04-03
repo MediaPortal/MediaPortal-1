119e03c4650ab39539f1eeb4b865cf74bb4206f8
tourettes <tourettes@team-mediaportal.com>
2012-04-03 21:21:16 +0300

    Increased AC3 filter's buffer count

--- DirectShowFilters/MPAudioRenderer/source/AC3EncoderFilter.cpp
+++ DirectShowFilters/MPAudioRenderer/source/AC3EncoderFilter.cpp
@@ -21,7 +21,7 @@
 #include "alloctracing.h"
 
 #define AC3_OUT_BUFFER_SIZE   ((AC3_MAX_COMP_FRAME_SIZE + AC3_BITSTREAM_OVERHEAD) * 5)
-#define AC3_OUT_BUFFER_COUNT  4
+#define AC3_OUT_BUFFER_COUNT  20
 
 template<class T> inline T odd2even(T x)
 {
