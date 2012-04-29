8db6701402bc2642bffd2c7b8377eb2e1a07bff7
dedahr <dedahr@gmail.com>
2012-04-29 21:22:15 +0200

    Put back coloring of user groups which was removed by mistake

--- mediaportal/WindowPlugins/GUIVideoFiles/GUIVideoTitle.cs
+++ mediaportal/WindowPlugins/GUIVideoFiles/GUIVideoTitle.cs
@@ -808,6 +808,7 @@ namespace MediaPortal.GUI.Video
           if (string.IsNullOrEmpty(movie.Title) && handler.CurrentLevel + 1 < handler.MaxLevels)
           {
             item.IsFolder = true;
+            item.IsRemote = true;
           }
           else
           {
