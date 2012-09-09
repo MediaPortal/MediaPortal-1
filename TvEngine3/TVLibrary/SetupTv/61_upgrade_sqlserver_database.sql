873b278209d47fabdc607ea8515dc36557487c5a
Rik Smith <webmaster@rsmith.nl>
2012-09-09 15:30:40 +0200

    Fix SQL server file

--- TvEngine3/TVLibrary/SetupTv/61_upgrade_sqlserver_database.sql
+++ TvEngine3/TVLibrary/SetupTv/61_upgrade_sqlserver_database.sql
@@ -2,7 +2,7 @@ USE %TvLibrary%;
 GO
 
 ALTER TABLE Channel ADD channelNumber INT NOT NULL CONSTRAINT [DF_Channel_ChannelNumber] DEFAULT 10000
-UPDATE Channel c SET c.channelNumber = (SELECT TOP 1 t.channelNumber FROM TuningDetail t WHERE t.idChannel = c.idChannel ORDER BY t.idTuning)
+UPDATE Channel SET channelNumber = (SELECT TOP 1 t.channelNumber FROM TuningDetail AS t WHERE t.idChannel = channel.idChannel ORDER BY t.idTuning)
 
 UPDATE Version SET versionNumber=61
 GO
