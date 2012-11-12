7457c57b2d5f0953c7c10873cede187bed6945d2
sebastiii <sebastiii@team-mediaportal.com>
2012-11-12 07:34:16 +0100

    Use ffmpeg.exe based on command line from Jay_UK's.

--- mediaportal/Core/Util/Util.cs
+++ mediaportal/Core/Util/Util.cs
@@ -1797,7 +1797,7 @@ namespace MediaPortal.Util
             }
             catch (Exception ex2)
             {
-              Log.Error("Util: Error setting process priority for {0}: {1}", aAppName, ex2.Message);
+              Log.Warn("Util: Error setting process priority for {0}: {1}", aAppName, ex2.Message);
             }
           }
           // Read in asynchronous  mode to avoid deadlocks (if error stream is full)
@@ -1812,7 +1812,7 @@ namespace MediaPortal.Util
 
           if (!success)
           {
-            Log.Error("Util: Error executing {0}: return code {1}", aAppName, ExternalProc.ExitCode);
+            Log.Warn("Util: Error executing {0}: return code {1}", aAppName, ExternalProc.ExitCode);
           }
 
           ExternalProc.OutputDataReceived -= new DataReceivedEventHandler(OutputDataHandler);
@@ -1820,7 +1820,7 @@ namespace MediaPortal.Util
         }
         catch (Exception ex)
         {
-          Log.Error("Util: Error executing {0}: {1}", aAppName, ex.Message);
+          Log.Warn("Util: Error executing {0}: {1}", aAppName, ex.Message);
         }
       }
       else
