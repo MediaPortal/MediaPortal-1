b2402e0ff54917afde849ff030bad550dc03212a
Scythe42 <scythe42@gmail.com>
2012-12-10 13:41:25 +0100

    Missing FullScreenSplashScreen.Designer.cs Update added

--- mediaportal/MediaPortal.Application/FullScreenSplashScreen.Designer.cs
+++ mediaportal/MediaPortal.Application/FullScreenSplashScreen.Designer.cs
@@ -109,8 +109,8 @@
       this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
       this.Text = "FullScreenSplashScreen";
       this.TopMost = true;
-      this.Load += new System.EventHandler(this.FullScreenSplashScreen_Load);
-      this.Activated += new System.EventHandler(this.FullScreenSplashScreen_Activated);
+      this.Load += new System.EventHandler(this.FullScreenSplashScreenLoad);
+      this.Activated += new System.EventHandler(this.FullScreenSplashScreenActivated);
       ((System.ComponentModel.ISupportInitialize)(this.pbBackground)).EndInit();
       this.ResumeLayout(false);
 
