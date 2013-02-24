d2ca56007a31409de79b5e9fdbb71eee3d2bbd25
morpheusxx <morpheus_xx@team-mediaportal.com>
2013-02-24 18:06:42 +0100

    fix for cache helper not being thread safe, double checked locking pattern added.

--- TvEngine3/Mediaportal/TV/Server/TVDatabase/TvBusinessLayer/Entities/Cache/EntityCacheHelper.cs
+++ TvEngine3/Mediaportal/TV/Server/TVDatabase/TvBusinessLayer/Entities/Cache/EntityCacheHelper.cs
@@ -7,8 +7,11 @@ namespace Mediaportal.TV.Server.TVDatabase.TVBusinessLayer.Entities.Cache
 {
   public class EntityCacheHelper : Singleton<EntityCacheHelper>
   {
-    private EntityCache<ProgramCategory, string> _programCategoryCache;
-    private EntityCache<Setting, string> _settingCache;
+    private volatile EntityCache<ProgramCategory, string> _programCategoryCache;
+    private volatile EntityCache<Setting, string> _settingCache;
+
+    private readonly object _settingCacheLock = new object();
+    private readonly object _programCategoryCacheLock = new object();
 
     public EntityCache<ProgramCategory, string> ProgramCategoryCache
     {
@@ -16,9 +19,16 @@ namespace Mediaportal.TV.Server.TVDatabase.TVBusinessLayer.Entities.Cache
       {
         if (_programCategoryCache == null)
         {
-          IList<ProgramCategory> allCats = ProgramCategoryManagement.ListAllProgramCategories();
-          Dictionary<string, ProgramCategory> cache = allCats.ToDictionary(programCategory => programCategory.Category);
-          _programCategoryCache = new EntityCache<ProgramCategory, string>(cache);
+          lock (_programCategoryCacheLock)
+          {
+            if (_programCategoryCache == null)
+            {
+              IList<ProgramCategory> allCats = ProgramCategoryManagement.ListAllProgramCategories();
+              Dictionary<string, ProgramCategory> cache =
+                allCats.ToDictionary(programCategory => programCategory.Category);
+              _programCategoryCache = new EntityCache<ProgramCategory, string>(cache);
+            }
+          }
         }
         return _programCategoryCache;
       }
@@ -30,9 +40,15 @@ namespace Mediaportal.TV.Server.TVDatabase.TVBusinessLayer.Entities.Cache
       {
         if (_settingCache == null)
         {
-          IList<Setting> allSettings = SettingsManagement.ListAllSettings();
-          Dictionary<string, Setting> cache = allSettings.ToDictionary(setting => setting.Tag);
-          _settingCache = new EntityCache<Setting, string>(cache);
+          lock (_settingCacheLock)
+          {
+            if (_settingCache == null)
+            {
+              IList<Setting> allSettings = SettingsManagement.ListAllSettings();
+              Dictionary<string, Setting> cache = allSettings.ToDictionary(setting => setting.Tag);
+              _settingCache = new EntityCache<Setting, string>(cache);
+            }
+          }
         }
         return _settingCache;
       }
