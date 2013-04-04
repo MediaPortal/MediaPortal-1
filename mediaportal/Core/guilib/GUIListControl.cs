e3127c139c70ef7c2e949e7d5a470b1eec920bb0
Scythe42 <scythe42@gmail.com>
2013-04-04 04:38:29 +0200

    Mantis #3983
    Fixed potential  division by zero exceptions when calculating number of pages for various controls. Checked every call before SetRange() calls. In addition adjusted SetRange to make sure that end >= start.

--- mediaportal/Core/guilib/GUIListControl.cs
+++ mediaportal/Core/guilib/GUIListControl.cs
@@ -2338,10 +2338,13 @@ namespace MediaPortal.GUI.Library
       int iPages = 1;
       if (_listItems.Count > 0)
       {
-        iPages = _listItems.Count / _itemsPerPage;
-        if ((_listItems.Count % _itemsPerPage) != 0)
+        iPages = _itemsPerPage == 0 ? 0 : _listItems.Count / _itemsPerPage;
+        if (_itemsPerPage != 0)
         {
-          iPages++;
+          if ((_listItems.Count % _itemsPerPage) != 0)
+          {
+            iPages++;
+          }
         }
       }
       _upDownControl.SetRange(1, iPages);
@@ -3378,9 +3381,12 @@ namespace MediaPortal.GUI.Library
         _listItems.Add(item);
       }
       int iPages = _itemsPerPage == 0 ? 0 : _listItems.Count / _itemsPerPage;
-      if (_itemsPerPage != 0 && (_listItems.Count % _itemsPerPage) != 0)
+      if (_itemsPerPage != 0)
       {
-        iPages++;
+        if ((_listItems.Count % _itemsPerPage) != 0)
+        {
+          iPages++;
+        }
       }
       _upDownControl.SetRange(1, iPages);
       _upDownControl.Value = 1;
@@ -3409,10 +3415,13 @@ namespace MediaPortal.GUI.Library
       {
         _listItems.Insert(index, item);
       }
-      int iPages = _listItems.Count / _itemsPerPage;
-      if ((_listItems.Count % _itemsPerPage) != 0)
+      int iPages = _itemsPerPage == 0 ? 0 : _listItems.Count / _itemsPerPage;
+      if (_itemsPerPage != 0)
       {
-        iPages++;
+        if ((_listItems.Count % _itemsPerPage) != 0)
+        {
+          iPages++;
+        }
       }
       _upDownControl.SetRange(1, iPages);
       _upDownControl.Value = 1;
--- mediaportal/Core/guilib/GUIThumbnailPanel.cs
+++ mediaportal/Core/guilib/GUIThumbnailPanel.cs
@@ -1351,9 +1351,12 @@ namespace MediaPortal.GUI.Library
           }
           int iItemsPerPage = _rowCount * _columnCount;
           int iPages = iItemsPerPage == 0 ? 0 : _listItems.Count / iItemsPerPage;
-          if ((_listItems.Count % iItemsPerPage) != 0)
+          if (iItemsPerPage != 0)
           {
-            iPages++;
+            if ((_listItems.Count % iItemsPerPage) != 0)
+            {
+              iPages++;
+            }
           }
           _controlUpDown.SetRange(1, iPages);
           _controlUpDown.Value = 1;
@@ -1896,9 +1899,12 @@ namespace MediaPortal.GUI.Library
 
       int iItemsPerPage = _rowCount * _columnCount;
       int iPages = iItemsPerPage == 0 ? 0 : _listItems.Count / iItemsPerPage;
-      if ((_listItems.Count % iItemsPerPage) != 0)
+      if (iItemsPerPage != 0)
       {
-        iPages++;
+        if ((_listItems.Count % iItemsPerPage) != 0)
+        {
+          iPages++;
+        }
       }
       _controlUpDown.SetRange(1, iPages);
       _controlUpDown.Value = 1;
@@ -2871,9 +2877,12 @@ namespace MediaPortal.GUI.Library
       _listItems.Add(item);
       int iItemsPerPage = _rowCount * _columnCount;
       int iPages = iItemsPerPage == 0 ? 0 : _listItems.Count / iItemsPerPage;
-      if (iItemsPerPage != 0 && (_listItems.Count % iItemsPerPage) != 0)
+      if (iItemsPerPage != 0)
       {
-        iPages++;
+        if ((_listItems.Count % iItemsPerPage) != 0)
+        {
+          iPages++;
+        }
       }
       _controlUpDown.SetRange(1, iPages);
       _controlUpDown.Value = 1;
@@ -2890,9 +2899,12 @@ namespace MediaPortal.GUI.Library
       _listItems.Insert(index, item);
       int iItemsPerPage = _rowCount * _columnCount;
       int iPages = iItemsPerPage == 0 ? 0 : _listItems.Count / iItemsPerPage;
-      if ((_listItems.Count % iItemsPerPage) != 0)
+      if (iItemsPerPage != 0)
       {
-        iPages++;
+        if ((_listItems.Count % iItemsPerPage) != 0)
+        {
+          iPages++;
+        }
       }
       _controlUpDown.SetRange(1, iPages);
       _controlUpDown.Value = 1;
