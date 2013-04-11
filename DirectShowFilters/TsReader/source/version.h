f68bef035752e311de89854a88e6345471b839f6
owlsroost <tony@owlsroost.org.uk>
2013-04-11 10:03:56 +0100

    Added fixes for Mantis 4252 (MP hangs at end of recording when skipping to end)

--- DirectShowFilters/TsReader/source/version.h
+++ DirectShowFilters/TsReader/source/version.h
@@ -17,8 +17,8 @@
 
 // Version numbers (used for resource and log display)
 #define TSREADER_MAJOR_VERSION 0,0
-#define TSREADER_VERSION 70
-#define TSREADER_VERSION_STR "0.0.70.0\0"
+#define TSREADER_VERSION 72
+#define TSREADER_VERSION_STR "0.0.72.0\0"
 
 #define MP_VERSION 1,2,300,0
 #define MP_VERSION_STR "1.2.300.0\0"
