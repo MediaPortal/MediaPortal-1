f8d85d533bc79deb5c3070706bd8452f30dd6faa
tourettes <tourettes@team-mediaportal.com>
2013-07-18 11:11:24 +0300

    Fix: MP1-4083 - Audio renderer can deadlock on stop
    Fixed threading in the logger implementation
    
    MPAR version 1.1.6
    (cherry picked from commit 2115b6defdeba2c098689b73ec6c92e87ec41848)

--- DirectShowFilters/MPAudioRenderer/source/Settings.cpp
+++ DirectShowFilters/MPAudioRenderer/source/Settings.cpp
@@ -132,7 +132,7 @@ AudioRendererSettings::AudioRendererSettings() :
   m_bExpandMonoToStereo(true)
 {
   LogRotate();
-  Log("MP Audio Renderer - v1.1.5");
+  Log("MP Audio Renderer - v1.1.6");
 
   LoadSettingsFromRegistry();
 }
