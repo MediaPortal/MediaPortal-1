f436d40cef7c41548aa7654690f8731ba56589ca
sebastiii <sebastiii@team-mediaportal.com>
2013-08-04 02:36:30 +0200

    MP1-4100 : Updated dshowhelper to add capability to disable Vsync correction and MPAR correction. (owlsroost patch) Also modified VMR9.cs to match and handle the logic if we use Win7 or Lower and if MP is start on primary screen.

--- mediaportal/Core/Player/VMR9.cs
+++ mediaportal/Core/Player/VMR9.cs
@@ -24,6 +24,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Windows.Forms;
 using DirectShowLib;
 using DShowNET.Helper;
 using MediaPortal.Configuration;
@@ -86,7 +87,8 @@ namespace MediaPortal.Player
 
     [DllImport("dshowhelper.dll", ExactSpelling = true, CharSet = CharSet.Auto, SetLastError = true)]
     private static extern unsafe bool EvrInit(IVMR9PresentCallback callback, uint dwD3DDevice, 
-                                              ref IBaseFilter vmr9Filter, uint monitor, int monitorIdx);
+                                              ref IBaseFilter vmr9Filter, uint monitor, int monitorIdx,
+                                              bool disVsyncCorr, bool disMparCorr);
 
     //, uint dwWindow);
     [DllImport("dshowhelper.dll", ExactSpelling = true, CharSet = CharSet.Auto, SetLastError = true)]
@@ -378,21 +380,47 @@ namespace MediaPortal.Player
       {
         if (GUIGraphicsContext.currentMonitorIdx != -1)
         {
-          EvrInit(_scene, (uint) upDevice.ToInt32(), ref _vmr9Filter, (uint) hMonitor.ToInt32(), GUIGraphicsContext.currentMonitorIdx);
+          if ((OSInfo.OSInfo.Win7OrLater() &&
+               Screen.AllScreens[GUIGraphicsContext.DX9Device.DeviceCaps.AdapterOrdinal].Primary) ||
+              OSInfo.OSInfo.Win8OrLater())
+          {
+            EvrInit(_scene, (uint) upDevice.ToInt32(), ref _vmr9Filter, (uint) hMonitor.ToInt32(),
+                    GUIGraphicsContext.currentMonitorIdx, false, false);
+          }
+          else
+          {
+            EvrInit(_scene, (uint) upDevice.ToInt32(), ref _vmr9Filter, (uint) hMonitor.ToInt32(),
+                    GUIGraphicsContext.currentMonitorIdx, true, true);
+            Log.Debug("VMR9: force disable vsync and bias correction for Win7 or lower - current primary is : {0}",
+                      Screen.AllScreens[GUIGraphicsContext.DX9Device.DeviceCaps.AdapterOrdinal].Primary);
+          }
         }
         else
         {
-          EvrInit(_scene, (uint)upDevice.ToInt32(), ref _vmr9Filter, (uint)hMonitor.ToInt32(), GUIGraphicsContext.DX9Device.DeviceCaps.AdapterOrdinal);
+          if ((OSInfo.OSInfo.Win7OrLater() &&
+               Screen.AllScreens[GUIGraphicsContext.DX9Device.DeviceCaps.AdapterOrdinal].Primary) ||
+              OSInfo.OSInfo.Win8OrLater())
+          {
+            EvrInit(_scene, (uint) upDevice.ToInt32(), ref _vmr9Filter, (uint) hMonitor.ToInt32(),
+                    GUIGraphicsContext.DX9Device.DeviceCaps.AdapterOrdinal, false, false);
+          }
+          else
+          {
+            EvrInit(_scene, (uint) upDevice.ToInt32(), ref _vmr9Filter, (uint) hMonitor.ToInt32(),
+                    GUIGraphicsContext.DX9Device.DeviceCaps.AdapterOrdinal, true, true);
+            Log.Debug("VMR9: force disable vsync and bias correction for Win7 or lower - current primary is : {0}",
+                      Screen.AllScreens[GUIGraphicsContext.DX9Device.DeviceCaps.AdapterOrdinal].Primary);
+          }
         }
         hr = new HResult(graphBuilder.AddFilter(_vmr9Filter, "Enhanced Video Renderer"));
         Log.Info("VMR9: added EVR Renderer to graph");
       }
       else
       {
-        _vmr9Filter = (IBaseFilter)new VideoMixingRenderer9();
+        _vmr9Filter = (IBaseFilter) new VideoMixingRenderer9();
         Log.Info("VMR9: added Video Mixing Renderer 9 to graph");
 
-        Vmr9Init(_scene, (uint)upDevice.ToInt32(), _vmr9Filter, (uint)hMonitor.ToInt32());
+        Vmr9Init(_scene, (uint) upDevice.ToInt32(), _vmr9Filter, (uint) hMonitor.ToInt32());
         hr = new HResult(graphBuilder.AddFilter(_vmr9Filter, "Video Mixing Renderer 9"));
       }
 
