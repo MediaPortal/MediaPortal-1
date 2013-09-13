3ec8ce9f34363d532472e850870bf87e26144200
HomeY <jeroen.sneller@gmail.com>
2013-09-13 19:57:35 +0200

    MP1-4189: Attempt to fix the warning on Win 8.1 RTM installs

--- Common-MP-TVE3/OsInfo/OSInfo.cs
+++ Common-MP-TVE3/OsInfo/OSInfo.cs
@@ -518,7 +518,7 @@ namespace OSInfo
                       osName = OSProductType == NT_WORKSTATION ? "Windows 8" : "Windows 2012";
                       break;
                     case 3:
-                      osName = OSProductType == NT_WORKSTATION ? "Windows 81" : "Windows 2012";
+                      osName = OSProductType == NT_WORKSTATION ? "Windows 81";
                       break;
                   }
                   break;
@@ -673,7 +673,7 @@ namespace OSInfo
     /// <returns>false means Win8 or previous</returns>
     public static bool Win81OrLater()
     {
-      return VerifyVersionGreaterEqual(6, 3);
+      return VerifyVersionGreaterEqual(6, 3, 9600);
     }
 
     /// <summary>
