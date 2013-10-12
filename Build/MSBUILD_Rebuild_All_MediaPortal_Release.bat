afd34274de636715f6976e9b12b4f51313eaa00d
sebastiii <sebastiii@team-mediaportal.com>
2013-10-12 19:23:53 +0200

    MP1-4069 : Fix MSBUILD_Rebuild_All_MediaPortal_Release.bat script

--- Build/MSBUILD_Rebuild_All_MediaPortal_Release.bat
+++ Build/MSBUILD_Rebuild_All_MediaPortal_Release.bat
@@ -57,7 +57,7 @@ set xml=Build_Report_%BUILD_TYPE%_TvLibrary.xml
 set html=Build_Report_%BUILD_TYPE%_TvLibrary.html
 set logger=/l:XmlFileLogger,"BuildReport\MSBuild.ExtensionPack.Loggers.dll";logfile=%xml%
 
-"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" %logger% /tv:3.5 /p:TargetFrameworkVersion=v3.5 /target:Rebuild /property:Configuration=%BUILD_TYPE%;Platform=x86 "%TVLibrary%\TvLibrary.sln" >> %log%
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" %logger% /target:Rebuild /property:Configuration=%BUILD_TYPE%;Platform=x86 "%TVLibrary%\TvLibrary.sln" >> %log%
 BuildReport\msxsl %xml% _BuildReport_Files\BuildReport.xslt -o %html%
 
 echo.
