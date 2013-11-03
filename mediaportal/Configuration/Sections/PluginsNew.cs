567261bd8ff7a47486f0017e6d18a0a14f719711
michael--t <michael_t@team-mediaportal.com>
2013-11-03 18:24:33 +0100

    MP1-4278 : PowerScheduler client plugin is shown in Configuration in standard mode even if it is disabled

--- mediaportal/Configuration/Sections/PluginsNew.cs
+++ mediaportal/Configuration/Sections/PluginsNew.cs
@@ -229,7 +229,7 @@ namespace MediaPortal.Configuration.Sections
           {
             continue;
           }
-          if (tag.IsProcess && !tag.IsEnabled && tag.SetupForm.PluginName() != "Audioscrobbler")
+          if (tag.IsProcess && !tag.IsEnabled && tag.SetupForm.PluginName() != "Audioscrobbler" && tag.SetupForm.PluginName() != "PowerScheduler")
           {
             continue;
           }
