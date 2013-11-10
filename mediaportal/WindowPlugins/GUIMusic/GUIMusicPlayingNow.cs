b50bdbb02715f970e63bf884e59d834b2cb890a7
hwahrmann <hwahrmann@team-mediaportal.com>
2013-11-10 22:13:00 +0100

    MP1-4258: Due to the latest rework a possible freeze no longer occurs and the BeginInvoke calls in NowPlaying can be changed back to Invoke

--- mediaportal/WindowPlugins/GUIMusic/GUIMusicPlayingNow.cs
+++ mediaportal/WindowPlugins/GUIMusic/GUIMusicPlayingNow.cs
@@ -271,7 +271,7 @@ namespace MediaPortal.GUI.Music
         return;
       }
 
-      GUIGraphicsContext.form.BeginInvoke(new PlaybackChangedDelegate(DoOnEnded), new object[] {type, filename});
+      GUIGraphicsContext.form.Invoke(new PlaybackChangedDelegate(DoOnEnded), new object[] {type, filename});
     }
 
     private void DoOnEnded(g_Player.MediaType type, string filename)
@@ -317,7 +317,7 @@ namespace MediaPortal.GUI.Music
         return;
       }
 
-      GUIGraphicsContext.form.BeginInvoke(new PlaybackChangedDelegate(DoOnStarted),
+      GUIGraphicsContext.form.Invoke(new PlaybackChangedDelegate(DoOnStarted),
                                      new object[] {g_Player.MediaType.Music, BassMusicPlayer.Player.CurrentFile});
     }
 
@@ -329,7 +329,7 @@ namespace MediaPortal.GUI.Music
         return;
       }
 
-      GUIGraphicsContext.form.BeginInvoke(new PlaybackChangedDelegate(DoOnStarted), new object[] {type, filename});
+      GUIGraphicsContext.form.Invoke(new PlaybackChangedDelegate(DoOnStarted), new object[] {type, filename});
     }
 
     private void DoOnStarted(g_Player.MediaType type, string filename)
@@ -833,7 +833,7 @@ namespace MediaPortal.GUI.Music
     {
       if (request.Equals(_lastAlbumRequest))
       {
-        GUIGraphicsContext.form.BeginInvoke(new AlbumInfoCompletedDelegate(DoUpdateAlbumInfo),
+        GUIGraphicsContext.form.Invoke(new AlbumInfoCompletedDelegate(DoUpdateAlbumInfo),
                                        new object[] {request, AlbumTracks});
       }
       else
@@ -970,7 +970,7 @@ namespace MediaPortal.GUI.Music
     {
       if (request.Equals(_lastArtistRequest))
       {
-        GUIGraphicsContext.form.BeginInvoke(new ArtistInfoCompletedDelegate(DoUpdateArtistInfo), new object[] {request, song});
+        GUIGraphicsContext.form.Invoke(new ArtistInfoCompletedDelegate(DoUpdateArtistInfo), new object[] {request, song});
       }
       else
       {
@@ -1007,7 +1007,7 @@ namespace MediaPortal.GUI.Music
     {
       if (request.Equals(_lastTagRequest))
       {
-        GUIGraphicsContext.form.BeginInvoke(new TagInfoCompletedDelegate(DoUpdateSimilarTrackInfo), new object[] {request, TagTracks});
+        GUIGraphicsContext.form.Invoke(new TagInfoCompletedDelegate(DoUpdateSimilarTrackInfo), new object[] {request, TagTracks});
       }
       else
       {
