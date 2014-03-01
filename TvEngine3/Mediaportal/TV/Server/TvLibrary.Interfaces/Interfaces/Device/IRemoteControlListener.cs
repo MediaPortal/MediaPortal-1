5dd0d5236b36163adf2033f37343d46e495fd4de
mm1352000 <music_man1352000@yahoo.com.au>
2014-03-02 11:44:24 +1300

    New interfaces.

---
+++ TvEngine3/Mediaportal/TV/Server/TvLibrary.Interfaces/Interfaces/Device/IRemoteControlListener.cs
@@ -0,0 +1,40 @@
+﻿#region Copyright (C) 2005-2011 Team MediaPortal
+
+// Copyright (C) 2005-2011 Team MediaPortal
+// http://www.team-mediaportal.com
+// 
+// MediaPortal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MediaPortal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+namespace Mediaportal.TV.Server.TVLibrary.Interfaces.TunerExtension
+{
+  /// <summary>
+  /// An interface for conditional access providers that implement menu access.
+  /// </summary>
+  public interface IRemoteControlListener
+  {
+    /// <summary>
+    /// Open the remote control interface and start listening for commands.
+    /// </summary>
+    /// <returns><c>true</c> if the interface is successfully opened, otherwise <c>false</c></returns>
+    bool OpenRemoteControlInterface();
+
+    /// <summary>
+    /// Close the remote control interface and stop listening for commands.
+    /// </summary>
+    /// <returns><c>true</c> if the interface is successfully closed, otherwise <c>false</c></returns>
+    bool CloseRemoteControlInterface();
+  }
+}
