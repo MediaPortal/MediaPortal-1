539baee626ad679a41f5ea5c795410ec92c4bbc6
mm1352000 <music_man1352000@yahoo.com.au>
2014-04-29 13:27:13 +1200

    Fix override/new warning.

--- TvEngine3/Mediaportal/TV/Server/TVDatabase/EntityModel/Repositories/GenericRepository.cs
+++ TvEngine3/Mediaportal/TV/Server/TVDatabase/EntityModel/Repositories/GenericRepository.cs
@@ -457,12 +457,5 @@ namespace Mediaportal.TV.Server.TVDatabase.EntityModel.Repositories
         }
       }
     }
-
-    public IQueryable<ProgramCategory> IncludeAllRelations(IQueryable<ProgramCategory> query)
-    {
-      var includeRelations = query.Include(p => p.IdTvGuideCategory);
-
-      return includeRelations;
-    }
   }
 }
