4869ac8957b29bd1085ae3fd750709f7a687853c
HomeY <HomeY@team-mediaportal.com>
2014-09-12 10:20:49 +0200

    MP1-4566: Version bumped and loglevel set to Info

--- Tools/MediaPortal.DeployTool/Utils.cs
+++ Tools/MediaPortal.DeployTool/Utils.cs
@@ -687,7 +687,7 @@ namespace MediaPortal.DeployTool
         case "max":
           major = 1;
           minor = 8;
-          revision = 0;
+          revision = 100;
           break;
       }
       Version ver = new Version(major, minor, revision);
@@ -707,8 +707,8 @@ namespace MediaPortal.DeployTool
     public static Version GetCurrentPackageVersion()
     {
       int major = 1;
-      int minor = 8;
-      int revision = 100;
+      int minor = 9;
+      int revision = 0;
 
       Version ver = new Version(major, minor, revision);
       return ver;
@@ -781,7 +781,7 @@ namespace MediaPortal.DeployTool
 
     public static string GetDisplayVersion()
     {
-      return "1.9.0 Pre Release";
+      return "1.9.0";
     }
 
     /// <summary>
