686d537eae49e9e8bf79f9b20f9b47c340108a30
tourettes <tourettes@team-mediaportal.com>
2014-10-03 23:38:31 +0300

    Test for bit streaming in CSampleRateConverter::NegotiateFormat()

--- DirectShowFilters/MPAudioRenderer/source/SampleRateConverterFilter.cpp
+++ DirectShowFilters/MPAudioRenderer/source/SampleRateConverterFilter.cpp
@@ -66,6 +66,18 @@ HRESULT CSampleRateConverter::NegotiateFormat(const WAVEFORMATEXTENSIBLE* pwfx, 
   if (!m_pNextSink)
     return VFW_E_TYPE_NOT_ACCEPTED;
 
+  if (m_pSettings->GetAllowBitStreaming() && CanBitstream(pwfx))
+  {
+    HRESULT hr = m_pNextSink->NegotiateFormat(pwfx, nApplyChangesDepth, pChOrder);
+    if (SUCCEEDED(hr))
+    {
+      m_bNextFormatPassthru = true;
+      m_bPassThrough = true;
+      m_chOrder = *pChOrder;
+      return hr;
+    }
+  }
+
   bool bApplyChanges = (nApplyChangesDepth != 0);
   if (nApplyChangesDepth != INFINITE && nApplyChangesDepth > 0)
     nApplyChangesDepth--;
