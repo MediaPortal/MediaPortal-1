e3bef33b525eea7ec5da5cb876f47e7d40b82e70
regeszter <regeszter@freemail.hu>
2014-10-22 08:37:48 +0200

    MP1-2538 : When_selected_skin_is_incompatible,_no_warning_is_given

--- mediaportal/Dialogs/Dialogs/GUIDialogOldSkin.cs
+++ mediaportal/Dialogs/Dialogs/GUIDialogOldSkin.cs
@@ -153,13 +153,15 @@ namespace MediaPortal.Dialogs
       GUIGraphicsContext.Skin = _userSkin;
       GUITextureManager.Clear();
       GUITextureManager.Init();
-      GUIFontManager.LoadFonts(GUIGraphicsContext.Skin + @"\fonts.xml");
+      SkinSettings.Load();
+      GUIFontManager.LoadFonts(GUIGraphicsContext.GetThemedSkinFile(@"\fonts.xml"));
       GUIFontManager.InitializeDeviceObjects();
       GUIExpressionManager.ClearExpressionCache();
       GUIControlFactory.ClearReferences();
-      GUIControlFactory.LoadReferences(GUIGraphicsContext.Skin + @"\references.xml");
+      GUIControlFactory.LoadReferences(GUIGraphicsContext.GetThemedSkinFile(@"\references.xml"));
       GUIWindowManager.OnResize();
       //GUIWindowManager.ActivateWindow(ActiveWindowID);
+
       using (Settings xmlreader = new MPSettings())
       {
         xmlreader.SetValue("general", "skinobsoletecount", 0);
