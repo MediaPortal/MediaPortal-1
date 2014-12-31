1a0f67e76a2bc2eebb8acbb42479b07329098e21
regeszter <regeszter@freemail.hu>
2014-12-31 10:19:41 +0100

    Wrong_thumb_on_Music_NowPlaying

--- mediaportal/WindowPlugins/Common.GUIPlugins/Music/GUIMusicPlayingNow.cs
+++ mediaportal/WindowPlugins/Common.GUIPlugins/Music/GUIMusicPlayingNow.cs
@@ -897,7 +897,7 @@ namespace MediaPortal.GUI.Music
     private void FlipPictures()
     {
       // Check if we should let the visualization window handle image flipping
-      if (_usingBassEngine && _showVisualization)
+      if (_usingBassEngine && _showVisualization || g_Player.currentFileName == string.Empty)
       {
         return;
       }
