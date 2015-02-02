72a724a54d003c24fc13f57d01ad29b8e5608a98
sebastiii <sebastiii@team-mediaportal.com>
2015-02-02 20:29:35 +0100

    MP1-4578 : Disable force mp focus.

--- mediaportal/MediaPortal.Application/MediaPortal.cs
+++ mediaportal/MediaPortal.Application/MediaPortal.cs
@@ -1819,9 +1819,6 @@ public class MediaPortalApp : D3D, IRender
 
           if (ps.PowerSetting == GUID_SYSTEM_AWAYMODE && ps.DataLength == Marshal.SizeOf(typeof(Int32)))
           {
-            // focus MP
-            ForceMPFocus();
-
             switch (ps.Data)
             {
               case 0:
@@ -1837,9 +1834,6 @@ public class MediaPortalApp : D3D, IRender
           // GUID_SESSION_DISPLAY_STATUS is only provided on Win8 and above
           else if ((ps.PowerSetting == GUID_MONITOR_POWER_ON || ps.PowerSetting == GUID_SESSION_DISPLAY_STATUS) && ps.DataLength == Marshal.SizeOf(typeof(Int32)))
           {
-            // focus MP
-            ForceMPFocus();
-
             switch (ps.Data)
             {
               case 0:
@@ -1860,9 +1854,6 @@ public class MediaPortalApp : D3D, IRender
           // GUIT_SESSION_USER_PRESENCE is only provide on Win8 and above
           else if (ps.PowerSetting == GUID_SESSION_USER_PRESENCE && ps.DataLength == Marshal.SizeOf(typeof(Int32)))
           {
-            // focus MP
-            ForceMPFocus();
-
             switch (ps.Data)
             {
               case 0:
@@ -2768,8 +2759,8 @@ public class MediaPortalApp : D3D, IRender
 
     RestoreFromTray();
 
-    // Force Focus after resume done (really weird sequence)
-    ForceMPFocus();
+    // Force focus after resume done (really weird sequence) disable for now
+    //ForceMPFocus();
 
     Log.Info("Main: OnResumeSuspend - Done");
   }
