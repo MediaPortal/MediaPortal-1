45f20e21b2ebe6551fecfed67d3194ebbb35b693
sebastiii <sebastiii@team-mediaportal.com>
2015-03-24 20:54:21 +0100

    Fork : Commit change from 1.10.0Fork-BD-J

--- Build/MSBUILD_Rebuild_All_MediaPortal_Release.bat
+++ Build/MSBUILD_Rebuild_All_MediaPortal_Release.bat
@@ -1,4 +1,50 @@
 @ECHO OFF
+cls
+Title Building MediaPortal
+
+:::::::::::::::::::::::::::::::::::::::::
+:: Automatically check & get admin rights
+:::::::::::::::::::::::::::::::::::::::::
+@echo off
+CLS 
+ECHO.
+ECHO =============================
+ECHO Running Admin shell
+ECHO =============================
+
+SET CURRENTPATH=%~dp0
+SET CURRENTLETTER=%~d0
+cd %~dp0
+rem echo %CURRENTPATH%
+rem echo %CURRENTLETTER%
+
+:checkPrivileges 
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges ) 
+
+:getPrivileges 
+if '%1'=='ELEV' (shift & goto gotPrivileges)  
+ECHO. 
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation 
+ECHO **************************************
+
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+setlocal EnableDelayedExpansion
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\OEgetPrivileges.vbs" 
+ECHO UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\OEgetPrivileges.vbs" 
+"%temp%\OEgetPrivileges.vbs" 
+exit /B
+
+:gotPrivileges 
+::::::::::::::::::::::::::::
+:START
+::::::::::::::::::::::::::::
+rem setlocal & pushd .
+
+%CURRENTLETTER%
+cd %CURRENTPATH%
 
 rem build init
 set project=MediaPortal
@@ -9,6 +55,10 @@ echo.
 echo Writing GIT revision assemblies...
 rem %DeployVersionGIT% /git="%GIT_ROOT%" /path="%MediaPortal%" >> %log%
 %DeployVersionGIT% /git="%GIT_ROOT%" /path="%CommonMPTV%" >> %log%
+
+echo.
+echo Building Libbluray Java...
+call ant -f %LibblurayJAR% -Dsrc_awt=:java-j2se
 
 echo.
 echo Building native components...
