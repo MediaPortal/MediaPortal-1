fb6934b2b8d100418fe40078014d5c8deb9d17b9
georgius <juraj.horvath.mail@gmail.com>
2015-04-16 23:08:21 +0200

    MP1-4155: added Conditional Access section and parser (will be used later)

--- DirectShowFilters/MPUrlSourceSplitter/MPUrlSourceSplitter/MPUrlSourceSplitter_libmpeg2ts/ConditionalAccessParser.h
+++ DirectShowFilters/MPUrlSourceSplitter/MPUrlSourceSplitter/MPUrlSourceSplitter_libmpeg2ts/ConditionalAccessParser.h
@@ -24,6 +24,7 @@
 #define __CONDITIONAL_ACCESS_PARSER_DEFINED
 
 #include "Parser.h"
+#include "ConditionalAccessSection.h"
 
 #define CONDITIONAL_ACCESS_PARSER_FLAG_NONE                           PARSER_FLAG_NONE
 
@@ -32,5 +33,61 @@
 #define CONDITIONAL_ACCESS_PARSER_FLAG_LAST                           (PARSER_FLAG_LAST + 1)
 
 #define CONDITIONAL_ACCESS_PARSER_PSI_PACKET_PID                      0x0001
+
+class CConditionalAccessParser : public CParser
+{
+public:
+  CConditionalAccessParser(HRESULT *result);
+  virtual ~CConditionalAccessParser(void);
+
+  /* get methods */
+
+  // gets current conditional access section
+  // @return : current conditional access section
+  CConditionalAccessSection *GetConditionalAccessSection(void);
+
+  // gets conditional access section parse result
+  // @return :
+  //  S_OK                                                        : complete conditional access section
+  //  S_FALSE                                                     : incomplete conditional access section
+  //  E_MPEG2TS_EMPTY_SECTION_AND_PSI_PACKET_WITHOUT_NEW_SECTION  : section is empty and PSI packet with section data
+  //  E_MPEG2TS_INCOMPLETE_SECTION                                : section is incomplete
+  //  E_MPEG2TS_SECTION_INVALID_CRC32                             : invalid section CRC32 (corrupted section)
+  //  other error code                                            : another error
+  HRESULT GetConditionalAccessSectionParseResult(void);
+
+  /* set methods */
+
+  /* other methods */
+
+  // tests if conditional access section is found
+  // @return : true if section is found, false otherwise
+  bool IsSectionFound(void);
+
+  // parses input MPEG2 TS packet
+  // @param packet : the MPEG2 TS packet to parse
+  // @return :
+  //  S_OK                                                        : complete conditional access section
+  //  S_FALSE                                                     : incomplete conditional access section
+  //  E_FAIL                                                      : not PSI packet or PSI packet PID not CONDITIONAL_ACCESS_PARSER_PSI_PACKET_PID
+  //  E_MPEG2TS_EMPTY_SECTION_AND_PSI_PACKET_WITHOUT_NEW_SECTION  : section is empty and PSI packet with section data
+  //  E_MPEG2TS_INCOMPLETE_SECTION                                : section is incomplete
+  //  E_MPEG2TS_SECTION_INVALID_CRC32                             : invalid section CRC32 (corrupted section)
+  //  other error code                                            : another error
+  virtual HRESULT Parse(CTsPacket *packet);
+
+  // clears instance to its default state
+  virtual void Clear(void);
+
+protected:
+
+  // holds last conditional access section parse result
+  HRESULT conditionalAccessSectionResult;
+
+  // holds current (maybe incomplete) conditional access section
+  CConditionalAccessSection *currentSection;
+
+  /* methods */
+};
 
 #endif
