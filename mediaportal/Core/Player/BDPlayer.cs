f12e10195f08e908e03bbd3c8317e52d828f1add
tourettes <tourettes@team-mediaportal.com>
2015-06-17 22:46:02 +0300

    Change order of IVideoWindow method calls on playback stop
    - removes brief MP focus lost with madVR
    - fixes some total loss of focus cases with blu-ray and TS players

--- mediaportal/Core/Player/BDPlayer.cs
+++ mediaportal/Core/Player/BDPlayer.cs
@@ -3005,8 +3005,8 @@ namespace MediaPortal.Player
         _videoWin = _graphBuilder as IVideoWindow;
         if (_videoWin != null)
         {
-          hr = _videoWin.put_Visible(OABool.False);
-          hr = _videoWin.put_Owner(IntPtr.Zero);
+          _videoWin.put_Owner(IntPtr.Zero);
+          _videoWin.put_Visible(OABool.False);
           _videoWin = null;
         }
 
