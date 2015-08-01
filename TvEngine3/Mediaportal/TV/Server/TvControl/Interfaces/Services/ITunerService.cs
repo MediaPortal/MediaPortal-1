21271b34c57b6f0a6edabd1ceb32ecfbaef9306e
mm1352000 <music_man1352000@yahoo.com.au>
2015-08-01 19:57:16 +1200

    Add critical missing files, sorry!

---
+++ TvEngine3/Mediaportal/TV/Server/TvControl/Interfaces/Services/ITunerService.cs
@@ -0,0 +1,119 @@
+using System.Collections.Generic;
+using System.ServiceModel;
+using Mediaportal.TV.Server.Common.Types.Enum;
+using Mediaportal.TV.Server.TVDatabase.Entities;
+using Mediaportal.TV.Server.TVDatabase.Entities.Enums;
+
+namespace Mediaportal.TV.Server.TVControl.Interfaces.Services
+{
+  [ServiceContract(Namespace = "http://www.team-mediaportal.com")]
+  public interface ITunerService
+  {
+    #region tuners
+
+    [OperationContract]
+    IList<Tuner> ListAllTuners();
+
+    [OperationContract(Name = "ListAllTunersWithSpecificRelations")]
+    IList<Tuner> ListAllTuners(TunerIncludeRelationEnum includeRelations);
+
+    [OperationContract]
+    Tuner GetTuner(int idTuner);
+
+    [OperationContract(Name = "GetTunerWithSpecificRelations")]
+    Tuner GetTuner(int idTuner, TunerIncludeRelationEnum includeRelations);
+
+    [OperationContract]
+    Tuner GetTunerByExternalId(string externalId);
+
+    [OperationContract(Name = "GetTunerByExternalIdWithSpecificRelations")]
+    Tuner GetTunerByExternalId(string externalId, TunerIncludeRelationEnum includeRelations);
+
+    [OperationContract]
+    Tuner SaveTuner(Tuner tuner);
+
+    [OperationContract]
+    IList<Tuner> SaveTuners(IEnumerable<Tuner> tuners);
+
+    [OperationContract]
+    void DeleteTuner(int idTuner);
+
+    #endregion
+
+    #region tuner properties
+
+    [OperationContract]
+    IList<TunerProperty> ListAllTunerPropertiesByTuner(int idTuner);
+
+    [OperationContract]
+    IList<TunerProperty> SaveTunerProperties(IEnumerable<TunerProperty> properties);
+
+    #endregion
+
+    #region tuner groups
+
+    [OperationContract]
+    IList<TunerGroup> ListAllTunerGroups();
+
+    [OperationContract]
+    TunerGroup GetTunerGroup(int idTunerGroup);
+
+    [OperationContract]
+    TunerGroup SaveTunerGroup(TunerGroup group);
+
+    [OperationContract]
+    void DeleteTunerGroup(int idTunerGroup);
+
+    #endregion
+
+    #region analog tuner settings
+
+    [OperationContract]
+    AnalogTunerSettings GetAnalogTunerSettings(int idAnalogTunerSettings);
+
+    [OperationContract]
+    AnalogTunerSettings SaveAnalogTunerSettings(AnalogTunerSettings settings);
+
+    #endregion
+
+    #region software encoders
+
+    [OperationContract]
+    IList<SoftwareEncoder> ListAvailableSoftwareEncodersVideo();
+
+    [OperationContract]
+    IList<SoftwareEncoder> ListAvailableSoftwareEncodersAudio();
+
+    #endregion
+
+    #region BDA network providers
+
+    [OperationContract]
+    IList<BdaNetworkProvider> ListAvailableBdaNetworkProviders();
+
+    #endregion
+
+    #region satellites
+
+    [OperationContract]
+    IList<Satellite> ListAllSatellites();
+
+    [OperationContract]
+    Satellite SaveSatellite(Satellite satellite);
+
+    #endregion
+
+    #region LNB types
+
+    [OperationContract]
+    IList<LnbType> ListAllLnbTypes();
+
+    [OperationContract]
+    LnbType GetLnbType(int idLnbType);
+
+    #endregion
+
+    [OperationContract]
+    DiseqcMotor SaveDiseqcMotor(DiseqcMotor motor);
+  }
+}
---
+++ TvEngine3/Mediaportal/TV/Server/TvControl/ServiceAgents/TunerServiceAgent.cs
@@ -0,0 +1,185 @@
+using System.Collections.Generic;
+using Mediaportal.TV.Server.Common.Types.Enum;
+using Mediaportal.TV.Server.TVControl.Interfaces.Services;
+using Mediaportal.TV.Server.TVDatabase.Entities;
+using Mediaportal.TV.Server.TVDatabase.Entities.Enums;
+
+namespace Mediaportal.TV.Server.TVControl.ServiceAgents
+{
+  public class TunerServiceAgent : ServiceAgent<ITunerService>, ITunerService
+  {
+    public TunerServiceAgent(string hostname) : base(hostname)
+    {
+    }
+
+    #region tuners
+
+    public IList<Tuner> ListAllTuners()
+    {
+      return _channel.ListAllTuners();
+    }
+
+    public IList<Tuner> ListAllTuners(TunerIncludeRelationEnum includeRelations)
+    {
+      return _channel.ListAllTuners(includeRelations);
+    }
+
+    public Tuner GetTuner(int idTuner)
+    {
+      return _channel.GetTuner(idTuner);
+    }
+
+    public Tuner GetTuner(int idTuner, TunerIncludeRelationEnum includeRelations)
+    {
+      return _channel.GetTuner(idTuner, includeRelations);
+    }
+
+    public Tuner GetTunerByExternalId(string externalId)
+    {
+      return _channel.GetTunerByExternalId(externalId);
+    }
+
+    public Tuner GetTunerByExternalId(string externalId, TunerIncludeRelationEnum includeRelations)
+    {
+      return _channel.GetTunerByExternalId(externalId, includeRelations);
+    }
+
+    public Tuner SaveTuner(Tuner tuner)
+    {
+      tuner.UnloadAllUnchangedRelationsForEntity();
+      return _channel.SaveTuner(tuner);
+    }
+
+    public IList<Tuner> SaveTuners(IEnumerable<Tuner> tuners)
+    {
+      foreach (Tuner tuner in tuners)
+      {
+        tuner.UnloadAllUnchangedRelationsForEntity();
+      }
+      return _channel.SaveTuners(tuners);
+    }
+
+    public void DeleteTuner(int idTuner)
+    {
+      _channel.DeleteTuner(idTuner);
+    }
+
+    #endregion
+
+    #region tuner properties
+
+    public IList<TunerProperty> ListAllTunerPropertiesByTuner(int idTuner)
+    {
+      return _channel.ListAllTunerPropertiesByTuner(idTuner);
+    }
+
+    public IList<TunerProperty> SaveTunerProperties(IEnumerable<TunerProperty> properties)
+    {
+      foreach (TunerProperty property in properties)
+      {
+        property.UnloadAllUnchangedRelationsForEntity();
+      }
+      return _channel.SaveTunerProperties(properties);
+    }
+
+    #endregion
+
+    #region tuner groups
+
+    public IList<TunerGroup> ListAllTunerGroups()
+    {
+      return _channel.ListAllTunerGroups();
+    }
+
+    public TunerGroup GetTunerGroup(int idTunerGroup)
+    {
+      return _channel.GetTunerGroup(idTunerGroup);
+    }
+
+    public TunerGroup SaveTunerGroup(TunerGroup group)
+    {
+      group.UnloadAllUnchangedRelationsForEntity();
+      return _channel.SaveTunerGroup(group);
+    }
+
+    public void DeleteTunerGroup(int idTunerGroup)
+    {
+      _channel.DeleteTunerGroup(idTunerGroup);
+    }
+
+    #endregion
+
+    #region analog tuner settings
+
+    public AnalogTunerSettings GetAnalogTunerSettings(int idAnalogTunerSettings)
+    {
+      return _channel.GetAnalogTunerSettings(idAnalogTunerSettings);
+    }
+
+    public AnalogTunerSettings SaveAnalogTunerSettings(AnalogTunerSettings settings)
+    {
+      settings.UnloadAllUnchangedRelationsForEntity();
+      return _channel.SaveAnalogTunerSettings(settings);
+    }
+
+    #endregion
+
+    #region software encoders
+
+    public IList<SoftwareEncoder> ListAvailableSoftwareEncodersVideo()
+    {
+      return _channel.ListAvailableSoftwareEncodersVideo();
+    }
+
+    public IList<SoftwareEncoder> ListAvailableSoftwareEncodersAudio()
+    {
+      return _channel.ListAvailableSoftwareEncodersAudio();
+    }
+
+    #endregion
+
+    #region BDA network providers
+
+    public IList<BdaNetworkProvider> ListAvailableBdaNetworkProviders()
+    {
+      return _channel.ListAvailableBdaNetworkProviders();
+    }
+
+    #endregion
+
+    #region satellites
+
+    public IList<Satellite> ListAllSatellites()
+    {
+      return _channel.ListAllSatellites();
+    }
+
+    public Satellite SaveSatellite(Satellite satellite)
+    {
+      satellite.UnloadAllUnchangedRelationsForEntity();
+      return _channel.SaveSatellite(satellite);
+    }
+
+    #endregion
+
+    #region LNB types
+
+    public IList<LnbType> ListAllLnbTypes()
+    {
+      return _channel.ListAllLnbTypes();
+    }
+
+    public LnbType GetLnbType(int idLnbType)
+    {
+      return _channel.GetLnbType(idLnbType);
+    }
+
+    #endregion
+
+    public DiseqcMotor SaveDiseqcMotor(DiseqcMotor motor)
+    {
+      motor.UnloadAllUnchangedRelationsForEntity();
+      return _channel.SaveDiseqcMotor(motor);
+    }
+  }
+}
