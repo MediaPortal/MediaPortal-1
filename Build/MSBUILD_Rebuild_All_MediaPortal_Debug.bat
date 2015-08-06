fb6d701fa912d00447ea567b9604dba972e9859a
sebastiii <sebastiii@team-mediaportal.com>
2015-08-06 19:50:17 +0200

    Fork : Change ant from %ant_home% for BD-J.

--- Build/MSBUILD_Rebuild_All_MediaPortal_Debug.bat
+++ Build/MSBUILD_Rebuild_All_MediaPortal_Debug.bat
@@ -12,7 +12,7 @@ rem %DeployVersionGIT% /git="%GIT_ROOT%" /path="%MediaPortal%" >> %log%
 
 echo.
 echo Building Libbluray Java...
-call ant -f %LibblurayJAR% -Dsrc_awt=:java-j2se
+call %ant_home%\bin\ant -f %LibblurayJAR% -Dsrc_awt=:java-j2se
 
 echo.
 echo Building native components...
