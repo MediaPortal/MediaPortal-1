171763c791d1e2e6dfb501ab61a48fbd86afae78
sebastiii <sebastiii@team-mediaportal.com>
2015-08-11 07:04:41 +0200

    Fork : Fix null exception on RadioRecorded.

--- TvEngine3/TVLibrary/TvPlugin/TvPlugin/RadioRecorded.cs
+++ TvEngine3/TVLibrary/TvPlugin/TvPlugin/RadioRecorded.cs
@@ -1344,11 +1344,6 @@ namespace TvPlugin
         {
           GUIPropertyManager.SetProperty("#selectedthumb", String.Empty);
           SetProperties(null);
-          if (pItem.IsFolder && pItem.Label == "..")
-          {
-            MediaPortal.Util.Utils.SetDefaultIcons(pItem);
-            GUIPropertyManager.SetProperty("#selectedthumb", pItem.IconImageBig);
-          }
           return;
         }
         rec = pItem.TVTag as Recording;
