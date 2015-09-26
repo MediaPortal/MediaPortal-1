196ea1787d22e1729135ce9056effc6cfbe69b69
sebastiii <sebastiii@team-mediaportal.com>
2015-09-26 16:13:30 +0200

    Fork : Added System.Management.Automation.dll

--- mediaportal/MediaPortal.Application/PostBuild.bat
+++ mediaportal/MediaPortal.Application/PostBuild.bat
@@ -193,5 +193,8 @@ xcopy %1\..\Packages\MediaPortal.TagLib.2.0.3.8\lib\taglib-sharp.dll ./Y /D
 REM SharpLibHid
 xcopy %1\..\Packages\SharpLibHid.1.1.0\lib\net20\SharpLibHid.dll . /Y /D
 
+REM System.Management.Automation
+xcopy %1\..\Packages\System.Management.Automation.6.1.7601.17515\lib\net40\System.Management.Automation.dll . /Y /D
+
 REM Enable >2GB for 32 bit process
 call %Build%\MSBUILD_MP_LargeAddressAware.bat %2
