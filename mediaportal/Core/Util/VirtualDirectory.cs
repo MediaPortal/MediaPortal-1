295fbe592e6b52b099a4f00fa15151f39270bfb6
sebastiii <sebastiii@team-mediaportal.com>
2015-12-02 07:47:27 +0100

    Fork : Adding more explicit log about share offline.

--- mediaportal/Core/Util/VirtualDirectory.cs
+++ mediaportal/Core/Util/VirtualDirectory.cs
@@ -1547,7 +1547,8 @@ namespace MediaPortal.Util
           if (!UNCTools.UNCFileFolderExists(item.Path) && !share.ShareWakeOnLan && !Util.Utils.IsDVD(item.Path))
           {
             share.ShareOffline = true;
-            Log.Debug("GetRootExt(): ShareOffline : '{0}' doesn't exists or host is offline, don't use it for later or enable WOL feature", share.Path);
+            Log.Debug("GetRootExt(): ShareOffline : '{0}' doesn't exists or offline, enable WOL feature for permanent loading", share.Path);
+            Log.Debug("GetRootExt(): ShareOffline : '{0}' can be refreshed from context menu in share view mode", share.Path);
           }
           else
           {
