c64c121c488db732594f62d8d337dd4b6ca35396
sebastiii <sebastiii@team-mediaportal.com>
2015-12-02 07:33:56 +0100

    MP1-4722 : Fix null exception in GUIMusicFiles.

--- mediaportal/WindowPlugins/GUIMusic/GUIMusicFiles.cs
+++ mediaportal/WindowPlugins/GUIMusic/GUIMusicFiles.cs
@@ -902,17 +902,20 @@ namespace MediaPortal.GUI.Music
           break;
 
         case 607: // Load (only CDROM)
-          Util.Utils.CloseCDROM(Path.GetPathRoot(item.Path));
+          if (item != null)
+          {
+            Util.Utils.CloseCDROM(Path.GetPathRoot(item.Path));
+          }
           break;
 
         case 654: // Eject
-          if (Util.Utils.getDriveType(item.Path) != 5)
+          if (item != null && Util.Utils.getDriveType(item.Path) != 5)
           {
             Util.Utils.EjectCDROM();
           }
           else
           {
-            if (item.Path != null)
+            if (item != null && item.Path != null)
             {
               var driveInfo = new DriveInfo(Path.GetPathRoot(item.Path));
 
@@ -990,7 +993,7 @@ namespace MediaPortal.GUI.Music
             for (int x = 0; x < handler.Views.Count; ++x)
             {
               ViewDefinition view = (ViewDefinition)handler.Views[x];
-              if (view.Name.ToLowerInvariant().IndexOf("artist") >= 0)
+              if (view.Name.ToLowerInvariant().IndexOf("artist", StringComparison.Ordinal) >= 0)
               {
                 viewNr = x;
               }
@@ -1026,7 +1029,7 @@ namespace MediaPortal.GUI.Music
         case 831:
           string message = string.Empty;
 
-          if (Util.Utils.IsUsbHdd(item.Path) || Util.Utils.IsRemovableUsbDisk(item.Path))
+          if (item != null && (Util.Utils.IsUsbHdd(item.Path) || Util.Utils.IsRemovableUsbDisk(item.Path)))
           {
           if (!RemovableDriveHelper.EjectDrive(item.Path, out message))
           {
@@ -1045,7 +1048,7 @@ namespace MediaPortal.GUI.Music
             pDlgOK.DoModal(GUIWindowManager.ActiveWindow);
           }
           }
-          else if (!RemovableDriveHelper.EjectMedia(item.Path, out message))
+          else if (item != null && !RemovableDriveHelper.EjectMedia(item.Path, out message))
           {
             GUIDialogOK pDlgOK = (GUIDialogOK)GUIWindowManager.GetWindow((int)Window.WINDOW_DIALOG_OK);
             pDlgOK.SetHeading(831);
