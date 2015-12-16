7203ce3cecbd5250c5f3387025b535faab18fe83
sebastiii <sebastiii@team-mediaportal.com>
2015-12-16 02:09:40 +0100

    MP1-4730 : Change video stream code from VideoType to VideoName, language is not the priority like it's a video, better to use the Name of the video track.

--- mediaportal/WindowPlugins/Common.GUIPlugins/Video/GUIVideoFullscreen.cs
+++ mediaportal/WindowPlugins/Common.GUIPlugins/Video/GUIVideoFullscreen.cs
@@ -816,7 +816,7 @@ namespace MediaPortal.GUI.Video
                                               (int)Control.LABEL_ROW1, 0, 0, null);
               g_Player.SwitchToNextVideo();
 
-              String language = g_Player.VideoLanguage(g_Player.CurrentVideoStream);
+              String language = g_Player.VideoName(g_Player.CurrentVideoStream);
               String languagetype = g_Player.VideoType(g_Player.CurrentVideoStream);
               if (String.Equals(language, "Video") || String.Equals(language, "") || String.Equals(language, languagetype))
               {
@@ -825,7 +825,7 @@ namespace MediaPortal.GUI.Video
               }
               else
               {
-                msg.Label = string.Format("{0} {1} ({2}/{3})", language,
+                msg.Label = string.Format("{0} - {1} ({2}/{3})", language,
                                           languagetype,
                                           g_Player.CurrentVideoStream + 1, g_Player.VideoStreams);
               }
@@ -1537,15 +1537,15 @@ namespace MediaPortal.GUI.Video
       for (int i = 0; i < count; i++)
       {
         string videoType = g_Player.VideoType(i);
-        string videoLang = g_Player.VideoLanguage(i);
+        string videoName = g_Player.VideoName(i);
         if (videoType == Strings.Unknown || String.Equals(videoType, "") ||
-            videoType.Equals(videoLang))
+            videoType.Equals(videoName))
         {
-          dlg.Add(videoLang);
+          dlg.Add(videoName);
         }
         else
         {
-          dlg.Add(String.Format("{0} {1}", videoLang, videoType));
+          dlg.Add(String.Format("{0} - {1}", videoName, videoType));
         }
       }
 
