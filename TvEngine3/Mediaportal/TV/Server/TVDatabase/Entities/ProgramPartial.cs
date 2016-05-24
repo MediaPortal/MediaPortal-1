74ca06e77e76f918436e7f4d098aaa36319a73b3
mm1352000 <music_man1352000@yahoo.com.au>
2016-05-25 10:58:24 +1200

    Centralise recording file name logic in the Program (ProgramPartial.cs) entity. This avoids code duplication in TV Server Configuration and the TV service scheduler.

---
+++ TvEngine3/Mediaportal/TV/Server/TVDatabase/Entities/ProgramPartial.cs
@@ -0,0 +1,137 @@
+﻿#region Copyright (C) 2005-2011 Team MediaPortal
+
+// Copyright (C) 2005-2011 Team MediaPortal
+// http://www.team-mediaportal.com
+// 
+// MediaPortal is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 2 of the License, or
+// (at your option) any later version.
+// 
+// MediaPortal is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with MediaPortal. If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+namespace Mediaportal.TV.Server.TVDatabase.Entities
+{
+  public partial class Program
+  {
+    public string GetRecordingFileName(string template, string channelName)
+    {
+      if (string.IsNullOrEmpty(template))
+      {
+        template = "%program_title%";
+      }
+      if (channelName == null)
+      {
+        channelName = "Unknown Channel";
+      }
+      else
+      {
+        channelName = channelName.Trim();
+      }
+
+      // Limit length of title and episode name to try to ensure the file name
+      // length is kept within limits (eg. MAX_PATH).
+      string programTitle = Title;
+      if (programTitle.Length > 80)
+      {
+        programTitle = programTitle.Substring(0, 77) + "...";
+      }
+      string programEpisodeName = EpisodeName ?? string.Empty;
+      if (programEpisodeName.Length > 80)
+      {
+        programEpisodeName = programEpisodeName.Substring(0, 77) + "...";
+      }
+
+      string programCategory = string.Empty;
+      if (ProgramCategory != null)
+      {
+        programCategory = ProgramCategory.Category.Trim();
+      }
+
+      Dictionary<string, string> tags = new Dictionary<string, string>
+      {
+        { "%program_title%", programTitle },
+        { "%episode_name%", programEpisodeName },
+        { "%series_number%", SeasonNumber.ToString() },
+        { "%episode_number%", EpisodeNumber.ToString() },
+        { "%episode_part%", EpisodePartNumber.ToString() },
+        { "%channel_name%", channelName },
+        { "%genre%", programCategory },
+        { "%date%", StartTime.ToString("yyyy-MM-dd") },
+        { "%start%", StartTime.ToShortTimeString() },
+        { "%end%", EndTime.ToShortTimeString() },
+        { "%start_year%", StartTime.ToString("yyyy") },
+        { "%start_month%", StartTime.ToString("MM") },
+        { "%start_day%", StartTime.ToString("dd") },
+        { "%start_hour%", StartTime.ToString("HH") },
+        { "%start_minute%", StartTime.ToString("mm") },
+        { "%end_year%", EndTime.ToString("yyyy") },
+        { "%end_month%", EndTime.ToString("MM") },
+        { "%end_day%", EndTime.ToString("dd") },
+        { "%end_hour%", EndTime.ToString("HH") },
+        { "%end_minute%", EndTime.ToString("mm") }
+      };
+
+      foreach (var tag in tags)
+      {
+        template = ReplaceTag(template, tag.Key, tag.Value);
+        if (!template.Contains("%"))
+        {
+          break;
+        }
+      }
+
+      template = template.Trim();
+      if (string.IsNullOrEmpty(template))
+      {
+        template = string.Format("{0}_{1}_{2}", channelName, programTitle, StartTime.ToString("yyyy-MM-dd_HHmm"));
+      }
+      return template;
+    }
+
+    private static string ReplaceTag(string line, string tag, string value)
+    {
+      value = value.Replace(Path.DirectorySeparatorChar, '_').Replace(Path.AltDirectorySeparatorChar, '_');
+
+      // This regex checks for optional sections of the form [*%tag%*].
+      // Nesting (eg. "Hello [%tag1% [%tag2% ]]world!") is not supported.
+      string regexPattern = string.Format(@"\[[^%]*{0}[^\]]*[\]]", tag.Replace("%", "\\%"));
+      Regex r;
+      try
+      {
+        r = new Regex(regexPattern);
+      }
+      catch
+      {
+        return line;
+      }
+
+      Match match = r.Match(line);
+      if (match.Success)  // means there are one or more optional sections
+      {
+        // Remove the entire optional section. If the tag has a value, reinsert
+        // the section without the square braces.
+        line = line.Remove(match.Index, match.Length);
+        if (!string.IsNullOrEmpty(value))
+        {
+          string m = match.Value.Substring(1, match.Value.Length - 2);
+          line = line.Insert(match.Index, m);
+        }
+      }
+
+      // Replace tags.
+      return line.Replace(tag, value);
+    }
+  }
+}
--- TvEngine3/Mediaportal/TV/Server/TvLibrary.Services/Scheduler/RecordingDetail.cs
+++ TvEngine3/Mediaportal/TV/Server/TvLibrary.Services/Scheduler/RecordingDetail.cs
@@ -19,8 +19,6 @@
 #endregion
 
 using System;
-using System.IO;
-using System.Text.RegularExpressions;
 using Mediaportal.TV.Server.TVControl;
 using Mediaportal.TV.Server.TVDatabase.Entities;
 using Mediaportal.TV.Server.TVDatabase.Entities.Factories;
@@ -57,14 +55,13 @@ namespace Mediaportal.TV.Server.TVLibrary.Scheduler
     /// constructor
     /// </summary>
     /// <param name="schedule">Schedule of this recording</param>
-    /// <param name="channel">Channel on which the recording is done</param>
     /// <param name="endTime">Date/Time the recording should start without pre-record interval</param>
     /// <param name="isSerie">Is serie recording</param>
-    public RecordingDetail(Schedule schedule, Channel channel, DateTime endTime, bool isSerie)
+    public RecordingDetail(Schedule schedule, DateTime endTime, bool isSerie)
     {
       _user = UserFactory.CreateSchedulerUser(schedule.IdSchedule);
       _schedule = new ScheduleBLL(schedule);
-      _channel = channel;
+      _channel = schedule.Channel;
       _endTime = endTime;
       _program = null;
       _isSerie = isSerie;
@@ -80,16 +77,12 @@ namespace Mediaportal.TV.Server.TVLibrary.Scheduler
         startTime = schedule.StartTime;
       }
 
-      Program program = ProgramManagement.GetProgramAt(startTime, schedule.Channel.IdChannel);
+      Program program = ProgramManagement.GetProgramAt(startTime, _channel.IdChannel);
       
       //no program? then treat this as a manual recording
       if (program == null)
       {
-        program = ProgramFactory.CreateEmptyProgram();
-        program.IdChannel = 0;
-        program.StartTime = DateTime.Now;
-        program.EndTime = endTime;
-        program.Title = "manual";
+        program = ProgramFactory.CreateProgram(_channel.IdChannel, DateTime.Now, endTime, "manual");
       }
       _program = new ProgramBLL(program);
     }
@@ -206,138 +199,18 @@ namespace Mediaportal.TV.Server.TVLibrary.Scheduler
     /// </summary>
     public void MakeFileName()
     {
-      string format;
+      string template;
       if (!IsSerie)
       {
         this.LogDebug("RecordingDetail: using movie naming format");
-        format = SettingsManagement.GetValue("moviesformat", "%title%");
+        template = SettingsManagement.GetValue("recordingNamingTemplateNonSeries", "%program_title% - %channel_name% - %date%");
       }
       else
       {
         this.LogDebug("RecordingDetail: using series naming format");
-        format = SettingsManagement.GetValue("seriesformat", "%title%");
+        template = SettingsManagement.GetValue("recordingNamingTemplateSeries", "%program_title%\\%program_title% - %date%[ - S%series_number%E%episode_number%][ - %episode_name%]");
       }
-      if (string.IsNullOrEmpty(format))
-      {
-        format = "%title%";
-      }
-
-      string[] tagNames = {
-                            "%program_title%",
-                            "%episode_name%",
-                            "%series_number%",
-                            "%episode_number%",
-                            "%episode_part%",
-                            "%channel_name%",
-                            "%genre%",
-                            "%date%",
-                            "%start%",
-                            "%end%",
-                            "%start_year%",
-                            "%start_month%",
-                            "%start_day%",
-                            "%start_hour%",
-                            "%start_minute%",
-                            "%end_year%",
-                            "%end_month%",
-                            "%end_day%",
-                            "%end_hour%",
-                            "%end_minute%"
-                          };
-
-      // Limit length of title and episode name to try to ensure the file name
-      // length is kept within limits (eg. MAX_PATH).
-      string programTitle = Program.Entity.Title;
-      if (programTitle.Length > 80)
-      {
-        programTitle = programTitle.Substring(0, 77) + "...";
-      }
-      string programEpisodeName = Program.Entity.EpisodeName ?? string.Empty;
-      if (programEpisodeName.Length > 80)
-      {
-        programEpisodeName = programEpisodeName.Substring(0, 77) + "...";
-      }
-
-      string channelName = _schedule.Entity.Channel.Name.Trim();
-      string programCategory = string.Empty;
-      if (Program.Entity.ProgramCategory != null)
-      {
-        programCategory = Program.Entity.ProgramCategory.Category.Trim();
-      }
-      string[] tagValues = {
-                             programTitle,
-                             programEpisodeName,
-                             Program.Entity.SeasonNumber.ToString(),
-                             Program.Entity.EpisodeNumber.ToString(),
-                             Program.Entity.EpisodePartNumber.ToString(),
-                             channelName,
-                             programCategory,
-                             Program.Entity.StartTime.ToString("yyyy-MM-dd"),
-                             Program.Entity.StartTime.ToShortTimeString(),
-                             Program.Entity.EndTime.ToShortTimeString(),
-                             Program.Entity.StartTime.ToString("yyyy"),
-                             Program.Entity.StartTime.ToString("MM"),
-                             Program.Entity.StartTime.ToString("dd"),
-                             Program.Entity.StartTime.ToString("HH"),
-                             Program.Entity.StartTime.ToString("mm"),
-                             Program.Entity.EndTime.ToString("yyyy"),
-                             Program.Entity.EndTime.ToString("MM"),
-                             Program.Entity.EndTime.ToString("dd"),
-                             Program.Entity.EndTime.ToString("HH"),
-                             Program.Entity.EndTime.ToString("mm")
-                           };
-
-      for (int i = 0; i < tagNames.Length; i++)
-      {
-        format = ReplaceTag(format, tagNames[i], tagValues[i]);
-        if (!format.Contains("%"))
-        {
-          break;
-        }
-      }
-
-      format = format.Trim();
-      if (string.IsNullOrEmpty(format))
-      {
-        format = string.Format("{0}_{1}_{2}", channelName, programTitle, Program.Entity.StartTime.ToString("yyyy-MM-dd_HHmm"));
-      }
-
-      _fileName = format;
-    }
-
-    private static string ReplaceTag(string line, string tag, string value)
-    {
-      value = value.Replace(Path.DirectorySeparatorChar, '_').Replace(Path.AltDirectorySeparatorChar, '_');
-
-      // This regex checks for optional sections of the form [*%tag%*].
-      // Nesting (eg. "Hello [%tag1% [%tag2% ]]world!") is not supported.
-      string regexPattern = string.Format(@"\[[^%]*{0}[^\]]*[\]]", tag.Replace("%", "\\%"));
-      Regex r;
-      try
-      {
-        r = new Regex(regexPattern);
-      }
-      catch (Exception ex)
-      {
-        Log.Error(ex, "RecordingDetail: recording file name tag replacement regex failed, regex = {0}", regexPattern);
-        return line;
-      }
-
-      Match match = r.Match(line);
-      if (match.Success)  // means there are one or more optional sections
-      {
-        // Remove the entire optional section. If the tag has a value, reinsert
-        // the section without the square braces.
-        line = line.Remove(match.Index, match.Length);
-        if (!string.IsNullOrEmpty(value))
-        {
-          string m = match.Value.Substring(1, match.Value.Length - 2);
-          line = line.Insert(match.Index, m);
-        }
-      }
-
-      // Replace tags.
-      return line.Replace(tag, value);
+      _fileName = Program.Entity.GetRecordingFileName(template, _schedule.Entity.Channel.Name);
     }
   }
 }
