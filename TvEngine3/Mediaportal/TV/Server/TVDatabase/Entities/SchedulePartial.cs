d03e2cf2817193c177165b8e629c4e9107fb7def
mm1352000 <music_man1352000@yahoo.com.au>
2016-05-25 10:52:53 +1200

    Remove the unused "HighestPriority" constant.

--- TvEngine3/Mediaportal/TV/Server/TVDatabase/Entities/SchedulePartial.cs
+++ TvEngine3/Mediaportal/TV/Server/TVDatabase/Entities/SchedulePartial.cs
@@ -25,6 +25,5 @@ namespace Mediaportal.TV.Server.TVDatabase.Entities
   public partial class Schedule 
   {
     public static DateTime MinSchedule = new DateTime(2000, 1, 1);
-    public static readonly int HighestPriority = Int32.MaxValue;
   }
 }
