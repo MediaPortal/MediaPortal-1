cf3de540dc16e5846bcb028778bfc67036be0d44
regeszter <regeszter@freemail.hu>
2016-11-05 16:03:27 +0100

    MP1-4826 Add_Ares_to_MP_repo

--- Tools/MediaPortal.DeployTool/Sections/SkinChoice.Designer.cs
+++ Tools/MediaPortal.DeployTool/Sections/SkinChoice.Designer.cs
@@ -60,7 +60,7 @@
       this.btnTitan.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
       this.btnTitan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.btnTitan.Image = global::MediaPortal.DeployTool.Images.Choose_button_off;
-      this.btnTitan.Location = new System.Drawing.Point(290, 130);
+      this.btnTitan.Location = new System.Drawing.Point(290, 116);
       this.btnTitan.Name = "btnTitan";
       this.btnTitan.Size = new System.Drawing.Size(33, 23);
       this.btnTitan.TabIndex = 2;
@@ -75,7 +75,7 @@
       this.btnDefaultWide.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
       this.btnDefaultWide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.btnDefaultWide.Image = global::MediaPortal.DeployTool.Images.Choose_button_off;
-      this.btnDefaultWide.Location = new System.Drawing.Point(290, 163);
+      this.btnDefaultWide.Location = new System.Drawing.Point(290, 149);
       this.btnDefaultWide.Name = "btnDefaultWide";
       this.btnDefaultWide.Size = new System.Drawing.Size(33, 23);
       this.btnDefaultWide.TabIndex = 3;
@@ -86,7 +86,7 @@
       // 
       this.lblTitan.AutoSize = true;
       this.lblTitan.ForeColor = System.Drawing.Color.White;
-      this.lblTitan.Location = new System.Drawing.Point(329, 135);
+      this.lblTitan.Location = new System.Drawing.Point(329, 121);
       this.lblTitan.Name = "lblTitan";
       this.lblTitan.Size = new System.Drawing.Size(137, 13);
       this.lblTitan.TabIndex = 5;
@@ -96,7 +96,7 @@
       // 
       this.lblDefaultWide.AutoSize = true;
       this.lblDefaultWide.ForeColor = System.Drawing.Color.White;
-      this.lblDefaultWide.Location = new System.Drawing.Point(329, 168);
+      this.lblDefaultWide.Location = new System.Drawing.Point(329, 154);
       this.lblDefaultWide.Name = "lblDefaultWide";
       this.lblDefaultWide.Size = new System.Drawing.Size(147, 13);
       this.lblDefaultWide.TabIndex = 6;
@@ -116,7 +116,7 @@
       // 
       this.lblExisting.AutoSize = true;
       this.lblExisting.ForeColor = System.Drawing.Color.White;
-      this.lblExisting.Location = new System.Drawing.Point(329, 102);
+      this.lblExisting.Location = new System.Drawing.Point(329, 88);
       this.lblExisting.Name = "lblExisting";
       this.lblExisting.Size = new System.Drawing.Size(270, 13);
       this.lblExisting.TabIndex = 11;
@@ -131,7 +131,7 @@
       this.btnExisting.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
       this.btnExisting.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.btnExisting.Image = global::MediaPortal.DeployTool.Images.Choose_button_off;
-      this.btnExisting.Location = new System.Drawing.Point(290, 97);
+      this.btnExisting.Location = new System.Drawing.Point(290, 83);
       this.btnExisting.Name = "btnExisting";
       this.btnExisting.Size = new System.Drawing.Size(33, 23);
       this.btnExisting.TabIndex = 10;
@@ -147,7 +147,7 @@
       this.btnAres.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
       this.btnAres.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
       this.btnAres.Image = global::MediaPortal.DeployTool.Images.Choose_button_off;
-      this.btnAres.Location = new System.Drawing.Point(290, 196);
+      this.btnAres.Location = new System.Drawing.Point(290, 182);
       this.btnAres.Name = "btnAres";
       this.btnAres.Size = new System.Drawing.Size(33, 23);
       this.btnAres.TabIndex = 4;
@@ -158,7 +158,7 @@
       // 
       this.lblAres.AutoSize = true;
       this.lblAres.ForeColor = System.Drawing.Color.White;
-      this.lblAres.Location = new System.Drawing.Point(329, 201);
+      this.lblAres.Location = new System.Drawing.Point(329, 187);
       this.lblAres.Name = "lblAres";
       this.lblAres.Size = new System.Drawing.Size(90, 13);
       this.lblAres.TabIndex = 12;
