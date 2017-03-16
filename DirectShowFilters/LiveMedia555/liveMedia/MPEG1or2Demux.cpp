8a784a5bef3565c6e012030233f3026b1d30556d
mm1352000 <music_man1352000@yahoo.com.au>
2017-03-16 22:34:05 +1300

    Upgrade LIVE555 to version 2017.01.26. There are currently no direct modifications to LIVE555 code. These will be re-added later. Our sub-classes are included in the MediaPortal sub-directory.

--- DirectShowFilters/LiveMedia555/liveMedia/MPEG1or2Demux.cpp
+++ DirectShowFilters/LiveMedia555/liveMedia/MPEG1or2Demux.cpp
@@ -1,7 +1,7 @@
 /**********
 This library is free software; you can redistribute it and/or modify it under
 the terms of the GNU Lesser General Public License as published by the
-Free Software Foundation; either version 2.1 of the License, or (at your
+Free Software Foundation; either version 3 of the License, or (at your
 option) any later version. (See <http://www.gnu.org/copyleft/lesser.html>.)
 
 This library is distributed in the hope that it will be useful, but WITHOUT
@@ -14,7 +14,7 @@ along with this library; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 **********/
 // "liveMedia"
-// Copyright (c) 1996-2016 Live Networks, Inc.  All rights reserved.
+// Copyright (c) 1996-2017 Live Networks, Inc.  All rights reserved.
 // Demultiplexer for a MPEG 1 or 2 Program Stream
 // Implementation
 
@@ -389,15 +389,6 @@ void MPEGProgramStreamParser::parsePackHeader() {
 
     // We're supposed to have a pack header here, but check also for
     // a system header or a PES packet, just in case:
-    // START Team MediaPortal modification - for MPFileWriter compatibility.
-    // I'm not sure of the specifics. Perhaps this it relates to a certain
-    // muxer, demuxer or encoder?
-    if ((first4Bytes & 0xffffff00) == 0x0001ba00)
-    {
-      skipBytes(3);
-      break;
-    }
-    // END Team MediaPortal modification
     if (first4Bytes == PACK_START_CODE) {
       skipBytes(4);
       break;
