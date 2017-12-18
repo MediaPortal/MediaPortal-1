7e0fcbf3db6cda50b3928021ea6d448f523ebf13
Andrew J.Swan <ajs@ukrpost.net>
2017-12-18 11:07:07 +0200

    MP1-4715 V3 Try fix GUILabel control wrong drawing without width

--- mediaportal/Core/guilib/GUILabelControl.cs
+++ mediaportal/Core/guilib/GUILabelControl.cs
@@ -403,7 +403,7 @@ namespace MediaPortal.GUI.Library
         }
         else
         {
-          if (base.Width == 0)
+          if (base.Width == 0 && _maxWidth > 0)
           {
             return TextWidth + 1; // + 1 - Margin for not fade last char in label text
           }
@@ -450,7 +450,7 @@ namespace MediaPortal.GUI.Library
         }
         else
         {
-          if (base.Height == 0)
+          if (base.Height == 0 && _maxHeight > 0)
           {
             return TextHeight;
           }
