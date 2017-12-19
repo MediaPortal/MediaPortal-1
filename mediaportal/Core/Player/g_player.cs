f02566a4b74dff5f90925c655c06f74f24a8159b
sebastiii <sebastiii@team-mediaportal.com>
2017-12-19 20:48:17 +0100

    MP1-4781V10 : madVR V414 (Added another seek call to start in a thread)

--- mediaportal/Core/Player/g_player.cs
+++ mediaportal/Core/Player/g_player.cs
@@ -2315,28 +2315,32 @@ namespace MediaPortal.Player
 
     public static void StepNow()
     {
-      if (_currentStep != 0 && _player != null)
+      // Start seek in a thread to avoid deadlock
+      new Thread(() =>
       {
-        if (_currentStep < 0 || (_player.CurrentPosition + 4 < _player.Duration) || !IsTV)
+        if (_currentStep != 0 && _player != null)
         {
-          double dTime = (int)_currentStep + _player.CurrentPosition;
-          Log.Debug("g_Player.StepNow() - Preparing to seek to {0}:{1}", _player.CurrentPosition, _player.Duration);
-          if (!IsTV && (dTime > _player.Duration)) dTime = _player.Duration - 5;
-          if (IsTV && (dTime + 3 > _player.Duration)) dTime = _player.Duration - 3; // Margin for live Tv
-          if (dTime < 0) dTime = 0d;
+          if (_currentStep < 0 || (_player.CurrentPosition + 4 < _player.Duration) || !IsTV)
+          {
+            double dTime = (int) _currentStep + _player.CurrentPosition;
+            Log.Debug("g_Player.StepNow() - Preparing to seek to {0}:{1}", _player.CurrentPosition, _player.Duration);
+            if (!IsTV && (dTime > _player.Duration)) dTime = _player.Duration - 5;
+            if (IsTV && (dTime + 3 > _player.Duration)) dTime = _player.Duration - 3; // Margin for live Tv
+            if (dTime < 0) dTime = 0d;
 
-          Log.Debug("g_Player.StepNow() - Preparing to seek to {0}:{1}:{2} isTv {3}", (int)(dTime / 3600d),
-                    (int)((dTime % 3600d) / 60d), (int)(dTime % 60d), IsTV);
-          _player.SeekAbsolute(dTime);
-          Speed = Speed;
-          GUIMessage msgUpdate = new GUIMessage(GUIMessage.MessageType.GUI_MSG_PLAYER_POSITION_CHANGED, 0, 0, 0, 0, 0,
-                                                null);
-          GUIGraphicsContext.SendMessage(msgUpdate);
+            Log.Debug("g_Player.StepNow() - Preparing to seek to {0}:{1}:{2} isTv {3}", (int) (dTime/3600d),
+              (int) ((dTime%3600d)/60d), (int) (dTime%60d), IsTV);
+            _player.SeekAbsolute(dTime);
+            Speed = Speed;
+            GUIMessage msgUpdate = new GUIMessage(GUIMessage.MessageType.GUI_MSG_PLAYER_POSITION_CHANGED, 0, 0, 0, 0, 0,
+              null);
+            GUIGraphicsContext.SendMessage(msgUpdate);
+          }
         }
-      }
-      _currentStep = 0;
-      _currentStepIndex = -1;
-      _seekTimer = DateTime.MinValue;
+        _currentStep = 0;
+        _currentStepIndex = -1;
+        _seekTimer = DateTime.MinValue;
+      }).Start();
     }
 
     /// <summary>
