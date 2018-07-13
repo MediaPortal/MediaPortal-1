b4d02528113000fd8ed95b1abacb4548c35cea0d
regeszter <regeszter@freemail.hu>
2018-07-13 10:09:07 +0200

    MP1-4920 Bump-version-number-and-set-debug-level-to-INFO-for-MP-1.20-Final

--- Common-MP-TVE3/SolutionInfo.cs
+++ Common-MP-TVE3/SolutionInfo.cs
@@ -1,6 +1,6 @@
-﻿#region Copyright (C) 2005-2011 Team MediaPortal
+﻿#region Copyright (C) 2005-2018 Team MediaPortal
 
-// Copyright (C) 2005-2011 Team MediaPortal
+// Copyright (C) 2005-2018 Team MediaPortal
 // http://www.team-mediaportal.com
 // 
 // MediaPortal is free software: you can redistribute it and/or modify
@@ -51,8 +51,8 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.19.100.0")]
-[assembly: AssemblyFileVersion("1.19.100.0")]
+[assembly: AssemblyVersion("1.20.0.0")]
+[assembly: AssemblyFileVersion("1.20.0.0")]
 
 //[assembly: AssemblyInformationalVersion("")]
 //[assembly: CompatibleVersion("1.1.8.*", "1.1.8.*")]
