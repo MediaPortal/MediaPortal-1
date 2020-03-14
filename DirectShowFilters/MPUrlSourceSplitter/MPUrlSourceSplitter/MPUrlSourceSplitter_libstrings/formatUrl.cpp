ed0afcd2ba05e5f8b7354be79ea1f1128940c17f
doskabouter <paul.doskabouter@gmail.com>
2020-03-14 15:39:51 +0100

    FormatAbsoluteUrl: Fixed wrong result for relative urls starting with '/'

--- DirectShowFilters/MPUrlSourceSplitter/MPUrlSourceSplitter/MPUrlSourceSplitter_libstrings/formatUrl.cpp
+++ DirectShowFilters/MPUrlSourceSplitter/MPUrlSourceSplitter/MPUrlSourceSplitter_libstrings/formatUrl.cpp
@@ -162,31 +162,10 @@ wchar_t *FormatAbsoluteUrl(const wchar_t *baseUrl, const wchar_t *relativeUrl)
     }
     else
     {
-      // URL is concatenation of base URL and relative URL
-      unsigned int baseUrlLength = wcslen(baseUrl);
-      unsigned int relativeUrlLength = wcslen(relativeUrl);
-      // we need one extra character for '/' between base URL and relative URL
-
-      if (EndsWith(baseUrl, L'/'))
-      {
-        baseUrlLength--;
-      }
-      if (wcsncmp(relativeUrl, L"/", 1) == 0)
-      {
-        // the first character is '/'
-        relativeUrlLength--;
-        relativeUrl++;
-      }
-
-      unsigned int length = baseUrlLength + relativeUrlLength + 1;
-
-      result = ALLOC_MEM_SET(result, wchar_t, (length + 1), 0);
-      if (result != NULL)
-      {
-        wcsncpy_s(result, length + 1, baseUrl, baseUrlLength);
-        wcscat_s(result, length + 1, L"/");
-        wcscat_s(result, length + 1, relativeUrl);
-      }
+      DWORD len;
+      InternetCombineUrlW(baseUrl, relativeUrl, result, &len, ICU_NO_ENCODE);
+      result = ALLOC_MEM_SET(result, wchar_t, len, 0);
+      InternetCombineUrlW(baseUrl, relativeUrl, result, &len, ICU_NO_ENCODE);
     }
   }
 
