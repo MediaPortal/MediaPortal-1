bc517e3b594d5c336521d07cfce374438aede150
Azzuro <Azzuro@users.noreply.github.com>
2020-12-08 11:47:34 +0100

    MP1-5043 : allow to use other offer of VS2019

--- Build/MSBUILD_MP_LargeAddressAware.bat
+++ Build/MSBUILD_MP_LargeAddressAware.bat
@@ -1,7 +1,15 @@
-@ECHO ON
+@ECHO OFF
 REM set other MP related paths
 set GIT_ROOT=..
 set MediaPortal="%GIT_ROOT%\mediaportal"
+
+if not defined progpath set progpath=%ProgramFiles%
+if not "%ProgramFiles(x86)%".=="". set progpath=%ProgramFiles(x86)%
+
+if not defined VsDevCmd_path set VsDevCmd_path=%progpath%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat
+if not exist "%VsDevCmd_path%" set VsDevCmd_path=%progpath%\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat
+if not exist "%VsDevCmd_path%" set VsDevCmd_path=%progpath%\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat
+if not exist "%VsDevCmd_path%" set VsDevCmd_path=%progpath%\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat
 
 SET CURRENTPATH=%~dp0
 SET CURRENTLETTER=%~d0
@@ -12,5 +20,5 @@ echo Make MediaPortal 2GB LARGEADDRESSAWARE...
 %CURRENTLETTER%
 cd %CURRENTPATH%
 REM call "%VS140COMNTOOLS%vsvars32.bat"
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
+call "%VsDevCmd_path%"
 EditBin.exe %MediaPortal%\MediaPortal.Application\bin\%1\MediaPortal.exe /LARGEADDRESSAWARE
