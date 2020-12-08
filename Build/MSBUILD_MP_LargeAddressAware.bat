b49bd709fb581f18dc158ae9cb097c5b15045fb3
Azzuro <Azzuro@users.noreply.github.com>
2020-12-08 11:27:53 +0100

    MP1-5043 Fix MP_largeAddressAware on VS2019

--- Build/MSBUILD_MP_LargeAddressAware.bat
+++ Build/MSBUILD_MP_LargeAddressAware.bat
@@ -1,4 +1,4 @@
-@ECHO OFF
+@ECHO ON
 REM set other MP related paths
 set GIT_ROOT=..
 set MediaPortal="%GIT_ROOT%\mediaportal"
@@ -11,5 +11,6 @@ echo.
 echo Make MediaPortal 2GB LARGEADDRESSAWARE...
 %CURRENTLETTER%
 cd %CURRENTPATH%
-call "%VS140COMNTOOLS%vsvars32.bat"
+REM call "%VS140COMNTOOLS%vsvars32.bat"
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat"
 EditBin.exe %MediaPortal%\MediaPortal.Application\bin\%1\MediaPortal.exe /LARGEADDRESSAWARE
