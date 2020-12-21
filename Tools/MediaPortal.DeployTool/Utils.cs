d75838bb8aec9fee3168c2460811644ef6ad1799
Andrew J.Swan <ajs.blackhole@gmail.com>
2020-12-21 23:24:03 +0200

    MP1-1.27Final: Bump version number and set DEBUG level to INFO

--- Tools/MediaPortal.DeployTool/Utils.cs
+++ Tools/MediaPortal.DeployTool/Utils.cs
@@ -718,7 +718,7 @@ namespace MediaPortal.DeployTool
         case "max":
           major = 1;
           minor = 26;
-          revision = 000;
+          revision = 100;
           break;
       }
       Version ver = new Version(major, minor, revision);
@@ -738,8 +738,8 @@ namespace MediaPortal.DeployTool
     public static Version GetCurrentPackageVersion()
     {
       int major = 1;
-      int minor = 26;
-      int revision = 100;
+      int minor = 27;
+      int revision = 000;
 
       Version ver = new Version(major, minor, revision);
       return ver;
@@ -812,7 +812,7 @@ namespace MediaPortal.DeployTool
 
     public static string GetDisplayVersion()
     {
-      return "1.27 Pre Release";
+      return "1.27";
     }
 
     /// <summary>
