74abff07b822b814e3844ef2589a3230bc7b5560
Andrew J.Swan <ajs.blackhole@gmail.com>
2020-12-21 22:30:02 +0200

    MP1-5049: Fix DatabaseUtility.Get: Field ... not found

--- mediaportal/Databases/Music/MusicDatabase.Lookups.cs
+++ mediaportal/Databases/Music/MusicDatabase.Lookups.cs
@@ -1,6 +1,6 @@
-#region Copyright (C) 2005-2019 Team MediaPortal
+#region Copyright (C) 2005-2020 Team MediaPortal
 
-// Copyright (C) 2005-2019 Team MediaPortal
+// Copyright (C) 2005-2020 Team MediaPortal
 // http://www.team-mediaportal.com
 // 
 // MediaPortal is free software: you can redistribute it and/or modify
@@ -63,15 +63,15 @@ namespace MediaPortal.Music.Database
       aSong.Composer = DatabaseUtility.Get(aResult, aRow, "tracks.strComposer").Trim(trimChars);
       aSong.Conductor = DatabaseUtility.Get(aResult, aRow, "tracks.strConductor").Trim(trimChars);
       aSong.Comment = DatabaseUtility.Get(aResult, aRow, "tracks.strComment").Trim(trimChars);
-      aSong.MusicBrainzArtistId = DatabaseUtility.Get(aResult, aRow, "strMBArtistId").Trim(trimChars);
-      aSong.MusicBrainzDiscId = DatabaseUtility.Get(aResult, aRow, "strMBDiscId").Trim(trimChars);
-      aSong.MusicBrainzReleaseArtistId = DatabaseUtility.Get(aResult, aRow, "strMBReleaseArtistId").Trim(trimChars);
-      aSong.MusicBrainzReleaseCountry = DatabaseUtility.Get(aResult, aRow, "strMBReleaseCountry").Trim(trimChars);
-      aSong.MusicBrainzReleaseGroupId = DatabaseUtility.Get(aResult, aRow, "strMBReleaseGroupId").Trim(trimChars);
-      aSong.MusicBrainzReleaseId = DatabaseUtility.Get(aResult, aRow, "strMBReleaseId").Trim(trimChars);
-      aSong.MusicBrainzReleaseStatus = DatabaseUtility.Get(aResult, aRow, "strMBReleaseStatus").Trim(trimChars);
-      aSong.MusicBrainzReleaseType = DatabaseUtility.Get(aResult, aRow, "strMBReleaseType").Trim(trimChars);
-      aSong.MusicBrainzTrackId = DatabaseUtility.Get(aResult, aRow, "strMBTrackId").Trim(trimChars);
+      aSong.MusicBrainzArtistId = DatabaseUtility.Get(aResult, aRow, "tracks.strMBArtistId").Trim(trimChars);
+      aSong.MusicBrainzDiscId = DatabaseUtility.Get(aResult, aRow, "tracks.strMBDiscId").Trim(trimChars);
+      aSong.MusicBrainzReleaseArtistId = DatabaseUtility.Get(aResult, aRow, "tracks.strMBReleaseArtistId").Trim(trimChars);
+      aSong.MusicBrainzReleaseCountry = DatabaseUtility.Get(aResult, aRow, "tracks.strMBReleaseCountry").Trim(trimChars);
+      aSong.MusicBrainzReleaseGroupId = DatabaseUtility.Get(aResult, aRow, "tracks.strMBReleaseGroupId").Trim(trimChars);
+      aSong.MusicBrainzReleaseId = DatabaseUtility.Get(aResult, aRow, "tracks.strMBReleaseId").Trim(trimChars);
+      aSong.MusicBrainzReleaseStatus = DatabaseUtility.Get(aResult, aRow, "tracks.strMBReleaseStatus").Trim(trimChars);
+      aSong.MusicBrainzReleaseType = DatabaseUtility.Get(aResult, aRow, "tracks.strMBReleaseType").Trim(trimChars);
+      aSong.MusicBrainzTrackId = DatabaseUtility.Get(aResult, aRow, "tracks.strMBTrackId").Trim(trimChars);
       aSong.FileType = DatabaseUtility.Get(aResult, aRow, "tracks.strFileType").Trim(trimChars);
       aSong.Codec = DatabaseUtility.Get(aResult, aRow, "tracks.strFullCodec").Trim(trimChars);
       aSong.BitRateMode = DatabaseUtility.Get(aResult, aRow, "tracks.strBitRateMode").Trim(trimChars);
