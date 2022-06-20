01dd38fa41c7f0fae7655250c8262daeb38abd35
Andrew J.Swan <ajs.blackhole@gmail.com>
2022-06-20 23:22:50 +0300

    MP1-1.30Final: Bump version number and set DEBUG level to INFO

--- Tools/MediaPortal.DeployTool/Utils.cs
+++ Tools/MediaPortal.DeployTool/Utils.cs
@@ -1,6 +1,6 @@
-#region Copyright (C) 2005-2021 Team MediaPortal
+#region Copyright (C) 2005-2022 Team MediaPortal
 
-// Copyright (C) 2005-2021 Team MediaPortal
+// Copyright (C) 2005-2022 Team MediaPortal
 // http://www.team-mediaportal.com
 // 
 // MediaPortal is free software: you can redistribute it and/or modify
@@ -718,7 +718,7 @@ namespace MediaPortal.DeployTool
         case "max":
           major = 1;
           minor = 29;
-          revision = 000;
+          revision = 100;
           break;
       }
       Version ver = new Version(major, minor, revision);
@@ -738,8 +738,8 @@ namespace MediaPortal.DeployTool
     public static Version GetCurrentPackageVersion()
     {
       int major = 1;
-      int minor = 29;
-      int revision = 100;
+      int minor = 30;
+      int revision = 000;
 
       Version ver = new Version(major, minor, revision);
       return ver;
@@ -812,7 +812,7 @@ namespace MediaPortal.DeployTool
 
     public static string GetDisplayVersion()
     {
-      return "1.30 Pre Release";
+      return "1.30 Ukraine";
     }
 
     /// <summary>
