dabe3533e4cd5e7f9eca7a765db93948a91668d4
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-12-04 20:12:42 +0200

    MP1.35.Final: Bump version number and set DEBUG level to INFO

--- Common-MP-TVE3/SolutionInfo.cs
+++ Common-MP-TVE3/SolutionInfo.cs
@@ -50,8 +50,8 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.35.000.0")]
-[assembly: AssemblyFileVersion("1.35.000.0")]
+[assembly: AssemblyVersion("1.36.000.0")]
+[assembly: AssemblyFileVersion("1.36.000.0")]
 
 //[assembly: AssemblyInformationalVersion("")]
 //[assembly: CompatibleVersion("1.1.8.*", "1.1.8.*")]
