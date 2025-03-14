ee9fef4dfebead1084ecfb27a78490e4c13e085d
epbk <epbk@epbk.cz>
2025-03-14 14:33:21 +0100

    MP1-5241: Core: update of new IPlayerService

--- mediaportal/Core/guilib/IPlayerService.cs
+++ mediaportal/Core/guilib/IPlayerService.cs
@@ -7,8 +7,12 @@ namespace MediaPortal.GUI.Library
 {
   public interface IPlayerService
   {
-    void RegisterService(int iWindowID);
-
+    /// <summary>
+    /// Executes call to get final playback url from given strLink argument.
+    /// </summary>
+    /// <param name="strLink">Original playback url link.</param>
+    /// <param name="result">Final playback link. Can be either url string or object passed as 'loadParametr' when the window is being activated.</param>
+    /// <returns>Result of the operation.</returns>
     PlayerServiceResultEnum GetPlaybackLink(string strLink, out object result);
   }
 }
