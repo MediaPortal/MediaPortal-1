1aecd7789fa5be226f72fa15845581445024fef8
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-08-04 19:26:42 +0300

    Revert "Test minor version bump"

--- Tools/MediaPortal.DeployTool/Utils.cs
+++ Tools/MediaPortal.DeployTool/Utils.cs
@@ -781,8 +781,8 @@ namespace MediaPortal.DeployTool
           break;
         case "max":
           major = 1;
-          minor = 38;
-          revision = 0;
+          minor = 37;
+          revision = 100;
           break;
       }
       Version ver = new Version(major, minor, revision);
