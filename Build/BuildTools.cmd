f59d957c53960b72dc199cfa236994dea85f6d10
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-04-28 12:20:47 +0300

    Create BuildTools.cmd

---
+++ Build/BuildTools.cmd
@@ -0,0 +1,23 @@
+@ECHO OFF
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: Build
+FOR %%p IN ("%PROGRAMFILES(x86)%" "%PROGRAMFILES%") DO (
+  FOR %%s IN (2019 2022) DO (
+    FOR %%e IN (Community Professional Enterprise BuildTools) DO (
+      SET PF=%%p
+      SET PF=!PF:"=!
+      SET MSBUILD_PATH="!PF!\Microsoft Visual Studio\%%s\%%e\MSBuild\Current\Bin\MSBuild.exe"
+      IF EXIST "!MSBUILD_PATH!" GOTO :BUILD
+    )
+  )
+)
+
+:BUILD
+
+:: DeployVersionGIT
+%MSBUILD_PATH% /target:Rebuild /property:Configuration=Release "%GIT_ROOT%\Tools\Script & Batch tools\DeployVersionGIT\DeployVersionGIT.sln"
+
+:: SetRights
+%MSBUILD_PATH% /target:Rebuild /property:Configuration=Release "%GIT_ROOT%\Tools\Script & Batch tools\SetRights\SetRights.sln"
