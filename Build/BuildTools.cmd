cb487835067615a4affefaab36fadd058311c7f1
Andrew J.Swan <ajs.blackhole@gmail.com>
2026-04-30 14:08:50 +0300

    Update BuildTools.cmd

--- Build/BuildTools.cmd
+++ Build/BuildTools.cmd
@@ -16,8 +16,16 @@ FOR %%p IN ("%PROGRAMFILES(x86)%" "%PROGRAMFILES%") DO (
 
 :BUILD
 
+ECHO.
+ECHO MSBuild Location: %MSBUILD_PATH%
+ECHO.
+
 :: DeployVersionGIT
+ECHO - Build DeployVersionGIT ...
 %MSBUILD_PATH% /target:Rebuild /property:Configuration=Release "%GIT_ROOT%\Tools\Script & Batch tools\DeployVersionGIT\DeployVersionGIT.sln"
+ECHO.
 
 :: SetRights
+ECHO - Build SetRights ...
 %MSBUILD_PATH% /target:Rebuild /property:Configuration=Release "%GIT_ROOT%\Tools\Script & Batch tools\SetRights\SetRights.sln"
+ECHO.
