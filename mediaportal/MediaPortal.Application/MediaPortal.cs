e743a48091fe308e079acb9c73255628ab289ffa
doskabouter <doskabouter@users.noreply.github.com>
2026-08-07 13:15:37 +0200

    MP1-5255: Skinproperties final reverting stuff (#503)
    * Add screen to track `GUIPropertyManager's` properties: fixed out of bounds exceptions
    * Add screen to track `GUIPropertyManager's` properties: bit of refactor
    * Added option to include spectrumanalyzer in `SkinProperties` mode
    * Reverted disabling of `SpectrumAnalyzer`
    * Reverted adding `MediaPortal Skin Properties Monitor` shortcuts
    * Fixed revert

--- mediaportal/MediaPortal.Application/MediaPortal.cs
+++ mediaportal/MediaPortal.Application/MediaPortal.cs
@@ -619,13 +619,12 @@ public class MediaPortalApp : D3D, IRender
           Log.SetLogLevel(Level.Debug);
         }
 
-        if (arg == "/SkinProperties" || arg == "/SkinPropertiesWithSpectrumAnalyzer")
+        if (arg == "/SkinProperties")
         {
           ShowSkinProperties = true;
           AlwaysOnTopOverride = true;
           WindowedOverride = true;
           HIDOverride.SuppressHID = true;
-          PluginManager.DisableSpectrumAnalyzer = arg == "/SkinProperties";
         }
 
         #if !DEBUG
